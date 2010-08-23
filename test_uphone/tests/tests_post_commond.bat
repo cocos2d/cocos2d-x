9d6a68f9e5da4e8d046afde3f5600d6963f08668
natural-law <bin@cocos2d-x.org>
2010-08-23 08:42:19 +0000

    issue #78 auto copy image file fire.png

--- test_uphone/tests/tests_post_commond.bat
+++ test_uphone/tests/tests_post_commond.bat
@@ -1,2 +1,3 @@
 mkdir D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
 copy .\Res\Images\*.*		D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
+copy .\Res\Images\fire.png		D:\Work7\NEWPLUS\TDA_DATA\UserData
