c8ab8c63cdcea562115c1c3f4a998234de23969e
natural-law <bin@cocos2d-x.org>
2010-08-25 03:37:22 +0000

    issue #78 modify Particle Test code

--- test_uphone/tests/tests_post_commond.bat
+++ test_uphone/tests/tests_post_commond.bat
@@ -1,3 +1,5 @@
 mkdir D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
 copy .\Res\Images\*.*		D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
 copy .\Res\Images\fire.png		D:\Work7\NEWPLUS\TDA_DATA\UserData
+copy .\Res\Images\fps_images.png		D:\Work7\NEWPLUS\TDA_DATA\UserData
+copy .\Res\Images\HelloWorld.png		D:\Work7\NEWPLUS\TDA_DATA\UserData
