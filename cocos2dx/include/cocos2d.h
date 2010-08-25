d2f44e2f6a71a7fd7c86945ac66a76a3ec40c714
Ming <ming@cocos2d-x.org>
2010-08-25 01:40:31 +0000

    issue #80: remove CCDirector.h to direction of include

--- cocos2dx/include/cocos2d.h
+++ cocos2dx/include/cocos2d.h
@@ -26,91 +26,60 @@ THE SOFTWARE.
 #ifndef __COCOS2D_H__
 #define __COCOS2D_H__
 
-//#include "platform/platform.h"
-
-
 //
 // all cocos2d include files
 //
-// #include "ccConfig.h"	// should be included first
-// 
-// #include "CCBlockSupport.h"
-// #include "CCAction.h"
-// #include "CCCamera.h"
-// #include "CCCameraAction.h"
-// #include "CCProtocols.h"
-// #include "CCNode.h"
+
+#include "ccConfig.h"	// should be included first
+#include "CCAction.h"
+#include "CCCamera.h"
+#include "CCCameraAction.h"
+#include "CCProtocols.h"
+#include "CCNode.h"
 #include "CCDirector.h"
 // #include "CCTouchDispatcher.h"
 // #include "CCTouchDelegateProtocol.h"
-// #include "CCInstantAction.h"
-// #include "CCIntervalAction.h"
-// #include "CCEaseAction.h"
-// #include "CCLabel.h"
+#include "CCInstantAction.h"
+#include "CCIntervalAction.h"
+#include "CCEaseAction.h"
+#include "CCLabel.h"
 #include "CCLayer.h"
-// #include "CCMenu.h"
-// #include "CCMenuItem.h"
-// #include "CCParticleSystem.h"
-// #include "CCPointParticleSystem.h"
-// #include "CCQuadParticleSystem.h"
-// #include "CCParticleExamples.h"
-// #include "CCDrawingPrimitives.h"
+#include "CCMenu.h"
+#include "CCMenuItem.h"
+#include "CCParticleSystem.h"
+#include "CCPointParticleSystem.h"
+#include "CCQuadParticleSystem.h"
+#include "CCParticleExample.h"
 #include "CCScene.h"
-// #include "CCScheduler.h"
 #include "CCSprite.h"
-// #include "CCSpriteFrame.h"
-// #include "CCSpriteSheet.h"
-// #include "CCSpriteFrameCache.h"
 #include "CCTextureCache.h"
-// #include "CCTransition.h"
-// #include "CCTextureAtlas.h"
-// #include "CCLabelAtlas.h"
-// #include "CCAtlasNode.h"
-// #include "CCEaseAction.h"
-// #include "CCTiledGridAction.h"
-// #include "CCGrabber.h"
-// #include "CCGrid.h"
-// #include "CCGrid3DAction.h"
-// #include "CCGridAction.h"
-// #include "CCBitmapFontAtlas.h"
+#include "CCTransition.h"
+#include "CCTextureAtlas.h"
+#include "CCLabelAtlas.h"
+#include "CCAtlasNode.h"
+#include "CCTiledGridAction.h"
+#include "CCGrid3DAction.h"
+#include "CCGridAction.h"
+#include "CCBitmapFontAtlas.h"
 // #include "CCParallaxNode.h"
-// #include "CCActionManager.h"
 // #include "CCTMXTiledMap.h"
 // #include "CCTMXLayer.h"
 // #include "CCTMXObjectGroup.h"
 // #include "CCTMXXMLParser.h"
-// #include "CCTileMapAtlas.h"
-// #include "CCRenderTexture.h"
-// #include "CCMotionStreak.h"
-// #include "CCPageTurn3DAction.h"
-// #include "CCPageTurnTransition.h"
-// #include "CCTexture2D.h"
-// #include "CCPVRTexture.h"
-// #include "CCTouchHandler.h"
-// #include "CCConfiguration.h"
-// #include "CCRadialTransition.h"
-// #include "CCProgressTimerActions.h"
-// #include "CCPropertyAction.h"
-// 
-// //
-// // cocos2d macros
-// //
-// #include "ccTypes.h"
-// #include "ccMacros.h"
-// 
-// //
-// // cocos2d helper files
-// //
-// #include "Support/OpenGL_Internal.h"
-// #include "Support/EAGLView.h"
-// #include "Support/CCFileUtils.h"
-// #include "Support/CGPointExtension.h"
-// #include "Support/ccCArray.h"
-// #include "Support/CCArray.h"
-
-#if CC_ENABLE_PROFILERS
-#include "Support/CCProfiling.h"
-#endif // CC_ENABLE_PROFILERS
+#include "CCRenderTexture.h"
+#include "CCMotionStreak.h"
+#include "CCPageTurn3DAction.h"
+#include "CCPageTurnTransition.h"
+#include "CCTexture2D.h"
+#include "CCPVRTexture.h"
+#include "CCRadialTransition.h"
+#include "CCProgressTimerActions.h"
+ 
+//
+// cocos2d macros
+//
+#include "ccTypes.h"
+#include "ccMacros.h"
 
 #include <string>
 namespace   cocos2d {
