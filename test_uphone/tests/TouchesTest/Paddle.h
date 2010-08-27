7231a0e1ce8dce588c0a4bf5b5f61aa3a6727f94
natural-law <bin@cocos2d-x.org>
2010-08-27 03:53:35 +0000

    fixed #97 add Touches Test code

---
+++ test_uphone/tests/TouchesTest/Paddle.h
@@ -0,0 +1,34 @@
+#ifndef _PADDLE_H_
+#define _PADDLE_H_
+
+#include "cocos2d.h"
+
+using namespace cocos2d;
+
+typedef enum tagPaddleState 
+{
+	kPaddleStateGrabbed,
+	kPaddleStateUngrabbed
+} PaddleState; 
+
+class Paddle : public CCSprite, public CCTargetedTouchDelegate
+{
+	PaddleState		m_state;
+
+public:
+	Paddle(void);
+	virtual ~Paddle(void);
+
+	CGRect rect();
+	bool initWithTexture(CCTexture2D* aTexture);
+	virtual void onEnter();
+	virtual void onExit();
+	bool containsTouchLocation(CCTouch* touch);
+	virtual bool ccTouchBegan(CCTouch* touch, UIEvent* event);
+	virtual void ccTouchMoved(CCTouch* touch, UIEvent* event);
+	virtual void ccTouchEnded(CCTouch* touch, UIEvent* event);
+
+	static Paddle* paddleWithTexture(CCTexture2D* aTexture);
+};
+
+#endif
---
+++ test_uphone/tests/TouchesTest/TouchesTest.cpp
@@ -0,0 +1,131 @@
+#include "TouchesTest.h"
+#include "Ball.h"
+#include "Paddle.h"
+#include "../testResource.h"
+
+enum tagPlayer 
+{
+	kHighPlayer,
+	kLowPlayer
+} Player;	
+
+#define kStatusBarHeight 20.0f
+//#define k1UpperLimit (480.0f - kStatusBarHeight)
+
+enum 
+{
+	kSpriteTag
+};
+
+
+//------------------------------------------------------------------
+//
+// PongScene
+//
+//------------------------------------------------------------------
+PongScene::PongScene()
+{
+	PongLayer *pongLayer = new PongLayer();//PongLayer::node();
+	addChild(pongLayer);
+}
+
+void PongScene::onExit()
+{
+	__super::onExit();
+}
+
+//------------------------------------------------------------------
+//
+// PongLayer
+//
+//------------------------------------------------------------------
+PongLayer::PongLayer()
+{
+	m_ballStartingVelocity = CGPointMake(20.0f, -100.0f);
+	
+    m_ball = Ball::ballWithTexture( CCTextureCache::sharedTextureCache()->addImage(s_Ball) );
+	m_ball->setPosition( CGPointMake(160.0f, 240.0f) );
+	m_ball->setVelocity( m_ballStartingVelocity );
+	addChild( m_ball );
+	m_ball->retain();
+	
+	CCTexture2D* paddleTexture = CCTextureCache::sharedTextureCache()->addImage(s_Paddle);
+	
+	NSMutableArray<NSObject *> *paddlesM = new NSMutableArray<NSObject *>(4);
+	
+	Paddle* paddle = Paddle::paddleWithTexture(paddleTexture);
+	paddle->setPosition( CGPointMake(160, 15) );
+	paddlesM->addObject( paddle );
+	
+	paddle = Paddle::paddleWithTexture( paddleTexture );
+	paddle->setPosition( CGPointMake(160, 480 - kStatusBarHeight - 15) );
+	paddlesM->addObject( paddle );
+	
+	paddle = Paddle::paddleWithTexture( paddleTexture );
+	paddle->setPosition( CGPointMake(160, 100) );
+	paddlesM->addObject( paddle );
+	
+	paddle = Paddle::paddleWithTexture( paddleTexture );
+	paddle->setPosition( CGPointMake(160, 480 - kStatusBarHeight - 100) );
+	paddlesM->addObject( paddle );
+	
+	m_paddles = paddlesM->copy();
+	
+    NSMutableArray<NSObject *>::NSMutableArrayIterator it;
+	for(it = m_paddles->begin(); it != m_paddles->end(); it++)
+	{
+		paddle = dynamic_cast<Paddle*>(*it);
+
+		if(!paddle)
+			break;
+
+		addChild(paddle);
+	}
+
+	paddlesM->release();
+
+	schedule( schedule_selector(PongLayer::doStep) );
+}
+
+PongLayer::~PongLayer()
+{
+	m_ball->release();
+	m_paddles->release();
+}
+
+void PongLayer::resetAndScoreBallForPlayer(int player)
+{
+	m_ballStartingVelocity = ccpMult(m_ballStartingVelocity, -1.1f);
+	m_ball->setVelocity( m_ballStartingVelocity );
+	m_ball->setPosition( CGPointMake(160.0f, 240.0f) );
+	
+	// TODO -- scoring
+}
+
+void PongLayer::doStep(ccTime delta)
+{
+	m_ball->move(delta);
+
+	Paddle* paddle;
+    NSMutableArray<NSObject *>::NSMutableArrayIterator it;
+	for(it = m_paddles->begin(); it != m_paddles->end(); it++)
+	{
+		paddle = dynamic_cast<Paddle*>(*it);
+
+		if(!paddle)
+			break;
+
+		m_ball->collideWithPaddle( paddle );
+	}
+
+	if (m_ball->getPosition().y > 480 - kStatusBarHeight + m_ball->radius())
+		resetAndScoreBallForPlayer( kLowPlayer );
+	else if (m_ball->getPosition().y < -m_ball->radius())
+		resetAndScoreBallForPlayer( kHighPlayer );
+    m_ball->draw();
+} 
+
+void PongScene::runThisTest()
+{
+    CCDirector::getSharedDirector()->replaceScene(this);
+}
