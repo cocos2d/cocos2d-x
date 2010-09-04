6e13a10ccf8cf4635bba2cf453bf6ceafb39176e
natural-law <bin@cocos2d-x.org>
2010-09-04 07:34:21 +0000

    fixed #115 add TileMapTest code

--- test_uphone/tests/tests_post_commond.bat
+++ test_uphone/tests/tests_post_commond.bat
@@ -2,7 +2,7 @@ mkdir D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
 mkdir D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\TileMaps
 mkdir D:\Work7\NEWPLUS\TDA_DATA\UserData
 copy .\Res\Images\*.*		            		D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\Images
-copy .\Res\TileMaps\*.*		            	D:\Work7\NEWPLUS\TDA_DATA\Data\cocos2d_tests\TileMaps
+copy .\Res\TileMaps\*.*		            	D:\Work7\NEWPLUS\TDA_DATA\UserData
 copy .\Res\fonts\*.*										D:\Work7\NEWPLUS\TDA_DATA\UserData
 copy .\Res\Images\fire.png		      		D:\Work7\NEWPLUS\TDA_DATA\UserData
 copy .\Res\Images\fps_images.png				D:\Work7\NEWPLUS\TDA_DATA\UserData
