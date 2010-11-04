21cdf219184a54c74ec7c8017f8a0c453fb6e712
Ming <ming@cocos2d-x.org>
2010-11-04 09:51:28 +0000

    issue #192: add helloworld-android

---
+++ HelloWorld-android/jni/helloworld/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := cocos2dapplication
+
+LOCAL_SRC_FILES := HelloWorld.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../cocos2dx \
+                   $(LOCAL_PATH)/../cocos2dx/include 
+                   
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../libs/armeabi -lcocos2d -llog
+
+LOCAL_CPPFLAGS := -D_ANDROID              
+include $(BUILD_SHARED_LIBRARY)
+                   
