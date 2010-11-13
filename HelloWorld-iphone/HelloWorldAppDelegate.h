cf6a4fd261ccdc3fc76f9d0ef74af6b38626db5a
Ming <ming@cocos2d-x.org>
2010-11-13 03:40:41 +0000

    fixed #202

---
+++ HelloWorld-iphone/HelloWorldAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  iphone_testAppDelegate.h
+//  iphone_test
+//
+//  Created by Walzer on 10-11-10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@end
+
