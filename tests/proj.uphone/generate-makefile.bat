aaf6a7f1ee319345ecc228b0cfa3ecc96b1b3edb
Walzer <walzer@cocos2d-x.org>
2010-11-13 01:47:25 +0000

    makefile for test_uphone arm

---
+++ tests/proj.uphone/generate-makefile.bat
@@ -0,0 +1,6 @@
+move ./test_uphone_Arm.TMK3 ../test_uphone_Arm.TMK3
+D:\Work7\PRJ_TG3\Common\TMK3\TMK3 ../test_uphone_Arm.TMK3
+move ../test_uphone_Arm.TMK3 ./test_uphone_Arm.TMK3
+move ../Makefile.ARM ./Makefile.ARM
+move ../test_uphone_Arm.TMK3 ./test_uphone_Arm.TMK3
+
