78d158fcf181bcb23e865a4025d869e5f904523c
Walzer <walzer@cocos2d-x.org>
2010-11-19 02:59:27 +0000

    issue #211, add batch build for uphone

--- tests/proj.uphone/generate-makefile.bat
+++
@@ -1,6 +0,0 @@
-move ./test_uphone_Arm.TMK3 ../test_uphone_Arm.TMK3
-D:\Work7\PRJ_TG3\Common\TMK3\TMK3 ../test_uphone_Arm.TMK3
-move ../test_uphone_Arm.TMK3 ./test_uphone_Arm.TMK3
-move ../Makefile.ARM ./Makefile.ARM
-move ../test_uphone_Arm.TMK3 ./test_uphone_Arm.TMK3
-
