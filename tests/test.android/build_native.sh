5a4c721f0c8645c4e498fd496d2db6000ba6b216
Walzer <walzer@cocos2d-x.org>
2010-12-17 15:43:36 +0800

    modify build_natvie.sh to use unix line feed character

--- tests/test.android/build_native.sh
+++ tests/test.android/build_native.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-
-ANDROID_NDK_ROOT=/cygdrive/e/android-ndk-r4-crystax
-COCOS2DX_ROOT=/cygdrive/d/Work7/cocos2d-x
-
-# build 
+# set params
+ANDROID_NDK_ROOT=/workspace/android-ndk-r5
+COCOS2DX_ROOT=/workspace/cocos2d-x
+# build
 pushd $ANDROID_NDK_ROOT
 ./ndk-build -C $COCOS2DX_ROOT/tests/test.android
 popd
+
