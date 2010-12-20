0b057f9224e95c1f2a422543982be8bc59d679ec
minggo <ming@cocos2d-x.org>
2010-12-20 14:17:51 +0800

    copy resources from top directory

--- HelloWorld/android/build_native.sh
+++ HelloWorld/android/build_native.sh
@@ -1,8 +1,21 @@
 # set params
 ANDROID_NDK_ROOT=/cygdrive/e/android-ndk-r4-crystax
 COCOS2DX_ROOT=/cygdrive/d/Work7/cocos2d-x
+
+# copy resources
+
+HELLOWORLD_ROOT=$COCOS2DX_ROOT/HelloWorld/android
+
+if [ -f $HELLOWORLD_ROOT/assets/CloseNormal.png ]; then
+    echo "resources already exist"
+else
+    cp $COCOS2DX_ROOT/HelloWorld/Resource/CloseNormal.png $HELLOWORLD_ROOT/assets
+    cp $COCOS2DX_ROOT/HelloWorld/Resource/CloseSelected.png $HELLOWORLD_ROOT/assets
+    cp $COCOS2DX_ROOT/HelloWorld/Resource/HelloWorld.png $HELLOWORLD_ROOT/assets
+fi
+
 # build
 pushd $ANDROID_NDK_ROOT
-./ndk-build -C $COCOS2DX_ROOT/HelloWorld/android
+./ndk-build -C $HELLOWORLD_ROOT
 popd
 
--- tests/test.android/build_native.sh
+++ tests/test.android/build_native.sh
@@ -1,8 +1,23 @@
 # set params
-ANDROID_NDK_ROOT=/workspace/android-ndk-r5
-COCOS2DX_ROOT=/workspace/cocos2d-x
+ANDROID_NDK_ROOT=/cygdrive/e/android-ndk-r4-crystax
+COCOS2DX_ROOT=/cygdrive/d/Work7/cocos2d-x
+
+# copy resources
+
+TESTS_ROOT=$COCOS2DX_ROOT/tests/test.android
+
+if [ -d $TESTS_ROOT/assets/animations ]; then
+    echo "resources already exist"
+else
+    cp -R $COCOS2DX_ROOT/tests/Res/animations $TESTS_ROOT/assets
+    cp -R $COCOS2DX_ROOT/tests/Res/fonts $TESTS_ROOT/assets
+    cp -R $COCOS2DX_ROOT/tests/Res/Images $TESTS_ROOT/assets
+    cp -R $COCOS2DX_ROOT/tests/Res/TileMaps $TESTS_ROOT/assets
+fi
+
+
 # build
 pushd $ANDROID_NDK_ROOT
-./ndk-build -C $COCOS2DX_ROOT/tests/test.android
+./ndk-build -C $TESTS_ROOT
 popd
 
