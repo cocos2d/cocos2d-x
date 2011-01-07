ec7fbd0e69de1686c4154c97ec103e94b453d08a
natural-law <bin@cocos2d-x.org>
2011-01-07 11:30:19 +0800

    Fix bugs. 1. Run tests and HelloWorld in cocos2d-android. 2. Modify the makefile of cocos2d-uphone.

--- cocos2dx/Android.mk
+++ cocos2dx/Android.mk
@@ -6,18 +6,17 @@ LOCAL_MODULE := libcocos2d
 
 LOCAL_SRC_FILES := \
 CCConfiguration.cpp \
-CCDirector.cpp \
 CCDrawingPrimitives.cpp \
 CCScheduler.cpp \
 CCamera.cpp \
 actions/CCAction.cpp \
-actions/CCActionManager.cpp \
 actions/CCActionCamera.cpp \
 actions/CCActionEase.cpp \
-actions/CCActionGrid3D.cpp \
 actions/CCActionGrid.cpp \
+actions/CCActionGrid3D.cpp \
 actions/CCActionInstant.cpp \
 actions/CCActionInterval.cpp \
+actions/CCActionManager.cpp \
 actions/CCActionPageTurn3D.cpp \
 actions/CCActionProgressTimer.cpp \
 actions/CCActionTiledGrid.cpp \
@@ -31,14 +30,17 @@ cocoa/NSSet.cpp \
 cocoa/NSZone.cpp \
 cocos2d.cpp \
 effects/CCGrabber.cpp \
-effects/CCGrid.cpp \
+event_dispatcher/CCEventDispatcher.cpp \
+event_dispatcher/CCKeyboardEventDelegate.cpp \
+event_dispatcher/CCMouseEventDelegate.cpp \
+keypad_dispatcher/CCKeypadDelegate.cpp \
+keypad_dispatcher/CCKeypadDispatcher.cpp \
+label_nodes/CCLabelAtlas.cpp \
 label_nodes/CCLabelBMFont.cpp \
 label_nodes/CCLabelTTF.cpp \
-label_nodes/CCLabelAtlas.cpp \
+layers_scenes_transitions_nodes/CCScene.cpp \
 layers_scenes_transitions_nodes/CCTransitionPageTurn.cpp \
 layers_scenes_transitions_nodes/CCTransitionRadial.cpp \
-layers_scenes_transitions_nodes/CCScene.cpp \
-layers_scenes_transitions_nodes/CCTransition.cpp \
 menu_nodes/CCMenuItem.cpp \
 misc_nodes/CCMotionStreak.cpp \
 misc_nodes/CCProgressTimer.cpp \
@@ -46,13 +48,15 @@ misc_nodes/CCRenderTexture.cpp \
 misc_nodes/CCRibbon.cpp \
 particle_nodes/CCParticleExamples.cpp \
 particle_nodes/CCParticleSystem.cpp \
-particle_nodes/CCPointParticleSystem.cpp \
-particle_nodes/CCQuadParticleSystem.cpp \
+particle_nodes/CCParticleSystemQuad.cpp \
+platform/CCDirector_mobile.cpp \
+platform/CCGrid_mobile.cpp \
 platform/CCLayer_mobile.cpp \
 platform/CCMenu_mobile.cpp \
 platform/CCNode_mobile.cpp \
+platform/CCParticleSystemPoint_mobile.cpp \
+platform/CCTransition_mobile.cpp \
 platform/android/CCTime.cpp \
-platform/android/ccxCommon_android.cpp \
 platform/android/CCXApplication_android.cpp \
 platform/android/CCXBitmapDC.cpp \
 platform/android/CCXEGLView_android.cpp \
@@ -61,8 +65,11 @@ platform/android/CCXUIAccelerometer_android.cpp \
 platform/android/CCXUIImage_android.cpp \
 platform/android/Cocos2dJni.cpp \
 platform/android/NSLock.cpp \
-platform/android/ZipUtils.cpp \
+platform/android/ccxCommon_android.cpp \
+sprite_nodes/CCAnimation.cpp \
+sprite_nodes/CCAnimationCache.cpp \
 sprite_nodes/CCSprite.cpp \
+sprite_nodes/CCSpriteBatchNode.cpp \
 sprite_nodes/CCSpriteFrame.cpp \
 sprite_nodes/CCSpriteFrameCache.cpp \
 sprite_nodes/CCSpriteSheet.cpp \
@@ -70,9 +77,11 @@ support/CCProfiling.cpp \
 support/CGPointExtension.cpp \
 support/TransformUtils.cpp \
 support/base64.cpp \
+support/ccUtils.cpp \
 support/file_support/FileUtils.cpp \
 support/image_support/TGAlib.cpp \
 support/opengl_support/glu.cpp \
+support/zip_support/ZipUtils.cpp \
 support/zip_support/ioapi.cpp \
 support/zip_support/unzip.cpp \
 textures/CCTexture2D.cpp \
@@ -85,9 +94,7 @@ tileMap_parallax_nodes/CCTMXTiledMap.cpp \
 tileMap_parallax_nodes/CCTMXXMLParser.cpp \
 tileMap_parallax_nodes/CCTileMapAtlas.cpp \
 touch_dispatcher/CCTouchDispatcher.cpp \
-touch_dispatcher/CCTouchHandler.cpp \
-keypad_dispatcher/CCKeypadDispatcher.cpp \
-keypad_dispatcher/CCKeypadDelegate.cpp
+touch_dispatcher/CCTouchHandler.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/ \
                     $(LOCAL_PATH)/include \
