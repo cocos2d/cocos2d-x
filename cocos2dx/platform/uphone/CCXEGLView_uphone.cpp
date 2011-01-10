653cb2d76db971528e07124f4c821059146ab4f2
Walzer <walzer@cocos2d-x.org>
2011-01-10 11:03:58 +0800

    [uphone] fixed #308, BGM can be paused after power key pressed to make uphone sleep.

--- cocos2dx/platform/uphone/CCXEGLView_uphone.cpp
+++ cocos2dx/platform/uphone/CCXEGLView_uphone.cpp
@@ -295,12 +295,14 @@ Boolean CCXEGLView::EventHandler(TApplication * pApp, EventType * pEvent)
     case EVENT_ScreenSwitchNotify:
         if (! pEvent->sParam1)  // turn off screen
         {
-            CCDirector::sharedDirector()->pause();
+            // CCDirector::sharedDirector()->pause();
+            CCXApplication::sharedApplication()->applicationDidEnterBackground();
             CCXApplication::sharedApplication()->StopMainLoop();
         }
         else
         {
-            CCDirector::sharedDirector()->resume();
+            // CCDirector::sharedDirector()->resume();
+            CCXApplication::sharedApplication()->applicationWillEnterForeground();
             CCXApplication::sharedApplication()->StartMainLoop();
         }
         break;
