e399a7736926439ca8c60995b4425927d229e63e
walzer <walzer@cocos2d-x.org>
2011-01-17 22:01:15 +0800

    fixed #306, invoke CCDirector::end() method in next msgloop, otherwise the flow will crashes when end() is called in an ActionInterval.

--- cocos2dx/include/CCDirector.h
+++ cocos2dx/include/CCDirector.h
@@ -459,6 +459,8 @@ protected:
 	* mobile platforms specific functions
 	**************************************************/
 	void purgeDirector();
+	bool m_bPurgeDirecotorInNextLoop; // this flag will be set to true in end()
+	
 	bool isOpenGLAttached(void);
 	void updateContentScaleFactor(void);
 
