9041d03d3b9f5055100721742baa24b8255080db
walzer <walzer@cocos2d-x.org>
2011-01-19 18:02:35 +0800

    [uphone] issue #307, config test.vcproj uphone for TestAudioEngine merger.

--- tests/test.uphone/main.cpp
+++ tests/test.uphone/main.cpp
@@ -1,16 +1,18 @@
 #include "TG3.h"
 #include "../AppDelegate.h"
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
 
 Int32 TG3AppMain(const TUChar * pAppID, UInt32 nCmd, void * pCmdParam)
 {
-	switch(nCmd)
-	{
-	case 0: // main entry
-		{
-			// run app
-			AppDelegate app;
-            app.Run();
-		}
-	}
+	AppDelegate app;
+
+// #ifndef _TRANZDA_VM_  // on uphone emulator, we copy resources files to Work7/TG3/APP/ folder instead of zip file
+	cocos2d::CCFileUtils::setResource(NULL, "TestCocos2dx.zip");
+	CocosDenshion::SimpleAudioEngine::setResource(NULL, "TestCocos2dx.zip");
+// #endif
+
+	app.Run();
+
 	return 1;
 }
