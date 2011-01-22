6ad3ec6bf04beca2b35678520fdf4909c4e4f1a4
yangws <yangws@cocos2d-x.org>
2011-01-22 17:30:00 +0800

    HelloWorld and tests can run after move ccxApplication.

--- tests/AppDelegate.h
+++ tests/AppDelegate.h
@@ -1,25 +1,37 @@
-#ifndef __APP_DELEGATE_H__
-#define __APP_DELEGATE_H__
+#ifndef  _APP_DELEGATE_H_
+#define  _APP_DELEGATE_H_
 
-#include "cocos2d.h"
+#include "CCXEGLView.h"
 
-class  AppDelegate  :  public  cocos2d::CCXApplication
+class  AppDelegate
 {
 public:
 	AppDelegate();
 	~AppDelegate();
 
+    /**
+    @brief	Implement Window create, CCDirector and CCScene init code here.
+    @return true    Initialize success, app continue.
+    @return false   Initialize failed, app terminate.
+    */
     virtual bool applicationDidFinishLaunching();
+
+    /**
+    @brief  The function be called when the application enter background
+    @param  the pointer of the application
+    */
     virtual void applicationDidEnterBackground();
+
+    /**
+    @brief  The function be called when the application enter foreground
+    @param  the pointer of the application
+    */
     virtual void applicationWillEnterForeground();
 
 protected:
-    cocos2d::CCXEGLView *m_pMainWnd;
-
-// private:
-//    Int32 m_nTimer;
+    cocos2d::CCXEGLView *   m_pMainWnd;
 };
  
 
-#endif	// __APP_DELEGATE_H__
+#endif // _APP_DELEGATE_H_
 
