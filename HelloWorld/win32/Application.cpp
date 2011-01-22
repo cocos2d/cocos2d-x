04c48f59c1f44b6cc49d835b5cdc3823c45711e1
yangws <yangws@cocos2d-x.org>
2011-01-22 16:27:08 +0800

    commit ccxApplication files.

---
+++ HelloWorld/win32/Application.cpp
@@ -0,0 +1,101 @@
+#include "Application.h"
+
+#include "CCDirector.h"
+USING_NS_CC;
+
+Application::Application()
+: m_hInstance(NULL)
+, m_hAccelTable(NULL)
+{
+    m_hInstance	= GetModuleHandle(NULL);
+    m_nAnimationInterval.QuadPart = 0;
+}
+
+Application::~Application()
+{
+
+}
+
+int Application::run()
+{
+    // Main message loop:
+    MSG msg;
+    LARGE_INTEGER nFreq;
+    LARGE_INTEGER nLast;
+    LARGE_INTEGER nNow;
+
+    QueryPerformanceFrequency(&nFreq);
+    QueryPerformanceCounter(&nLast);
+
+    // Make sharedApplication work correctly.
+    setSharedApplication(*this);
+
+    // Initialize AppDelegate.
+    if (! m_Delegate.applicationDidFinishLaunching())
+    {
+        return 0;
+    }
+
+    while (1)
+    {
+        if (! PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            // Get current time tick.
+            QueryPerformanceCounter(&nNow);
+
+            // If it's the time to draw next frame, draw it, else sleep a while.
+            if (nNow.QuadPart - nLast.QuadPart > m_nAnimationInterval.QuadPart)
+            {
+                nLast.QuadPart = nNow.QuadPart;
+                CCDirector::sharedDirector()->mainLoop();
+            }
+            else
+            {
+                Sleep(0);
+            }
+            continue;
+        }
+
+        if (WM_QUIT == msg.message)
+        {
+            // Quit message loop.
+            break;
+        }
+
+        // Deal with windows message.
+        if (! m_hAccelTable || ! TranslateAccelerator(msg.hwnd, m_hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+    return (int) msg.wParam;
+}
+
+void Application::setAnimationInterval(double interval)
+{
+    LARGE_INTEGER nFreq;
+    QueryPerformanceFrequency(&nFreq);
+    m_nAnimationInterval.QuadPart = (LONGLONG)(interval * nFreq.QuadPart);
+}
+
+Application::Orientation Application::setOrientation(Application::Orientation orientation)
+{
+//     // swap width and height
+//     CCXEGLView * pView = CCDirector::sharedDirector()->getOpenGLView();
+//     if (pView)
+//     {
+//         return (ccDeviceOrientation)pView->setDeviceOrientation(eOritation);
+//     }
+//     return CCDirector::sharedDirector()->getDeviceOrientation();
+    return kOrientationPortrait;
+}
+
+void Application::statusBarFrame(cocos2d::CGRect * rect)
+{
+    if (rect)
+    {
+        // Windows doesn't have status bar.
+        *rect = CGRectMake(0, 0, 0, 0);
+    }
+}
---
+++ platform_support/include/ccxApplication.h
@@ -0,0 +1,81 @@
+/****************************************************************************
+Copyright (c) 2010 cocos2d-x.org
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#ifndef __CCX_APP_DELEGATE_H_YANGWS_20110122__
+#define __CCX_APP_DELEGATE_H_YANGWS_20110122__
+
+#include "ccxCommon.h"
+
+NS_CC_BEGIN;
+
+class CGRect;
+
+class CCX_DLL_PS ccxApplication
+{
+public:
+    virtual ~ccxApplication() {}
+
+    /**
+    @brief	Callback by CCDirector for limit FPS.
+    @interval       The time, which expressed in second in second, between current frame and next. 
+    */
+    virtual void    setAnimationInterval(double interval) = 0;
+
+    typedef enum
+    {
+        /// Device oriented vertically, home button on the bottom
+        kOrientationPortrait = 0,
+        /// Device oriented vertically, home button on the top
+        kOrientationPortraitUpsideDown = 1,
+        /// Device oriented horizontally, home button on the right
+        kOrientationLandscapeLeft = 2,
+        /// Device oriented horizontally, home button on the left
+        kOrientationLandscapeRight = 3,
+    } Orientation;
+    /**
+    @brief	Callback by CCDirector for change device orientation.
+    @orientation    The defination of orientation which CCDirector want change to.
+    @return         The actual orientation of the application.
+    */
+    virtual Orientation setOrientation(Orientation orientation) = 0;
+
+    /**
+    @brief	Get status bar rectangle in EGLView window.
+    */
+    virtual void    statusBarFrame(CGRect * rect) = 0;
+
+    /**
+    @brief	Get current applicaiton instance.
+    @return Current application instance pointer.
+    */
+    static ccxApplication& sharedApplication();
+
+protected:
+    ccxApplication() {}
+    static void setSharedApplication(ccxApplication& app);
+};
+
+NS_CC_END;
+
+#endif	// __CCX_APP_DELEGATE_H_YANGWS_20110122__
