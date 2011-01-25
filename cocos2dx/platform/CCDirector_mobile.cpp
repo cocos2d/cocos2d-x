462ac4823ff054f4ce82af6cff1a40721e7bf295
walzer <walzer@cocos2d-x.org>
2011-01-25 11:33:03 +0800

    bug fix, CCDirect_mobile.cpp crash m_bPurgeDirecotorInNextLoop, according to this post http://www.cocos2d-x.org/boards/6/topics/422

--- cocos2dx/platform/CCDirector_mobile.cpp
+++ cocos2dx/platform/CCDirector_mobile.cpp
@@ -549,10 +549,13 @@ void CCDirector::purgeDirector()
 	// They are needed in case the director is run again
 	CCTouchDispatcher::sharedDispatcher()->removeAllDelegates();
 
-	m_pRunningScene->onExit();
-	m_pRunningScene->cleanup();
-	m_pRunningScene->release();
-
+    if (m_pRunningScene)
+    {
+    	m_pRunningScene->onExit();
+    	m_pRunningScene->cleanup();
+    	m_pRunningScene->release();
+    }
+    
 	m_pRunningScene = NULL;
 	m_pNextScene = NULL;
 
@@ -937,6 +940,7 @@ void CCDisplayLinkDirector::mainLoop(void)
 	if (m_bPurgeDirecotorInNextLoop)
 	{
 		purgeDirector();
+        m_bPurgeDirecotorInNextLoop = false;
 	}
 	else if (! m_bInvalid)
  	{
