12cf423fb7d807e2940a30722bb52c37e7352f47
Macarse <macarse@gmail.com>
2011-02-05 20:17:24 -0300

    Added logic to disable sensors from the c++ code

--- cocos2dx/platform/android/Cocos2dJni.cpp
+++ cocos2dx/platform/android/Cocos2dJni.cpp
@@ -30,6 +30,9 @@ THE SOFTWARE.
 #include "platform/android/CCXUIAccelerometer_android.h"
 #include <android/log.h>
 
+#define  LOG_TAG    "Cocos2dJni"
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+
 extern "C"
 {
 	static cocos2d::CCTouch s_touch;
@@ -86,13 +89,74 @@ extern "C"
 	// record the java vm
 	
 	JavaVM *gJavaVM = NULL;
+	jclass classOfCocos2dxActivity = 0;
+	JNIEnv *env = 0;
 
 	jint JNI_OnLoad(JavaVM *vm, void *reserved)
 	{
 		gJavaVM = vm;
 		return JNI_VERSION_1_4;
 	}
-	
+
+	static jmethodID getMethodID(const char *methodName, const char *paramCode)
+	{
+		jmethodID ret = 0;
+
+		// get jni environment and java class for Cocos2dxActivity
+		if (gJavaVM->GetEnv((void**)&env, JNI_VERSION_1_4) != JNI_OK)
+		{
+			LOGD("Failed to get the environment using GetEnv()");
+			return 0;
+		}
+
+		if (gJavaVM->AttachCurrentThread(&env, 0) < 0)
+		{
+			LOGD("Failed to get the environment using AttachCurrentThread()");
+			return 0;
+		}
+
+		classOfCocos2dxActivity = env->FindClass("org/cocos2dx/lib/Cocos2dxActivity");
+		if (! classOfCocos2dxActivity)
+		{
+			LOGD("Failed to find class of org/cocos2dx/lib/Cocos2dxActivity");
+			return 0;
+		}
+
+		if (env != 0 && classOfCocos2dxActivity != 0)
+		{
+			ret = env->GetStaticMethodID(classOfCocos2dxActivity, methodName, paramCode);
+		}
+
+		if (! ret)
+		{
+			LOGD("get method id of %s error", methodName);
+		}
+
+		return ret;
+	}
+
+	void enableAccelerometerJNI()
+	{
+		// void stopBackgroundMusic()
+		jmethodID methodID = getMethodID("enableAccelerometer", "()V");
+
+		if (methodID)
+		{
+			env->CallStaticVoidMethod(classOfCocos2dxActivity, methodID);
+		}
+	}
+
+	void disableAccelerometerJNI()
+	{
+		// void stopBackgroundMusic()
+		jmethodID methodID = getMethodID("disableAccelerometer", "()V");
+
+		if (methodID)
+		{
+			env->CallStaticVoidMethod(classOfCocos2dxActivity, methodID);
+		}
+	}
+
 	// native renderer
 
 	void Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeRender(JNIEnv* env)
