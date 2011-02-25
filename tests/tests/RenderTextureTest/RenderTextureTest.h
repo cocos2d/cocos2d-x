33cf53b262629aeafa326b7eb60e8aa953068261
natural-law <bin@cocos2d-x.org>
2011-02-25 11:31:49 +0800

    Update the tests from 0.99.4 to 0.99.5.

--- tests/tests/RenderTextureTest/RenderTextureTest.h
+++ tests/tests/RenderTextureTest/RenderTextureTest.h
@@ -4,16 +4,39 @@
 #include "cocos2d.h"
 #include "../testBasic.h"
 
-class RenderTextureTest : public CCLayer
+class RenderTextureTestDemo : public CCLayer
 {
 public:
-	RenderTextureTest();
-	~RenderTextureTest();
-	virtual void ccTouchesMoved(NSSet* touches, UIEvent* event);
+	virtual void onEnter();
+    virtual std::string title();
+    virtual std::string subtitle();
+
+    void restartCallback(NSObject* pSender);
+    void nextCallback(NSObject* pSender);
+    void backCallback(NSObject* pSender);
+};
+
+/**
+@todo refactor the save image feature
+*/
+class RenderTextureTest : public RenderTextureTestDemo
+{
+public:
+    RenderTextureTest();
+    ~RenderTextureTest();
+    virtual void ccTouchesMoved(NSSet* touches, UIEvent* event);
 
 private:
     CCRenderTexture*	m_target;
     CCSprite*			m_brush;
+};
+
+class RenderTextureIssue937 : public RenderTextureTestDemo
+{
+public:
+    RenderTextureIssue937();
+    virtual std::string title();
+    virtual std::string subtitle();
 };
 
 class RenderTextureScene : public TestScene
