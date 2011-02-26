d78897da5e9fd51508acf1dcb5dd21778fccaed5
yangws <yangws@cocos2d-x.org>
2011-02-26 11:09:49 +0800

    CCImage initWithString support line breaks and wrapping. CCLabelTTF support multi-text on win32 now.

--- platform_support/src/win32/ccxImage_win32.cpp
+++ platform_support/src/win32/ccxImage_win32.cpp
@@ -102,17 +102,35 @@ public:
         return bRet;
     }
 
-    bool getTextExtentPoint(const char * pszText, int nLen, SIZE& tSize)
+    SIZE sizeWithText(const char * pszText, int nLen, DWORD dwFmt, LONG nWidthLimit)
     {
-        bool bRet = false;
+        SIZE tRet = {0};
         do 
         {
-            CCX_BREAK_IF(! pszText);
+            CCX_BREAK_IF(! pszText || nLen <= 0);
+
+            RECT rc = {0, 0, 0, 0};
+            DWORD dwCalcFmt = DT_CALCRECT;
+
+            if (nWidthLimit > 0)
+            {
+                rc.right = nWidthLimit;
+                dwCalcFmt |= DT_WORDBREAK
+                    | (dwFmt & DT_CENTER)
+                    | (dwFmt & DT_RIGHT);
+            }
+            // use current font to measure text extent
             HGDIOBJ hOld = SelectObject(m_hDC, m_hFont);
-            bRet = (GetTextExtentPointA(m_hDC, pszText, nLen, &tSize)) ? true : false;
+
+            // measure text size
+            DrawTextA(m_hDC, pszText, nLen, &rc, dwCalcFmt);
             SelectObject(m_hDC, hOld);
+
+            tRet.cx = rc.right;
+            tRet.cy = rc.bottom;
         } while (0);
-        return bRet;
+
+        return tRet;
     }
 
     bool prepareBitmap(int nWidth, int nHeight)
@@ -134,18 +152,87 @@ public:
         return true;
     }
 
-    int drawText(const char * pszText, int nLen, SIZE& tSize, DWORD dwFmt)
+    int drawText(const char * pszText, SIZE& tSize, ccxImage::ETextAlign eAlign)
     {
         int nRet = 0;
         do 
         {
             CCX_BREAK_IF(! pszText);
 
-            if (tSize.cx <= 0 || tSize.cy <= 0)
+            DWORD dwFmt = DT_WORDBREAK;
+            DWORD dwHoriFlag = eAlign & 0x0f;
+            DWORD dwVertFlag = (eAlign & 0xf0) >> 4;
+
+            switch (dwHoriFlag)
             {
-                CCX_BREAK_IF(! getTextExtentPoint(pszText, nLen, tSize));
+            case 1: // left
+                dwFmt |= DT_LEFT;
+                break;
+            case 2: // right
+                dwFmt |= DT_RIGHT;
+                break;
+            case 3: // center
+                dwFmt |= DT_CENTER;
+                break;
             }
-            CCX_BREAK_IF(tSize.cx <= 0 || tSize.cy <= 0);
+
+            int nLen = strlen(pszText);
+            SIZE newSize = sizeWithText(pszText, nLen, dwFmt, tSize.cx);
+
+            RECT rcText = {0};
+            // if content width is 0, use text size as content size
+            if (tSize.cx <= 0)
+            {
+                tSize = newSize;
+                rcText.right  = newSize.cx;
+                rcText.bottom = newSize.cy;
+            }
+            else
+            {
+
+                LONG offsetX = 0;
+                LONG offsetY = 0;
+                rcText.right = newSize.cx; // store the text width to rectangle
+
+                // calculate text horizontal offset
+                if (1 != dwHoriFlag          // and text isn't align to left
+                    && newSize.cx < tSize.cx)   // and text's width less then content width,
+                {                               // then need adjust offset of X.
+                    offsetX = (2 == dwHoriFlag) ? tSize.cx - newSize.cx     // align to right
+                        : (tSize.cx - newSize.cx) / 2;                      // align to center
+                }
+
+                // if content height is 0, use text height as content height
+                // else if content height less than text height, use content height to draw text
+                if (tSize.cy <= 0)
+                {
+                    tSize.cy = newSize.cy;
+                    dwFmt   |= DT_NOCLIP;
+                    rcText.bottom = newSize.cy; // store the text height to rectangle
+                }
+                else if (tSize.cy < newSize.cy)
+                {
+                    // content height larger than text height need, clip text to rect
+                    rcText.bottom = tSize.cy;
+                }
+                else
+                {
+                    rcText.bottom = newSize.cy; // store the text height to rectangle
+
+                    // content larger than text, need adjust vertical position
+                    dwFmt |= DT_NOCLIP;
+
+                    // calculate text vertical offset
+                    offsetY = (2 == dwVertFlag) ? tSize.cy - newSize.cy     // align to bottom
+                        : (3 == dwVertFlag) ? (tSize.cy - newSize.cy) / 2   // align to middle
+                        : 0;                                                // align to top
+                }
+
+                if (offsetX || offsetY)
+                {
+                    OffsetRect(&rcText, offsetX, offsetY);
+                }
+            }
 
             CCX_BREAK_IF(! prepareBitmap(tSize.cx, tSize.cy));
 
@@ -156,7 +243,7 @@ public:
             RECT rc = {0, 0, tSize.cx, tSize.cy};
             SetBkMode(m_hDC, TRANSPARENT);
             SetTextColor(m_hDC, RGB(255, 255, 255)); // white color
-            nRet = DrawTextA(m_hDC, pszText, nLen, &rc, dwFmt);
+            nRet = DrawTextA(m_hDC, pszText, nLen, &rcText, dwFmt);
 
             SelectObject(m_hDC, hOldBmp);
             SelectObject(m_hDC, hOldFont);
@@ -193,55 +280,14 @@ bool ccxImage::initWithString(
 
         BitmapDC& dc = sharedBitmapDC();
 
-        dc.setFont(pFontName, nSize);
-
-        SIZE size = {0, 0};
-        if (nWidth > 0 && nHeight > 0)
+        if (! dc.setFont(pFontName, nSize))
         {
-            size.cx = nWidth;
-            size.cy = nHeight;
-        }
-        else
-        {
-            dc.getTextExtentPoint(pText, nLen, size);
-        }
-        CCX_BREAK_IF(! size.cx || ! size.cy);
-
-        // set style
-        DWORD dwFmt = DT_SINGLELINE;
-
-        switch (eAlignMask)
-        {
-        case kAlignCenter:
-            dwFmt |= DT_CENTER | DT_VCENTER;
-            break;
-        case kAlignTop:
-            dwFmt |= DT_CENTER | DT_TOP;
-            break;
-        case kAlignTopRight:
-            dwFmt |= DT_RIGHT | DT_TOP;
-            break;
-        case kAlignRight:
-            dwFmt |= DT_RIGHT | DT_VCENTER;
-            break;
-        case kAlignBottomRight:
-            dwFmt |= DT_RIGHT | DT_BOTTOM;
-            break;
-        case kAlignBottom:
-            dwFmt |= DT_CENTER | DT_BOTTOM;
-            break;
-        case kAlignBottomLeft:
-            dwFmt |= DT_LEFT | DT_BOTTOM;
-            break;
-        case kAlignLeft:
-            dwFmt |= DT_LEFT | DT_VCENTER;
-            break;
-        case kAlignTopLeft:
-            dwFmt |= DT_LEFT | DT_TOP;
-            break;
+            CCXLog("Can't found font(%s), use system default", pFontName);
         }
 
-        CCX_BREAK_IF(! dc.drawText(pText, nLen, size, dwFmt));
+        // draw text
+        SIZE size = {nWidth, nHeight};
+        CCX_BREAK_IF(! dc.drawText(pText, size, eAlignMask));
 
         m_pData.reset(new ccxByte[size.cx * size.cy * 4]);
         CCX_BREAK_IF(! m_pData.get());
