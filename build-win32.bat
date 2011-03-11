dd475e59268cb5dfabdd5e5f4d39c7e2fed2f5ca
yangws <yangws@cocos2d-x.org>
2011-03-11 15:12:49 +0800

    Modify build-win32.bat, let msbuild build debug and release configuration seprately.

--- build-win32.bat
+++ build-win32.bat
@@ -27,7 +27,8 @@ call %VSVARS%
 if %VC_VER%==90 (
     vcbuild cocos2d-win32.vc2008.sln $ALL
 ) else if %VC_VER%==100 (
-    msbuild cocos2d-win32.vc2010.sln /p:Configuration="Debug" /p:Configuration="Release"
+    msbuild cocos2d-win32.vc2010.sln /p:Configuration="Debug" 
+    msbuild cocos2d-win32.vs2010.sln /p:Configuration="Release"
 ) else (
     echo Script error.
     goto ERROR
