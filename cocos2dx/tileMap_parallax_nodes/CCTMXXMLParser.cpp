28a940ab575e796bb66b6e8fc92f0a3cdfd823cb
natural-law <bin@cocos2d-x.org>
2011-03-26 13:52:33 +0800

    issue #434,refactor class CCFileUtils on win32 and wophone.

--- cocos2dx/tileMap_parallax_nodes/CCTMXXMLParser.cpp
+++ cocos2dx/tileMap_parallax_nodes/CCTMXXMLParser.cpp
@@ -27,7 +27,7 @@ THE SOFTWARE.
 #include "CCTMXXMLParser.h"
 #include "CCTMXTiledMap.h"
 #include "ccMacros.h"
-#include "support/file_support/FileData.h"
+#include "CCFileUtils.h"
 #include "support/zip_support/ZipUtils.h"
 #include "CCPointExtension.h"
 #include "support/base64.h"
