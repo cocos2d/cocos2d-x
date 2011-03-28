e20f2377ffad62210fa31e859cb2af9c2523a149
yangws <yangws@cocos2d-x.org>
2011-03-28 17:20:39 +0800

    fixed #396 wophone modified.

--- cocos2dx/platform/wophone/CCImage_wophone.cpp
+++ cocos2dx/platform/wophone/CCImage_wophone.cpp
@@ -305,12 +305,12 @@ bool CCImage::initWithString(
         CC_BREAK_IF(nWidth <= 0 || nHeight <= 0);
 
         INT32 nDataLen = pBitmap->GetRowBytes() * nHeight;
-        m_pData.reset(new ccxByte[nDataLen]);
-        CC_BREAK_IF(! m_pData.get());
-        memcpy((void*) m_pData.get(), pBitmap->GetDataPtr(), nDataLen);
+        m_pData = new unsigned char[nDataLen];
+        CC_BREAK_IF(! m_pData);
+        memcpy((void*) m_pData, pBitmap->GetDataPtr(), nDataLen);
 
-        m_nWidth    = (ccxInt16)nWidth;
-        m_nHeight   = (ccxInt16)nHeight;
+        m_nWidth    = (short)nWidth;
+        m_nHeight   = (short)nHeight;
         m_bHasAlpha = true;
         m_bPreMulti = true;
         m_nBitsPerComponent = pBitmap->GetDepth() / 4;
