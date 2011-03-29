55817879b22d088f82cdef761a25a48b3ba916d5
minggo <ming@cocos2d-x.org>
2011-03-29 14:20:05 +0800

    [android] fixed #422: multitouch for android

--- tests/test.android/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ tests/test.android/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -1,119 +1,139 @@
 package org.cocos2dx.lib;
 
-import org.cocos2dx.lib.touch.metalev.multitouch.controller.MultiTouchController;
-import org.cocos2dx.lib.touch.metalev.multitouch.controller.MultiTouchController.MultiTouchObjectCanvas;
-import org.cocos2dx.lib.touch.metalev.multitouch.controller.MultiTouchController.PointInfo;
-import org.cocos2dx.lib.touch.metalev.multitouch.controller.MultiTouchController.PositionAndScale;
-
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.Log;
 import android.view.MotionEvent;
 
-public class Cocos2dxGLSurfaceView extends GLSurfaceView implements
-        MultiTouchObjectCanvas {
+public class Cocos2dxGLSurfaceView extends GLSurfaceView{
     private static final String TAG = Cocos2dxGLSurfaceView.class
             .getCanonicalName();
-    private MultiTouchController<Void> mTouchController;
     private Cocos2dxRenderer mRenderer;
+    private final boolean debug = false;
 
     public Cocos2dxGLSurfaceView(Context context) {
         super(context);
         mRenderer = new Cocos2dxRenderer();
         setRenderer(mRenderer);
-
-        mTouchController = new MultiTouchController<Void>(this);
     }
 
     public boolean onTouchEvent(final MotionEvent event) {
-        if (mTouchController.onTouchEvent(event)) {
-            final PointInfo pt = mTouchController.mCurrPt;
-            final int ids[] = new int[pt.getNumTouchPoints()];
-            final float xs[] = new float[pt.getNumTouchPoints()];
-            final float ys[] = new float[pt.getNumTouchPoints()];
+    	// these data are used in ACTION_MOVE and ACTION_CANCEL
+    	final int pointerNumber = event.getPointerCount();
+    	final int[] ids = new int[pointerNumber];
+    	final float[] xs = new float[pointerNumber];
+    	final float[] ys = new float[pointerNumber];
 
-            for (int i = 0; i < pt.getNumTouchPoints(); i++) {
-                ids[i] = pt.getPointerIds()[i];
-                xs[i] = pt.getXs()[i];
-                ys[i] = pt.getYs()[i];
-                Log.d(TAG,
-                        "ACTION: " + pt.getAction() + "id[i]="
-                                + pt.getPointerIds()[i] + "x[i]= "
-                                + pt.getXs()[i] + " y[i]= " + pt.getYs()[i]);               
-            }
-            
-            switch (pt.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-            case 261:
-            case MotionEvent.ACTION_POINTER_DOWN:
-                Log.d(TAG, "ACTION_DOWN");
-                queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        mRenderer.handleActionDown(ids, xs, ys);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "ACTION_MOVE");
-                queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        mRenderer.handleActionMove(ids, xs, ys);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_POINTER_UP:
-                Log.d(TAG, "ACTION_UP");
-                queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        mRenderer.handleActionUp(ids, xs, ys);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_CANCEL:
-                queueEvent(new Runnable() {
-                   @Override
-                    public void run() {
-                        mRenderer.handleActionCancel(ids, xs, ys);
-                    }
-                });
-                break;
-            }
-            
-            return true;
+    	for (int i = 0; i < pointerNumber; i++) {
+            ids[i] = event.getPointerId(i);
+            xs[i] = event.getX(i);
+            ys[i] = event.getY(i);                          
         }
-        return false;
+        
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        case MotionEvent.ACTION_POINTER_DOWN:           	
+        	final int idPointerDown = event.getAction() >> MotionEvent.ACTION_POINTER_ID_SHIFT;                        
+            final float xPointerDown = event.getX(idPointerDown);
+            final float yPointerDown = event.getY(idPointerDown);
+
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    mRenderer.handleActionDown(idPointerDown, xPointerDown, yPointerDown);
+                }
+            });
+            break;
+            
+        case MotionEvent.ACTION_DOWN:     
+        	// there are only one finger on the screen
+        	final int idDown = event.getPointerId(0);                            
+            final float xDown = event.getX(idDown);
+            final float yDown = event.getY(idDown);
+            
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    mRenderer.handleActionDown(idDown, xDown, yDown);
+                }
+            });
+            break;
+
+        case MotionEvent.ACTION_MOVE:                        
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    mRenderer.handleActionMove(ids, xs, ys);
+                }
+            });
+            break;
+
+        case MotionEvent.ACTION_POINTER_UP:           	            	
+        	final int idPointerUp = event.getAction() >> MotionEvent.ACTION_POINTER_ID_SHIFT;                        
+            final float xPointerUp = event.getX(idPointerUp);
+            final float yPointerUp = event.getY(idPointerUp);
+            
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    mRenderer.handleActionUp(idPointerUp, xPointerUp, yPointerUp);
+                }
+            });
+            break;
+            
+        case MotionEvent.ACTION_UP:  
+        	// there are only one finger on the screen
+        	final int idUp = event.getPointerId(0);
+            final float xUp = event.getX(idUp);
+            final float yUp = event.getY(idUp);
+            
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    mRenderer.handleActionUp(idUp, xUp, yUp);
+                }
+            });
+            break;
+
+        case MotionEvent.ACTION_CANCEL:
+            queueEvent(new Runnable() {
+               @Override
+                public void run() {
+                    mRenderer.handleActionCancel(ids, xs, ys);
+                }
+            });
+            break;
+        }
+      
+        if (debug){
+        	dumpEvent(event);
+        }
+        return true;
     }
-
-    @Override
-    public Object getDraggableObjectAtPoint(PointInfo touchPoint) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void getPositionAndScale(Object obj,
-            PositionAndScale objPosAndScaleOut) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public boolean setPositionAndScale(Object obj,
-            PositionAndScale newObjPosAndScale, PointInfo touchPoint) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void selectObject(Object obj, PointInfo touchPoint) {
-        // TODO Auto-generated method stub
-
+    
+    // Show an event in the LogCat view, for debugging
+    private void dumpEvent(MotionEvent event) {
+       String names[] = { "DOWN" , "UP" , "MOVE" , "CANCEL" , "OUTSIDE" ,
+          "POINTER_DOWN" , "POINTER_UP" , "7?" , "8?" , "9?" };
+       StringBuilder sb = new StringBuilder();
+       int action = event.getAction();
+       int actionCode = action & MotionEvent.ACTION_MASK;
+       sb.append("event ACTION_" ).append(names[actionCode]);
+       if (actionCode == MotionEvent.ACTION_POINTER_DOWN
+             || actionCode == MotionEvent.ACTION_POINTER_UP) {
+          sb.append("(pid " ).append(
+          action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+          sb.append(")" );
+       }
+       sb.append("[" );
+       for (int i = 0; i < event.getPointerCount(); i++) {
+          sb.append("#" ).append(i);
+          sb.append("(pid " ).append(event.getPointerId(i));
+          sb.append(")=" ).append((int) event.getX(i));
+          sb.append("," ).append((int) event.getY(i));
+          if (i + 1 < event.getPointerCount())
+             sb.append(";" );
+       }
+       sb.append("]" );
+       Log.d(TAG, sb.toString());
     }
 }
