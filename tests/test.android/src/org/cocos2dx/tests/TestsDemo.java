0203141aafca9194c47d986355c7e8a3085085c3
Macarse <macarse@gmail.com>
2011-04-11 00:28:12 -0300

    Now the size depends on the GLView size

--- tests/test.android/src/org/cocos2dx/tests/TestsDemo.java
+++ tests/test.android/src/org/cocos2dx/tests/TestsDemo.java
@@ -3,12 +3,12 @@ package org.cocos2dx.tests;
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
-
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class TestsDemo extends Cocos2dxActivity{
-	protected void onCreate(Bundle savedInstanceState){
+	private Cocos2dxGLSurfaceView mGLView;
+
+    protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
 		// get the packageName,it's used to set the resource path
@@ -16,8 +16,19 @@ public class TestsDemo extends Cocos2dxActivity{
 		super.setPackgeName(packageName);
 
         setContentView(R.layout.test_demo);
-	}	
-	
+        mGLView = (Cocos2dxGLSurfaceView) findViewById(R.id.test_demo_gl_surfaceview);
+
+        // Get the size of the mGLView after the layout happens
+        mGLView.post(new Runnable() {
+            
+            @Override
+            public void run() {
+                Cocos2dxActivity.screenHeight = mGLView.getHeight();
+                Cocos2dxActivity.screenWidth = mGLView.getWidth();
+            }
+        });
+	}
+
 	 @Override
 	 protected void onPause() {
 	     super.onPause();
