fb123e526624065f539ef76027fcf46158e39a1f
yangws <yangws@cocos2d-x.org>
2011-04-28 14:19:12 +0800

    [ios] tests add RootViewController for rotate with ui object.

--- tests/test.ios/Classes/testsAppDelegate.h
+++ tests/test.ios/Classes/testsAppDelegate.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class RootViewController;
+
 @interface AppController : NSObject <UIApplicationDelegate> {
     UIWindow *window;
+    RootViewController	*viewController;
 }
 
 @end
