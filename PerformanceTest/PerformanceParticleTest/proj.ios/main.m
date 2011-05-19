c177e4ef0764441da6919899373e4e9555b27b62
natural-law <bin@cocos2d-x.org>
2011-05-19 15:07:22 +0800

    Add performance test for ios.

---
+++ PerformanceTest/PerformanceParticleTest/proj.ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PerformanceParticleTest
+//
+//  Created by Walzer on 11-5-19.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"PerformanceParticleTestAppController");
+	[pool release];
+	return retVal;
+}
