b4b3ddb746bedd881cb582c7bd43cff0a4f5be42
Walzer <walzer@cocos2d-x.org>
2011-06-13 14:31:25 +0800

    issue #398, cocos2d-x only template for xcode is ok now. hura!

--- template/xcode4/cocos2d-x.xctemplate/Classes/HelloWorldScene.h
+++ template/xcode4/cocos2d-x.xctemplate/Classes/HelloWorldScene.h
@@ -1,36 +1,22 @@
-//
-//  HelloWorldScene.h
-//  ___PROJECTNAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
-//
-
-#ifndef __HELLOW_WORLD_H__
-#define __HELLOW_WORLD_H__
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
 
-// include Chipmunk headers
-#include "chipmunk.h"
-
-// HelloWorld Layer
-class HelloWorld : public cocos2d::CCLayer {
+class HelloWorld : public cocos2d::CCLayer
+{
 public:
-        HelloWorld();
-        ~HelloWorld();
-    
-        static cocos2d::CCScene* scene();
-        void step(cocos2d::ccTime dt);
-        void addNewSpriteX(float x, float y);
-        virtual void onEnter();
-        virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent *event);
-        
-        LAYER_NODE_FUNC(HelloWorld);
-    
-private:
-        bool init();
-        cpSpace *space;
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual bool init();  
+
+	// there's no 'id' in cpp, so we recommand to return the exactly class pointer
+	static cocos2d::CCScene* scene();
+	
+	// a selector callback
+	virtual void menuCloseCallback(CCObject* pSender);
+
+	// implement the "static node()" method manually
+	LAYER_NODE_FUNC(HelloWorld);
 };
 
-#endif // __HELLOW_WORLD_H__
+#endif // __HELLOWORLD_SCENE_H__
