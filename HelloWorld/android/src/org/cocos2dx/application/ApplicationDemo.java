00568c229a8293d37cbc4ae6e0d1a426af687364
Walzer <walzer@cocos2d-x.org>
2011-06-25 11:21:16 +0800

    [android] back to shared library link

--- HelloWorld/android/src/org/cocos2dx/application/ApplicationDemo.java
+++ HelloWorld/android/src/org/cocos2dx/application/ApplicationDemo.java
@@ -43,6 +43,8 @@ public class ApplicationDemo extends Cocos2dxActivity{
 	 }
 	
      static {
+    	 System.loadLibrary("cocos2d");
+    	 System.loadLibrary("cocosdenshion");
          System.loadLibrary("helloworld");
      }
 }
