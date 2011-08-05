4316873a48ba1040833584193c6df718a35ecbef
minggo <ming@cocos2d-x.org>
2011-08-05 11:02:42 +0800

    fixed #622: add comment of CCUserDefault

--- cocos2dx/include/CCUserDefault.h
+++ cocos2dx/include/CCUserDefault.h
@@ -1,6 +1,5 @@
 /****************************************************************************
 Copyright (c) 2010-2011 cocos2d-x.org
-Copyright (c) 2008-2010 Ricardo Quesada
 
 http://www.cocos2d-x.org
 
@@ -32,23 +31,68 @@ THE SOFTWARE.
 
 NS_CC_BEGIN;
 
+/**
+ * CCUserDefault acts as a tiny database. You can save and get base type values by it.
+ * For example, setBoolForKey("played", true) will add a bool value true into the database.
+ * Its key is "played". You can get the value of the key by getBoolForKey("played").
+ * 
+ * It supports the following base types:
+ * bool, int, float, double, string
+ */
 class CC_DLL CCUserDefault
 {
 public:
 	~CCUserDefault();
 
 	// get value methods
+
+	/**
+	@brief Get bool value by key, if the key doesn't exist, a default value will return.
+	 You can set the default value, or it is false.
+	*/
 	bool	getBoolForKey(const char* pKey, bool defaultValue = false);
+	/**
+	@brief Get integer value by key, if the key doesn't exist, a default value will return.
+	 You can set the default value, or it is 0.
+	*/
 	int		getIntegerForKey(const char* pKey, int defaultValue = 0);
+	/**
+	@brief Get float value by key, if the key doesn't exist, a default value will return.
+	 You can set the default value, or it is 0.0f.
+	*/
 	float	getFloatForKey(const char* pKey, float defaultValue=0.0f);
+	/**
+	@brief Get double value by key, if the key doesn't exist, a default value will return.
+	 You can set the default value, or it is 0.0.
+	*/
 	double  getDoubleForKey(const char* pKey, double defaultValue=0.0);
+	/**
+	@brief Get string value by key, if the key doesn't exist, a default value will return.
+	You can set the default value, or it is "".
+	*/
 	std::string getStringForKey(const char* pKey, const std::string & defaultValue = "");
 
 	// set value methods
+
+	/**
+	@brief Set bool value by key.
+	*/
 	void	setBoolForKey(const char* pKey, bool value);
+	/**
+	@brief Set integer value by key.
+	*/
 	void	setIntegerForKey(const char* pKey, int value);
+	/**
+	@brief Set float value by key.
+	*/
 	void	setFloatForKey(const char* pKey, float value);
+	/**
+	@brief Set double value by key.
+	*/
 	void	setDoubleForKey(const char* pKey, double value);
+	/**
+	@brief Set string value by key.
+	*/
 	void	setStringForKey(const char* pKey, const std::string & value);
 
 	static CCUserDefault* sharedUserDefault();
