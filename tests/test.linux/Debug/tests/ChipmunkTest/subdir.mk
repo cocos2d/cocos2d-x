e25f3dfb8091d232cb2e3f2e41b99e38deb8177c
laschweinski <laschweinski@laschweinski-desktop.(none)>
2011-08-28 17:09:40 +0800

    test debug and release

---
+++ tests/test.linux/Debug/tests/ChipmunkTest/subdir.mk
@@ -0,0 +1,224 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Bounce.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Joints.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/LogoSmash.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/MagnetsElectric.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/OneWay.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Planet.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Player.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Plink.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Pump.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/PyramidStack.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/PyramidTopple.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Query.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Sensors.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Simple.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Springies.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Tank.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/TheoJansen.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Tumble.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/UnsafeOps.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/cocos2dChipmunkDemo.cpp \
+/home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/drawSpace.cpp 
+
+OBJS += \
+./tests/ChipmunkTest/Bounce.o \
+./tests/ChipmunkTest/Joints.o \
+./tests/ChipmunkTest/LogoSmash.o \
+./tests/ChipmunkTest/MagnetsElectric.o \
+./tests/ChipmunkTest/OneWay.o \
+./tests/ChipmunkTest/Planet.o \
+./tests/ChipmunkTest/Player.o \
+./tests/ChipmunkTest/Plink.o \
+./tests/ChipmunkTest/Pump.o \
+./tests/ChipmunkTest/PyramidStack.o \
+./tests/ChipmunkTest/PyramidTopple.o \
+./tests/ChipmunkTest/Query.o \
+./tests/ChipmunkTest/Sensors.o \
+./tests/ChipmunkTest/Simple.o \
+./tests/ChipmunkTest/Springies.o \
+./tests/ChipmunkTest/Tank.o \
+./tests/ChipmunkTest/TheoJansen.o \
+./tests/ChipmunkTest/Tumble.o \
+./tests/ChipmunkTest/UnsafeOps.o \
+./tests/ChipmunkTest/cocos2dChipmunkDemo.o \
+./tests/ChipmunkTest/drawSpace.o 
+
+CPP_DEPS += \
+./tests/ChipmunkTest/Bounce.d \
+./tests/ChipmunkTest/Joints.d \
+./tests/ChipmunkTest/LogoSmash.d \
+./tests/ChipmunkTest/MagnetsElectric.d \
+./tests/ChipmunkTest/OneWay.d \
+./tests/ChipmunkTest/Planet.d \
+./tests/ChipmunkTest/Player.d \
+./tests/ChipmunkTest/Plink.d \
+./tests/ChipmunkTest/Pump.d \
+./tests/ChipmunkTest/PyramidStack.d \
+./tests/ChipmunkTest/PyramidTopple.d \
+./tests/ChipmunkTest/Query.d \
+./tests/ChipmunkTest/Sensors.d \
+./tests/ChipmunkTest/Simple.d \
+./tests/ChipmunkTest/Springies.d \
+./tests/ChipmunkTest/Tank.d \
+./tests/ChipmunkTest/TheoJansen.d \
+./tests/ChipmunkTest/Tumble.d \
+./tests/ChipmunkTest/UnsafeOps.d \
+./tests/ChipmunkTest/cocos2dChipmunkDemo.d \
+./tests/ChipmunkTest/drawSpace.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+tests/ChipmunkTest/Bounce.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Bounce.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Joints.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Joints.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/LogoSmash.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/LogoSmash.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/MagnetsElectric.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/MagnetsElectric.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/OneWay.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/OneWay.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Planet.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Planet.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Player.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Player.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Plink.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Plink.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Pump.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Pump.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/PyramidStack.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/PyramidStack.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/PyramidTopple.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/PyramidTopple.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Query.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Query.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Sensors.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Sensors.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Simple.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Simple.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Springies.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Springies.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Tank.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Tank.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/TheoJansen.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/TheoJansen.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/Tumble.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/Tumble.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/UnsafeOps.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/UnsafeOps.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/cocos2dChipmunkDemo.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/cocos2dChipmunkDemo.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/ChipmunkTest/drawSpace.o: /home/laschweinski/git/cocos2d-x/tests/tests/ChipmunkTest/drawSpace.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DLINUX -I../../ -I../../../cocos2dx/platform/third_party/linux -I../../../cocos2dx/include -I../../../cocos2dx -I../../../cocos2dx/platform -I../../../CocosDenshion/include -I../../..//chipmunk/include/chipmunk -I../../../Box2D -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
