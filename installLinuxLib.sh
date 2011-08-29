442bbe82da190af580181c011ec7d4131063d35d
laschweinski <laschweinski@laschweinski-desktop.(none)>
2011-08-29 10:28:00 +0800

    update eclipse project file

--- installLinuxLib.sh
+++ installLinuxLib.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-linuxLibFolder="Release"
-if [ "$1" = "Debug" ];then
-	echo "debug"
-	linuxLibFolder="Debug"
-fi
-cp lib/linux/$linuxLibFolder/libcocos*.so /usr/lib/
-if [ -d "/usr/include/cocos2dx" ];then
-	echo "exist"
-else
-	echo "build cocos2dx folder"
-	mkdir /usr/include/cocos2dx
-fi
-
-if [ "$2" = "move" ];then
-cp cocos2dx/*.h /usr/include/cocos2dx/
-cp -rf cocos2dx/include /usr/include/cocos2dx/
-cp -f cocos2dx/platform/*.h /usr/include/cocos2dx/platform
-cp -f cocos2dx/platform/Linux/*.h /usr/include/cocos2dx/platform/Linux/
-fi
+#just copy all fmod lib into /usr/lib
+#run it in root mode
+cp CocosDenshion/third_party/fmod/api/lib/* /usr/lib
