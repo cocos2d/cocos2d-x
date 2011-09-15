194f47bc5475d97044e8d37ca7d8ec27a34d2716
minggo <ming@cocos2d-x.org>
2011-09-15 14:17:07 +0800

    fixed #718: get button value from string.xml & add copyright for java codes

--- HelloLua/android/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ HelloLua/android/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -1,46 +1,31 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * This is a small port of the "San Angeles Observation" demo
- * program for OpenGL ES 1.x. For more details, see:
- *
- *    http://jet.ro/visuals/san-angeles-observation/
- *
- * This program demonstrates how to use a GLSurfaceView from Java
- * along with native OpenGL calls to perform frame rendering.
- *
- * Touching the screen will start/stop the animation.
- *
- * Note that the demo runs much faster on the emulator than on
- * real devices, this is mainly due to the following facts:
- *
- * - the demo sends bazillions of polygons to OpenGL without
- *   even trying to do culling. Most of them are clearly out
- *   of view.
- *
- * - on a real device, the GPU bus is the real bottleneck
- *   that prevent the demo from getting acceptable performance.
- *
- * - the software OpenGL engine used in the emulator uses
- *   the system bus instead, and its code rocks :-)
- *
- * Fixing the program to send less polygons to the GPU is left
- * as an exercise to the reader. As always, patches welcomed :-)
- */
+/****************************************************************************
+Copyright (c) 2010-2011 cocos2d-x.org
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
 
 package org.cocos2dx.lib;
+
+
+import org.cocos2dx.hellolua.R;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -238,7 +223,7 @@ public class Cocos2dxActivity extends Activity{
     	Dialog dialog = new AlertDialog.Builder(this)
 	    .setTitle(title)
 	    .setMessage(message)
-	    .setPositiveButton("Ok", 
+	    .setPositiveButton(R.string.button_ok, 
 	    new DialogInterface.OnClickListener()
 	    {
 	    	public void onClick(DialogInterface dialog, int whichButton){
--- HelloWorld/android/gen/org/cocos2dx/application/R.java
+++ HelloWorld/android/gen/org/cocos2dx/application/R.java
@@ -19,8 +19,10 @@ public final class R {
     }
     public static final class layout {
         public static final int helloworld_demo=0x7f030000;
+        public static final int mylayout=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_ok=0x7f040001;
     }
 }
