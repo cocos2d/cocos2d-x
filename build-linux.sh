3524bc4ecbe97ef444a5cf582bf7c12c6f733692
dumganhar <dumganhar@163.com>
2011-09-20 18:13:24 +0800

    modify makefile

--- build-linux.sh
+++ build-linux.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#p=$(dirname $_)
+#echo "$p"
+
+path=$(dirname $0)
+path=${path/\./$(pwd)}
+echo $path
 
 if [ ! -d "lib" ]; then
 mkdir "lib"
@@ -6,10 +12,6 @@ fi
 
 if [ ! -d "lib/linux" ]; then
 mkdir "lib/linux"
-fi
-
-if [ ! -d "lib/android" ]; then
-mkdir "lib/android"
 fi
 
 if [ ! -d "lib/linux/Debug" ]; then
@@ -20,54 +22,29 @@ if [ ! -d "lib/linux/Release" ]; then
 mkdir "lib/linux/Release"
 fi
 
-if [ ! -d "lib/android/Debug" ]; then
-mkdir "lib/android/Debug"
-fi
-
-if [ ! -d "lib/android/Release" ]; then
-mkdir "lib/android/Release"
-fi
-
-
-
 if [ $# -ne 0 ]; then
 	if [ $1 = "clean" ]; then
-		cd ./cocos2dx/proj.linux
+		cd $p/cocos2dx/proj.linux
 		make clean
 
 		cd ../../
-		cd ./CocosDenshion/proj.linux
+		cd $p/CocosDenshion/proj.linux
 		make clean
 
 		cd ../../
-		cd ./HelloWorld/Linux
-		make clean
-
-		cd ../../
-		cd ./tests/test.linux
-		make clean
-
-		cd ../../
-		rm -r lib
+		rm -r lib/linux/Debug/*.so
 	fi
 	
 else
-	cd ./cocos2dx/proj.linux
+	cd $p/cocos2dx/proj.linux
 	make
 
 	cp -f libcocos2d.so ../../lib/linux/Debug
 	cd ../../
-	cd ./CocosDenshion/proj.linux
+	cd $p/CocosDenshion/proj.linux
 	make
 
 	cp -f libCocosDenshion.so ../../lib/linux/Debug
-	cd ../../
-	cd ./HelloWorld/Linux
-	make
-
-	cd ../../
-	cd ./tests/test.linux
-	make
 
 	cd ../../
 fi
