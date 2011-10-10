1d30b797147deeefd4f55d7cf70cda69369afd24
dumganhar <dumganhar@163.com>
2011-10-10 14:33:29 +0800

    update project

--- bada2.0_copy_libs.bat
+++ bada2.0_copy_libs.bat
@@ -6,7 +6,7 @@ copy CocosDenshion\proj.bada\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib
 copy cocos2dx\platform\third_party\bada\src\libjpeg\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib\Target-Debug
 copy cocos2dx\platform\third_party\bada\src\png\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib\Target-Debug
 copy cocos2dx\platform\third_party\bada\src\zlib\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib\Target-Debug
-copy cocos2dx\proj.bada\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib\Target-Debug
+::copy cocos2dx\proj.bada\sdk2.0\.Target-Debug\*.a HelloWorld\bada\sdk2.0\lib\Target-Debug
 
 copy Box2D\proj.bada\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
 copy chipmunk\proj.bada\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
@@ -14,7 +14,7 @@ copy CocosDenshion\proj.bada\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\l
 copy cocos2dx\platform\third_party\bada\src\libjpeg\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
 copy cocos2dx\platform\third_party\bada\src\png\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
 copy cocos2dx\platform\third_party\bada\src\zlib\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
-copy cocos2dx\proj.bada\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
+::copy cocos2dx\proj.bada\sdk2.0\.Target-Release\*.a HelloWorld\bada\sdk2.0\lib\Target-Release
 
 :: copy libs to tests project
 copy Box2D\proj.bada\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
@@ -23,7 +23,7 @@ copy CocosDenshion\proj.bada\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib
 copy cocos2dx\platform\third_party\bada\src\libjpeg\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
 copy cocos2dx\platform\third_party\bada\src\png\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
 copy cocos2dx\platform\third_party\bada\src\zlib\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
-copy cocos2dx\proj.bada\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
+::copy cocos2dx\proj.bada\sdk2.0\.Target-Debug\*.a tests\test.bada\sdk2.0\lib\Target-Debug
 
 copy Box2D\proj.bada\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
 copy chipmunk\proj.bada\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
@@ -31,6 +31,6 @@ copy CocosDenshion\proj.bada\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\l
 copy cocos2dx\platform\third_party\bada\src\libjpeg\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
 copy cocos2dx\platform\third_party\bada\src\png\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
 copy cocos2dx\platform\third_party\bada\src\zlib\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
-copy cocos2dx\proj.bada\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
+::copy cocos2dx\proj.bada\sdk2.0\.Target-Release\*.a tests\test.bada\sdk2.0\lib\Target-Release
 
 pause
