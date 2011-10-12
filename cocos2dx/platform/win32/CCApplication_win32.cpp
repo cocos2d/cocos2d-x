5feee662e708d7020707fb77d89360d10cd3e7b6
moadib <moadib73rus@gmail.com>
2011-10-12 09:32:59 +0400

    More simple and correct way to detect language for win32 application, also added detection of all languages contained in ccLanguageType enumeration .
    Added support for detect russian language.

--- cocos2dx/platform/win32/CCApplication_win32.cpp
+++ cocos2dx/platform/win32/CCApplication_win32.cpp
@@ -127,22 +127,32 @@ CCApplication& CCApplication::sharedApplication()
 
 ccLanguageType CCApplication::getCurrentLanguage()
 {
-    int arrayChineseCode[] = {
-        2052, 4100, 1028, 3076, 5124
-    };
-
-    LCID LanguageID = GetUserDefaultLCID();
     ccLanguageType ret = kLanguageEnglish;
 
-    int nCount = sizeof(arrayChineseCode) / sizeof(int);
-    for (int i = 0; i < nCount; ++i)
-    {
-        if (arrayChineseCode[i] == LanguageID)
-        {
-            ret = kLanguageChinese;
-            break;
-        }
-    }
+	LCID localeID = GetUserDefaultLCID();
+	unsigned short primaryLanguageID = localeID & 0xFF;
+	
+	switch (primaryLanguageID)
+	{
+		case LANG_CHINESE:
+			ret = kLanguageChinese;
+			break;
+		case LANG_FRENCH:
+			ret = kLanguageFrench;
+			break;
+		case LANG_ITALIAN:
+			ret = kLanguageItalian;
+			break;
+		case LANG_GERMAN:
+			ret = kLanguageGerman;
+			break;
+		case LANG_SPANISH:
+			ret = kLanguageSpanish;
+			break;
+		case LANG_RUSSIAN:
+			ret = kLanguageRussian;
+			break;
+	}
 
     return ret;
 }
