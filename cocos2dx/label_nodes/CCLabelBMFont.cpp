630a3dfdfad685c29bcecdb6250635316e8fbee1
dumganhar <dumganhar@163.com>
2011-11-08 18:11:43 +0800

    use unix text format

--- cocos2dx/label_nodes/CCLabelBMFont.cpp
+++ cocos2dx/label_nodes/CCLabelBMFont.cpp
@@ -147,7 +147,8 @@ namespace cocos2d{
         // parse spacing / padding
         std::string line;
         std::string strLeft(pBuffer, nBufSize);
-        while (strLeft.length() > 0)        {
+        while (strLeft.length() > 0)
+        {
             int pos = strLeft.find('\n');
 
             if (pos != (int)std::string::npos)
@@ -553,7 +554,8 @@ namespace cocos2d{
                 {
                     pNode->setIsVisible(false);
                 }
-            }		}
+            }
+		}
 		this->createFontChars();
 	}
 
@@ -581,7 +583,8 @@ namespace cocos2d{
                 {
                     pNode->setColor(m_tColor);
                 }
-            }		}
+            }
+		}
 	}
 	const ccColor3B& CCLabelBMFont::getColor()
 	{
@@ -605,7 +608,8 @@ namespace cocos2d{
                         pRGBAProtocol->setOpacity(m_cOpacity);
                     }
                 }
-            }		}
+            }
+		}
 	}
 	GLubyte CCLabelBMFont::getOpacity()
 	{
@@ -628,7 +632,8 @@ namespace cocos2d{
                         pRGBAProtocol->setIsOpacityModifyRGB(m_bIsOpacityModifyRGB);
                     }
                 }
-            }		}
+            }
+		}
 	}
 	bool CCLabelBMFont::getIsOpacityModifyRGB()
 	{
