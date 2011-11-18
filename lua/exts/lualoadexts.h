f77ed5c2f3cb6d1878d6f8a5fbf6de8526486133
YuLei Liao <dualface@gmail.com>
2011-11-18 21:31:01 +0800

    * import lua/exts (luasocket)

---
+++ lua/exts/lualoadexts.h
@@ -0,0 +1,16 @@
+//
+//  lualoadexts.h
+//  Mystic-Fish-Puzzle
+//
+//  Created by  on 11-10-3.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef __LUALOADEXTS_H_
+#define __LUALOADEXTS_H_
+
+#include "lauxlib.h"
+
+void luax_initpreload(lua_State *L);
+
+#endif // __LUALOADEXTS_H_
