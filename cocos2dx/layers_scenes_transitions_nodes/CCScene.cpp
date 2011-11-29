90ae1aabc738ce9a03d496f0931eda5aaa696eba
YuLei Liao <dualface@gmail.com>
2011-11-29 17:10:58 +0800

    * add CCScene::registerScriptEventsHandler(), CCScene::unregisterScriptEventsHandler()

--- cocos2dx/layers_scenes_transitions_nodes/CCScene.cpp
+++ cocos2dx/layers_scenes_transitions_nodes/CCScene.cpp
@@ -1,36 +1,44 @@
 /****************************************************************************
-Copyright (c) 2010-2011 cocos2d-x.org
-Copyright (c) 2008-2010 Ricardo Quesada
-Copyright (c) 2011      Zynga Inc.
-
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ Copyright (c) 2010-2011 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ Copyright (c) 2011      Zynga Inc.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 
 #include "CCScene.h"
 #include "CCPointExtension.h"
 #include "CCDirector.h"
 
-namespace   cocos2d {
+#if LUA_ENGINE
+#include "CCLuaEngine.h"
+#endif
+
+namespace cocos2d
+{
 
 CCScene::CCScene()
+#if LUA_ENGINE
+: m_eventsFunctionRefID(0)
+#endif
 {
     m_bIsRelativeAnchorPoint = false;
     setAnchorPoint(ccp(0.5f, 0.5f));
@@ -39,6 +47,9 @@ CCScene::CCScene()
 
 CCScene::~CCScene()
 {
+#if LUA_ENGINE
+    unregisterScriptEventsHandler();
+#endif
 }
 
 bool CCScene::init()
@@ -69,4 +80,46 @@ CCScene *CCScene::node()
         return NULL;
     }
 }
+
+
+#if LUA_ENGINE
+
+void CCScene::onEnter()
+{
+    if (m_eventsFunctionRefID)
+    {
+        CCLuaEngine::sharedEngine()->executeFunctionWithBooleanData(m_eventsFunctionRefID, true);
+    }
+    CCNode::onEnter();
+}
+
+void CCScene::onExit()
+{
+    if (m_eventsFunctionRefID)
+    {
+        CCLuaEngine::sharedEngine()->executeFunctionWithBooleanData(m_eventsFunctionRefID, false);
+    }
+    CCNode::onExit();
+}
+
+void CCScene::registerScriptEventsHandler(int functionRefID)
+{
+    unregisterScriptEventsHandler();
+    m_eventsFunctionRefID = functionRefID;
+    
+    CCLOG("[LUA] ADD Scene events handler: %d", functionRefID);
+}
+
+void CCScene::unregisterScriptEventsHandler(void)
+{
+    if (m_eventsFunctionRefID)
+    {
+        CCLuaEngine::sharedEngine()->removeLuaFunctionRef(m_eventsFunctionRefID);
+        CCLOG("[LUA] DEL Scene events handler: %d", m_eventsFunctionRefID);
+    }
+    m_eventsFunctionRefID = 0;
+}
+
+#endif // LUA_ENGINE
+    
 }//namespace   cocos2d
