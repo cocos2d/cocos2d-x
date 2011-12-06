dfbf2c0cdb77a623d57535e4d2d0b966cce6f81d
dumganhar <dumganhar@163.com>
2011-12-06 15:03:14 +0800

    add tests project for blackberry

---
+++ tests/test.qnx/main.cpp
@@ -0,0 +1,20 @@
+#include "cocos2d.h"
+#include "AppDelegate.h"
+
+#define RESOURCE_PATH        "./app/native/Resource"
+
+USING_NS_CC;
+
+int main(int argc, char **argv)
+{
+	AppDelegate* app;
+	int ret = 0;
+
+    CCFileUtils::setResourcePath(RESOURCE_PATH);
+
+	app = new AppDelegate();
+	ret = cocos2d::CCApplication::sharedApplication().run();
+	delete app;
+
+	return ret;
+}
