1184559ce1ef4f8042bbcfe5a4e9b55734b9243b
YuLei Liao <dualface@gmail.com>
2011-12-08 00:30:46 +0800

    * add tolua_stack_dump()

--- lua/cocos2dx_support/tolua_fix.h
+++ lua/cocos2dx_support/tolua_fix.h
@@ -21,6 +21,8 @@ TOLUA_API int tolua_ref_function(lua_State* L, int lo, int def);
 TOLUA_API void tolua_remove_function_by_refid(lua_State* L, int refid);
 
 TOLUA_API int tolua_isfunction(lua_State* L, int lo, tolua_Error* err);
+    
+TOLUA_API void tolua_stack_dump(lua_State *L);
 
 #ifdef __cplusplus
 } // extern "C"
