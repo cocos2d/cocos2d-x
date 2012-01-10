7a231969fe4f7abce3f90501c11c019c0e5de942
dumganhar <dumganhar@163.com>
2012-01-10 17:18:42 +0800

    add prompt of creating project successfully

--- create_qnx_project.sh
+++ create_qnx_project.sh
@@ -77,6 +77,7 @@ create_qnx_project(){
     modify_file_content bar-descriptor.xml
     copy_cpp_h_from_helloworld
     copy_resouces
+    echo "Congratulations, the '$PROJECT_NAME' project have been created successfully, please input the project from QNX IDE!"
 }
 
 create_qnx_project
