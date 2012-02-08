52de292e20ab072eeb980802e26a78394743e94f
minggo <ming@cocos2d-x.org>
2012-02-08 11:14:47 +0800

    fixed #970: use modules in the android ndk build

--- HelloLua/Classes/Android.mk
+++ HelloLua/Classes/Android.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := game_logic
+
+LOCAL_MODULE := game_logic_shared
+
+LOCAL_MODULE_FILENAME := libgame_logic
 
 LOCAL_SRC_FILES := \
 AppDelegate.cpp \
@@ -10,20 +14,8 @@ AppDelegate.cpp \
 ../../lua/cocos2dx_support/LuaEngine.cpp \
 ../../lua/CocosDenshion_support/LuaSimpleAudioEngine.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../cocos2dx \
-                    $(LOCAL_PATH)/../../cocos2dx/platform \
-                    $(LOCAL_PATH)/../../cocos2dx/include \
-                    $(LOCAL_PATH)/../../cocos2dx/lua_support \
-                    $(LOCAL_PATH)/../../CocosDenshion/include \
-                    $(LOCAL_PATH)/../../lua/lua \
-                    $(LOCAL_PATH)/../../lua/tolua \
-                    $(LOCAL_PATH)/../../lua/cocos2dx_support \
-                    $(LOCAL_PATH)/../../lua/CocosDenshion_support
- 
-LOCAL_LDLIBS := -L$(call host-path, $(LOCAL_PATH)/../android/libs/$(TARGET_ARCH_ABI)) \
-		-L$(call host-path, $(LOCAL_PATH)/../../cocos2dx/platform/third_party/android/libraries/$(TARGET_ARCH_ABI)) -lcurl \
-		-lcocos2d \
-		-lcocosdenshion \
-		-llua
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES := cocos2dx_shared cocosdenshion_shared lua_shared
             
 include $(BUILD_SHARED_LIBRARY)
