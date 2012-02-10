8b855a917fb858f48c77152eaf52487da3b43e06
minggo <ming@cocos2d-x.org>
2012-02-10 12:02:46 +0800

    issue #973: refactor lua support

--- lua/lua/llex.c
+++ lua/lua/llex.c
@@ -177,16 +177,21 @@ static void buffreplace (LexState *ls, char from, char to) {
 
 
 static void trydecpoint (LexState *ls, SemInfo *seminfo) {
-  /* format error: try to update decimal point separator */
-  struct lconv *cv = localeconv();
-  char old = ls->decpoint;
-  ls->decpoint = (cv ? cv->decimal_point[0] : '.');
-  buffreplace(ls, old, ls->decpoint);  /* try updated decimal separator */
-  if (!luaO_str2d(luaZ_buffer(ls->buff), &seminfo->r)) {
-    /* format error with correct decimal point: no more options */
-    buffreplace(ls, ls->decpoint, '.');  /* undo change (for error message) */
-    luaX_lexerror(ls, "malformed number", TK_NUMBER);
-  }
+    /* format error: try to update decimal point separator */
+#ifdef ANDROID
+    char old = ls->decpoint;
+    ls->decpoint =  '.';
+#else
+    struct lconv *cv = localeconv();
+    char old = ls->decpoint;
+    ls->decpoint = (cv ? cv->decimal_point[0] : '.');
+#endif
+    buffreplace(ls, old, ls->decpoint);  /* try updated decimal separator */
+    if (!luaO_str2d(luaZ_buffer(ls->buff), &seminfo->r)) {
+        /* format error with correct decimal point: no more options */
+        buffreplace(ls, ls->decpoint, '.');  /* undo change (for error message) */
+        luaX_lexerror(ls, "malformed number", TK_NUMBER);
+    }
 }
 
 
