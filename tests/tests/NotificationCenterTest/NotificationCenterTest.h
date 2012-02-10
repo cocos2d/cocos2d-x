81ec2c8586914f2c064836c6b07b7c5944f69492
James Chen <jianhua.chen@cocos2d-x.org>
2012-02-10 11:46:18 +0800

    issue #972: add support for CCNotificationCenter, and add the test demo for it. modify the project setting for VS2008 and VS2010

---
+++ tests/tests/NotificationCenterTest/NotificationCenterTest.h
@@ -0,0 +1,24 @@
+#ifndef __NOTIFICATIONCENTERTEST_H__
+#define __NOTIFICATIONCENTERTEST_H__
+
+#include "cocos2d.h"
+#include "../testBasic.h"
+#include "platform/platform.h"
+
+class NotificationCenterTest : public CCLayer
+{
+public:
+    NotificationCenterTest();
+    void toggleSwitch(cocos2d::CCObject *sender);
+    void connectToSwitch(cocos2d::CCObject *sender);
+private:
+    bool m_bShowImage;
+};
+
+class NotificationCenterTestScene : public TestScene
+{
+public:
+    virtual void runThisTest();
+};
+
+#endif /* __NOTIFICATIONCENTERTEST_H__ */
