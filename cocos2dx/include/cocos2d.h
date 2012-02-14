4af7420a2a82e0c1fdb93b4537a59093d6d182a2
James Chen <jianhua.chen@cocos2d-x.org>
2012-02-14 15:13:54 +0800

    issue #972: Modify project setting for all platforms. Use new icons for ios template.

--- cocos2dx/include/cocos2d.h
+++ cocos2dx/include/cocos2d.h
@@ -115,7 +115,9 @@ THE SOFTWARE.
 #include "CCImage.h"
 #include "CCFileUtils.h"
 #include "CCAccelerometer.h"
-#include "support/CCNotificationCenter.h"
+
+// extensions
+#include "support/extensions/CCNotificationCenter.h"
 
 //
 // cocos2d macros
