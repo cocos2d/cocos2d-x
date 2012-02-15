96382590c6b71756e580729ed51f7b5448018198
James Chen <jianhua.chen@cocos2d-x.org>
2012-02-15 17:39:10 +0800

    modify linux template.

--- template/linux/mycopy_files.sh
+++ template/linux/mycopy_files.sh
@@ -13,34 +13,27 @@ sh $(dirname "$0")/../android/copy_files.sh $1 $2 $3 $4
 
 #copy linux's
 move_files_into_linux(){
-   mkdir $APP_DIR/Linux
-   for file in `ls $HELLOWORLD_ROOT/Linux/* | grep -E '.*\.[cpp|h]' `
+   mkdir $APP_DIR/linux
+   for file in `ls $HELLOWORLD_ROOT/linux/* | grep -E '.*\.[cpp|h]' `
     do
         if [ -f $file ];then
-            cp $file $APP_DIR/Linux
+            cp $file $APP_DIR/linux
         fi
     done
 }
 
-# set resources path to Resources in AppDelegate.cpp, when #925 is resolved, it should be deleted
-change_resources_path(){
-    sed "s/CCFileUtils::setResourcePath(\"\.\.\\/Resource\/\")/CCFileUtils::setResourcePath(\"\.\.\\/Resources\/\")/" $APP_DIR/Classes/AppDelegate.cpp > $APP_DIR/Classes/temp.cpp
-    rm -f $APP_DIR/Classes/AppDelegate.cpp
-    mv $APP_DIR/Classes/temp.cpp $APP_DIR/Classes/AppDelegate.cpp
-}
 
 #copy eclipse configures
 move_eclipse_configures_into(){
-        for file in `ls -a $HELLOWORLD_ROOT/Linux/ | grep -E '\..*project' `
+        for file in `ls -a $HELLOWORLD_ROOT/linux/ | grep -E '\..*project' `
 		do
-			cp $HELLOWORLD_ROOT/Linux/$file $APP_DIR/Linux/
+			cp $HELLOWORLD_ROOT/linux/$file $APP_DIR/linux/
 	    	done
 
-	sed -i "s/HelloWorld/$APP_NAME/" $APP_DIR/Linux/.project
+	sed -i "s/HelloWorld/$APP_NAME/" $APP_DIR/linux/.project
 
-	sed -i "s/HelloWorld/$APP_NAME/" $APP_DIR/Linux/.cproject
+	sed -i "s/HelloWorld/$APP_NAME/" $APP_DIR/linux/.cproject
 }
 
 move_files_into_linux
-change_resources_path
 move_eclipse_configures_into
