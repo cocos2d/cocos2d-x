1810ffc86c20b59d06a0f08e305784d845e922bc
folecr <folecr@gmail.com>
2012-02-27 21:53:19 -0800

    HelloWorld build Use static libraries
    Native (NDK) build
     * Updated build file with modules
     * Use either prebuilt or source modules for dependencies of Cocos2d-x
     * -s switch to build_native.sh to switch between source and prebuilt modules
     * Use static cocos2d-x
     * Use gnustl_static for C++
     * Does not use CocosDenshion. Remove.
     * Don't force APP_OPTIM to be release. Let this be defined by the build flavor
     * System.loadLibrary() of just the one shared native library now
    
    Java (SDK) build
     * HelloWorld ant build files
     * Remove HelloWorld files generated during build process

--- HelloWorld/android/jni/Android.mk
+++ HelloWorld/android/jni/Android.mk
@@ -1,10 +1,19 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-subdirs := $(addprefix $(LOCAL_PATH)/../../../,$(addsuffix /Android.mk, \
-           cocos2dx \
-           CocosDenshion/android \
-	))
-subdirs += $(LOCAL_PATH)/../../Classes/Android.mk $(LOCAL_PATH)/helloworld/Android.mk
+LOCAL_MODULE := helloworld_shared
 
-include $(subdirs)
+LOCAL_MODULE_FILENAME := libhelloworld
+
+LOCAL_SRC_FILES := helloworld/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/HelloWorldScene.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,cocos2dx)
