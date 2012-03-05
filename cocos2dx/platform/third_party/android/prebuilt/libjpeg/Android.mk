54ecf3b1636128e98d080316400f87e7e9197f6c
folecr <folecr@gmail.com>
2012-03-05 12:26:11 -0800

    Modify module names so that the prebuilt modules match those built from source.

--- cocos2dx/platform/third_party/android/prebuilt/libjpeg/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/libjpeg/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := jpeg_static_prebuilt
+LOCAL_MODULE := cocos_jpeg_static
 LOCAL_MODULE_FILENAME := jpeg
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libjpeg.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
--- cocos2dx/platform/third_party/android/prebuilt/libxml2/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/libxml2/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := xml2_static_prebuilt
+LOCAL_MODULE := cocos_libxml2_static
 LOCAL_MODULE_FILENAME := xml2
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libxml2.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
