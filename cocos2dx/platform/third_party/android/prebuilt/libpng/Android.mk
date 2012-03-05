1d8f8853b7ea765ad4701e43972d8a432c9e3a34
folecr <folecr@gmail.com>
2012-03-05 12:26:11 -0800

    Modify module names so that the prebuilt modules match those built from source.

--- cocos2dx/platform/third_party/android/prebuilt/libpng/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/libpng/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := png_static_prebuilt
+LOCAL_MODULE := cocos_libpng_static
 LOCAL_MODULE_FILENAME := png
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libpng.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
