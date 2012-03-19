c6f2b02c633f664d63349538793c8ecf445a8563
James Chen <jianhua.chen@cocos2d-x.org>
2012-03-19 14:53:44 +0800

    add CCDictionary based on UTHASH, improve the performance.

---
+++ cocos2dx/cocoa/CCDictionary.cpp
@@ -0,0 +1,226 @@
+#include "CCDictionary.h"
+#include "CCString.h"
+#include "CCInteger.h"
+
+NS_CC_BEGIN
+
+CCDictionary2::CCDictionary2()
+: m_pElements(NULL)
+, m_eDictType(kCCDictUnknown)
+, m_eOldDictType(kCCDictUnknown)
+{
+
+}
+
+CCDictionary2::~CCDictionary2()
+{
+	removeAllObjects();
+}
+
+unsigned int CCDictionary2::count()
+{
+	return HASH_COUNT(m_pElements);
+}
+
+CCArray* CCDictionary2::allKeys()
+{
+	int iKeyCount = this->count();
+	if (iKeyCount <= 0) return NULL;
+	CCArray* pArray = CCArray::arrayWithCapacity(iKeyCount);
+
+	tDictElement *pElement, *tmp;
+	if (m_eDictType == kCCDictStr)
+	{
+		HASH_ITER(hh, m_pElements, pElement, tmp) 
+		{
+			CCString* pOneKey = new CCString(pElement->szKey);
+			pOneKey->autorelease();
+			pArray->addObject(pOneKey);
+		}
+	}
+	else if (m_eDictType == kCCDictInt)
+	{
+		HASH_ITER(hh, m_pElements, pElement, tmp) 
+		{
+			CCInteger* pOneKey = new CCInteger(pElement->iKey);
+			pOneKey->autorelease();
+			pArray->addObject(pOneKey);
+		}
+	}
+	
+	return pArray;
+}
+
+CCArray* CCDictionary2::allKeysForObject(CCObject* object)
+{
+	int iKeyCount = this->count();
+	if (iKeyCount <= 0) return NULL;
+	CCArray* pArray = CCArray::array();
+
+	tDictElement *pElement, *tmp;
+
+	if (m_eDictType == kCCDictStr)
+	{
+		HASH_ITER(hh, m_pElements, pElement, tmp) 
+		{
+			if (object == pElement->object)
+			{
+				CCString* pOneKey = new CCString(pElement->szKey);
+				pOneKey->autorelease();
+				pArray->addObject(pOneKey);
+			}
+		}
+	}
+	else if (m_eDictType == kCCDictInt)
+	{
+		HASH_ITER(hh, m_pElements, pElement, tmp) 
+		{
+			if (object == pElement->object)
+			{
+				CCInteger* pOneKey = new CCInteger(pElement->iKey);
+				pOneKey->autorelease();
+				pArray->addObject(pOneKey);
+			}
+		}
+	}
+	return pArray;
+}
+
+CCObject* CCDictionary2::objectForKey(const char* key)
+{
+	CCAssert(m_eDictType == kCCDictStr, "this dictionary does not use string as key.");
+
+	CCObject* pRetObject = NULL;
+	tDictElement *pElement = NULL;
+	HASH_FIND_STR(m_pElements,key, pElement);
+	if (pElement != NULL)
+	{
+		pRetObject = pElement->object;
+	}
+	return pRetObject;
+}
+
+CCObject* CCDictionary2::objectForKey(int key)
+{
+	CCAssert(m_eDictType == kCCDictInt, "this dictionary does not use integer as key.");
+
+	CCObject* pRetObject = NULL;
+	tDictElement *pElement = NULL;
+	HASH_FIND_INT(m_pElements, &key, pElement);
+	if (pElement != NULL)
+	{
+		pRetObject = pElement->object;
+	}
+	return pRetObject;
+}
+
+bool CCDictionary2::setObject(CCObject* pObject, const char* key)
+{
+	CCAssert(key != NULL && strlen(key) > 0 && pObject != NULL, "Invalid Argument!");
+	if (m_eOldDictType == kCCDictUnknown)
+	{
+		m_eOldDictType = kCCDictStr;
+	}
+	m_eDictType = kCCDictStr;
+	CCAssert(m_eDictType == m_eOldDictType, "this dictionary does not use string as key.");
+
+	bool bRet = false;
+	tDictElement *pElement = NULL;
+	HASH_FIND_STR(m_pElements, key, pElement);
+	if (pElement == NULL)
+	{
+		pElement = (tDictElement*)calloc(sizeof(*pElement), 1);
+		strcpy(pElement->szKey, key);
+		pObject->retain();
+		pElement->object = pObject;
+		HASH_ADD_STR(m_pElements, szKey, pElement);
+		bRet = true;
+	}
+	else
+	{
+		pElement->object->retain();
+		removeObjectForKey(key);
+		setObject(pObject, key);
+		pElement->object->release();
+		bRet = true;
+	}
+	return bRet;
+}
+
+bool CCDictionary2::setObject(CCObject* pObject, int key)
+{
+	CCAssert(pObject != NULL, "Invalid Argument!");
+	if (m_eOldDictType == kCCDictUnknown)
+	{
+		m_eOldDictType = kCCDictInt;
+	}
+	m_eDictType = kCCDictInt;
+	CCAssert(m_eDictType == m_eOldDictType, "this dictionary does not use integer as key.");
+
+	bool bRet = false;
+	tDictElement *pElement = NULL;
+	HASH_FIND_INT(m_pElements, &key, pElement);
+	if (pElement == NULL)
+	{
+		pElement = (tDictElement*)calloc(sizeof(*pElement), 1);
+		pElement->iKey = key;
+		pObject->retain();
+		pElement->object = pObject;
+		HASH_ADD_INT(m_pElements, iKey, pElement);
+		bRet = true;
+	}
+	else
+	{
+		pElement->object->retain();
+		removeObjectForKey(key);
+		setObject(pObject, key);
+		pElement->object->release();
+		bRet = true;
+	}
+	return bRet;
+}
+
+void CCDictionary2::removeObjectForKey(const char* key)
+{
+	CCAssert(m_eDictType == kCCDictStr, "this dictionary does not use string as its key");
+	CCAssert(key != NULL && strlen(key) > 0, "Invalid Argument!");
+	tDictElement *pElement = NULL;
+	HASH_FIND_STR(m_pElements, key, pElement);
+	if (pElement)
+	{
+		HASH_DEL(m_pElements, pElement);
+		pElement->object->release();
+		free(pElement);
+	}
+}
+
+void CCDictionary2::removeObjectForKey(int key)
+{
+	CCAssert(m_eDictType == kCCDictInt, "this dictionary does not use integer as its key");
+	tDictElement *pElement = NULL;
+	HASH_FIND_INT(m_pElements, &key, pElement);
+	if (pElement != NULL)
+	{
+		HASH_DEL(m_pElements, pElement);
+		pElement->object->release();
+		free(pElement);
+	}
+}
+
+void CCDictionary2::removeAllObjects()
+{
+	tDictElement *pElement, *tmp;
+	HASH_ITER(hh, m_pElements, pElement, tmp) 
+	{
+		HASH_DEL(m_pElements, pElement);
+		pElement->object->release();
+		free(pElement);
+	}
+}
+
+CCDictionary2* CCDictionary2::dictionaryWithDictionary(CCDictionary2* srcDict)
+{
+	return NULL;
+}
+
+NS_CC_END
---
+++ cocos2dx/include/CCDictionary.h
@@ -0,0 +1,90 @@
+/****************************************************************************
+Copyright (c) 2010 cocos2d-x.org
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#ifndef __CCDICTIONARY_H__
+#define __CCDICTIONARY_H__
+
+#include "support/data_support/uthash.h"
+#include "CCObject.h"
+#include "CCArray.h"
+
+NS_CC_BEGIN
+
+typedef struct _dictElement
+{
+	char szKey[20]; /* hash key of string type*/
+	int  iKey;      /* hash key of integer type */
+	CCObject* object;/* hash value */
+	UT_hash_handle hh; /* makes this class hashable */  
+}tDictElement;
+
+#define CCDICT_FOREACH(__dict__, __object__)	                   \
+for (tDictElement* pElement = __dict__->m_pElements;               \
+	pElement != NULL && (__object__ = pElement->object) != NULL;   \
+	pElement = (tDictElement*)pElement->hh.next)
+
+class CC_DLL CCDictionary2 : public CCObject
+{
+public:
+	CCDictionary2();
+	~CCDictionary2();
+
+	/// return the number of items
+	unsigned int count();
+
+	/// return all the keys
+	CCArray* allKeys();
+
+	/** @warning : We use '==' to compare two objects*/
+	CCArray* allKeysForObject(CCObject* object);
+
+	CCObject* objectForKey(const char* key);
+	CCObject* objectForKey(int key);
+
+	bool setObject(CCObject* pObject, const char* key);
+	bool setObject(CCObject* pObject, int key);
+
+	void removeObjectForKey(const char* key);
+	void removeObjectForKey(int key);
+
+	void removeAllObjects();
+
+	static CCDictionary2* dictionaryWithDictionary(CCDictionary2* srcDict);
+
+public:
+	tDictElement* m_pElements;
+private:
+	enum CCDictType
+	{
+		kCCDictUnknown = 0,
+		kCCDictStr,
+		kCCDictInt
+	};
+	CCDictType m_eDictType;
+	CCDictType m_eOldDictType;
+};
+
+NS_CC_END
+
+#endif /* __CCDICTIONARY_H__ */
