394bb24cf55fda0fe3146d73f4b9c16499749288
James Chen <jianhua.chen@cocos2d-x.org>
2012-03-27 14:11:28 +0800

    issue #1056: Reverted opengles emulator version to 2.09, because there are some issues in v2.10.

--- cocos2dx/platform/third_party/win32/OGLES/KHR/khrplatform.h
+++ cocos2dx/platform/third_party/win32/OGLES/KHR/khrplatform.h
@@ -26,7 +26,7 @@
 
 /* Khronos platform-specific types and definitions.
  *
- * $Revision: 1.5 $ on $Date: 2011/05/31 15:09:03 $
+ * $Revision: 1.5 $ on $Date: 2010/06/03 16:51:55 $
  *
  * Adopters may modify this file to suit their platform. Adopters are
  * encouraged to submit platform specific modifications to the Khronos
@@ -97,8 +97,8 @@
  *-------------------------------------------------------------------------
  * This precedes the return type of the function in the function prototype.
  */
- 
-#if (defined(_WIN32) || defined(__VC32__)) && !defined(__SCITECH_SNAP__)
+
+#if (defined(_WIN32) || defined(__VC32__)) && !defined(__SCITECH_SNAP__) && !defined(__WINSCW__)
 #   if defined (_DLL_EXPORTS)
 #       define KHRONOS_APICALL __declspec(dllexport)
 #   else
@@ -120,7 +120,7 @@
  * This follows the return type of the function  and precedes the function
  * name in the function prototype.
  */
-#if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(__SCITECH_SNAP__)
+#if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(__SCITECH_SNAP__) && !defined(__WINSCW__)
     /* Win32 but not WinCE */
 #   define KHRONOS_APIENTRY __stdcall
 #else
