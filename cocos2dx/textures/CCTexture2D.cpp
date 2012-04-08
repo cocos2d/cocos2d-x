f5c40d4712e3c34eeb8d0939c1a5cfd0d8d59491
minggo <ming@cocos2d-x.org>
2012-04-08 22:37:58 +0800

    return correct content size

--- cocos2dx/textures/CCTexture2D.cpp
+++ cocos2dx/textures/CCTexture2D.cpp
@@ -112,9 +112,14 @@ GLuint CCTexture2D::getName()
 	return m_uName;
 }
 
-const CCSize& CCTexture2D::getContentSize()
+CCSize CCTexture2D::getContentSize()
 {
-	return m_tContentSize;
+	//return m_tContentSize;
+    CCSize ret;
+    ret.width = m_tContentSize.width / CC_CONTENT_SCALE_FACTOR();
+    ret.height = m_tContentSize.height / CC_CONTENT_SCALE_FACTOR();
+    
+    return ret;
 }
 
 const CCSize& CCTexture2D::getContentSizeInPixels()
