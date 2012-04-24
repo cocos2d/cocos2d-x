f1c5cfef1ef4b9577c8c42f06ce10867ff055b07
James Chen <jianhua.chen@cocos2d-x.org>
2012-04-24 17:13:13 +0800

    fixed #1177: Added CCApplicationProtocol.h.

---
+++ cocos2dx/platform/CCApplicationProtocol.h
@@ -0,0 +1,56 @@
+#ifndef __CC_APPLICATION_PROTOCOL_H__
+#define __CC_APPLICATION_PROTOCOL_H__
+
+#include "ccTypes.h"
+
+NS_CC_BEGIN
+
+class CCRect;
+
+class CC_DLL CCApplicationProtocol
+{
+public:
+
+    virtual ~CCApplicationProtocol() {}
+
+    /**
+    @brief    Implement for initialize OpenGL instance, set source path, etc...
+    */
+    virtual bool initInstance() = 0;
+
+    /**
+    @brief    Implement CCDirector and CCScene init code here.
+    @return true    Initialize success, app continue.
+    @return false   Initialize failed, app terminate.
+    */
+    virtual bool applicationDidFinishLaunching() = 0;
+
+    /**
+    @brief  The function be called when the application enter background
+    @param  the pointer of the application
+    */
+    virtual void applicationDidEnterBackground() = 0;
+
+    /**
+    @brief  The function be called when the application enter foreground
+    @param  the pointer of the application
+    */
+    virtual void applicationWillEnterForeground() = 0;
+
+    /**
+    @brief    Callback by CCDirector for limit FPS.
+    @interval       The time, which expressed in second in second, between current frame and next. 
+    */
+    virtual void setAnimationInterval(double interval) = 0;
+
+    /**
+    @brief Get current language config
+    @return Current language config
+    */
+    virtual ccLanguageType getCurrentLanguage() = 0;
+
+};
+
+NS_CC_END
+
+#endif    // __CC_APPLICATION_PROTOCOL_H__
