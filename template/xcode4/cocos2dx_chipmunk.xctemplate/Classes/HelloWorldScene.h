41b1bca7401c68980e21b9fa5de51f6fa6f423ef
James Chen <jianhua.chen@cocos2d-x.org>
2012-05-02 14:06:12 +0800

    issue #1194: Updated the chipmuck template for ios platform.

--- template/xcode4/cocos2dx_chipmunk.xctemplate/Classes/HelloWorldScene.h
+++ template/xcode4/cocos2dx_chipmunk.xctemplate/Classes/HelloWorldScene.h
@@ -14,23 +14,38 @@
 // include Chipmunk headers
 #include "chipmunk.h"
 
+class ChipmunkPhysicsSprite : public cocos2d::CCSprite
+{
+public:
+    ChipmunkPhysicsSprite();
+    virtual ~ChipmunkPhysicsSprite();
+    void setPhysicsBody(cpBody* body);
+    virtual bool isDirty(void);
+    virtual cocos2d::CCAffineTransform nodeToParentTransform(void);
+private:
+    cpBody* m_pBody;    // strong ref
+};
+
 // HelloWorld Layer
 class HelloWorld : public cocos2d::CCLayer {
 public:
-        HelloWorld();
-        ~HelloWorld();
+    HelloWorld();
+    ~HelloWorld();
+ 
+    static cocos2d::CCScene* scene();
+    LAYER_NODE_FUNC(HelloWorld);
     
-        static cocos2d::CCScene* scene();
-        void step(cocos2d::ccTime dt);
-        void addNewSpriteX(float x, float y);
-        virtual void onEnter();
-        virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent *event);
-        
-        LAYER_NODE_FUNC(HelloWorld);
-    
+    void initPhysics();
+    void addNewSpriteAtPosition(CCPoint p);
+    void update(ccTime dt);
+    virtual void ccTouchesEnded(CCSet* touches, CCEvent* event);
+    virtual void didAccelerate(CCAcceleration* pAccelerationValue);
+
 private:
-        bool init();
-        cpSpace *space;
+    CCTexture2D* m_pSpriteTexture; // weak ref
+    cpSpace* m_pSpace; // strong ref
+    cpShape* m_pWalls[4];
+
 };
 
 #endif // __HELLOW_WORLD_H__
