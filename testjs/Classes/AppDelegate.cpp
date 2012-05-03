5639e6cbc1c28698ac6302cf402376826c2de66b
Rolando Abarca <m@rolando.cl>
2012-05-03 09:38:51 -0700

    main test is test_actions

--- testjs/Classes/AppDelegate.cpp
+++ testjs/Classes/AppDelegate.cpp
@@ -90,10 +90,11 @@ bool AppDelegate::applicationDidFinishLaunching()
 	// set FPS. the default value is 1.0/60 if you don't call this
 	pDirector->setAnimationInterval(1.0 / 60);
 
+//	pDirector->runWithScene(HelloWorld::scene());
 	// run the main script
-	ScriptingCore::getInstance().runScript("JS/1to1/test_ease_actions.js");
+//	ScriptingCore::getInstance().runScript("JS/1to1/test_ease_actions.js");
 //	ScriptingCore::getInstance().runScript("JS/1to1/test_particles.js");
-//	ScriptingCore::getInstance().runScript("JS/1to1/test_actions.js");
+	ScriptingCore::getInstance().runScript("JS/1to1/test_actions.js");
 //	ScriptingCore::getInstance().runScript("JS/1to1/test_layer.js");
 
 	return true;
