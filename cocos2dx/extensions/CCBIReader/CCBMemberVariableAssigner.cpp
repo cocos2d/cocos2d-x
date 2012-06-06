185e69a40a234e2ace2c754a3acba305686fc54d
Nicolas Gramlich <ngramlich@zynga.com>
2012-06-06 11:06:28 -0700

    Removed zombie file: CCBMemberVariableAssigner.cpp.

--- cocos2dx/extensions/CCBIReader/CCBMemberVariableAssigner.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CCBMemberVariableAssigner.cpp
-//  CCBIReaderTest
-//
-//  Created by Nicolas Fabian Gramlich on 6/4/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
