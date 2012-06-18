37e8bfd59191ff1bd9a74efce6b5b06e207773c1
James Chen <jianhua.chen@cocos2d-x.org>
2012-06-18 19:00:26 +0800

    issue #1333: Added CCScrollViewLoader. Not implement.

---
+++ cocos2dx/extensions/CCBReader/CCScrollViewLoader.h
@@ -0,0 +1,28 @@
+#ifndef _CCB_CCSCROLLVIEWLOADER_H_
+#define _CCB_CCSCROLLVIEWLOADER_H_
+
+#include "CCNodeLoader.h"
+
+NS_CC_EXT_BEGIN
+
+/* Forward declaration. */
+class CCBReader;
+
+class CC_DLL CCScrollViewLoader : public CCNodeLoader {
+    public:
+        CCB_STATIC_NEW_AUTORELEASE_OBJECT_METHOD(CCScrollViewLoader, loader);
+
+    protected:
+        virtual CCScrollView * createCCNode(CCNode *, CCBReader *);
+
+        virtual void onHandlePropTypeColor3(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, ccColor3B, CCBReader *);
+        virtual void onHandlePropTypeByte(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, unsigned char, CCBReader *);
+        virtual void onHandlePropTypeBlendFunc(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, ccBlendFunc, CCBReader *);
+        virtual void onHandlePropTypeSpriteFrame(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, CCSpriteFrame *, CCBReader *);
+        virtual void onHandlePropTypeFlip(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, bool *, CCBReader *);
+        virtual void onHandlePropTypeFloat(CCNode * pNode, CCNode * pParent, CCString * pPropertyName, float pFloat, CCBReader * pCCBReader);
+};
+
+NS_CC_EXT_END
+
+#endif
