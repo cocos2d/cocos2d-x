55eca9f516b1f69a503113170988fee7613ff365
minggo <ming@cocos2d-x.org>
2012-06-22 12:00:27 +0800

    fixed #1343: make xcode template work ok

--- template/xcode4/cocos2dx_lua.xctemplate/Classes/AppDelegate.cpp
+++ template/xcode4/cocos2dx_lua.xctemplate/Classes/AppDelegate.cpp
@@ -58,7 +58,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
-    CCDirector::sharedDirector()->pause();
+    CCDirector::sharedDirector()->stopAnimation();
 
     // if you use SimpleAudioEngine, it must be pause
     // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
@@ -67,7 +67,7 @@ void AppDelegate::applicationDidEnterBackground()
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
-    CCDirector::sharedDirector()->resume();
+    CCDirector::sharedDirector()->startAnimation();
     
     // if you use SimpleAudioEngine, it must resume here
     // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
