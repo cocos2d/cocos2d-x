eb082897f2d90aae487be3cd1bf89642df73ccbf
James Chen <jianhua.chen@cocos2d-x.org>
2012-06-29 15:47:47 +0800

    Renamed namespace 'cocos' to 'cc' in javascript bindings.

--- testjs/Resources/JS/1to1/test_bindings.js
+++ testjs/Resources/JS/1to1/test_bindings.js
@@ -3,40 +3,40 @@
 var simple1 = new SimpleNativeClass();
 
 // testing the getters and the constructor (that set some fields)
-cocos.log("someField: " + simple1.someField);
-cocos.log("someOtherField: " + simple1.someOtherField);
-cocos.log("anotherMoreComplexField: " + simple1.anotherMoreComplexField);
+cc.log("someField: " + simple1.someField);
+cc.log("someOtherField: " + simple1.someOtherField);
+cc.log("anotherMoreComplexField: " + simple1.anotherMoreComplexField);
 
 // testing the setters
 simple1.someField = 1313;
 simple1.someOtherField = 999;
 simple1.anotherMoreComplexField = "this is a js string";
 
-cocos.log("someField: " + simple1.someField);
-cocos.log("someOtherField: " + simple1.someOtherField);
-cocos.log("anotherMoreComplexField: " + simple1.anotherMoreComplexField);
+cc.log("someField: " + simple1.someField);
+cc.log("someOtherField: " + simple1.someOtherField);
+cc.log("anotherMoreComplexField: " + simple1.anotherMoreComplexField);
 
 // testing std::string conversion
 
-cocos.log("someProcessing: " + simple1.doSomeProcessing("this is some js string", "this is another javascript string"));
+cc.log("someProcessing: " + simple1.doSomeProcessing("this is some js string", "this is another javascript string"));
 
 
 var another = new AnotherClass();
 // testing the getters and the constructor (that set some fields)
-cocos.log("justOneField: " + another.justOneField);
-cocos.log("aPublicField: " + another.aPublicField);
+cc.log("justOneField: " + another.justOneField);
+cc.log("aPublicField: " + another.aPublicField);
 
 // testing setters, the setter for justOneField should not work (since it's not defined correctly in the native class)
 another.justOneField = 8888;
 another.aPublicField = 6667;
-cocos.log("justOneField: " + another.justOneField);
-cocos.log("aPublicField: " + another.aPublicField);
+cc.log("justOneField: " + another.justOneField);
+cc.log("aPublicField: " + another.aPublicField);
 
 // should print something in stderr
 another.doSomethingSimple();
 
 //should print the enum
-cocos.log("enum: " + someThingEnumerated.kValue1);
-cocos.log("enum: " + someThingEnumerated.kValue2);
-cocos.log("enum: " + someThingEnumerated.kValue3);
-cocos.log("enum: " + someThingEnumerated.kValue4);
+cc.log("enum: " + someThingEnumerated.kValue1);
+cc.log("enum: " + someThingEnumerated.kValue2);
+cc.log("enum: " + someThingEnumerated.kValue3);
+cc.log("enum: " + someThingEnumerated.kValue4);
