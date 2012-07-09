859ef82a366c00cca254d111a5aa1cd0123f2121
Nat Weiss <nat@natweiss.com>
2012-07-08 22:37:19 -0700

    Fixes a few Xcode 4.5-specific error messages.

--- cocos2dx/textures/CCTexturePVR.cpp
+++ cocos2dx/textures/CCTexturePVR.cpp
@@ -88,8 +88,8 @@ static const unsigned int tableFormats[][7] = {
 	{ kPVRTexturePixelTypeI_8,		 GL_LUMINANCE,	GL_LUMINANCE,	GL_UNSIGNED_BYTE,		8,	false, kCCTexture2DPixelFormat_I8		},
 	{ kPVRTexturePixelTypeAI_88,	 GL_LUMINANCE_ALPHA,	GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE,16,	false, kCCTexture2DPixelFormat_AI88	},
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)  
-	{ kPVRTexturePixelTypePVRTC_2,	 GL_COMPRESSED_RGBA_PVRTC_2BPPV1_IMG, -1, -1,			2,	true, kCCTexture2DPixelFormat_PVRTC2	},
-	{ kPVRTexturePixelTypePVRTC_4,	 GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG, -1, -1,			4,	true, kCCTexture2DPixelFormat_PVRTC4	},
+	{ kPVRTexturePixelTypePVRTC_2,	 GL_COMPRESSED_RGBA_PVRTC_2BPPV1_IMG, (unsigned int)-1, (unsigned int)-1,			2,	true, kCCTexture2DPixelFormat_PVRTC2	},
+	{ kPVRTexturePixelTypePVRTC_4,	 GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG, (unsigned int)-1, (unsigned int)-1,			4,	true, kCCTexture2DPixelFormat_PVRTC4	},
 
 	{ kPVRTexturePixelTypeBGRA_8888, GL_RGBA,	GL_BGRA, GL_UNSIGNED_BYTE,					32,	false, kCCTexture2DPixelFormat_RGBA8888	},
 #endif // iphone only
