bfc53e3453ab0bbd04f4c6399948ecd298389142
mustime <pengyinghui@martinnetwork.com>
2012-07-26 15:30:09 +0800

    issue #1284: using OpenSL for playing effects on I9100

--- CocosDenshion/android/Android.mk
+++ CocosDenshion/android/Android.mk
@@ -6,7 +6,26 @@ LOCAL_MODULE := cocosdenshion_static
 LOCAL_MODULE_FILENAME := libcocosdenshion
 
 LOCAL_SRC_FILES := SimpleAudioEngine.cpp \
-jni/SimpleAudioEngineJni.cpp
+		   jni/SimpleAudioEngineJni.cpp 
+
+# for OpenSLES
+ifeq ($(APP_PLATFORM), android-9)
+
+# define # define the macro to compile through CocosDenshion/android/SimpleAudioEngine.cpp
+LOCAL_CFLAGS := -DENABLE_OPENSL
+
+LOCAL_SRC_FILES += opensl/OpenSLEngine.cpp \
+		   opensl/SimpleAudioEngineOpenSL.cpp
+
+LOCAL_EXPORT_LDLIBS := -lOpenSLES\
+                       -landroid 
+
+LOCAL_LDLIBS    := -lOpenSLES
+LOCAL_LDLIBS    += -llog
+LOCAL_LDLIBS    += -landroid
+
+endif
+
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include
 
@@ -14,7 +33,5 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include \
                     $(LOCAL_PATH)/../../cocos2dx \
                     $(LOCAL_PATH)/../../cocos2dx/include \
                     $(LOCAL_PATH)/../../cocos2dx/platform/android
-                   
-LOCAL_LDLIBS := -llog
             
 include $(BUILD_STATIC_LIBRARY)
