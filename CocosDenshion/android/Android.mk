d52c38d5030b54e0de414d91bd2394b15707da36
folecr <folecr@gmail.com>
2012-07-30 18:38:36 -0700

    Dynamic typing for cocosdenshion's JS bindings

--- CocosDenshion/android/Android.mk
+++ CocosDenshion/android/Android.mk
@@ -6,9 +6,11 @@ LOCAL_MODULE := cocosdenshion_static
 LOCAL_MODULE_FILENAME := libcocosdenshion
 
 LOCAL_SRC_FILES := SimpleAudioEngine.cpp \
-		   jni/SimpleAudioEngineJni.cpp \
-		   opensl/OpenSLEngine.cpp \
-		   opensl/SimpleAudioEngineOpenSL.cpp
+                   jni/SimpleAudioEngineJni.cpp \
+                   opensl/OpenSLEngine.cpp \
+                   opensl/SimpleAudioEngineOpenSL.cpp
+
+LOCAL_WHOLE_STATIC_LIBRARIES := scriptingcore-spidermonkey
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include
 
@@ -16,5 +18,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include \
                     $(LOCAL_PATH)/../../cocos2dx \
                     $(LOCAL_PATH)/../../cocos2dx/include \
                     $(LOCAL_PATH)/../../cocos2dx/platform/android
-            
+
 include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,targets/spidermonkey/common)
