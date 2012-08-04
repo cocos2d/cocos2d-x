fb3a65f5f6e25bfbbcf4184bb3ac3d4e5d8a0037
qingyun <greathqy@qingyunmatoMacBook-Air.local>
2012-08-04 18:12:39 +0800

    add greathqy to AUTHORS

---
+++ samples/TestCpp/Classes/ExtensionsTest/HttpRequestTest/HttpRequestTest.cpp
@@ -0,0 +1,9 @@
+//
+//  HttpRequestTest.cpp
+//  TestCpp
+//
+//  Created by qingyun on 12-8-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ samples/TestCpp/Classes/ExtensionsTest/HttpRequestTest/HttpRequestTest.h
@@ -0,0 +1,14 @@
+//
+//  HttpRequestTest.h
+//  TestCpp
+//
+//  Created by qingyun on 12-8-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef TestCpp_HttpRequestTest_h
+#define TestCpp_HttpRequestTest_h
+
+
+
+#endif
