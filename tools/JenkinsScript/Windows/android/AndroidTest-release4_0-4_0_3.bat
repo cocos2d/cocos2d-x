7032b77601fcf180e9fb53deef6d3ef18180b734
Caidongyang <yangguangzaidongji@hotmail.com>
2012-08-24 10:23:00 +0800

    issue #1404:Modify some parameters in script files for windows.

--- tools/JenkinsScript/Windows/android/AndroidTest-release4_0-4_0_3.bat
+++ tools/JenkinsScript/Windows/android/AndroidTest-release4_0-4_0_3.bat
@@ -10,8 +10,8 @@ cd ..\..
 cd samples\TestCpp\proj.android\bin
 
 ::Copy test apk to tools directory.
-copy %cd%\Tests-release-14.apk %ANDROID_HOME%\tools
-copy %cd%\Tests-release-15.apk %ANDROID_HOME%\tools
+copy %cd%\TestCpp-release-14.apk %ANDROID_HOME%\tools
+copy %cd%\TestCpp-release-15.apk %ANDROID_HOME%\tools
 
 ::Enter tools directory.
 set ANDROID_ROOT=%ANDROID_HOME:~0,2%
@@ -21,14 +21,14 @@ cd %ANDROID_HOME%\tools
 ::If monkeyrunner test failed,it automatically exit and make ERRORLEVEL nonzero.
 
 ::Running monkeyrunner test(release,API level:14).
-ren Tests-release-14.apk Tests-release.apk
-monkeyrunner Monkeyrunner_TestCpp.py
-rm Tests-release.apk
+ren TestCpp-release-14.apk TestCpp-release.apk
+monkeyrunner Monkeyrunner_TestCpp.py TestCpp-release.apk
+rm TestCpp-release.apk
 
 ::Running monkeyrunner test(release,API level:15).
-ren Tests-release-15.apk Tests-release.apk
-monkeyrunner Monkeyrunner_TestCpp.py
-rm Tests-release.apk
+ren TestCpp-release-15.apk TestCpp-release.apk
+monkeyrunner Monkeyrunner_TestCpp.py TestCpp-release.apk
+rm TestCpp-release.apk
 
 rm Monkeyrunner_TestCpp.py
 
