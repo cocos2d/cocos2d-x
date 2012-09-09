7e4615cd7bdd7ac7ae64b8a30d2e07370fe36cc9
Walzer <walzer@cocos2d-x.org>
2012-09-09 20:20:29 +0800

    upgrade Cocos2dSimpleGame to v2.0.2 framework, add it into samples folder

---
+++ samples/SimpleGame/Classes/AppDelegate.cpp
@@ -0,0 +1,81 @@
+#include "AppDelegate.h"
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate() {
+
+}
+
+AppDelegate::~AppDelegate() 
+{
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    // initialize director
+    CCDirector *pDirector = CCDirector::sharedDirector();
+
+    pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
+    
+    TargetPlatform target = getTargetPlatform();
+    
+    if (target == kTargetIpad)
+    {
+        // ipad
+        
+        CCFileUtils::sharedFileUtils()->setResourceDirectory("hd");
+        
+        // don't enable retina because we don't have ipad hd resource
+        CCEGLView::sharedOpenGLView()->setDesignResolutionSize(960, 640, kResolutionNoBorder);
+    }
+    else if (target == kTargetIphone)
+    {        
+        if (pDirector->enableRetinaDisplay(true))
+        {
+            // well, it's a iPhone 4 and above, with 960x480 Retina resolution
+            CCFileUtils::sharedFileUtils()->setResourceDirectory("hd");
+        }
+        else 
+        {
+            // iPhone 3GS and before, with 480x320 resolution
+            CCFileUtils::sharedFileUtils()->setResourceDirectory("sd");
+        }
+    }
+    else 
+    {
+        // android, windows, blackberry, linux or mac
+        // use 960*640 resources as design resolution size
+        CCFileUtils::sharedFileUtils()->setResourceDirectory("hd");
+        CCEGLView::sharedOpenGLView()->setDesignResolutionSize(960, 640, kResolutionNoBorder);
+    }
+
+    // turn on display FPS
+    pDirector->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    pDirector->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+    CCScene *pScene = HelloWorld::scene();
+
+    // run
+    pDirector->runWithScene(pScene);
+
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground() {
+    CCDirector::sharedDirector()->stopAnimation();
+
+    // if you use SimpleAudioEngine, it must be pause
+    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground() {
+    CCDirector::sharedDirector()->startAnimation();
+
+    // if you use SimpleAudioEngine, it must resume here
+    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+}
