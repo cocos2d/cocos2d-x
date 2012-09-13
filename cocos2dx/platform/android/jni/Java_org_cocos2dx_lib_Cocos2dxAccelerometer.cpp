b4040fccccd6166798c776e98bab5f8735e59ef7
Nicolas Gramlich <ngramlich@zynga.com>
2012-09-13 09:34:30 -0700

    Added proper JNIEXPORT/JNICALL markers to android platform jni bindings.

--- cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxAccelerometer.cpp
+++ cocos2dx/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxAccelerometer.cpp
@@ -8,7 +8,7 @@
 using namespace cocos2d;
 
 extern "C" {
-    void Java_org_cocos2dx_lib_Cocos2dxAccelerometer_onSensorChanged(JNIEnv*  env, jobject thiz, jfloat x, jfloat y, jfloat z, jlong timeStamp) {
+    JNIEXPORT void JNICALL Java_org_cocos2dx_lib_Cocos2dxAccelerometer_onSensorChanged(JNIEnv*  env, jobject thiz, jfloat x, jfloat y, jfloat z, jlong timeStamp) {
         CCDirector* pDirector = CCDirector::sharedDirector();
         pDirector->getAccelerometer()->update(x, y, z, timeStamp);
     }    
