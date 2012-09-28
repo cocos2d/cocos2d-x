78a4060524e9636010aa23525405dfed919dc90e
walzer <walzer@cocos2d-x.org>
2012-09-28 12:00:03 +0800

    add iOS 5.0, 6.0 build targets to jenkins_scripts

--- tools/jenkins_scripts/mac/ios/build-ios-all.sh
+++ tools/jenkins_scripts/mac/ios/build-ios-all.sh
@@ -1,21 +1,47 @@
-# cpp
+# HelloCpp
+xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
 xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/HelloCpp/proj.ios/HelloCpp.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
 
+# TestCpp
+xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
 xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestCpp/proj.ios/TestCpp.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
 
-# lua
+# HelloLua
+xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
 xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/HelloLua/proj.ios/HelloLua.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
 
+# TestLua
+xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
-xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator5.1-configuration Release
+xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestLua/proj.ios/TestLua.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
 
-# javascript
+# TestJavascript
+xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
 xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/TestJavascript/proj.ios/TestJavascript.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
 
-# other sample games
+# SimpleGame
+xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator5.0 -configuration Release
 xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Debug
 xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator5.1 -configuration Release
+xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Debug
+xcodebuild clean build -project ../../../../samples/SimpleGame/proj.ios/SimpleGame.xcodeproj -alltargets -sdk iphonesimulator6.0 -configuration Release
