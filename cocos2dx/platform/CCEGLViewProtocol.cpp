ff66d7c07f52146bd50afba5ea6fe5049527058c
James Chen <jianhua.chen@cocos2d-x.org>
2012-10-11 17:02:27 +0800

    issue #1486: A bug fix.

--- cocos2dx/platform/CCEGLViewProtocol.cpp
+++ cocos2dx/platform/CCEGLViewProtocol.cpp
@@ -91,7 +91,8 @@ void CCEGLViewProtocol::setDesignResolutionSize(float width, float height, Resol
     
     // reset director's member variables to fit visible rect
     CCDirector::sharedDirector()->createStatsLabel();
-    CCDirector::sharedDirector()->m_obWinSizeInPoints = CCDirector::sharedDirector()->m_obWinSizeInPixels = getSize(); 
+    CCDirector::sharedDirector()->m_obWinSizeInPoints = getSize();
+    CCDirector::sharedDirector()->m_obWinSizeInPixels = m_obScreenSize;
     CCDirector::sharedDirector()->setGLDefaultValues();
 }
 
