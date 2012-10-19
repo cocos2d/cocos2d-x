57f324940e3e1a93030e98f23c35199f41de8d73
James Chen <jianhua.chen@cocos2d-x.org>
2012-10-19 17:39:55 +0800

    Renamed "setFrameZoom" to "setFrameZoomFactor", added 'getFrameZoomFactor'.

--- samples/HelloCpp/proj.linux/main.cpp
+++ samples/HelloCpp/proj.linux/main.cpp
@@ -32,6 +32,6 @@ int main(int argc, char **argv)
     eglView->setFrameSize(2048, 1536);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoom'(only valid on desktop(win32, mac, linux)) to make the window smaller.
-    eglView->setFrameZoom(0.4f);
+    eglView->setFrameZoomFactor(0.4f);
     return CCApplication::sharedApplication()->run();
 }
