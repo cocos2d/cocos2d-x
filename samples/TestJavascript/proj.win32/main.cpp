8f2d3eb7819cedce8cbabcfae6cd60210f0131fc
James Chen <jianhua.chen@cocos2d-x.org>
2012-11-05 18:46:23 +0800

    issue #1549: Made the window size same as cocos2d-html5 (800x450) on win32.

--- samples/TestJavascript/proj.win32/main.cpp
+++ samples/TestJavascript/proj.win32/main.cpp
@@ -25,7 +25,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
-    eglView->setFrameSize(480, 320);
+    eglView->setFrameSize(800, 450);
 
     int ret = CCApplication::sharedApplication()->run();
 
