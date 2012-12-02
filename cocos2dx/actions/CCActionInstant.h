030f5e7079c199a0355ca423d0af671188ede9d7
dualface <dualface@gmail.com>
2012-12-02 15:01:12 +0800

    [luabinding] cleanup script handler when release CCCallFunc

--- cocos2dx/actions/CCActionInstant.h
+++ cocos2dx/actions/CCActionInstant.h
@@ -219,11 +219,8 @@ public:
 		, m_nScriptHandler(0)
     {
     }
-    virtual ~CCCallFunc()
-    {
-        CC_SAFE_RELEASE(m_pSelectorTarget);
-    }
-    /** creates the action with the callback 
+    virtual ~CCCallFunc();
+    /** creates the action with the callback
     @deprecated: This interface will be deprecated sooner or later.
     typedef void (CCObject::*SEL_CallFunc)();
     */
