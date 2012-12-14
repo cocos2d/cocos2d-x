f769d164ced3ac7a4d01c22bf5b9d2590829ab7f
dualface <dualface@gmail.com>
2012-12-14 18:12:21 +0800

    fix font render blur

--- cocos2dx/platform/ios/CCImage.mm
+++ cocos2dx/platform/ios/CCImage.mm
@@ -266,6 +266,9 @@ static bool _initWithString(const char * pText, cocos2d::CCImage::ETextAlign eAl
             dim.height = constrainSize.height;
         }         
         
+        dim.width = (int)(dim.width / 2) * 2 + 2;
+        dim.height = (int)(dim.height / 2) * 2 + 2;
+        
         unsigned char* data = new unsigned char[(int)(dim.width * dim.height * 4)];
         memset(data, 0, (int)(dim.width * dim.height * 4));
         
