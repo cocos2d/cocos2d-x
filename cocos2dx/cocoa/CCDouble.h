7ef97d73c3b3d306693e8134eadfe1e174416e56
Tomoaki Shimizu <doraemonsss@gmail.com>
2012-12-28 12:24:28 +0900

    The variable name was changed.The check of pRet was added.

--- cocos2dx/cocoa/CCDouble.h
+++ cocos2dx/cocoa/CCDouble.h
@@ -14,17 +14,20 @@ class CC_DLL CCDouble : public CCObject
 {
 public:
     CCDouble(double v)
-        : m_nValue(v) {}
-    double getValue() const {return m_nValue;}
+        : m_dValue(v) {}
+    double getValue() const {return m_dValue;}
 
     static CCDouble* create(double v)
     {
         CCDouble* pRet = new CCDouble(v);
-        pRet->autorelease();
+        if (pRet)
+        {
+            pRet->autorelease();
+        }
         return pRet;
     }
 private:
-    double m_nValue;
+    double m_dValue;
 };
 
 // end of data_structure group
