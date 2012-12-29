59615f6d0cc4c8a5d379fdeca8192d2a38b38807
James Chen <jianhua.chen@cocos2d-x.org>
2012-12-29 14:50:00 +0800

    fixed #1627: Adding view name for sample applications.

--- samples/Cpp/HelloCpp/proj.win32/main.cpp
+++ samples/Cpp/HelloCpp/proj.win32/main.cpp
@@ -15,6 +15,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
+    eglView->setViewName("HelloCpp");
     eglView->setFrameSize(2048, 1536);
     // The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
     // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
