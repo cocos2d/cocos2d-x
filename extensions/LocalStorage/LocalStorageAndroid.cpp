b662d5c569a183a5a2ca8ecce809992c7e357120
James Chen <jianhua.chen@cocos2d-x.org>
2013-01-07 18:06:53 +0800

    fixed #1620: Added license. Removed LocalStorage.h from cocos-ext.h.

--- extensions/LocalStorage/LocalStorageAndroid.cpp
+++ extensions/LocalStorage/LocalStorageAndroid.cpp
@@ -1,7 +1,8 @@
 /*
  
  Copyright (c) 2012 - Zynga Inc.
- 
+ Copyright (c) 2013 cocos2d-x.org
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
