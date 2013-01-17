31e91e554e18210f7e305b870039b91fffe417ab
James Chen <jianhua.chen@cocos2d-x.org>
2013-01-17 17:07:18 +0800

    issue #1661: Added CrystalCraze for win32 and fix some compilation errors.

--- samples/Javascript/WatermelonWithMe/proj.win32/main.h
+++ samples/Javascript/WatermelonWithMe/proj.win32/main.h
@@ -4,7 +4,6 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 // Windows Header Files:
-#include <windows.h>
 #include <tchar.h>
 
 // C RunTime Header Files
