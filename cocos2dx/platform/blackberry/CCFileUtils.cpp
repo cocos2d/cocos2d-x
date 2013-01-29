170fb32b6c1108785aa3c3f060c2d6714fc4313f
James Chen <jianhua.chen@cocos2d-x.org>
2013-01-29 15:07:36 +0800

    Updating CCFileUtils.cpp for BlackBerry.

--- cocos2dx/platform/blackberry/CCFileUtils.cpp
+++ cocos2dx/platform/blackberry/CCFileUtils.cpp
@@ -104,6 +104,7 @@ std::string CCFileUtils::fullPathForFilename(const char* pszFileName)
     CCAssert(pszFileName != NULL, "CCFileUtils: Invalid path");
 
     // Return directly if it's absolute path.
+    std::string strFileName = pszFileName;
     if (pszFileName[0] == '/' || strFileName.find(m_strDefaultResRootPath) == 0)
     {
 		CCLOG("Return absolute path( %s ) directly.", pszFileName);
