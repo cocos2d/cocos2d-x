e6089ffb75e461ec3170400432241574095b0a51
billy1380 <william.shakour@gmail.com>
2013-02-15 14:42:55 +0000

    CCLens3D can now be set to concave lens

--- cocos2dx/actions/CCActionGrid3D.cpp
+++ cocos2dx/actions/CCActionGrid3D.cpp
@@ -367,6 +367,7 @@ bool CCLens3D::initWithPosition(const CCPoint& pos, float r, const ccGridSize& g
         setPosition(pos);
         m_fRadius = r;
         m_fLensEffect = 0.7f;
+        m_bConcave = false;
         m_bDirty = true;
 
         return true;
@@ -438,7 +439,7 @@ void CCLens3D::update(float time)
                     {
                         vect = ccpNormalize(vect);
                         CCPoint new_vect = ccpMult(vect, new_r);
-                        v.z += ccpLength(new_vect) * m_fLensEffect;
+                        v.z = (m_bConcave ? -1.0f : 1.0f) * ccpLength(new_vect) * m_fLensEffect;
                     }
                 }
                 
