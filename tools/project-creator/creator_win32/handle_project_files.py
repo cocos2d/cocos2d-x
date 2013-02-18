ad5d128efe6b1d73bb64271699cfea616b4e8688
walzer <walzer@cocos2d-x.org>
2013-02-19 00:02:42 +0800

    issue #1725, project-creator, HelloJavascript on win32 works ok

--- tools/project-creator/creator_win32/handle_project_files.py
+++ tools/project-creator/creator_win32/handle_project_files.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python
-# process_proj.py
+# handle_project_files.py for Win32
 # Copyright (c) 2012 cocos2d-x.org
 # Author: WangZhe
 
+# Win32
+
 import os
 
-def process_proj(context):
-    proj_path = context["project_path"] + context["dst_project_name"] + "/proj.win32/"
+def handle_project_files(context):    
+    # determine proj_path
+    proj_path = context["dst_project_path"] + "/proj.win32/"
+                      
+    # rename files and folders
+    os.rename(proj_path + context["src_project_name"] + ".vcxproj",
+              proj_path + context["dst_project_name"] + ".vcxproj")
+    os.rename(proj_path + context["src_project_name"] + ".vcxproj.filters",
+              proj_path + context["dst_project_name"] + ".vcxproj.filters")
+    os.rename(proj_path + context["src_project_name"] + ".vcxproj.user",
+              proj_path + context["dst_project_name"] + ".vcxproj.user")
     
-    # remove useless files.
-    os.popen("rm -rf " + proj_path)
-    print "proj.win32       : TBD, Python script for win32 hasn't been implemented yet. Call Microsoft for help :)"
+    # replaceString function is implemented in ../create-project.py
+    import replaces
+    # package_name should be replaced at first. Don't change this sequence
+    replaces.replaceString(proj_path + context["dst_project_name"] + ".vcxproj",         context["src_project_name"], context["dst_project_name"])
+    replaces.replaceString(proj_path + context["dst_project_name"] + ".vcxproj.filters", context["src_project_name"], context["dst_project_name"])
+    replaces.replaceString(proj_path + context["dst_project_name"] + ".vcxproj.user",    context["src_project_name"], context["dst_project_name"])
+    replaces.replaceString(proj_path + "main.cpp",    context["src_project_name"], context["dst_project_name"])
+    # done!
+    print "proj.win32       : Done!"
