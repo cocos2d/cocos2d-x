e81c841ad6fc8240e54432614522e1f7eb2dcf5a
walzer <walzer@cocos2d-x.org>
2013-02-20 23:42:48 +0800

    issue #1733, improve the help description of create_project.py

--- tools/project-creator/create_project.py
+++ tools/project-creator/create_project.py
@@ -31,8 +31,10 @@ def dumpUsage():
     print "  -package   PACKAGE_NAME          Package name, for example: com.MyCompany.MyAwesomeGame"
     print "  -language  PROGRAMING_LANGUAGE   Major programing lanauge you want to used, should be cpp | lua | javascript"
     print "                                   If this param is nil, we will create a cpp project by default"
-    print "Sample 1: ./create-project.py -project MyGame -package com.MyCompany.AwesomeGame"
-    print "Sample 2: ./create-project.py -project MyGame -package com.MyCompany.AwesomeGame -language javascript"
+    print ""
+    print "Sample 1: ./create_project.py -project MyGame -package com.MyCompany.AwesomeGame"
+    print "Sample 2: ./create_project.py -project MyGame -package com.MyCompany.AwesomeGame -language javascript"
+    print ""
 
 def checkParams(context):
     # generate our internal params
