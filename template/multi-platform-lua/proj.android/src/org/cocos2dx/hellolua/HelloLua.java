fb23323e55f0f17bc2c760d0cbc7174e83c3abfe
James Chen <jianhua.chen@cocos2d-x.org>
2013-02-20 13:54:07 +0800

    issue #1733: Renamed testlua to hellolua.

--- template/multi-platform-lua/proj.android/src/org/cocos2dx/hellolua/HelloLua.java
+++ template/multi-platform-lua/proj.android/src/org/cocos2dx/hellolua/HelloLua.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package org.cocos2dx.testlua;
+package org.cocos2dx.hellolua;
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
@@ -39,7 +39,7 @@ public class HelloLua extends Cocos2dxActivity{
     }
 
     static {
-        System.loadLibrary("testlua");
+        System.loadLibrary("cocos2dlua");
     }
 }
 
