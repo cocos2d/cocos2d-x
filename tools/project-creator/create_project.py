3dcee98cb86cec24285effd5110b73c80e3a9587
walzer <walzer@cocos2d-x.org>
2013-02-22 16:32:12 +0800

    issue #1733, use json config files instead of python modules for multi-platforms.

--- tools/project-creator/create_project.py
+++ tools/project-creator/create_project.py
@@ -20,7 +20,8 @@ platforms_list = []
 # begin
 import sys
 import os, os.path
-import shutil # for copy folders and files
+import json
+import shutil
 
 def dumpUsage():
     print "Usage: create-project.py -project PROJECT_NAME -package PACKAGE_NAME -language PROGRAMING_LANGUAGE"
@@ -101,6 +102,74 @@ def checkParams(context):
                           "win32"]
 # end of checkParams(context) function
 
+def replaceString(filepath, src_string, dst_string):
+    content = ""
+    f1 = open(filepath, "rb")
+    for line in f1:
+        if src_string in line:
+            content += line.replace(src_string, dst_string)
+        else:
+            content += line
+    f1.close()
+    f2 = open(filepath, "wb")
+    f2.write(content)
+    f2.close()
+# end of replaceString
+
+def processPlatformProjects(platform):
+    # determine proj_path
+    proj_path = context["dst_project_path"] + "/proj.%s/" % platform
+    java_package_path = ""
+
+    # read josn config file or the current platform
+    f = open("%s.json" % platform)
+    data = json.load(f)
+
+    # rename package path, like "org.cocos2dx.hello" to "com.company.game". This is a special process for android
+    if (platform == "android"):
+        src_pkg = context["src_package_name"].split('.')
+        dst_pkg = context["dst_package_name"].split('.')
+        os.rename(proj_path + "src/" + src_pkg[0],
+                  proj_path + "src/" + dst_pkg[0])
+        os.rename(proj_path + "src/" + dst_pkg[0] + "/" + src_pkg[1],
+                  proj_path + "src/" + dst_pkg[0] + "/" + dst_pkg[1])
+        os.rename(proj_path + "src/" + dst_pkg[0] + "/" + dst_pkg[1] + "/" + src_pkg[2],
+                  proj_path + "src/" + dst_pkg[0] + "/" + dst_pkg[1] + "/" + dst_pkg[2])
+        java_package_path = dst_pkg[0] + "/" + dst_pkg[1] + "/" + dst_pkg[2]
+
+    # rename files and folders
+    for i in range(0, len(data["rename"])):
+        tmp = data["rename"][i].replace("PACKAGE_PATH", java_package_path)
+        src = tmp.replace("PROJECT_NAME", context["src_project_name"])
+        dst = tmp.replace("PROJECT_NAME", context["dst_project_name"])
+        if (os.path.exists(proj_path + src) == True):
+            os.rename(proj_path + src, proj_path + dst)
+
+    # remove useless files and folders
+    for i in range(0, len(data["remove"])):
+        dst = data["remove"][i].replace("PROJECT_NAME", context["dst_project_name"])
+        if (os.path.exists(proj_path + dst) == True):
+            shutil.rmtree(proj_path + dst)
+    
+    # rename package_name. This should be replaced at first. Don't change this sequence
+    for i in range(0, len(data["replace_package_name"])):
+        tmp = data["replace_package_name"][i].replace("PACKAGE_PATH", java_package_path)
+        dst = tmp.replace("PROJECT_NAME", context["dst_project_name"])
+        if (os.path.exists(proj_path + dst) == True):
+            replaceString(proj_path + dst, context["src_package_name"], context["dst_package_name"])
+    
+    # rename project_name
+    for i in range(0, len(data["replace_project_name"])):
+        tmp = data["replace_project_name"][i].replace("PACKAGE_PATH", java_package_path)
+        dst = tmp.replace("PROJECT_NAME", context["dst_project_name"])
+        if (os.path.exists(proj_path + dst) == True):
+            replaceString(proj_path + dst, context["src_project_name"], context["dst_project_name"])
+                  
+    # done!
+    print "proj.%s\t\t: Done!" % platform
+# end of processPlatformProjects
+
+
 
 # -------------- main --------------
 # dump argvs
@@ -113,16 +182,19 @@ checkParams(context)
 
 # copy "lauguage"(cpp/lua/javascript) platform.proj into cocos2d-x/projects/<project_name>/folder
 if (os.path.exists(context["dst_project_path"]) == True):
-    print "Error:" + context["dst_project_path"] + " folder is already existing"
-    print "Please remove the old project or choose a new PROJECT_NAME in -project parameter"
-    sys.exit()
-else:
-    shutil.copytree(context["src_project_path"], context["dst_project_path"], True)
+    shutil.rmtree(context["dst_project_path"])
+    #print "Error:" + context["dst_project_path"] + " folder is already existing"
+    #print "Please remove the old project or choose a new PROJECT_NAME in -project parameter"
+    #sys.exit()
+#else:
+
+shutil.copytree(context["src_project_path"], context["dst_project_path"], True)
 
 # call process_proj from each platform's script folder          
 for platform in platforms_list:
-    exec "import %s.handle_project_files" % (platform)
-    exec "%s.handle_project_files.handle_project_files(context)" % (platform)
+    processPlatformProjects(platform)
+#    exec "import %s.handle_project_files" % (platform)
+#    exec "%s.handle_project_files.handle_project_files(context)" % (platform)
 
 print "New project has been created in this path: " + context["dst_project_path"].replace("/tools/project-creator/../..", "")
 print "Have Fun!"
