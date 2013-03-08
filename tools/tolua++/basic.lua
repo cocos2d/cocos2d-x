957e7768e403baccfcdff7cf138edbd1e26654ea
Sam Clegg <sbc@google.com>
2013-03-08 10:45:35 -0800

    Cleanup the tolua++ bindings build system.
    Update basic.lua to do all the required patching of
    the output file without helper scripts such as
    build.xml or build.php.
    
    Update README file with current instructions.
    
    Re-generate output file using new build system.
    
    Add build.sh and build.bat for building on POSIX and
    win32 respectively.

--- tools/tolua++/basic.lua
+++ tools/tolua++/basic.lua
@@ -1,3 +1,5 @@
+-- usage: (use instead of ant)
+-- tolua++ "-L" "basic.lua" "-o" "../../scripting/lua/cocos2dx_support/LuaCocos2d.cpp" "Cocos2d.pkg"
 
 _is_functions = _is_functions or {}
 _to_functions = _to_functions or {}
@@ -23,10 +25,10 @@ local CCObjectTypes = {
     "CCAccelDeccelAmplitude",
     "CCActionCamera",
     "CCOrbitCamera",
-	"CCCardinalSplineTo",
-	"CCCardinalSplineBy",
-	"CCCatmullRomTo",
-	"CCCatmullRomBy",
+    "CCCardinalSplineTo",
+    "CCCardinalSplineBy",
+    "CCCatmullRomTo",
+    "CCCatmullRomBy",
     "CCActionEase",
     "CCEaseBackIn",
     "CCEaseBackInOut",
@@ -142,13 +144,13 @@ local CCObjectTypes = {
     "CCTransitionSplitCols",
     "CCTransitionSplitRows",
     "CCTransitionTurnOffTiles",
-	"CCTransitionProgress",
-	"CCTransitionProgressRadialCCW",
-	"CCTransitionProgressRadialCW",
-	"CCTransitionProgressHorizontal",
-	"CCTransitionProgressVertical",
-	"CCTransitionProgressInOut",
-	"CCTransitionProgressOutIn",
+    "CCTransitionProgress",
+    "CCTransitionProgressRadialCCW",
+    "CCTransitionProgressRadialCW",
+    "CCTransitionProgressHorizontal",
+    "CCTransitionProgressVertical",
+    "CCTransitionProgressInOut",
+    "CCTransitionProgressOutIn",
     "CCSprite",
     "CCLabelTTF",
     "CCTextFieldTTF",
@@ -176,17 +178,17 @@ local CCObjectTypes = {
     "CCTouch",
     "CCTouchDispatcher",
     "CCTouchHandler",
-	"CCParticleFire",
-	"CCParticleFireworks",
-	"CCParticleSun",
-	"CCParticleGalaxy",
-	"CCParticleFlower",
-	"CCParticleMeteor",
-	"CCParticleSpiral",
-	"CCParticleExplosion",
-	"CCParticleSmoke",
-	"CCParticleSnow",
-	"CCParticleRain",
+    "CCParticleFire",
+    "CCParticleFireworks",
+    "CCParticleSun",
+    "CCParticleGalaxy",
+    "CCParticleFlower",
+    "CCParticleMeteor",
+    "CCParticleSpiral",
+    "CCParticleExplosion",
+    "CCParticleSmoke",
+    "CCParticleSnow",
+    "CCParticleRain",
 }
 
 -- register CCObject types
@@ -199,3 +201,97 @@ _to_functions["LUA_FUNCTION"] = "toluafix_ref_function"
 _is_functions["LUA_FUNCTION"] = "toluafix_isfunction"
 _to_functions["LUA_TABLE"] = "toluafix_totable"
 _is_functions["LUA_TABLE"] = "toluafix_istable"
+
+local toWrite = {}
+local currentString = ''
+local out
+local WRITE, OUTPUT = write, output
+
+function output(s)
+    out = _OUTPUT
+    output = OUTPUT -- restore
+    output(s)
+end
+
+function write(a)
+    if out == _OUTPUT then
+        currentString = currentString .. a
+        if string.sub(currentString,-1) == '\n'  then
+            toWrite[#toWrite+1] = currentString
+            currentString = ''
+        end
+    else
+        WRITE(a)
+    end
+end
+
+function post_output_hook(package)
+    local result = table.concat(toWrite)
+    local function replace(pattern, replacement)
+        local k = 0
+        local nxt, currentString = 1, ''
+        repeat
+            local s, e = string.find(result, pattern, nxt, true)
+            if e then
+                currentString = currentString .. string.sub(result, nxt, s-1) .. replacement
+                nxt = e + 1
+                k = k + 1
+            end
+        until not e
+        result = currentString..string.sub(result, nxt)
+        if k == 0 then print('Pattern not replaced', pattern) end
+    end
+
+    replace([[#ifndef __cplusplus
+#include "stdlib.h"
+#endif
+#include "string.h"
+
+#include "tolua++.h"]],
+      [[/****************************************************************************
+ Copyright (c) 2011 cocos2d-x.org
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/]])
+
+      replace([[/* Exported function */
+TOLUA_API int  tolua_Cocos2d_open (lua_State* tolua_S);]], [[]])
+
+      replace([[*((LUA_FUNCTION*)]], [[(]])
+
+      replace([[unsigned void* tolua_ret = (unsigned void*)  self->getTiles();]],
+        [[unsigned int* tolua_ret = (unsigned int*)  self->getTiles();]])
+
+      replace([[ccColor3B color = *((ccColor3B*)  tolua_tousertype(tolua_S,4,(void*)&(const ccColor3B)ccBLACK));]],
+        [[const ccColor3B clr = ccBLACK;
+  ccColor3B color = *((ccColor3B*)  tolua_tousertype(tolua_S,4,(void*)&clr));]])
+
+      replace([[tolua_usertype(tolua_S,"LUA_FUNCTION");]], [[]])
+
+      replace([[toluafix_pushusertype_ccobject(tolua_S,(void*)tolua_ret]],
+        [[int nID = (tolua_ret) ? (int)tolua_ret->m_uID : -1;
+    int* pLuaID = (tolua_ret) ? &tolua_ret->m_nLuaID : NULL;
+    toluafix_pushusertype_ccobject(tolua_S, nID, pLuaID, (void*)tolua_ret]])
+
+      replace('\t', '    ')
+
+    WRITE(result)
+end
