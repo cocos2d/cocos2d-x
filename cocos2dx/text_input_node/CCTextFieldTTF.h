6d6a0f23eb0fb953c204d6219ab0af1b9dea8651
Marcel Bloemendaal <m.bloemendaal@yipyip.nl>
2013-03-28 15:08:33 +0100

    Added support for secureTextEntry (passwords)
    Added a secureTextEntry property. Enabling this property causes the
    TextField to display the entered text as bullets.

--- cocos2dx/text_input_node/CCTextFieldTTF.h
+++ cocos2dx/text_input_node/CCTextFieldTTF.h
@@ -145,6 +145,11 @@ public:
 protected:
     std::string * m_pPlaceHolder;
     ccColor3B m_ColorSpaceHolder;
+public:
+	virtual void setSecureTextEntry(bool value);
+	virtual bool getSecureTextEntry();
+protected:
+	bool m_pSecureTextEntry;
 protected:
 
     virtual void draw();
