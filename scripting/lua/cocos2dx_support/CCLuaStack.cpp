2b5b7a6d348bedb5003908a0472f05e533225d52
Deniz Piri <denizpiri@gmail.com>
2013-04-01 19:12:58 +0200

    LUA: print() function should use CCLOG internally.
    CCLOG uses __android_log_print internally, this allows lua's print() function
    to print to android log output and allows developer to see print output from
    LUA using "adb logcat".

--- scripting/lua/cocos2dx_support/CCLuaStack.cpp
+++ scripting/lua/cocos2dx_support/CCLuaStack.cpp
@@ -40,6 +40,50 @@ extern "C" {
 #include "Cocos2dxLuaLoader.h"
 #endif
 
+namespace {
+int lua_print(lua_State * luastate)
+{
+    int nargs = lua_gettop(luastate);
+
+    std::string t;
+    for (int i=1; i <= nargs; i++)
+    {
+        if (lua_istable(luastate, i))
+            t += "table";
+        else if (lua_isnone(luastate, i))
+            t += "none";
+        else if (lua_isnil(luastate, i))
+            t += "nil";
+        else if (lua_isboolean(luastate, i))
+        {
+            if (lua_toboolean(luastate, i) != 0)
+                t += "true";
+            else
+                t += "false";
+        }
+        else if (lua_isfunction(luastate, i))
+            t += "function";
+        else if (lua_islightuserdata(luastate, i))
+            t += "lightuserdata";
+        else if (lua_isthread(luastate, i))
+            t += "thread";
+        else
+        {
+            const char * str = lua_tostring(luastate, i);
+            if (str)
+                t += lua_tostring(luastate, i);
+            else
+                t += lua_typename(luastate, lua_type(luastate, i));
+        }
+        if (i!=nargs)
+            t += "\t";
+    }
+    CCLOG("[LUA-print] %s", t.c_str());
+
+    return 0;
+}
+}  // namespace {
+
 NS_CC_BEGIN
 
 CCLuaStack *CCLuaStack::create(void)
@@ -64,6 +108,13 @@ bool CCLuaStack::init(void)
     luaL_openlibs(m_state);
     tolua_Cocos2d_open(m_state);
     toluafix_open(m_state);
+
+    // Register our version of the global "print" function
+    const luaL_reg global_functions [] = {
+        {"print", lua_print},
+        {NULL, NULL}
+    };
+    luaL_register(m_state, "_G", global_functions);
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
     CCLuaObjcBridge::luaopen_luaoc(m_state);
