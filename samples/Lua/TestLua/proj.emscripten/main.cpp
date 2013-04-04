340467638a31ac896a063f1793ef6297d3cd2c23
James Gregory <james@james.id.au>
2013-04-03 18:29:15 -0700

    Add main.cpp files for app samples

---
+++ samples/Lua/TestLua/proj.emscripten/main.cpp
@@ -0,0 +1,19 @@
+#include "../Classes/AppDelegate.h"
+#include "cocos2d.h"
+#include "CCEGLView.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+
+USING_NS_CC;
+
+int main(int argc, char **argv)
+{
+    // create the application instance
+    AppDelegate app;
+    CCEGLView* eglView = CCEGLView::sharedOpenGLView();
+    eglView->setFrameSize(800, 480);
+    return CCApplication::sharedApplication()->run();
+}
