bf61fa3c89ec21d8f172828ef66170d1e0962fe1
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-05 16:13:04 +0800

    Adding Texture2dTest. And format some lua codes.

--- samples/Lua/TestLua/Resources/luaScript/mainMenu.lua
+++ samples/Lua/TestLua/Resources/luaScript/mainMenu.lua
@@ -11,58 +11,58 @@ local BeginPos = {x = 0, y = 0}
 
 -- create scene
 local function CreateTestScene(nIdx)
-	CCDirector:sharedDirector():purgeCachedData()
+    CCDirector:sharedDirector():purgeCachedData()
 
-	local scene = nil
-	if nIdx == Test_Table.TEST_ACTIONS then
-		scene = ActionsTest()
-	elseif nIdx == Test_Table.TEST_TRANSITIONS then
-		scene = TransitionsTest()
-	elseif nIdx == Test_Table.TEST_PROGRESS_ACTIONS then
-		scene = ProgressActionsTest()
+    local scene = nil
+    if nIdx == Test_Table.TEST_ACTIONS then
+        scene = ActionsTest()
+    elseif nIdx == Test_Table.TEST_TRANSITIONS then
+        scene = TransitionsTest()
+    elseif nIdx == Test_Table.TEST_PROGRESS_ACTIONS then
+        scene = ProgressActionsTest()
     elseif nIdx == Test_Table.TEST_EFFECTS then
-		scene = EffectsTest()
+        scene = EffectsTest()
     elseif nIdx == Test_Table.TEST_CLICK_AND_MOVE then
-		scene = ClickAndMoveTest()
+        scene = ClickAndMoveTest()
     elseif nIdx == Test_Table.TEST_ROTATE_WORLD then
-		scene = RotateWorldTest()
+        scene = RotateWorldTest()
     elseif nIdx == Test_Table.TEST_PARTICLE then
-		scene = ParticleTest()
+        scene = ParticleTest()
     elseif nIdx == Test_Table.TEST_EASE_ACTIONS then
-		scene = EaseActionsTest()
+        scene = EaseActionsTest()
     elseif nIdx == Test_Table.TEST_MOTION_STREAK then
-		scene = MotionStreakTest()
+        scene = MotionStreakTest()
     elseif nIdx == Test_Table.TEST_DRAW_PRIMITIVES then
-		scene = DrawPrimitivesTest()
+        scene = DrawPrimitivesTest()
     elseif nIdx == Test_Table.TEST_COCOSNODE then
-		scene = CocosNodeTest()
+        scene = CocosNodeTest()
     elseif nIdx == Test_Table.TEST_TOUCHES then
-		scene = TouchesTest()
+        scene = TouchesTest()
     elseif nIdx == Test_Table.TEST_MENU then
-		scene = MenuTestMain()
+        scene = MenuTestMain()
     elseif nIdx == Test_Table.TEST_ACTION_MANAGER then
-		scene = ActionManagerTestMain()
+        scene = ActionManagerTestMain()
     elseif nIdx == Test_Table.TEST_LAYER then
-		scene = LayerTestMain()
+        scene = LayerTestMain()
     elseif nIdx == Test_Table.TEST_SCENE then
-		scene = SceneTestMain()
+        scene = SceneTestMain()
     elseif nIdx == Test_Table.TEST_PARALLAX then
-		scene = ParallaxTestMain()
+        scene = ParallaxTestMain()
     elseif nIdx == Test_Table.TEST_TILE_MAP then
-		scene = TileMapTestMain()
+        scene = TileMapTestMain()
     elseif nIdx == Test_Table.TEST_INTERVAL then
-		scene = IntervalTestMain()
+        scene = IntervalTestMain()
     elseif nIdx == Test_Table.TEST_CHIPMUNKACCELTOUCH then
---#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
---        pScene = new ChipmunkAccelTouchTestScene()
---#else
---#ifdef MARMALADEUSECHIPMUNK
---#if    (MARMALADEUSECHIPMUNK == 1)
---        pScene = new ChipmunkAccelTouchTestScene();
---#endif
---        break;
---#endif
---#endif
+        --#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
+        --        pScene = new ChipmunkAccelTouchTestScene()
+        --#else
+        --#ifdef MARMALADEUSECHIPMUNK
+        --#if    (MARMALADEUSECHIPMUNK == 1)
+        --        pScene = new ChipmunkAccelTouchTestScene();
+        --#endif
+        --        break;
+        --#endif
+        --#endif
     elseif nIdx == Test_Table.TEST_LABEL then
         scene = LabelTest()
     elseif nIdx == Test_Table.TEST_TEXT_INPUT then
@@ -75,7 +75,7 @@ local function CreateTestScene(nIdx)
     elseif nIdx == Test_Table.TEST_RENDERTEXTURE then
 
     elseif nIdx == Test_Table.TEST_TEXTURE2D then
-
+        scene = Texture2dTestMain()
     elseif nIdx == Test_Table.TEST_BOX2D then
 
     elseif nIdx == Test_Table.TEST_BOX2DBED then
@@ -84,23 +84,23 @@ local function CreateTestScene(nIdx)
 
     elseif nIdx == Test_Table.TEST_ACCELEROMRTER then
 
---#if (CC_TARGET_PLATFORM != CC_PLATFORM_BADA)
---    elseif nIdx == Test_Table.TEST_KEYPAD then
---        pScene = new KeypadTestScene()
---#endif
+        --#if (CC_TARGET_PLATFORM != CC_PLATFORM_BADA)
+        --    elseif nIdx == Test_Table.TEST_KEYPAD then
+        --        pScene = new KeypadTestScene()
+        --#endif
     elseif nIdx == Test_Table.TEST_COCOSDENSHION then
 
     elseif nIdx == Test_Table.TEST_PERFORMANCE then
-		scene = PerformanceTest()
+        scene = PerformanceTest()
     elseif nIdx == Test_Table.TEST_ZWOPTEX then
 
---#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
--- bada don't support libcurl
---#if (CC_TARGET_PLATFORM != CC_PLATFORM_BADA)
---   elseif nIdx == Test_Table.TEST_CURL then
+        --#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
+        -- bada don't support libcurl
+        --#if (CC_TARGET_PLATFORM != CC_PLATFORM_BADA)
+        --   elseif nIdx == Test_Table.TEXT_CURL then
 
---#endif
---#endif
+        --#endif
+        --#endif
     elseif nIdx == Test_Table.TEST_USERDEFAULT then
 
     elseif nIdx == Test_Table.TEST_BUGS then
@@ -109,88 +109,88 @@ local function CreateTestScene(nIdx)
 
     elseif nIdx == Test_Table.TEST_CURRENT_LANGUAGE then
 
---#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
---   elseif nIdx == Test_Table.TEST_TEXTURECACHE then pScene = new TextureCacheTestScene()
---#endif
+        --#if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
+        --   elseif nIdx == Test_Table.TEST_TEXTURECACHE then pScene = new TextureCacheTestScene()
+        --#endif
     elseif nIdx == Test_Table.TEST_EXTENSIONS then
 
     elseif nIdx == Test_Table.TEST_SHADER then
 
     elseif nIdx == Test_Table.TEST_MUTITOUCH then
 
-	end
+    end
 
-	return scene
+    return scene
 end
 
 -- create menu
 function CreateTestMenu()
-	local menuLayer = CCLayer:create()
+    local menuLayer = CCLayer:create()
 
-	local function closeCallback()
-		CCDirector:sharedDirector():endToLua()
-	end
+    local function closeCallback()
+        CCDirector:sharedDirector():endToLua()
+    end
 
-	local function menuCallback(tag)
+    local function menuCallback(tag)
         print(tag)
-		local Idx = tag - 10000
-		local testScene = CreateTestScene(Idx)
-		if testScene then
-			CCDirector:sharedDirector():replaceScene(testScene)
-		end
-	end
+        local Idx = tag - 10000
+        local testScene = CreateTestScene(Idx)
+        if testScene then
+            CCDirector:sharedDirector():replaceScene(testScene)
+        end
+    end
 
-	-- add close menu
-	local s = CCDirector:sharedDirector():getWinSize()
-	local CloseItem = CCMenuItemImage:create(s_pPathClose, s_pPathClose)
-	CloseItem:registerScriptTapHandler(closeCallback)
-	CloseItem:setPosition(ccp(s.width - 30, s.height - 30))
+    -- add close menu
+    local s = CCDirector:sharedDirector():getWinSize()
+    local CloseItem = CCMenuItemImage:create(s_pPathClose, s_pPathClose)
+    CloseItem:registerScriptTapHandler(closeCallback)
+    CloseItem:setPosition(ccp(s.width - 30, s.height - 30))
 
     local CloseMenu = CCMenu:create()
     CloseMenu:setPosition(0, 0)
-	CloseMenu:addChild(CloseItem)
-	menuLayer:addChild(CloseMenu)
+    CloseMenu:addChild(CloseItem)
+    menuLayer:addChild(CloseMenu)
 
-	-- add menu items for tests
+    -- add menu items for tests
     local MainMenu = CCMenu:create()
     for index, labelName in pairs(Test_Name) do
-		local testLabel = CCLabelTTF:create(labelName, "Arial", 24)
+        local testLabel = CCLabelTTF:create(labelName, "Arial", 24)
         local testMenuItem = CCMenuItemLabel:create(testLabel)
 
-		testMenuItem:registerScriptTapHandler(menuCallback)
-		testMenuItem:setPosition(ccp(s.width / 2, (s.height - (index + 1) * LINE_SPACE)))
+        testMenuItem:registerScriptTapHandler(menuCallback)
+        testMenuItem:setPosition(ccp(s.width / 2, (s.height - (index + 1) * LINE_SPACE)))
         MainMenu:addChild(testMenuItem, index + 10000, index + 10000)
     end
 
     MainMenu:setContentSize(CCSizeMake(s.width, (Test_Table.TESTS_COUNT + 1) * (LINE_SPACE)))
-	MainMenu:setPosition(CurPos.x, CurPos.y)
-	menuLayer:addChild(MainMenu)
+    MainMenu:setPosition(CurPos.x, CurPos.y)
+    menuLayer:addChild(MainMenu)
 
-	-- handling touch events
+    -- handling touch events
     local function onTouchBegan(x, y)
-		BeginPos = {x = x, y = y}
+        BeginPos = {x = x, y = y}
         -- CCTOUCHBEGAN event must return true
         return true
     end
 
     local function onTouchMoved(x, y)
         local nMoveY = y - BeginPos.y
-		local curPosx, curPosy = MainMenu:getPosition()
-		local nextPosy = curPosy + nMoveY
-		local winSize = CCDirector:sharedDirector():getWinSize()
-		if nextPosy < 0 then
-			MainMenu:setPosition(0, 0)
-			return
-		end
+        local curPosx, curPosy = MainMenu:getPosition()
+        local nextPosy = curPosy + nMoveY
+        local winSize = CCDirector:sharedDirector():getWinSize()
+        if nextPosy < 0 then
+            MainMenu:setPosition(0, 0)
+            return
+        end
 
-		if nextPosy > ((Test_Table.TESTS_COUNT + 1) * LINE_SPACE - winSize.height) then
-			MainMenu:setPosition(0, ((Test_Table.TESTS_COUNT + 1) * LINE_SPACE - winSize.height))
-			return
-		end
+        if nextPosy > ((Test_Table.TESTS_COUNT + 1) * LINE_SPACE - winSize.height) then
+            MainMenu:setPosition(0, ((Test_Table.TESTS_COUNT + 1) * LINE_SPACE - winSize.height))
+            return
+        end
 
-		MainMenu:setPosition(curPosx, nextPosy)
-		BeginPos = {x = x, y = y}
-		CurPos = {x = curPosx, y = nextPosy}
+        MainMenu:setPosition(curPosx, nextPosy)
+        BeginPos = {x = x, y = y}
+        CurPos = {x = curPosx, y = nextPosy}
     end
 
     local function onTouch(eventType, x, y)
@@ -201,7 +201,7 @@ function CreateTestMenu()
         end
     end
 
-	menuLayer:setTouchEnabled(true)
+    menuLayer:setTouchEnabled(true)
     menuLayer:registerScriptTouchHandler(onTouch)
 
     return menuLayer
