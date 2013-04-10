ad0859543dc2b1e40d6366b6f9cd0a7486fd7d01
Physwf <joe.physwf@hotmail.com>
2013-04-10 06:32:26 -0700

    新增实验工程

---
+++ JoeCocosApp/Classes/AppDelegate.cpp
@@ -0,0 +1,54 @@
+#include "cocos2d.h"
+#include "CCEGLView.h"
+#include "AppDelegate.h"
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+
+using namespace CocosDenshion;
+
+USING_NS_CC;
+
+AppDelegate::AppDelegate()
+{
+}
+
+AppDelegate::~AppDelegate()
+{
+    SimpleAudioEngine::end();
+}
+
+bool AppDelegate::applicationDidFinishLaunching()
+{
+    // initialize director
+    CCDirector *pDirector = CCDirector::sharedDirector();
+    pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
+
+    // turn on display FPS
+    pDirector->setDisplayStats(true);
+
+    // set FPS. the default value is 1.0/60 if you don't call this
+    pDirector->setAnimationInterval(1.0 / 60);
+
+    // create a scene. it's an autorelease object
+    CCScene *pScene = HelloWorld::scene();
+
+    // run
+    pDirector->runWithScene(pScene);
+    return true;
+}
+
+// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+void AppDelegate::applicationDidEnterBackground()
+{
+    CCDirector::sharedDirector()->stopAnimation();
+
+    SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+}
+
+// this function will be called when the app is active again
+void AppDelegate::applicationWillEnterForeground()
+{
+    CCDirector::sharedDirector()->startAnimation();
+
+    SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+}
---
+++ JoeCocosApp/Classes/HelloWorldScene.cpp
@@ -0,0 +1,97 @@
+#include "HelloWorldScene.h"
+
+using namespace cocos2d;
+
+CCScene* HelloWorld::scene()
+{
+    CCScene * scene = NULL;
+    do 
+    {
+        // 'scene' is an autorelease object
+        scene = CCScene::create();
+        CC_BREAK_IF(! scene);
+
+        // 'layer' is an autorelease object
+        HelloWorld *layer = HelloWorld::create();
+        CC_BREAK_IF(! layer);
+
+        // add layer as a child to scene
+        scene->addChild(layer);
+    } while (0);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    bool bRet = false;
+    do 
+    {
+        //////////////////////////////////////////////////////////////////////////
+        // super init first
+        //////////////////////////////////////////////////////////////////////////
+
+        CC_BREAK_IF(! CCLayer::init());
+
+        //////////////////////////////////////////////////////////////////////////
+        // add your codes below...
+        //////////////////////////////////////////////////////////////////////////
+
+        // 1. Add a menu item with "X" image, which is clicked to quit the program.
+
+        // Create a "close" menu item with close icon, it's an auto release object.
+        CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
+            "CloseNormal.png",
+            "CloseSelected.png",
+            this,
+            menu_selector(HelloWorld::menuCloseCallback));
+        CC_BREAK_IF(! pCloseItem);
+
+        // Place the menu item bottom-right conner.
+        pCloseItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20));
+
+        // Create a menu with the "close" menu item, it's an auto release object.
+        CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
+        pMenu->setPosition(CCPointZero);
+        CC_BREAK_IF(! pMenu);
+
+        // Add the menu to HelloWorld layer as a child layer.
+        this->addChild(pMenu, 1);
+
+        // 2. Add a label shows "Hello World".
+
+        // Create a label and initialize with string "Hello World".
+        CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
+        CC_BREAK_IF(! pLabel);
+
+        // Get window size and place the label upper. 
+        CCSize size = CCDirector::sharedDirector()->getWinSize();
+        pLabel->setPosition(ccp(size.width / 2, size.height - 50));
+
+        // Add the label to HelloWorld layer as a child layer.
+        this->addChild(pLabel, 1);
+
+        // 3. Add add a splash screen, show the cocos2d splash image.
+        CCSprite* pSprite = CCSprite::create("HelloWorld.png");
+        CC_BREAK_IF(! pSprite);
+
+        // Place the sprite on the center of the screen
+        pSprite->setPosition(ccp(size.width/2, size.height/2));
+
+        // Add the sprite to HelloWorld layer as a child layer.
+        this->addChild(pSprite, 0);
+
+        bRet = true;
+    } while (0);
+
+    return bRet;
+}
+
+void HelloWorld::menuCloseCallback(CCObject* pSender)
+{
+    // "close" menu item clicked
+    CCDirector::sharedDirector()->end();
+}
+
