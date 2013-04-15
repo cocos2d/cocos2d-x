16b9506690856cf6c9154ef32e7b25e3703a4ddb
billy1380 <william.shakour@gmail.com>
2013-04-15 22:49:29 +0100

    fixed a bug that was causing http client to fail on mac os x with semaphore error

--- extensions/network/HttpClient.cpp
+++ extensions/network/HttpClient.cpp
@@ -41,7 +41,7 @@ static pthread_mutex_t  s_responseQueueMutex;
 static sem_t *          s_pSem = NULL;
 static unsigned long    s_asyncRequestCount = 0;
 
-#if CC_TARGET_PLATFORM == CC_PLATFORM_IOS
+#if CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC
 #define CC_ASYNC_HTTPREQUEST_USE_NAMED_SEMAPHORE 1
 #else
 #define CC_ASYNC_HTTPREQUEST_USE_NAMED_SEMAPHORE 0
