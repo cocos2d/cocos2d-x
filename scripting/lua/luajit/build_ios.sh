bbbf139c8de074afc447f350790b2f87333ef21e
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-16 18:19:25 +0800

    Adding luajit iOS prebuilt library.

--- scripting/lua/luajit/build_ios.sh
+++ scripting/lua/luajit/build_ios.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIPO="xcrun -sdk iphoneos lipo"
+STRIP="xcrun -sdk iphoneos strip"
+
 SRCDIR=$DIR/LuaJit-2.0.1
-DESTDIR=$DIR/proj.ios
+DESTDIR=$DIR/ios
 IXCODE=`xcode-select -print-path`
 ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
 ISDKVER=iPhoneOS6.1.sdk
@@ -13,18 +16,21 @@ cd $SRCDIR
 make clean
 ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER"
 make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit2-armv7.a
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7.a
 
-# make clean
-# ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER"
-# make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
-# mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit2-armv7s.a
+make clean
+ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER"
+make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7s.a
 
-# make clean
-# make CC="gcc -m32 -arch i386" clean all
-# mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit2-i386.a
+make clean
+make CC="gcc -m32 -arch i386" clean all
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-i386.a
 
-# lipo -create "$DESTDIR"/libluajit2-*.a -output "$DESTDIR"/libluajit2.a
-# rm "$DESTDIR"/libluajit2-*.a
+$LIPO -create "$DESTDIR"/libluajit-*.a -output "$DESTDIR"/libluajit.a
+$STRIP -S "$DESTDIR"/libluajit.a
+$LIPO -info "$DESTDIR"/libluajit.a
 
-# make clean
+rm "$DESTDIR"/libluajit-*.a
+
+make clean
