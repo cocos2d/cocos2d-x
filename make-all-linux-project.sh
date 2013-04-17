52791bd85d5d457ae12875b4b380c3995eed2218
Sam Clegg <sam@superduper.net>
2013-04-17 08:26:01 -0700

    Add .travis.yml
    This builds all libraries and examples in both debug
    and release for Linux and NaCl platforms.
    
    Add travis build status image to README.mdown.
    
    Also, split the linux dependency installation into
    its own script: install-deps-linux.sh

--- make-all-linux-project.sh
+++ make-all-linux-project.sh
@@ -8,40 +8,14 @@
 # Exit of first error.
 set -e
 
+# Change directory to the location of this script
+cd $(dirname ${BASH_SOURCE[0]})
+
 [ -z "$COCOS2DX_USEAPT" ] && COCOS2DX_USEAPT=true
 
 if $COCOS2DX_USEAPT; then
-    DEPENDS='libx11-dev'
-    DEPENDS+=' libxmu-dev'
-    DEPENDS+=' libglu1-mesa-dev'
-    DEPENDS+=' libgl2ps-dev'
-    DEPENDS+=' libxi-dev'
-    DEPENDS+=' libglfw-dev'
-    DEPENDS+=' g++'
-    DEPENDS+=' libzip-dev'
-    DEPENDS+=' libcurl4-gnutls-dev'
-    DEPENDS+=' libfontconfig1-dev'
-    DEPENDS+=' libsqlite3-dev'
-    DEPENDS+=' libglew*-dev'
-
-    MISSING=
-    echo "Checking for missing packages ..."
-    for i in $DEPENDS; do
-        if ! dpkg-query -W --showformat='${Status}\n' $i | grep "install ok installed" > /dev/null; then
-            MISSING+="$i "
-        fi
-    done
-
-    if [ -n "$MISSING" ]; then
-        TXTCOLOR_DEFAULT="\033[0;m"
-        TXTCOLOR_GREEN="\033[0;32m"
-        echo -e $TXTCOLOR_GREEN"Missing packages: $MISSING.\nYou may be asked for your password for package installation."$TXTCOLOR_DEFAULT
-        sudo apt-get --force-yes --yes install $MISSING
-    fi
+    ./install-deps-linux.sh
 fi
-
-# Change directory to the location of this script
-cd $(dirname ${BASH_SOURCE[0]})
 
 export MAKEFLAGS=-j10
 
