8d96400f3ccf0ec7504012841739a99b6ded0ff4
Sergey Shambir <sergey.shambir.auto@gmail.com>
2013-04-19 11:47:14 +0400

    Added CC_FORMAT_PRINTF(x,y) macro to warn on CCLog() format errors
    Uses printf format. Enabled for GCC and clang.

--- cocos2dx/platform/CCCommon.h
+++ cocos2dx/platform/CCCommon.h
@@ -40,7 +40,7 @@ static const int kMaxLogLen = 16*1024;
 /**
 @brief Output Debug message.
 */
-void CC_DLL CCLog(const char * pszFormat, ...);
+void CC_DLL CCLog(const char * pszFormat, ...) CC_FORMAT_PRINTF(1, 2);
 
 /**
  * lua can not deal with ...
