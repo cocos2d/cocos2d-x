09cb62ef88608a19cc160d894a9ea97cbee4c584
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-23 10:52:51 +0800

    fix a typo.

--- scripting/javascript/bindings/cocos2d_specifics.cpp
+++ scripting/javascript/bindings/cocos2d_specifics.cpp
@@ -2241,7 +2241,7 @@ JSBool js_cocos2dx_CCTMXLayer_getTiles(JSContext *cx, uint32_t argc, jsval *vp)
         JSObject* array = JS_NewUint32Array(cx, count);
         if (NULL == array) {
             JS_ReportError(cx, "Can't allocate enough memory.", argc, 0);
-            return FALSE;
+            return JS_FALSE;
         }
         uint32_t* bufdata = (uint32_t*)JS_GetArrayBufferViewData(array);
         memcpy(bufdata, ret, count*sizeof(uint32_t));
