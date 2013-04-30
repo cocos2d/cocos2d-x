de9df010c36821d20ea41e944729005563cd205a
James Gregory <james@james.id.au>
2013-04-29 20:53:38 -0700

    Enable texture cache on Emscripten. Not sure yet if this is the best balance, since we're both short on memory *and* compute time.

--- cocos2dx/platform/CCPlatformMacros.h
+++ cocos2dx/platform/CCPlatformMacros.h
@@ -86,7 +86,7 @@ It's new in cocos2d-x since v0.99.5
     #define CC_ENABLE_CACHE_TEXTURE_DATA       0
 #endif
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_EMSCRIPTEN)
     /* Application will crash in glDrawElements function on some win32 computers and some android devices.
        Indices should be bound again while drawing to avoid this bug.
      */
