89f53955c7de18c4ebe19a42f675bcceb01752db
Lee, Jae-Hong <pyrasis@delphigames.com>
2013-05-09 20:54:23 +0900

    Remove unused headers and defines.

--- cocos2dx/platform/tizen/CCStdC.h
+++ cocos2dx/platform/tizen/CCStdC.h
@@ -27,15 +27,9 @@ THE SOFTWARE.
 
 #include "platform/CCPlatformMacros.h"
 
-#include <float.h>
 #include <math.h>
-#include <string.h>
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <sys/time.h>
-#include <stdint.h>
 
 #ifndef MIN
 #define MIN(x,y) (((x) > (y)) ? (y) : (x))
@@ -44,11 +38,5 @@ THE SOFTWARE.
 #ifndef MAX
 #define MAX(x,y) (((x) < (y)) ? (y) : (x))
 #endif  // MAX
-
-// some function linux do not have
-#define tanf tan
-#define sqrtf sqrt
-#define cosf cos
-#define sinf sin
 
 #endif  // __CC_STD_C_H__
