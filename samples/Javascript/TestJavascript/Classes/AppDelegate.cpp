ebf235642d86e4284daffd2e158296e0cde0e660
folecr <folecr@gmail.com>
2013-05-10 16:17:31 -0700

    Call enableDebugger

--- samples/Javascript/TestJavascript/Classes/AppDelegate.cpp
+++ samples/Javascript/TestJavascript/Classes/AppDelegate.cpp
@@ -53,6 +53,9 @@ bool AppDelegate::applicationDidFinishLaunching()
 #ifdef JS_OBFUSCATED
     ScriptingCore::getInstance()->runScript("game.js");
 #else
+#if JSB_ENABLE_DEBUGGER
+    ScriptingCore::getInstance()->enableDebugger();
+#endif // JSB_ENABLE_DEBUGGER
     ScriptingCore::getInstance()->runScript("tests-boot-jsb.js");
 #endif
     return true;
