7009491521b79ee456da31e6a8899a37d50fc1bb
James Chen <jianhua.chen@cocos2d-x.org>
2013-05-22 17:31:07 +0800

    fixed #2214: Upgrading Spidermonkey to Firefox v21

--- scripting/javascript/spidermonkey-win32/include/mozilla/TypeTraits.h
+++ scripting/javascript/spidermonkey-win32/include/mozilla/TypeTraits.h
@@ -7,6 +7,14 @@
 #ifndef mozilla_TypeTraits_h_
 #define mozilla_TypeTraits_h_
 
+/*
+ * These traits are approximate copies of the traits and semantics from C++11's
+ * <type_traits> header.  Don't add traits not in that header!  When all
+ * platforms provide that header, we can convert all users and remove this one.
+ */
+
+#include <wchar.h>
+
 namespace mozilla {
 
 namespace detail {
@@ -156,7 +164,7 @@ struct Conditional<false, A, B>
  *   template<typename T>
  *   class PodVector // vector optimized to store POD (memcpy-able) types
  *   {
- *      EnableIf<IsPodType<T>, T>::Type* vector;
+ *      EnableIf<IsPod<T>, T>::Type* vector;
  *      size_t length;
  *      ...
  *   };
@@ -169,6 +177,75 @@ template<typename T>
 struct EnableIf<true, T>
 {
     typedef T Type;
+};
+
+/**
+ * IsSame tests whether two types are the same type.
+ *
+ * mozilla::IsSame<int, int>::value is true;
+ * mozilla::IsSame<int*, int*>::value is true;
+ * mozilla::IsSame<int, unsigned int>::value is false;
+ * mozilla::IsSame<void, void>::value is true;
+ * mozilla::IsSame<const int, int>::value is false;
+ * mozilla::IsSame<struct S, struct S>::value is true.
+ */
+template<typename T, typename U>
+struct IsSame
+{
+    static const bool value = false;
+};
+
+template<typename T>
+struct IsSame<T, T>
+{
+    static const bool value = true;
+};
+
+/*
+ * Traits class for identifying POD types. Until C++0x, there is no automatic
+ * way to detect PODs, so for the moment it is done manually.
+ */
+template<typename T>
+struct IsPod
+{
+    static const bool value = false;
+};
+template<> struct IsPod<char>               { static const bool value = true; };
+template<> struct IsPod<signed char>        { static const bool value = true; };
+template<> struct IsPod<unsigned char>      { static const bool value = true; };
+template<> struct IsPod<short>              { static const bool value = true; };
+template<> struct IsPod<unsigned short>     { static const bool value = true; };
+template<> struct IsPod<int>                { static const bool value = true; };
+template<> struct IsPod<unsigned int>       { static const bool value = true; };
+template<> struct IsPod<long>               { static const bool value = true; };
+template<> struct IsPod<unsigned long>      { static const bool value = true; };
+template<> struct IsPod<long long>          { static const bool value = true; };
+template<> struct IsPod<unsigned long long> { static const bool value = true; };
+template<> struct IsPod<bool>               { static const bool value = true; };
+template<> struct IsPod<float>              { static const bool value = true; };
+template<> struct IsPod<double>             { static const bool value = true; };
+template<> struct IsPod<wchar_t>            { static const bool value = true; };
+template<typename T> struct IsPod<T*>       { static const bool value = true; };
+
+/**
+ * IsPointer determines whether a type is a pointer type (but not a pointer-to-
+ * member type).
+ *
+ * mozilla::IsPointer<struct S*>::value is true;
+ * mozilla::IsPointer<int**>::value is true;
+ * mozilla::IsPointer<void (*)(void)>::value is true;
+ * mozilla::IsPointer<int>::value is false;
+ * mozilla::IsPointer<struct S>::value is false.
+ */
+template<typename T>
+struct IsPointer
+{
+    static const bool value = false;
+};
+template<typename T>
+struct IsPointer<T*>
+{
+    static const bool value = true;
 };
 
 } /* namespace mozilla */
