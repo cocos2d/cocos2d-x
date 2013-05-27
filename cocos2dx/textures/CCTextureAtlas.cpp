f0505c57d865127336b5c80a4725e93741b9aa46
Yongqin Liu <yongqin.liu@linaro.org>
2013-05-27 14:19:23 +0800

    CCTextureAtlas::updateQuad change to the same as before
    add blank lines as before
    
    Signed-off-by: Yongqin Liu<liuyq0307@thundersoft.com>

--- cocos2dx/textures/CCTextureAtlas.cpp
+++ cocos2dx/textures/CCTextureAtlas.cpp
@@ -310,6 +310,8 @@ void CCTextureAtlas::updateQuad(ccV3F_C4B_T2F_Quad *quad, unsigned int index)
     m_uTotalQuads = MAX( index+1, m_uTotalQuads);
 
     m_pQuads[index] = *quad;
+
+
     m_bDirty = true;
 
 }
