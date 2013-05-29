c2a9dca0c4b2d5c18c30a1b31ea89d90149fa594
franck <franck@secretfatty.net>
2013-05-29 10:16:22 +0200

    adding Norwegian language support

--- cocos2dx/platform/ios/CCApplication.mm
+++ cocos2dx/platform/ios/CCApplication.mm
@@ -119,7 +119,9 @@ ccLanguageType CCApplication::getCurrentLanguage()
     else if ([languageCode isEqualToString:@"ar"]){
         ret = kLanguageArabic;
     }
-
+    else if ([languageCode isEqualToString:@"nb"]){
+        ret = kLanguageNorwegian;
+    }
     return ret;
 }
 
