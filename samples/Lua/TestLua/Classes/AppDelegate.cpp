7101bc437710f70522c9cedbc4e9f91766f09dfc
samuele3hu <qingkui.hu@cocos2d-x.org>
2013-06-03 14:59:04 +0800

    issue #2103 remove  simple cocosbuilder sample

--- samples/Lua/TestLua/Classes/AppDelegate.cpp
+++ samples/Lua/TestLua/Classes/AppDelegate.cpp
@@ -30,47 +30,6 @@ bool AppDelegate::applicationDidFinishLaunching()
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
     
-    TargetPlatform platform = CCApplication::sharedApplication()->getTargetPlatform();
-    if (platform == kTargetIphone || platform == kTargetIpad)
-    {
-        std::vector<std::string> searchPaths = CCFileUtils::sharedFileUtils()->getSearchPaths();
-        
-        searchPaths.insert(searchPaths.begin(), "Published-iOS");
-        CCSize screenSize = CCEGLView::sharedOpenGLView()->getFrameSize();
-        if (screenSize.height > 768)
-        {// Ipadhd
-            searchPaths.insert(searchPaths.begin(),"Published-iOS/resources-ipadhd");
-        }
-        else if (screenSize.height > 640)
-        {// Ipad
-            searchPaths.insert(searchPaths.begin(),"Published-iOS/resources-ipad");
-        }
-        else if (screenSize.height > 320)
-        {// Iphone-hd 3.5 and 4.0 inch
-            searchPaths.insert(searchPaths.begin(),"Published-iOS/resources-iphonehd");
-        }
-        else
-        {// Iphone
-            searchPaths.insert(searchPaths.begin(),"Published-iOS/resources-iphone");
-        }
-        CCFileUtils::sharedFileUtils()->setSearchPaths(searchPaths);
-    }
-    
-    if (platform == kTargetAndroid )
-    {
-        std::vector<std::string> searchPaths = CCFileUtils::sharedFileUtils()->getSearchPaths();
-        
-        searchPaths.insert(searchPaths.begin(), "Published-Android");
-        CCSize screenSize = CCEGLView::sharedOpenGLView()->getFrameSize();
-        searchPaths.push_back("Published-Android/resources-xlarge");
-        searchPaths.push_back("Published-Android/resources-large");
-        searchPaths.push_back("Published-Android/resources-medium");
-        searchPaths.push_back("Published-Android/resources-small");
-        searchPaths.push_back("Published-Android/resources-xsmall");
-        CCFileUtils::sharedFileUtils()->setSearchPaths(searchPaths);
-    }
-
-
     // register lua engine
     CCLuaEngine* pEngine = CCLuaEngine::defaultEngine();
     CCScriptEngineManager::sharedManager()->setScriptEngine(pEngine);
