1fc9d43a06c73bff37d84580018d7306559c644e
James Chen <jianhua.chen@cocos2d-x.org>
2013-06-20 16:02:39 +0800

    issue #2129: Removing CC prefix for plugin module.

--- plugin/samples/HelloAds/proj.ios/AppController.mm
+++ plugin/samples/HelloAds/proj.ios/AppController.mm
@@ -49,7 +49,7 @@ static AppDelegate s_sharedApplication;
     
     [[UIApplication sharedApplication] setStatusBarHidden:true];
     
-    cocos2d::CCApplication::sharedApplication()->run();
+    cocos2d::Application::sharedApplication()->run();
 
     return YES;
 }
@@ -60,14 +60,14 @@ static AppDelegate s_sharedApplication;
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-    cocos2d::CCDirector::sharedDirector()->pause();
+    cocos2d::Director::sharedDirector()->pause();
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-    cocos2d::CCDirector::sharedDirector()->resume();
+    cocos2d::Director::sharedDirector()->resume();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -75,14 +75,14 @@ static AppDelegate s_sharedApplication;
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
      */
-    cocos2d::CCApplication::sharedApplication()->applicationDidEnterBackground();
+    cocos2d::Application::sharedApplication()->applicationDidEnterBackground();
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
-    cocos2d::CCApplication::sharedApplication()->applicationWillEnterForeground();
+    cocos2d::Application::sharedApplication()->applicationWillEnterForeground();
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
