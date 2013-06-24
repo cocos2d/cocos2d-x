ef5de27a98aa1ec129a8666c430c57077a70007c
zhangbin <zhangbin@cocos2d-x.org>
2013-06-24 14:30:38 +0800

    Rename the class ProtocolSocial to ProtocolShare.

--- plugin/protocols/proj.android/jni/Android.mk
+++ plugin/protocols/proj.android/jni/Android.mk
@@ -15,7 +15,7 @@ $(addprefix ../../platform/android/, \
     ProtocolAnalytics.cpp \
     ProtocolIAP.cpp \
     ProtocolAds.cpp \
-    ProtocolSocial.cpp \
+    ProtocolShare.cpp \
 ) \
 ../../PluginManager.cpp \
 ../../PluginParam.cpp
