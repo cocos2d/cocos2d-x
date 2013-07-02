ec5edeb8f673ff43991f1304eab2e1860d64e57c
James Chen <jianhua.chen@cocos2d-x.org>
2013-07-02 10:42:50 +0800

    issue #2325: Updating title for Mac projects.

--- samples/Javascript/MoonWarriors/proj.mac/AppController.mm
+++ samples/Javascript/MoonWarriors/proj.mac/AppController.mm
@@ -36,7 +36,7 @@
 		// create the window
 		// note that using NSResizableWindowMask causes the window to be a little
 		// smaller and therefore ipad graphics are not loaded
-        NSRect rect = NSMakeRect(100, 100, 800, 450);
+        NSRect rect = NSMakeRect(100, 100, 320, 480);
 		window = [[NSWindow alloc] initWithContentRect:rect
 			styleMask:( NSClosableWindowMask | NSTitledWindowMask )
 			backing:NSBackingStoreBuffered
@@ -58,7 +58,7 @@
 		// set window parameters
 		[window becomeFirstResponder];
 		[window setContentView:glView];
-		[window setTitle:@"TestCpp"];
+		[window setTitle:@"JS MoonWarriors"];
 		[window makeKeyAndOrderFront:self];
 		[window setAcceptsMouseMovedEvents:NO];
 
--- samples/Javascript/WatermelonWithMe/proj.mac/AppController.mm
+++ samples/Javascript/WatermelonWithMe/proj.mac/AppController.mm
@@ -58,7 +58,7 @@
 		// set window parameters
 		[window becomeFirstResponder];
 		[window setContentView:glView];
-		[window setTitle:@"TestCpp"];
+		[window setTitle:@"JS WatermelonWithMe"];
 		[window makeKeyAndOrderFront:self];
 		[window setAcceptsMouseMovedEvents:NO];
 
