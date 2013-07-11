eb538ecdffc9769a9c63e53c0202491fa6ca5572
James Chen <jianhua.chen@cocos2d-x.org>
2013-07-11 14:13:01 +0800

    issue #2395: Adding Scale9SpriteTest for other platforms.

--- samples/Cpp/TestCpp/Android.mk
+++ samples/Cpp/TestCpp/Android.mk
@@ -66,6 +66,7 @@ Classes/ExtensionsTest/NetworkTest/WebSocketTest.cpp \
 Classes/ExtensionsTest/EditBoxTest/EditBoxTest.cpp \
 Classes/ExtensionsTest/TableViewTest/TableViewTestScene.cpp \
 Classes/ExtensionsTest/TableViewTest/CustomTableViewCell.cpp \
+Classes/ExtensionsTest/Scale9SpriteTest/Scale9SpriteTest.cpp \
 Classes/FontTest/FontTest.cpp \
 Classes/IntervalTest/IntervalTest.cpp \
 Classes/KeypadTest/KeypadTest.cpp \
