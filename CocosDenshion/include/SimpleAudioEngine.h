3d23b65ad9767a8b38c2aad171433f0d498de3c0
rafael.x <cpp.development-at-rocketmail-dot-com>
2013-07-14 17:27:00 +0200

    Changed the order of function calls within CocosDenshion::SimpleAudioEngine::end() to first detach the OpenAL source from the buffers so that sound buffers can be successfully deleted afterwards.
    Additionally, improved the documentation within the header file a little bit.

--- CocosDenshion/include/SimpleAudioEngine.h
+++ CocosDenshion/include/SimpleAudioEngine.h
@@ -55,9 +55,9 @@ static inline unsigned int getHashCodeByString(const char *key)
 }
 
 /**
-@class          SimpleAudioEngine
-@brief          offer a VERY simple interface to play background music & sound effect
-*/
+  @class          SimpleAudioEngine
+  @brief          Offers a VERY simple interface to play background music & sound effects.
+ */
 
 class EXPORT_DLL SimpleAudioEngine : public TypeInfo
 {
@@ -70,108 +70,136 @@ public:
     }
 
     /**
-    @brief Get the shared Engine object,it will new one when first time be called
-    */
+    @brief Get the shared Engine object, it will new one when called the first time. 
+     */
     static SimpleAudioEngine* sharedEngine();
 
     /**
     @brief Release the shared Engine object
-    @warning It must be called before the application exit, or a memroy leak will be casued.
-    */
+    @warning It must be called before the application exit, or a memroy leak will be caused.
+     */
     static void end();
 
     /**
      @brief Preload background music
-     @param pszFilePath The path of the background music file,or the FileName of T_SoundResInfo
+     @param pszFilePath The path of the background music file, or the FileName of T_SoundResInfo
      */
     void preloadBackgroundMusic(const char* pszFilePath);
     
     /**
     @brief Play background music
-    @param pszFilePath The path of the background music file,or the FileName of T_SoundResInfo
-    @param bLoop Whether the background music loop or not
-    */
+    @param pszFilePath The path of the background music file, or the FileName of T_SoundResInfo
+    @param bLoop Determines whether the background music should loop or not
+     */
     void playBackgroundMusic(const char* pszFilePath, bool bLoop);
+
+    /**
+    @brief Play background music once
+    @param pszFilePath The path of the background music file, or the FileName of T_SoundResInfo
+    @see CocosDenshion::playBackgroundMusic(const char* pszFilePath, bool bLoop)
+     */
     void playBackgroundMusic(const char* pszFilePath) {
     	this->playBackgroundMusic(pszFilePath, false);
     }
 
     /**
     @brief Stop playing background music
-    @param bReleaseData If release the background music data or not.As default value is false
-    */
+    @param bReleaseData Determines whether the background music data should be released (true) or not (false). Default behaviour is to not release.
+     */
     void stopBackgroundMusic(bool bReleaseData);
+
+    /**
+    @brief Stop playing background music without releasing the background music data.
+    @see CocosDenshion::stopBackgroundMusic(bool bReleaseData)
+     */
     void stopBackgroundMusic() {
     	this->stopBackgroundMusic(false);
     }
 
     /**
     @brief Pause playing background music
-    */
+     */
     void pauseBackgroundMusic();
 
     /**
     @brief Resume playing background music
-    */
+     */
     void resumeBackgroundMusic();
 
     /**
     @brief Rewind playing background music
-    */
+     */
     void rewindBackgroundMusic();
 
+    /**
+     @brief Indicates whether any background music can be played or not.
+     @return <i>true</i> if background music can be played, otherwise <i>false</i>.
+     @remark Returns currently always true.
+     */
     bool willPlayBackgroundMusic();
 
     /**
-    @brief Whether the background music is playing
-    @return If is playing return true,or return false
+    @brief Indicates whether the background music is playing
+    @return <i>true</i> if the background music is playing, otherwise <i>false</i>
     */
     bool isBackgroundMusicPlaying();
 
+    // 
     // properties
+    //
+
     /**
-    @brief The volume of the background music max value is 1.0,the min value is 0.0
+    @brief The volume of the background music within the range of 0.0 as the minimum and 1.0 as the maximum.
     */
     float getBackgroundMusicVolume();
 
     /**
-    @brief set the volume of background music
-    @param volume must be in 0.0~1.0
+    @brief Set the volume of background music
+    @param volume must be within the range of 0.0 as the minimum and 1.0 as the maximum
     */
     void setBackgroundMusicVolume(float volume);
 
     /**
-    @brief The volume of the effects max value is 1.0,the min value is 0.0
+    @brief The volume of the effects within the range of 0.0 as the minimum and 1.0 as the maximum.
     */
     float getEffectsVolume();
 
     /**
-    @brief set the volume of sound effecs
-    @param volume must be in 0.0~1.0
+    @brief Set the volume of sound effects
+    @param volume must be within the range of 0.0 as the minimum and 1.0 as the maximum
     */
     void setEffectsVolume(float volume);
 
+    // 
     // for sound effects
+    //
+
     /**
     @brief Play sound effect
-    @param pszFilePath The path of the effect file,or the FileName of T_SoundResInfo
-    @param bLoop Whether to loop the effect playing, default value is false
+    @param pszFilePath The path of the effect file, or the FileName of T_SoundResInfo
+    @param bLoop Determines whether to loop the effect playing or not. The default value is false.
     */
     unsigned int playEffect(const char* pszFilePath, bool bLoop) {
         return this->playEffect(pszFilePath, bLoop, 1.0, 0.0, 1.0);
     }
 
+    /**
+    @brief Play sound effect once
+    @param pszFilePath The path of the effect file, or the FileName of T_SoundResInfo
+    @see CocosDenshion::playEffect(const char* pszFilePath, bool bLoop)
+    */
     unsigned int playEffect(const char* pszFilePath) {
         return this->playEffect(pszFilePath, false);
     }
 
     /**
-    @brief Play sound effect  with a file path, pitch, pan and gain
-    @param pszFilePath The path of the effect file,or the FileName of T_SoundResInfo
-    @param bLoop Whether to loop the effect playing, default value is false
+    @brief Play sound effect with a file path, pitch, pan and gain
+    @param pszFilePath The path of the effect file, or the FileName of T_SoundResInfo
+    @param bLoop Determines whether to loop the effect playing or not. The default value is false.
     @param pitch Frequency, normal value is 1.0. Will also change effect play time.
-    @param pan   Stereo effect, in range [-1..1] where -1 enables only left channel.
-    @param gain  Volume, normal value is 1. Works for range [0..1].
+    @param pan   Stereo effect, in the range of [-1..1] where -1 enables only left channel.
+    @param gain  Volume, in the range of [0..1]. The normal value is 1.
+    @return the OpenAL source id
 
     @note Full support is under development, now there are limitations:
         - no pitch effect on Samsung Galaxy S2 with OpenSL backend enabled;
@@ -217,14 +245,14 @@ public:
 
     /**
     @brief          preload a compressed audio file
-    @details        the compressed audio will be decode to wave, then write into an 
-    internal buffer in SimpleaudioEngine
+    @details        the compressed audio will be decoded to wave, then written into an internal buffer in SimpleAudioEngine
+    @param pszFilePath The path of the effect file, or the FileName of T_SoundResInfo
     */
     void preloadEffect(const char* pszFilePath);
 
     /**
     @brief          unload the preloaded effect from internal buffer
-    @param[in]        pszFilePath        The path of the effect file,or the FileName of T_SoundResInfo
+    @param pszFilePath        The path of the effect file, or the FileName of T_SoundResInfo
     */
     void unloadEffect(const char* pszFilePath);
 };
