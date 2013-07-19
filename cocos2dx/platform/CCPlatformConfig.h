25825824866820aabc70a2d19fd85d5a4c5bad68
Thomas Perl <thomas.perl@jollamobile.com>
2013-07-19 01:29:04 +0000

    Add new platform: CC_PLATFORM_QT5

--- cocos2dx/platform/CCPlatformConfig.h
+++ cocos2dx/platform/CCPlatformConfig.h
@@ -46,6 +46,7 @@ Config of cocos2d-x project, per target platform.
 #define CC_PLATFORM_NACL               9
 #define CC_PLATFORM_EMSCRIPTEN        10
 #define CC_PLATFORM_TIZEN             11
+#define CC_PLATFORM_QT5               12
 
 // Determine target platform by compile environment macro.
 #define CC_TARGET_PLATFORM             CC_PLATFORM_UNKNOWN
@@ -114,6 +115,12 @@ Config of cocos2d-x project, per target platform.
 #if defined(TIZEN)
     #undef  CC_TARGET_PLATFORM
     #define CC_TARGET_PLATFORM     CC_PLATFORM_TIZEN
+#endif
+
+// qt5
+#if defined(CC_TARGET_QT5)
+    #undef  CC_TARGET_PLATFORM
+    #define CC_TARGET_PLATFORM     CC_PLATFORM_QT5
 #endif
 
 //////////////////////////////////////////////////////////////////////////
--- cocos2dx/proj.qt5/common.pri
+++ cocos2dx/proj.qt5/common.pri
@@ -1,4 +1,11 @@
 
+DEFINES += CC_TARGET_QT5
+
+CONFIG += silent
+
+QMAKE_CXXFLAGS += -Wno-ignored-qualifiers -Wno-unused-parameter -Wno-psabi
+QMAKE_CFLAGS += -Wno-ignored-qualifiers -Wno-unused-parameter -Wno-psabi
+
 OS_TYPE = linux
 
 CONFIG(debug, debug|release) {
