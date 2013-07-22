137c075515ec7092dea6a149e2b101e17008dcd4
Thomas Perl <thomas.perl@jollamobile.com>
2013-07-22 17:58:48 +0000

    Platform abstraction implementation with Qt 5
    Based partially on the Linux platform abstraction, therefore
    this right now works only with Qt 5 on Linux.

--- cocos2dx-qt5.pro
+++ cocos2dx-qt5.pro
@@ -3,6 +3,11 @@ TEMPLATE = subdirs
 
 CONFIG += ordered
 
+# Cocos2D-X Core Library
 SUBDIRS += external/chipmunk/proj.qt5/chipmunk.pro
 SUBDIRS += external/Box2D/proj.qt5/box2d.pro
+SUBDIRS += cocos2dx/proj.qt5/cocos2dx.pro
+
+# Examples
+SUBDIRS += samples/Cpp/HelloCpp/proj.qt5/HelloCpp.pro
 
---
+++ cocos2dx/platform/qt5/CCApplication.h
@@ -0,0 +1,90 @@
+/**
+ *
+ * Cocos2D-X Qt 5 Platform
+ *
+ * Copyright (C) 2013 Jolla Ltd.
+ * Contact: Thomas Perl <thomas.perl@jollamobile.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **/
+
+
+#ifndef CCAPPLICATION_QT5_H
+#define CCAPPLICATION_QT5_H
+
+#include "platform/CCCommon.h"
+#include "platform/CCApplicationProtocol.h"
+#include <string>
+
+#include <QGuiApplication>
+
+NS_CC_BEGIN
+
+class CCApplication : public CCApplicationProtocol {
+    public:
+        CCApplication();
+        virtual ~CCApplication();
+
+        /**
+          @brief	Callback by CCDirector for limit FPS.
+          @interval       The time, which expressed in second in second, between current frame and next.
+          */
+        void setAnimationInterval(double interval);
+
+        /**
+          @brief	Run the message loop.
+          */
+        int run();
+
+        /**
+          @brief	Get current applicaiton instance.
+          @return Current application instance pointer.
+          */
+        static CCApplication* sharedApplication();
+
+        /* override functions */
+        virtual ccLanguageType getCurrentLanguage();
+
+        /**
+         *  Sets the Resource root path.
+         *  @deprecated Please use CCFileUtils::sharedFileUtils()->setSearchPaths() instead.
+         */
+        CC_DEPRECATED_ATTRIBUTE void setResourceRootPath(const std::string &rootResDir);
+
+        /**
+         *  Gets the Resource root path.
+         *  @deprecated Please use CCFileUtils::sharedFileUtils()->getSearchPaths() instead.
+         */
+        CC_DEPRECATED_ATTRIBUTE const std::string &getResourceRootPath();
+
+        /**
+          @brief Get target platform
+          */
+        virtual TargetPlatform getTargetPlatform();
+
+    protected:
+        QGuiApplication *m_application;
+        long m_animationInterval;
+        std::string m_resourceRootPath;
+};
+
+NS_CC_END
+
+#endif /* CCAPPLICATION_QT5_H */
