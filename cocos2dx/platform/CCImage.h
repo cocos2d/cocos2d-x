563608ce64ead3cbaa59e1c9996d3652b3f843d0
minggo <ming@cocos2d-x.org>
2013-07-26 18:07:40 +0800

    issue #2430:fix some errors

--- cocos2dx/platform/CCImage.h
+++ cocos2dx/platform/CCImage.h
@@ -73,9 +73,9 @@ public:
         TOP           = 0x13, ///< Horizontal center and vertical top.
         TOP_RIGHT     = 0x12, ///< Horizontal right and vertical top.
         RIGHT         = 0x32, ///< Horizontal right and vertical center.
-        BOTTOEM_RIGHT = 0x22, ///< Horizontal right and vertical bottom.
+        BOTTOM_RIGHT = 0x22, ///< Horizontal right and vertical bottom.
         BOTTOM        = 0x23, ///< Horizontal center and vertical bottom.
-        BOTTOEM_LEFT  = 0x21, ///< Horizontal left and vertical bottom.
+        BOTTOM_LEFT  = 0x21, ///< Horizontal left and vertical bottom.
         LEFT          = 0x31, ///< Horizontal left and vertical center.
         TOP_LEFT      = 0x11, ///< Horizontal left and vertical top.
     };
