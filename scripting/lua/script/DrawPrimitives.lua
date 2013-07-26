817316d6f51ebd045a75a8f659249d62ea2aa6c2
samuele3hu <qingkui.hu@cocos2d-x.org>
2013-07-26 14:10:45 +0800

    issue #2404:replace some deprecated enums using in the lua

--- scripting/lua/script/DrawPrimitives.lua
+++ scripting/lua/script/DrawPrimitives.lua
@@ -5,13 +5,13 @@ local dp_color = { 1.0, 1.0, 1.0, 1.0 }
 local dp_pointSizeLocation = -1
 local dp_pointSize = 1.0
 
-local GLProgram::SHADER_NAME_POSITION_U_COLOR = "ShaderPosition_uColor"
+local SHADER_NAME_POSITION_U_COLOR = "ShaderPosition_uColor"
 
 local targetPlatform = CCApplication:getInstance():getTargetPlatform()
 
 local function lazy_init()
     if not dp_initialized then
-        dp_shader = CCShaderCache:getInstance():getProgram(GLProgram::SHADER_NAME_POSITION_U_COLOR)
+        dp_shader = CCShaderCache:getInstance():getProgram(SHADER_NAME_POSITION_U_COLOR)
         --dp_shader:retain()
         if nil ~= dp_shader then
             dp_colorLocation = gl.getUniformLocation( dp_shader:getProgram(), "u_color")
