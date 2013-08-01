1553e59f127ed4a543988a8a6f250f15e385fef0
Timothy Qiu <timothyqiu32@gmail.com>
2013-08-01 16:57:42 +0800

    Surround code in document with code/endcode tags.

--- cocos2dx/actions/CCActionTween.h
+++ cocos2dx/actions/CCActionTween.h
@@ -46,16 +46,18 @@ public:
  ActionTween is an action that lets you update any property of an object.
  For example, if you want to modify the "width" property of a target from 200 to 300 in 2 seconds, then:
 
-    auto modifyWidth = ActionTween::create(2, "width", 200, 300);
-    target->runAction(modifyWidth);
-
+ @code
+     auto modifyWidth = ActionTween::create(2, "width", 200, 300);
+     target->runAction(modifyWidth);
+ @endcode
 
  Another example: ScaleTo action could be rewritten using PropertyAction:
 
-    // scaleA and scaleB are equivalents
-    auto scaleA = ScaleTo::create(2, 3);                 // (duration, to)
-    auto scaleB = ActionTween::create(2, "scale", 1, 3); // (duration, key, from, to)
-
+ @code
+     // scaleA and scaleB are equivalents
+     auto scaleA = ScaleTo::create(2, 3);                 // (duration, to)
+     auto scaleB = ActionTween::create(2, "scale", 1, 3); // (duration, key, from, to)
+ @endcode
 
  @since v0.99.2
  */
