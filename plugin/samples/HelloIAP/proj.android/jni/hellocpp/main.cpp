f5ea25777733659f740d344bce58ca465d249e85
James Chen <jianhua.chen@cocos2d-x.org>
2013-08-21 11:45:21 +0800

    [2.1.5] Re-order of recovering GL status in jni/xxx/main.cpp.

--- plugin/samples/HelloIAP/proj.android/jni/hellocpp/main.cpp
+++ plugin/samples/HelloIAP/proj.android/jni/hellocpp/main.cpp
@@ -57,10 +57,9 @@ void Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit(JNIEnv*  env, jobject thi
     }
     else
     {
-        ccDrawInit();
         ccGLInvalidateStateCache();
-        
         CCShaderCache::sharedShaderCache()->reloadDefaultShaders();
+        ccDrawInit();
         CCTextureCache::reloadAllTextures();
         CCNotificationCenter::sharedNotificationCenter()->postNotification(EVENT_COME_TO_FOREGROUND, NULL);
         CCDirector::sharedDirector()->setGLDefaultValues(); 
