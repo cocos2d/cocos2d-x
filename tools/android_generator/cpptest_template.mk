da566a3c16e93801b6cb05ef0050c198d8ebffac
boyu0 <boyuzero@gmail.com>
2013-08-22 11:49:34 +0800

    issue #2434: add android_generator and generate cocos2dx, extensions and TestCpp's android.mk files add PathUtils.py for python library.

---
+++ tools/android_generator/cpptest_template.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_testcpp_common
+
+LOCAL_MODULE_FILENAME := libtestcppcommon
+
+dirs := $(shell find $(LOCAL_PATH)/Classes -type d -print)
+find_files = $(subst $(LOCAL_PATH)/,,$(wildcard $(dir)/*.cpp)) 
+
+LOCAL_SRC_FILES := %s
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+            
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,CocosDenshion/android)
+$(call import-module,external/Box2D)
+$(call import-module,external/chipmunk)
+$(call import-module,cocos2dx)
+$(call import-module,extensions)
---
+++ tools/android_generator/extensions_template.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := cocos_extension_static
+
+LOCAL_MODULE_FILENAME := libextension
+
+dirs := $(shell find $(LOCAL_PATH) -type d -print)
+find_files = $(subst $(LOCAL_PATH)/,,$(wildcard $(dir)/*.cpp)) 
+
+LOCAL_SRC_FILES :=%s
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_curl_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
+LOCAL_WHOLE_STATIC_LIBRARIES += libwebsockets_static
+
+LOCAL_CXXFLAGS += -fexceptions
+
+LOCAL_CFLAGS += -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+LOCAL_EXPORT_CFLAGS += -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) \
+                           $(LOCAL_PATH)/CCBReader \
+                           $(LOCAL_PATH)/GUI/CCControlExtension \
+                           $(LOCAL_PATH)/GUI/CCScrollView \
+                           $(LOCAL_PATH)/network \
+                           $(LOCAL_PATH)/LocalStorage 
+                    
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,cocos2dx)
+$(call import-module,CocosDenshion/android)
+$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
+$(call import-module,external/Box2D)
+$(call import-module,external/chipmunk)
+$(call import-module,external/libwebsockets/android)
