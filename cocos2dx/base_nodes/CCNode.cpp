57da7bcea7d6dc4717424657254608335125a2af
Ricardo Quesada <ricardoquesada@gmail.com>
2013-08-25 10:50:29 -0700

    Adds missing include

--- cocos2dx/base_nodes/CCNode.cpp
+++ cocos2dx/base_nodes/CCNode.cpp
@@ -24,8 +24,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-#include "cocoa/CCString.h"
+
 #include "CCNode.h"
+
+#include <algorithm>
+
+#include "cocoa/CCString.h"
 #include "support/TransformUtils.h"
 #include "CCCamera.h"
 #include "effects/CCGrid.h"
