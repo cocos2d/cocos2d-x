b6a90b81aaef39bd983d9a56e34ccbfb258f7e14
Ricardo Quesada <ricardoquesada@gmail.com>
2013-08-25 10:50:29 -0700

    Adds missing include

--- cocos2dx/sprite_nodes/CCSpriteBatchNode.cpp
+++ cocos2dx/sprite_nodes/CCSpriteBatchNode.cpp
@@ -24,7 +24,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
+
 #include "CCSpriteBatchNode.h"
+
+#include <algorithm>
+
 #include "ccConfig.h"
 #include "CCSprite.h"
 #include "effects/CCGrid.h"
