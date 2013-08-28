498d8d9e6d14cd490351a05988514ad3e20411b7
shinriyo <shinriyo@gmail.com>
2013-08-28 11:53:18 +0900

    Python2.x 3.x runnable
    For Python2.x 3.x runnable, I used print()

--- tools/project_creator/create_project.py
+++ tools/project_creator/create_project.py
@@ -72,8 +72,8 @@ def checkParams():
         context["src_package_name"] = "org.cocos2dx.hellojavascript"
         context["src_project_path"] = os.path.join(template_dir, "multi-platform-js")
     else:
-        print "Your language parameter doesn\'t exist." \
-            "Check correct language option\'s parameter"
+        print ("Your language parameter doesn\'t exist." \
+            "Check correct language option\'s parameter")
         sys.exit()
     platforms_list = PLATFORMS.get(context["language"], [])
     return context, platforms_list
