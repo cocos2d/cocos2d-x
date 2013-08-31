8dfaf21154b1a948755d8120b45a56b4a2226b9c
Ricardo Quesada <ricardoquesada@gmail.com>
2013-08-31 15:18:28 -0700

    objectComparisonLess -> nodeComparisonLess
    and unified it.
    No need to have 3 different copies.
    The only copy is in CCNode.cpp

--- cocos2dx/sprite_nodes/CCSpriteBatchNode.cpp
+++ cocos2dx/sprite_nodes/CCSpriteBatchNode.cpp
@@ -245,36 +245,12 @@ void SpriteBatchNode::removeAllChildrenWithCleanup(bool bCleanup)
     _textureAtlas->removeAllQuads();
 }
 
-#if CC_USE_ARRAY_VECTOR
-static bool objectComparisonLess(const RCPtr<Object>& pp1, const RCPtr<Object>& pp2)
-{
-    Object *p1 = static_cast<Object*>(pp1);
-    Object *p2 = static_cast<Object*>(pp2);
-    Node *n1 = static_cast<Node*>(p1);
-    Node *n2 = static_cast<Node*>(p2);
-
-    return( n1->getZOrder() < n2->getZOrder() ||
-           ( n1->getZOrder() == n2->getZOrder() && n1->getOrderOfArrival() < n2->getOrderOfArrival() )
-           );
-}
-#else
-static bool objectComparisonLess(Object* p1, Object* p2)
-{
-    Node *n1 = static_cast<Node*>(p1);
-    Node *n2 = static_cast<Node*>(p2);
-
-    return( n1->getZOrder() < n2->getZOrder() ||
-           ( n1->getZOrder() == n2->getZOrder() && n1->getOrderOfArrival() < n2->getOrderOfArrival() )
-           );
-}
-#endif
-
 //override sortAllChildren
 void SpriteBatchNode::sortAllChildren()
 {
     if (_reorderChildDirty)
     {
-#if 0
+#if 1
         int i = 0,j = 0,length = _children->count();
 
         // insertion sort
@@ -297,7 +273,7 @@ void SpriteBatchNode::sortAllChildren()
             _children->fastSetObject(tempI, j+1);
         }
 #else
-        std::sort(std::begin(*_children), std::end(*_children), objectComparisonLess);
+        std::sort(std::begin(*_children), std::end(*_children), nodeComparisonLess);
 #endif
 
         //sorted now check all children
