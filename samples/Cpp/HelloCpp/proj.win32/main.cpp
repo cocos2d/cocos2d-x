ba9fdc577483fac05c4bf5cfeccd8a3f70050604
James Chen <jianhua.chen@cocos2d-x.org>
2013-09-08 11:30:50 +0800

    Reverts samples/Cpp/HelloCpp/proj.win32/main.cpp

--- samples/Cpp/HelloCpp/proj.win32/main.cpp
+++ samples/Cpp/HelloCpp/proj.win32/main.cpp
@@ -16,6 +16,5 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     AppDelegate app;
     EGLView* eglView = new EGLView();
     eglView->init("TestCPP",900,640);
-	eglView->setFrameZoomFactor(0.5f);
     return Application::getInstance()->run();
 }
