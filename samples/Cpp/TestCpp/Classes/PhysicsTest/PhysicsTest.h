b25d04813a172053747dc466d5ff28e140b43aed
boyu0 <boyuzero@gmail.com>
2013-09-10 17:41:06 +0800

    issue #2771:  Add PhysicsTest to TestCpp. Add initTest to TestScene, move Scene::init() from constructor to TestScene::initTest()

---
+++ samples/Cpp/TestCpp/Classes/PhysicsTest/PhysicsTest.h
@@ -0,0 +1,29 @@
+#ifndef _PHYSICS_TEST_H_
+#define _PHYSICS_TEST_H_
+
+#include "cocos2d.h"
+#include "../testBasic.h"
+
+class PhysicsTestLayer : public Layer
+{
+    Texture2D* _spriteTexture;    // weak ref
+    
+public:
+    PhysicsTestLayer();
+    ~PhysicsTestLayer();
+    
+    void initPhysics();
+    void createResetButton();
+    
+    void addNewSpriteAtPosition(Point p);
+    virtual void ccTouchesEnded(Set* touches, Event* event);
+} ;
+
+class PhysicsTestScene : public TestScene
+{
+public:
+    virtual bool initTest() override;
+    virtual void runThisTest();
+};
+
+#endif
