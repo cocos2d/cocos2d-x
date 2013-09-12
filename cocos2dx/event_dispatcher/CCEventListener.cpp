1c7287dd59f28efe34551900231abb0c2e02eff8
James Chen <jianhua.chen@cocos2d-x.org>
2013-09-12 16:01:14 +0800

    [Dispatcher] Updating license of CCEventListener.cpp.

--- cocos2dx/event_dispatcher/CCEventListener.cpp
+++ cocos2dx/event_dispatcher/CCEventListener.cpp
@@ -1,10 +1,26 @@
-//
-//  CCEventListener.cpp
-//  cocos2d_libs
-//
-//  Created by James Chen on 9/11/13.
-//
-//
+/****************************************************************************
+ Copyright (c) 2013 cocos2d-x.org
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 
 #include "CCEventListener.h"
 #include "platform/CCCommon.h"
@@ -24,7 +40,7 @@ EventListener::EventListener(const std::string& t, std::function<void(Event*)>ca
     
 EventListener::~EventListener() 
 {
-	CCLOG("In the destructor of EventListener.");
+	CCLOGINFO("In the destructor of EventListener. %p", this);
 }
 
 bool EventListener::checkAvaiable()
