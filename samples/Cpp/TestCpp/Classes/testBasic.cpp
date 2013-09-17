513237d5f60445689e10288b94a3e30d3d7e19a0
James Chen <jianhua.chen@cocos2d-x.org>
2013-09-17 23:03:33 +0800

    Fixing armature test crash.

--- samples/Cpp/TestCpp/Classes/testBasic.cpp
+++ samples/Cpp/TestCpp/Classes/testBasic.cpp
@@ -1,5 +1,6 @@
 #include "testBasic.h"
 #include "controller.h"
+#include "cocos-ext.h"
 
 TestScene::TestScene(bool bPortrait)
 {
@@ -45,6 +46,8 @@ void TestScene::onEnter()
             Director::getInstance()->replaceScene(scene);
             scene->release();
         }
+        
+        cocos2d::extension::armature::ArmatureDataManager::destoryInstance();
 	});
 
     auto menu =Menu::create(menuItem, NULL);
