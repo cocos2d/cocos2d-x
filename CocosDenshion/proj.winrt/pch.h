f9a0ed9638001114a4a39a23eefb626d9fb8a665
Dale Stammen <stammen@gmail.com>
2013-09-18 19:45:49 -0700

    added winrt project

---
+++ CocosDenshion/proj.winrt/pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <wrl.h>
+#include <d3d11_1.h>
+#include <agile.h>
+#include <DirectXMath.h>
+#include <memory>
+
+#define XAUDIO2_HELPER_FUNCTIONS 1
+#include <xaudio2.h>
+#include <xaudio2fx.h>
+#include <mmreg.h>
+#include <mfidl.h>
+#include <mfapi.h>
+#include <mfreadwrite.h>
+#include <mfmediaengine.h>
