5ea5c394e75477c750ce48ab9dc822f8b9c87671
Dhilan007 <wenhai.lin@chukong-inc.com>
2013-10-10 17:17:10 +0800

    fix compile fail on vs2012.

--- samples/Cpp/TestCpp/Classes/PerformanceTest/PerformanceSpriteTest.h
+++ samples/Cpp/TestCpp/Classes/PerformanceTest/PerformanceSpriteTest.h
@@ -70,11 +70,11 @@ protected:
     int            subtestNumber;
     std::vector<float> _vecFPS;
     int            _executeTimes;
-    const  int MAX_AUTO_TEST_TIMES  = 25;
-    const  int MAX_SPRITE_TEST_CASE = 7;
-    const  int MAX_SUB_TEST_NUMS    = 9;
-    const  int AUTO_TEST_NODE_NUM1  = 500;
-    const  int AUTO_TEST_NODE_NUM2  = 1500;
+    static const  int MAX_AUTO_TEST_TIMES  = 25;
+    static const  int MAX_SPRITE_TEST_CASE = 7;
+    static const  int MAX_SUB_TEST_NUMS    = 9;
+    static const  int AUTO_TEST_NODE_NUM1  = 500;
+    static const  int AUTO_TEST_NODE_NUM2  = 1500;
 };
 
 class SpritePerformTest1 : public SpriteMainScene
