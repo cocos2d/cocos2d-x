54ac8f3537bdbbf9c421c94e6bcad50e8a4a770d
garfield_ho <garfield_ho@qq.com>
2013-10-19 16:44:14 +0800

    Add cc.RESOLUTION_POLICY to jsb_cocos2d.js,so it can use setDesignResolutionSize in cc.EGLView

--- cocos/scripting/javascript/script/jsb_cocos2d.js
+++ cocos/scripting/javascript/script/jsb_cocos2d.js
@@ -4,6 +4,30 @@
 
 var cc = cc || {};
 
+cc.RESOLUTION_POLICY = {
+    // The entire application is visible in the specified area without trying to preserve the original aspect ratio.
+    // Distortion can occur, and the application may appear stretched or compressed.
+EXACTFIT:0,
+    // The entire application fills the specified area, without distortion but possibly with some cropping,
+    // while maintaining the original aspect ratio of the application.
+NOBORDER:1,
+    // The entire application is visible in the specified area without distortion while maintaining the original
+    // aspect ratio of the application. Borders can appear on two sides of the application.
+SHOW_ALL:2,
+    // The application takes the height of the design resolution size and modifies the width of the internal
+    // canvas so that it fits the aspect ratio of the device
+    // no distortion will occur however you must make sure your application works on different
+    // aspect ratios
+HEIGHT:3,
+    // The application takes the width of the design resolution size and modifies the height of the internal
+    // canvas so that it fits the aspect ratio of the device
+    // no distortion will occur however you must make sure your application works on different
+    // aspect ratios
+WIDTH:4,
+    
+UNKNOWN:5
+};
+
 cc.LANGUAGE_ENGLISH    = 0;
 cc.LANGUAGE_CHINESE    = 1;
 cc.LANGUAGE_FRENCH     = 2;
