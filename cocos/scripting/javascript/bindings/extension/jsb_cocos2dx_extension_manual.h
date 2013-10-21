fc162db63671c1417b4aa40dbaa4465ff2ac6fae
Dhilan007 <wenhai.lin@chukong-inc.com>
2013-10-21 22:21:11 +0800

    issues #2905:add new vs project

---
+++ cocos/scripting/javascript/bindings/extension/jsb_cocos2dx_extension_manual.h
@@ -0,0 +1,16 @@
+//
+//  jsb_cocos2d_extension_manual.h
+//
+//  Created by James Chen on 3/11/13.
+//
+//
+
+#ifndef __jsb_cocos2dx_extension_manual__
+#define __jsb_cocos2dx_extension_manual__
+
+#include "jsapi.h"
+#include "jsfriendapi.h"
+
+void register_all_cocos2dx_extension_manual(JSContext* cx, JSObject* global);
+
+#endif /* defined(__jsb_cocos2dx_extension_manual__) */
