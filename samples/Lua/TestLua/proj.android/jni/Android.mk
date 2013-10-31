94c4a02ba6d6afdb270ef42ac0a3ccff53b1f07c
samuelhu <qingkui.hu@cocos2d-x.org>
2013-10-31 21:52:28 +0800

    issue #3037:Add assetsmananger lua binding and a releated test sample

--- samples/Lua/TestLua/proj.android/jni/Android.mk
+++ samples/Lua/TestLua/proj.android/jni/Android.mk
@@ -7,10 +7,14 @@ LOCAL_MODULE := testlua_shared
 LOCAL_MODULE_FILENAME := libtestlua
 
 LOCAL_SRC_FILES := testlua/main.cpp \
-                   ../../Classes/AppDelegate.cpp
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/lua_assetsmanager_test_sample.cpp
 
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+                    $(LOCAL_PATH)/../../../../external/lua/tolua \
+                    $(LOCAL_PATH)/../../../../cocos/scripting/lua/bindings
+
 
 LOCAL_STATIC_LIBRARIES := curl_static_prebuilt
 
