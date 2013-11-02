f97e8bc595cd7bb9f3f9a933a37cd52912361f6f
James Chen <jianhua.chen@cocos2d-x.org>
2013-11-02 22:08:08 +0800

    closed #3106: fixed: EventListeners can't be removed sometimes.

--- cocos/2d/CCEventDispatcher.cpp
+++ cocos/2d/CCEventDispatcher.cpp
@@ -442,11 +442,15 @@ void EventDispatcher::removeEventListener(EventListener* listener)
             break;
     }
 
-    if (isFound) {
+    if (isFound)
+    {
         CC_SAFE_RELEASE(listener);
-    } else {
-        for(auto iter=_toAddedListeners.begin(); iter != _toAddedListeners.end(); ++iter) {
-            if ((*iter)->listener == listener)
+    }
+    else
+    {
+        for(auto iter = _toAddedListeners.begin(); iter != _toAddedListeners.end(); ++iter)
+        {
+            if (*iter == listener)
             {
                 _toAddedListeners.erase(iter);
                 break;
@@ -1046,6 +1050,18 @@ void EventDispatcher::removeEventListenersForListenerID(EventListener::ListenerI
             delete listeners;
             _listeners.erase(listenerItemIter);
             _priorityDirtyFlagMap.erase(listenerID);
+        }
+    }
+    
+    for(auto iter = _toAddedListeners.begin(); iter != _toAddedListeners.end();)
+    {
+        if ((*iter)->getListenerID() == listenerID)
+        {
+            iter = _toAddedListeners.erase(iter);
+        }
+        else
+        {
+            ++iter;
         }
     }
 }
