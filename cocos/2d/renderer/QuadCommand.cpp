390d681a36e25f5336dd42c83f5ef0b296629ea8
Nite Luo <darkdukey@gmail.com>
2013-11-06 14:57:42 -0800

    Subclass QuadCommand

--- cocos/2d/renderer/QuadCommand.cpp
+++ cocos/2d/renderer/QuadCommand.cpp
@@ -4,3 +4,67 @@
 
 
 #include "QuadCommand.h"
+
+NS_CC_BEGIN
+
+QuadCommand::QuadCommand(int viewport, int32_t depth, GLuint textureID, GLuint shaderID, BlendFunc blendType, kmMat4 transform, V3F_C4B_T2F_Quad quad)
+:RenderCommand()
+,_viewport(viewport)
+,_depth(depth)
+,_textureID(textureID)
+,_shaderID(shaderID)
+,_blendType(blendType)
+,_quad(quad)
+{
+    _type = QUAD_COMMAND;
+    kmMat4Assign(&_transform, &transform);
+}
+
+int64_t QuadCommand::generateID()
+{
+    _id = 0;
+
+    //Generate Material ID
+    //TODO fix shader ID generation
+    CCASSERT(_shaderID < 64, "ShaderID is greater than 64");
+    //TODO fix texture ID generation
+    CCASSERT(_textureID < 1024, "TextureID is greater than 1024");
+
+    //TODO fix blend id generation
+    int blendID = 0;
+    if(_blendType == BlendFunc::DISABLE)
+    {
+        blendID = 0;
+    }
+    else if(_blendType == BlendFunc::ALPHA_PREMULTIPLIED)
+    {
+        blendID = 1;
+    }
+    else if(_blendType == BlendFunc::ALPHA_NON_PREMULTIPLIED)
+    {
+        blendID = 2;
+    }
+    else if(_blendType == BlendFunc::ADDITIVE)
+    {
+        blendID = 3;
+    }
+    else
+    {
+        blendID = 4;
+    }
+
+    _materialID = (int32_t)_shaderID << 28
+            | (int32_t)blendID << 24
+            | (int32_t)_textureID << 14;
+
+    //Generate RenderCommandID
+    _id = (int64_t)_viewport << 61
+            | (int64_t)1 << 60 //translucent
+            | (int64_t)0 << 59 //is command
+            | (int64_t)_depth << 35
+            | (int64_t)_materialID << 11;
+
+    return _id;
+}
+
+NS_CC_END
--- cocos/2d/renderer/RenderCommand.cpp
+++ cocos/2d/renderer/RenderCommand.cpp
@@ -8,59 +8,11 @@
 NS_CC_BEGIN
 
 RenderCommand::RenderCommand()
-:_viewport(0)
-,_isTranslucent(false)
-,_isCommand(false)
-,_depth(0)
-,_materialID(0)
 {
-
+    _id = 0;
+    _type = UNKNOWN_COMMAND;
 }
 
-void RenderCommand::generateID()
-{
-    _renderCommandId = 0;
-
-    //Generate Material ID
-    //TODO fix shader ID generation
-    CCASSERT(_shaderID < 64, "ShaderID is greater than 64");
-    //TODO fix texture ID generation
-    CCASSERT(_textureID < 1024, "TextureID is greater than 1024");
-
-    //TODO fix blend id generation
-    int blendID = 0;
-    if(_blendType == BlendFunc::DISABLE)
-    {
-        blendID = 0;
-    }
-    else if(_blendType == BlendFunc::ALPHA_PREMULTIPLIED)
-    {
-        blendID = 1;
-    }
-    else if(_blendType == BlendFunc::ALPHA_NON_PREMULTIPLIED)
-    {
-        blendID = 2;
-    }
-    else if(_blendType == BlendFunc::ADDITIVE)
-    {
-        blendID = 3;
-    }
-    else
-    {
-        blendID = 4;
-    }
-
-    _materialID = (int32_t)_shaderID << 28
-                | (int32_t)blendID << 24
-                | (int32_t)_textureID << 14;
-
-    //Generate RenderCommandID
-    _renderCommandId = (int64_t)_viewport << 61
-                    | (int64_t)_isTranslucent << 60
-                    | (int64_t)_isCommand << 59
-                    | (int64_t)_depth << 35
-                    | (int64_t)_materialID << 11;
-}
 
 void printBits(size_t const size, void const * const ptr)
 {
@@ -83,29 +35,8 @@ void printBits(size_t const size, void const * const ptr)
 void RenderCommand::printID()
 {
     printf("CommandID: ");
-    printBits(sizeof(_renderCommandId), &_renderCommandId);
+    printBits(sizeof(_id), &_id);
     printf("\n");
-}
-
-void RenderCommand::setKeyData(int viewport, bool isTranslucent, bool isCommand, int32_t depth)
-{
-    _viewport = viewport;
-    _isTranslucent = isTranslucent;
-    _isCommand = isCommand;
-    _depth = depth;
-}
-
-void RenderCommand::setMaterialData(GLuint textureID, GLuint shaderID, BlendFunc blendType)
-{
-    _textureID = textureID;
-    _shaderID = shaderID;
-    _blendType = blendType;
-}
-
-void RenderCommand::setQuadData(kmMat4 *transform, V3F_C4B_T2F_Quad quad)
-{
-    kmMat4Assign(&_transform, transform);
-    _quad = quad;
 }
 
 NS_CC_END
