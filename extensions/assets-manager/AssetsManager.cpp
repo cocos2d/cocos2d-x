2c76d8c1507b047eb033fae5f6b637403227718a
garfield_ho <garfield_ho@qq.com>
2013-11-08 11:23:06 +0800

    There are not directory entry in some case.So we need to test whether the file directory exists when uncompressing file entry, if does not exist then create directory

--- extensions/assets-manager/AssetsManager.cpp
+++ extensions/assets-manager/AssetsManager.cpp
@@ -308,6 +308,47 @@ bool AssetsManager::uncompress()
         }
         else
         {
+            //There are not directory entry in some case.
+            //So we need to test whether the file directory exists when uncompressing file entry
+            //, if does not exist then create directory
+            string fileNameStr(fileName);
+            
+            size_t startIndex=0;
+            
+            size_t index=fileNameStr.find("/",startIndex);
+            
+            while(index!=-1)
+            {
+                string dir=_storagePath+fileNameStr.substr(0,index);
+                
+                FILE *out = fopen(dir.c_str(), "r");
+                
+                if(!out)
+                {
+                    if (!createDirectory(dir.c_str()))
+                    {
+                        CCLOG("can not create directory %s", dir.c_str());
+                        unzClose(zipfile);
+                        return false;
+                    }
+                    else
+                    {
+                        CCLOG("create directory %s",dir.c_str());
+                    }
+                }
+                else
+                {
+                    fclose(out);
+                }
+                
+                startIndex=index+1;
+                
+                index=fileNameStr.find("/",startIndex);
+                
+            }
+            
+            
+            
             // Entry is a file, so extract it.
             
             // Open current file.
