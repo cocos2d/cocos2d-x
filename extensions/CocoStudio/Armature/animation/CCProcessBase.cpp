eb792bf5483434bb9b79980bef20560cbd4880dd
2youyou2 <501251991@qq.com>
2013-11-14 19:36:02 +0800

    fix calculate percent

--- extensions/CocoStudio/Armature/animation/CCProcessBase.cpp
+++ extensions/CocoStudio/Armature/animation/CCProcessBase.cpp
@@ -71,8 +71,6 @@ void CCProcessBase::stop()
 {
     m_bIsComplete = true;
     m_bIsPlaying = false;
-    m_fCurrentFrame = 0;
-    m_fCurrentPercent = 0;
 }
 
 void CCProcessBase::play(int durationTo, int durationTween,  int loop, int tweenEasing)
@@ -153,7 +151,7 @@ void CCProcessBase::gotoFrame(int frameIndex)
 
 int CCProcessBase::getCurrentFrameIndex()
 {
-    m_iCurFrameIndex = m_iRawDuration * m_fCurrentPercent;
+    m_iCurFrameIndex = (m_iRawDuration-1) * m_fCurrentPercent;
     return m_iCurFrameIndex;
 }
 
