fd3274d83a91041ccc5e672017d5af3c0b513d5a
yinkaile <501251991@qq.com>
2013-11-21 13:44:05 +0800

    fixed movement event

--- extensions/CocoStudio/Armature/animation/CCArmatureAnimation.cpp
+++ extensions/CocoStudio/Armature/animation/CCArmatureAnimation.cpp
@@ -333,14 +333,24 @@ void CCArmatureAnimation::update(float dt)
 
     while (m_sFrameEventQueue.size() > 0)
     {
-        CCFrameEvent *frameEvent = m_sFrameEventQueue.front();
+        CCFrameEvent *event = m_sFrameEventQueue.front();
         m_sFrameEventQueue.pop();
 
         m_bIgnoreFrameEvent = true;
-        (m_sFrameEventTarget->*m_sFrameEventCallFunc)(frameEvent->bone, frameEvent->frameEventName, frameEvent->originFrameIndex, frameEvent->currentFrameIndex);
+        (m_sFrameEventTarget->*m_sFrameEventCallFunc)(event->bone, event->frameEventName, event->originFrameIndex, event->currentFrameIndex);
         m_bIgnoreFrameEvent = false;
 
-        CC_SAFE_DELETE(frameEvent);
+        CC_SAFE_DELETE(event);
+    }
+
+    while (m_sMovementEventQueue.size() > 0)
+    {
+        CCMovementEvent *event = m_sMovementEventQueue.front();
+        m_sMovementEventQueue.pop();
+
+        (m_sMovementEventTarget->*m_sMovementEventCallFunc)(event->armature, event->movementType, event->movementID);
+
+        CC_SAFE_DELETE(event);
     }
 }
 
@@ -365,7 +375,7 @@ void CCArmatureAnimation::updateHandler()
 
                 if (m_sMovementEventTarget && m_sMovementEventCallFunc)
                 {
-                    (m_sMovementEventTarget->*m_sMovementEventCallFunc)(m_pArmature, START, m_strMovementID.c_str());
+                    movementEvent(m_pArmature, START, m_strMovementID.c_str());
                 }
 
                 break;
@@ -381,7 +391,7 @@ void CCArmatureAnimation::updateHandler()
 
             if (m_sMovementEventTarget && m_sMovementEventCallFunc)
             {
-                (m_sMovementEventTarget->*m_sMovementEventCallFunc)(m_pArmature, COMPLETE, m_strMovementID.c_str());
+                movementEvent(m_pArmature, COMPLETE, m_strMovementID.c_str());
             }
         }
         break;
@@ -394,7 +404,7 @@ void CCArmatureAnimation::updateHandler()
 
             if (m_sMovementEventTarget && m_sMovementEventCallFunc)
             {
-                (m_sMovementEventTarget->*m_sMovementEventCallFunc)(m_pArmature, START, m_strMovementID.c_str());
+                movementEvent(m_pArmature, START, m_strMovementID.c_str());
             }
         }
         break;
@@ -406,7 +416,7 @@ void CCArmatureAnimation::updateHandler()
 
             if (m_sMovementEventTarget && m_sMovementEventCallFunc)
             {
-                (m_sMovementEventTarget->*m_sMovementEventCallFunc)(m_pArmature, LOOP_COMPLETE, m_strMovementID.c_str());
+                movementEvent(m_pArmature, LOOP_COMPLETE, m_strMovementID.c_str());
             }
         }
         break;
@@ -460,4 +470,18 @@ void CCArmatureAnimation::frameEvent(CCBone *bone, const char *frameEventName, i
         m_sFrameEventQueue.push(frameEvent);
     }
 }
+
+void CCArmatureAnimation::movementEvent(CCArmature *armature, MovementEventType movementType, const char *movementID)
+{
+    if (m_sMovementEventTarget && m_sMovementEventCallFunc)
+    {
+        CCMovementEvent *movementEvent = new CCMovementEvent();
+        movementEvent->armature = armature;
+        movementEvent->movementType = movementType;
+        movementEvent->movementID = movementID;
+
+        m_sMovementEventQueue.push(movementEvent);
+    }
+}
+
 NS_CC_EXT_END
