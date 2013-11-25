da451e48c669e7015c5d144eb299bf655a946d42
boyu0 <boyuzero@gmail.com>
2013-11-25 14:50:11 +0800

    upgrade box2d from 2.2.1 to 2.3.0

--- external/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp
+++ external/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp
@@ -21,30 +21,29 @@
 #include <Box2D/Dynamics/b2Fixture.h>
 
 #include <new>
-using namespace std;
 
 b2Contact* b2EdgeAndPolygonContact::Create(b2Fixture* fixtureA, int32, b2Fixture* fixtureB, int32, b2BlockAllocator* allocator)
 {
-    void* mem = allocator->Allocate(sizeof(b2EdgeAndPolygonContact));
-    return new (mem) b2EdgeAndPolygonContact(fixtureA, fixtureB);
+	void* mem = allocator->Allocate(sizeof(b2EdgeAndPolygonContact));
+	return new (mem) b2EdgeAndPolygonContact(fixtureA, fixtureB);
 }
 
 void b2EdgeAndPolygonContact::Destroy(b2Contact* contact, b2BlockAllocator* allocator)
 {
-    ((b2EdgeAndPolygonContact*)contact)->~b2EdgeAndPolygonContact();
-    allocator->Free(contact, sizeof(b2EdgeAndPolygonContact));
+	((b2EdgeAndPolygonContact*)contact)->~b2EdgeAndPolygonContact();
+	allocator->Free(contact, sizeof(b2EdgeAndPolygonContact));
 }
 
 b2EdgeAndPolygonContact::b2EdgeAndPolygonContact(b2Fixture* fixtureA, b2Fixture* fixtureB)
 : b2Contact(fixtureA, 0, fixtureB, 0)
 {
-    b2Assert(m_fixtureA->GetType() == b2Shape::e_edge);
-    b2Assert(m_fixtureB->GetType() == b2Shape::e_polygon);
+	b2Assert(m_fixtureA->GetType() == b2Shape::e_edge);
+	b2Assert(m_fixtureB->GetType() == b2Shape::e_polygon);
 }
 
 void b2EdgeAndPolygonContact::Evaluate(b2Manifold* manifold, const b2Transform& xfA, const b2Transform& xfB)
 {
-    b2CollideEdgeAndPolygon(    manifold,
-                                (b2EdgeShape*)m_fixtureA->GetShape(), xfA,
-                                (b2PolygonShape*)m_fixtureB->GetShape(), xfB);
+	b2CollideEdgeAndPolygon(	manifold,
+								(b2EdgeShape*)m_fixtureA->GetShape(), xfA,
+								(b2PolygonShape*)m_fixtureB->GetShape(), xfB);
 }
