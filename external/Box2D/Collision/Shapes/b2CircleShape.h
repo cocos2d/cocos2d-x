e55c81c9b6b09d2cf2ec577fe7caf1d0716eea6d
boyu0 <boyuzero@gmail.com>
2013-11-25 14:50:11 +0800

    upgrade box2d from 2.2.1 to 2.3.0

--- external/Box2D/Collision/Shapes/b2CircleShape.h
+++ external/Box2D/Collision/Shapes/b2CircleShape.h
@@ -25,67 +25,67 @@
 class b2CircleShape : public b2Shape
 {
 public:
-    b2CircleShape();
+	b2CircleShape();
 
-    /// Implement b2Shape.
-    b2Shape* Clone(b2BlockAllocator* allocator) const;
+	/// Implement b2Shape.
+	b2Shape* Clone(b2BlockAllocator* allocator) const;
 
-    /// @see b2Shape::GetChildCount
-    int32 GetChildCount() const;
+	/// @see b2Shape::GetChildCount
+	int32 GetChildCount() const;
 
-    /// Implement b2Shape.
-    bool TestPoint(const b2Transform& transform, const b2Vec2& p) const;
+	/// Implement b2Shape.
+	bool TestPoint(const b2Transform& transform, const b2Vec2& p) const;
 
-    /// Implement b2Shape.
-    bool RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
-                const b2Transform& transform, int32 childIndex) const;
+	/// Implement b2Shape.
+	bool RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
+				const b2Transform& transform, int32 childIndex) const;
 
-    /// @see b2Shape::ComputeAABB
-    void ComputeAABB(b2AABB* aabb, const b2Transform& transform, int32 childIndex) const;
+	/// @see b2Shape::ComputeAABB
+	void ComputeAABB(b2AABB* aabb, const b2Transform& transform, int32 childIndex) const;
 
-    /// @see b2Shape::ComputeMass
-    void ComputeMass(b2MassData* massData, float32 density) const;
+	/// @see b2Shape::ComputeMass
+	void ComputeMass(b2MassData* massData, float32 density) const;
 
-    /// Get the supporting vertex index in the given direction.
-    int32 GetSupport(const b2Vec2& d) const;
+	/// Get the supporting vertex index in the given direction.
+	int32 GetSupport(const b2Vec2& d) const;
 
-    /// Get the supporting vertex in the given direction.
-    const b2Vec2& GetSupportVertex(const b2Vec2& d) const;
+	/// Get the supporting vertex in the given direction.
+	const b2Vec2& GetSupportVertex(const b2Vec2& d) const;
 
-    /// Get the vertex count.
-    int32 GetVertexCount() const { return 1; }
+	/// Get the vertex count.
+	int32 GetVertexCount() const { return 1; }
 
-    /// Get a vertex by index. Used by b2Distance.
-    const b2Vec2& GetVertex(int32 index) const;
+	/// Get a vertex by index. Used by b2Distance.
+	const b2Vec2& GetVertex(int32 index) const;
 
-    /// Position
-    b2Vec2 m_p;
+	/// Position
+	b2Vec2 m_p;
 };
 
 inline b2CircleShape::b2CircleShape()
 {
-    m_type = e_circle;
-    m_radius = 0.0f;
-    m_p.SetZero();
+	m_type = e_circle;
+	m_radius = 0.0f;
+	m_p.SetZero();
 }
 
 inline int32 b2CircleShape::GetSupport(const b2Vec2 &d) const
 {
-    B2_NOT_USED(d);
-    return 0;
+	B2_NOT_USED(d);
+	return 0;
 }
 
 inline const b2Vec2& b2CircleShape::GetSupportVertex(const b2Vec2 &d) const
 {
-    B2_NOT_USED(d);
-    return m_p;
+	B2_NOT_USED(d);
+	return m_p;
 }
 
 inline const b2Vec2& b2CircleShape::GetVertex(int32 index) const
 {
-    B2_NOT_USED(index);
-    b2Assert(index == 0);
-    return m_p;
+	B2_NOT_USED(index);
+	b2Assert(index == 0);
+	return m_p;
 }
 
 #endif
--- external/Box2D/Common/b2BlockAllocator.h
+++ external/Box2D/Common/b2BlockAllocator.h
@@ -35,28 +35,28 @@ struct b2Chunk;
 class b2BlockAllocator
 {
 public:
-    b2BlockAllocator();
-    ~b2BlockAllocator();
+	b2BlockAllocator();
+	~b2BlockAllocator();
 
-    /// Allocate memory. This will use b2Alloc if the size is larger than b2_maxBlockSize.
-    void* Allocate(int32 size);
+	/// Allocate memory. This will use b2Alloc if the size is larger than b2_maxBlockSize.
+	void* Allocate(int32 size);
 
-    /// Free memory. This will use b2Free if the size is larger than b2_maxBlockSize.
-    void Free(void* p, int32 size);
+	/// Free memory. This will use b2Free if the size is larger than b2_maxBlockSize.
+	void Free(void* p, int32 size);
 
-    void Clear();
+	void Clear();
 
 private:
 
-    b2Chunk* m_chunks;
-    int32 m_chunkCount;
-    int32 m_chunkSpace;
+	b2Chunk* m_chunks;
+	int32 m_chunkCount;
+	int32 m_chunkSpace;
 
-    b2Block* m_freeLists[b2_blockSizes];
+	b2Block* m_freeLists[b2_blockSizes];
 
-    static int32 s_blockSizes[b2_blockSizes];
-    static uint8 s_blockSizeLookup[b2_maxBlockSize + 1];
-    static bool s_blockSizeLookupInitialized;
+	static int32 s_blockSizes[b2_blockSizes];
+	static uint8 s_blockSizeLookup[b2_maxBlockSize + 1];
+	static bool s_blockSizeLookupInitialized;
 };
 
 #endif
