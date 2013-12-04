ae551ae294b709fbc528ba5d83165d399ba3467f
Nite Luo <darkdukey@gmail.com>
2013-12-04 11:54:57 -0800

    Convert RenderTexture to use RenderCommands

--- cocos/2d/renderer/CCNewRenderTexture.h
+++ cocos/2d/renderer/CCNewRenderTexture.h
@@ -14,11 +14,34 @@ NS_CC_BEGIN
 class NewRenderTexture : public RenderTexture
 {
 public:
+    static NewRenderTexture* create(int w, int h, Texture2D::PixelFormat eFormat, GLuint uDepthStencilFormat);
+    static NewRenderTexture* create(int w, int h, Texture2D::PixelFormat eFormat);
+    static NewRenderTexture* create(int w, int h);
+
+    void beginWithClear(float r, float g, float b, float a);
+    void beginWithClear(float r, float g, float b, float a, float depthValue);
+    void beginWithClear(float r, float g, float b, float a, float depthValue, int stencilValue);
+    void beginWithClear(float r, float g, float b, float a, float depthValue, int stencilValue, GLbitfield flags);
+
+    virtual void begin() override;
+    virtual void end() override;
+    virtual void draw() override;
+
+    void clearDepth(float depthValue);
 
 protected:
     NewRenderTexture();
+    virtual ~NewRenderTexture();
 
+    void onBegin();
+    void onEnd();
 
+    //Clear render buffer
+    void onClear();
+    void onClearDepth();
+
+    kmMat4 _oldTransMatrix, _oldProjMatrix;
+    kmMat4 _transformMatrix, _projectionMatrix;
 };
 
 NS_CC_END
