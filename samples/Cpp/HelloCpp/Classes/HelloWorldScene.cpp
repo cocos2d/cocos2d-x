8491778666f3bc628e345bd09fa1456727819f37
James Chen <jianhua.chen@cocos2d-x.org>
2013-12-04 17:51:53 +0800

    issue #2790: Reverts HelloWorldScene.cpp.

--- samples/Cpp/HelloCpp/Classes/HelloWorldScene.cpp
+++ samples/Cpp/HelloCpp/Classes/HelloWorldScene.cpp
@@ -2,9 +2,9 @@
 #include "AppMacros.h"
 
 #include "CCEventListenerTouch.h"
-#include "CCVector.h"
 
 USING_NS_CC;
+
 
 Scene* HelloWorld::scene()
 {
@@ -21,94 +21,9 @@ Scene* HelloWorld::scene()
     return scene;
 }
 
-void showSprites(const Vector<Sprite*>& sprites)
-{
-    log("container size = %ld", sprites.count());
-    for (auto& sp : sprites)
-    {
-        log("sp tag: %d, ref count = %d", sp->getTag(), sp->retainCount());
-    }
-}
-
-Vector<Sprite*> createAllSprites()
-{
-    Vector<Sprite*> ret;
-    ret.addObject(Sprite::create());
-    ret.addObject(Sprite::create());
-    return ret;
-}
-
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    
-    Vector<Sprite*> container;
-    
-    for (int i = 0; i < 10; ++i)
-    {
-        auto sp = Sprite::create();
-        sp->setTag(i);
-        container.addObject(sp);
-    }
-
-//
-//    showSprites(container);
-//    
-//    Vector<Sprite*> containerCopy = container;
-//    
-//    showSprites(containerCopy);
-//    
-//    containerCopy = container;
-//    
-//    showSprites(containerCopy);
-    
-//    Vector<Sprite*> moveVector(createAllSprites());
-//    showSprites(moveVector);
-//    
-//    CCLOG("-------------  2  ----------");
-//    moveVector = createAllSprites();
-//    showSprites(moveVector);
-//    
-//    CCLOG("-------------  3  ----------");
-//    Vector<Sprite*> aaa;
-//    aaa.addObject(Sprite::create());
-//    moveVector = aaa;
-//    showSprites(moveVector);
-    
-    log("size of Vector = %ld", sizeof(Vector<Sprite*>));
-    
-    Map<std::string, Sprite*> map;
-    map.setObject(Sprite::create(), "1");
-    map.setObject(Sprite::create(), "2");
-    map.setObject(Sprite::create(), "3");
-
-    auto showMap = [](const Map<std::string, Sprite*>& map)
-    {
-        for (auto iter = map.begin(); iter != map.end(); ++iter)
-        {
-            log("key = %s, value = %p, ref = %d", iter->first.c_str(), iter->second, iter->second ? iter->second->retainCount() : 0);
-        }
-    };
-    
-    showMap(map);
-    
-//    auto iter = std::find(map.begin(), map.end(), std::string("111"));
-//    if (iter != map.end())
-//    {
-//        log("found!");
-//    }
-//    else
-//    {
-//        log("not found!");
-//    }
-    
-//    map["111"];
-//    log("key[1]=%p", map["1"]);
-//    log("----------------------");
-//    map["11"]->setPosition(Point(100, 100));
-    
-    showMap(map);
-    
     //////////////////////////////
     // 1. super init first
     if ( !Layer::init() )
@@ -159,10 +74,6 @@ bool HelloWorld::init()
 
     // add the sprite as a child to this layer
     this->addChild(sprite);
-    
-    this->getChildren().forEach([](Node* node){
-        log("node = %p, name = %s", node, typeid(*node).name());
-    });
     
     return true;
 }
