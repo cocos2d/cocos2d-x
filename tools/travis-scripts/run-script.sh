7b24494558ce4cea5df944449a504720e17ee07d
James Chen <jianhua.chen@cocos2d-x.org>
2013-12-06 17:09:36 +0800

    issue #2790: A typo fix.

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -50,7 +50,7 @@ elif [ "$PLATFORM"x = "android"x ]; then
     # Build all samples
     echo "Building all samples ..."
     cd $COCOS2DX_ROOT/build
-    ./android-build.py -n "NDK_BUG=0 -j10" hellocpp testcpp simplegame testjavascipt
+    ./android-build.py -n "NDK_BUG=0 -j10" hellocpp testcpp simplegame testjavascript
 
     # Build template
     echo "Building template ..."
