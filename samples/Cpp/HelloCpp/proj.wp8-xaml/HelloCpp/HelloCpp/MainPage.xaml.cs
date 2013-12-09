fc0b21bcc372cfe1bfbcda748ed5baa1db5e9ff2
Dale Stammen <dalestam@microsoft.com>
2013-12-09 13:44:06 -0800

    updated HelloCpp wp8-xaml project

--- samples/Cpp/HelloCpp/proj.wp8-xaml/HelloCpp/HelloCpp/MainPage.xaml.cs
+++ samples/Cpp/HelloCpp/proj.wp8-xaml/HelloCpp/HelloCpp/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using PhoneDirect3DXamlAppComponent;
+using Windows.Graphics.Display;
 
 namespace PhoneDirect3DXamlAppInterop
 {
@@ -30,49 +31,19 @@ namespace PhoneDirect3DXamlAppInterop
             {
                 m_d3dBackground = new Direct3DBackground();
 
-                // Set window bounds in dips
-                // since this is a Landscape only app we need to swap width and height
-                m_d3dBackground.WindowBounds = new Windows.Foundation.Size(
-                    (float)Application.Current.Host.Content.ActualHeight,
-                    (float)Application.Current.Host.Content.ActualWidth
-                   );
-
                 // Set native resolution in pixels
-                m_d3dBackground.NativeResolution = new Windows.Foundation.Size(
+                m_d3dBackground.RenderResolution = new Windows.Foundation.Size(
                     (float)Math.Floor(Application.Current.Host.Content.ActualWidth * Application.Current.Host.Content.ScaleFactor / 100.0f + 0.5f),
                     (float)Math.Floor(Application.Current.Host.Content.ActualHeight * Application.Current.Host.Content.ScaleFactor / 100.0f + 0.5f)
                     );
 
-                // Set render resolution to the full native resolution
-                m_d3dBackground.RenderResolution = m_d3dBackground.NativeResolution;
-
                 // Hook-up native component to DrawingSurfaceBackgroundGrid
                 DrawingSurfaceBackground.SetBackgroundContentProvider(m_d3dBackground.CreateContentProvider());
                 DrawingSurfaceBackground.SetBackgroundManipulationHandler(m_d3dBackground);
+
+
+                m_d3dBackground.WindowOrientation = DisplayOrientations.Landscape;
             }
         }
-
-        private void PhoneApplicationPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
-        {
-            float width = (float)Application.Current.Host.Content.ActualWidth;
-            float heigth = (float)Application.Current.Host.Content.ActualHeight;
-
-
-            // Switch the placement of the buttons based on an orientation change.
-            if ((e.Orientation & PageOrientation.Portrait) == (PageOrientation.Portrait))
-            {
-                m_d3dBackground.WindowBounds = new Windows.Foundation.Size(width, heigth);
-
-            }
-            // If not in portrait, move buttonList content to visible row and column.
-            else
-            {
-                m_d3dBackground.WindowBounds = new Windows.Foundation.Size(heigth, width);
-
-            }
-
-            m_d3dBackground.OrientationChanged = true;
-        } 
-
     }
 }
