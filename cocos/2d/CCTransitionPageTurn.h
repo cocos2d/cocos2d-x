c766a82243a017c45e05229def475f3c103ac7f9
Huabing.Xu <dabingnn@gmail.com>
2013-12-12 13:44:29 +0800

    TransitionPageTurn can work now

--- cocos/2d/CCTransitionPageTurn.h
+++ cocos/2d/CCTransitionPageTurn.h
@@ -30,6 +30,8 @@ THE SOFTWARE.
 
 NS_CC_BEGIN
 
+class GridNode;
+
 /**
  * @addtogroup transition
  * @{
@@ -83,11 +85,14 @@ public:
     // Overrides
     //
     virtual void onEnter() override;
+    virtual void onExit() override;
 
 protected:
     virtual void sceneOrder() override;
 
 protected:
+    GridNode* _inSceneProxy;
+    GridNode* _outSceneProxy;
     bool    _back;    
     static float POLYGON_OFFSET_FACTOR;
     static float POLYGON_OFFSET_UNITS;
