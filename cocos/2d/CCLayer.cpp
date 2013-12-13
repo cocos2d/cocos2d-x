db053b60b961363a72b9df148b3b1c0d66222e42
James Chen <jianhua.chen@cocos2d-x.org>
2013-12-13 16:51:57 +0800

    Some fixes for XXX:getDescription.

--- cocos/2d/CCLayer.cpp
+++ cocos/2d/CCLayer.cpp
@@ -1073,7 +1073,7 @@ void LayerMultiplex::switchToAndReleaseMe(int n)
 
 std::string LayerMultiplex::getDescription() const
 {
-    return StringUtils::format("<LayerMultiplex | Tag = %d, Layers = %d", _tag, _children.size());
+    return StringUtils::format("<LayerMultiplex | Tag = %d, Layers = %zd", _tag, _children.size());
 }
 
 NS_CC_END
