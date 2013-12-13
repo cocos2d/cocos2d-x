fedf3ea4836aa2900bddbeb85f055031484076b3
boyu0 <boyuzero@gmail.com>
2013-12-13 17:36:58 +0800

    issue #2771: add and change some physics API, and add more documents

--- cocos/physics/CCPhysicsWorld.h
+++ cocos/physics/CCPhysicsWorld.h
@@ -82,32 +82,43 @@ typedef PhysicsQueryRectCallbackFunc PhysicsQueryPointCallbackFunc;
 class PhysicsWorld
 {
 public:
-    static const int DEBUGDRAW_NONE;
-    static const int DEBUGDRAW_SHAPE;
-    static const int DEBUGDRAW_JOINT;
-    static const int DEBUGDRAW_CONTACT;
-    static const int DEBUGDRAW_ALL;
+    static const int DEBUGDRAW_NONE;        ///< draw nothing
+    static const int DEBUGDRAW_SHAPE;       ///< draw shapes
+    static const int DEBUGDRAW_JOINT;       ///< draw joints
+    static const int DEBUGDRAW_CONTACT;     ///< draw contact
+    static const int DEBUGDRAW_ALL;         ///< draw all
     
 public:
     /** Adds a joint to the physics world.*/
     virtual void addJoint(PhysicsJoint* joint);
-    /** Removes a joint from the physics world.*/
+    /** Remove a joint from physics world.*/
     virtual void removeJoint(PhysicsJoint* joint, bool destroy);
-    /** Remove all joints from the physics world.*/
+    /** Remove all joints from physics world.*/
     virtual void removeAllJoints(bool destroy);
     
+    /** Remove a body from physics world. */
     virtual void removeBody(PhysicsBody* body);
+    /** Remove body by tag. */
     virtual void removeBody(int tag);
+    /** Remove all bodies from physics world. */
     virtual void removeAllBodies();
     
-    void rayCast(PhysicsRayCastCallbackFunc func, const Point& point1, const Point& point2, void* data);
+    /** Searches for physics shapes that intersects the ray. */
+    void rayCast(PhysicsRayCastCallbackFunc func, const Point& start, const Point& end, void* data);
+    /** Searches for physics shapes that contains in the rect. */
     void queryRect(PhysicsQueryRectCallbackFunc func, const Rect& rect, void* data);
+    /** Searches for physics shapes that contains the point. */
     void queryPoint(PhysicsQueryPointCallbackFunc func, const Point& point, void* data);
+    /** Get phsyics shapes that contains the point. */
     Vector<PhysicsShape*> getShapes(const Point& point) const;
+    /** return physics shape that contains the point. */
     PhysicsShape* getShape(const Point& point) const;
+    /** Get all the bodys that in the physics world. */
     const Vector<PhysicsBody*>& getAllBodies() const;
+    /** Get body by tag */
     PhysicsBody* getBody(int tag) const;
     
+    /** Get scene contain this physics world */
     inline Scene& getScene() const { return *_scene; }
     /** get the gravity value */
     inline Vect getGravity() const { return _gravity; }
@@ -126,8 +137,9 @@ public:
     /** get the update rate */
     inline int getUpdateRate() { return _updateRate; }
     
-    /** set the debug draw */
+    /** set the debug draw mask */
     void setDebugDrawMask(int mask);
+    /** get the bebug draw mask */
     inline int getDebugDrawMask() { return _debugDrawMask; }
     
 protected:
