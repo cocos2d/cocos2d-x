903844c7bd8c85c9f5048b63b93df8105501baef
Ricardo Quesada <ricardoquesada@gmail.com>
2013-12-18 11:43:45 -0800

    Removes unneeded files
    New functionality is merged into the 'official' classes

--- cocos/2d/CCDirector.cpp
+++ cocos/2d/CCDirector.cpp
@@ -45,7 +45,6 @@ THE SOFTWARE.
 #include "platform/CCFileUtils.h"
 #include "CCApplication.h"
 #include "CCLabelBMFont.h"
-#include "CCNewLabelAtlas.h"
 #include "CCActionManager.h"
 #include "CCAnimationCache.h"
 #include "CCTouch.h"
@@ -929,17 +928,17 @@ void Director::createStatsLabel()
      */
     float factor = EGLView::getInstance()->getDesignResolutionSize().height / 320.0f;
 
-    _FPSLabel = new NewLabelAtlas;
+    _FPSLabel = new LabelAtlas;
     _FPSLabel->setIgnoreContentScaleFactor(true);
     _FPSLabel->initWithString("00.0", texture, 12, 32 , '.');
     _FPSLabel->setScale(factor);
 
-    _SPFLabel = new NewLabelAtlas;
+    _SPFLabel = new LabelAtlas;
     _SPFLabel->setIgnoreContentScaleFactor(true);
     _SPFLabel->initWithString("0.000", texture, 12, 32, '.');
     _SPFLabel->setScale(factor);
 
-    _drawsLabel = new NewLabelAtlas;
+    _drawsLabel = new LabelAtlas;
     _drawsLabel->setIgnoreContentScaleFactor(true);
     _drawsLabel->initWithString("000", texture, 12, 32, '.');
     _drawsLabel->setScale(factor);
