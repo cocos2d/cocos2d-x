7b3d71f239ba423110763ddaaac3802cc47a76bd
zhangcheng <zhangcheng@chukong-inc.com>
2013-12-18 21:49:08 +0800

    fixed #Trigger

---
+++ extensions/CocoStudio/Trigger/ObjectFactory.h
@@ -0,0 +1,71 @@
+/****************************************************************************
+Copyright (c) 2013 cocos2d-x.org
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#ifndef __TRIGGERFACTORY_H__
+#define __TRIGGERFACTORY_H__
+
+#include "cocos2d.h"
+#include "cocos-ext.h"
+#include "ExtensionMacros.h"
+#include <string>
+#include <map>
+
+NS_CC_EXT_BEGIN
+
+typedef cocos2d::CCObject* (*Instance)(void);
+
+struct TInfo
+{
+    TInfo(void);
+    TInfo(const std::string& type, Instance ins = NULL);
+    TInfo(const TInfo &t);
+    ~TInfo(void);
+    TInfo& operator= (const TInfo &t);
+    std::string _class;
+    Instance _fun;
+};
+
+
+class ObjectFactory
+{
+    typedef std::map<std::string, TInfo>  FactoryMap;
+public:
+    ObjectFactory(void);
+    virtual ~ObjectFactory(void);
+public:
+    static ObjectFactory* sharedFactory();
+    void purgeFactory();
+public:
+    CCObject* createObject(const char *name);
+    void registerType(const TInfo &t);
+    void removeAll();
+private:
+    static ObjectFactory *_sharedFactory;
+    FactoryMap _typeMap;
+};
+
+
+NS_CC_EXT_END
+
+#endif
