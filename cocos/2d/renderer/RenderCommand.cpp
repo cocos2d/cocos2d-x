e5dfe35ed1ff64998ff0be399dcbf1806eb3d276
Nite Luo <darkdukey@gmail.com>
2013-12-18 10:33:13 +0800

    remove a warning

--- cocos/2d/renderer/RenderCommand.cpp
+++ cocos/2d/renderer/RenderCommand.cpp
@@ -23,7 +23,7 @@ void printBits(size_t const size, void const * const ptr)
     unsigned char byte;
     int i, j;
 
-    for (i=size-1;i>=0;i--)
+    for (i=(int)size-1;i>=0;i--)
     {
         for (j=7;j>=0;j--)
         {
