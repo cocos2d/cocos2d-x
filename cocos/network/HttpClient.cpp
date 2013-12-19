2f2eb453cfe5cd922e841d6874dd138bbf7f4cd9
colprog <colprog@gmail.com>
2013-12-20 04:45:07 +0800

    fix

--- cocos/network/HttpClient.cpp
+++ cocos/network/HttpClient.cpp
@@ -65,7 +65,8 @@ static bool pollCurlEvents();
 // Callback function used by libcurl for collect response data
 static size_t writeData(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-    std::vector<char> *recvBuffer = (std::vector<char>*)stream;
+    HttpResponse* resp = (HttpResponse*)stream;
+    std::vector<char>* recvBuffer = resp->getResponseData();
     size_t sizes = size * nmemb;
     
     // add data to the end of recvBuffer
@@ -87,7 +88,8 @@ size_t writeDataToFile(void *ptr, size_t size, size_t nmemb, void *stream)
 // Callback function used by libcurl for collect header data
 static size_t writeHeaderData(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-    std::vector<char> *recvBuffer = (std::vector<char>*)stream;
+    HttpResponse* resp = (HttpResponse*)stream;
+    std::vector<char>* recvBuffer = resp->getResponseHeader();
     size_t sizes = size * nmemb;
     
     // add data to the end of recvBuffer
