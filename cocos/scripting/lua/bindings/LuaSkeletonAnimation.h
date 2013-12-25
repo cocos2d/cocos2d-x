9cf45a292b5bddcc3d4b50378098bf5b589c66fb
samuele3hu <qingkui.hu@cocos2d-x.org>
2013-12-25 16:42:40 +0800

    Delete some annotated  statement about spine and add some reserve manual lua binding functions

--- cocos/scripting/lua/bindings/LuaSkeletonAnimation.h
+++ cocos/scripting/lua/bindings/LuaSkeletonAnimation.h
@@ -9,18 +9,15 @@
 #define LUASKELETONANIMATION_H_
 #include "spine/spine-cocos2dx.h"
 
-#define SCRIPT_SPINE_EVENT 10001
-
-class LuaSkeletonAnimation: public spine::CCSkeletonAnimation {
+class LuaSkeletonAnimation: public spine::SkeletonAnimation {
 private:
-	void animationStateEvent (spine::CCSkeletonAnimation* node, int trackIndex, spEventType type, spEvent* event, int loopCount);
+	void animationStateEvent (spine::SkeletonAnimation* node, int trackIndex, spEventType type, spEvent* event, int loopCount);
 
 public:
 	static LuaSkeletonAnimation* createWithFile (const char* skeletonDataFile, const char* atlasFile, float scale = 1);
 
 	LuaSkeletonAnimation (const char* skeletonDataFile, const char* atlasFile, float scale = 1);
 
-	//LuaSkeletonAnimation();
 	virtual ~LuaSkeletonAnimation();
 };
 
