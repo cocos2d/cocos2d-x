fac87108f6e8e1e2eace806c630da0665e117b5b
Huabing.Xu <dabingnn@gmail.com>
2013-12-25 10:54:31 +0800

    fix actionTest->Box2dTest

--- samples/Cpp/TestCpp/Classes/Box2DTest/Box2dTest.cpp
+++ samples/Cpp/TestCpp/Classes/Box2DTest/Box2dTest.cpp
@@ -1,6 +1,9 @@
 #include "Box2dTest.h"
 #include "../testResource.h"
 #include "extensions/cocos-ext.h"
+#include "renderer/CCRenderer.h"
+#include "renderer/CCCustomCommand.h"
+
 USING_NS_CC_EXT;
 
 #define PTM_RATIO 32
@@ -144,12 +147,27 @@ void Box2DTestLayer::draw()
     GL::enableVertexAttribs( cocos2d::GL::VERTEX_ATTRIB_FLAG_POSITION );
 
     kmGLPushMatrix();
+    kmGLGetMatrix(KM_GL_MODELVIEW, &_modelViewMV);
 
-    world->DrawDebugData();
+    CustomCommand *cmd = CustomCommand::getCommandPool().generateCommand();
+    cmd->init(0, _vertexZ);
+    cmd->func = CC_CALLBACK_0(Box2DTestLayer::onDraw, this);
+    Director::getInstance()->getRenderer()->addCommand(cmd);
 
     kmGLPopMatrix();
 #endif
 }
+
+#if CC_ENABLE_BOX2D_INTEGRATION
+void Box2DTestLayer::onDraw()
+{
+    kmMat4 oldMV;
+    kmGLGetMatrix(KM_GL_MODELVIEW, &oldMV);
+    kmGLLoadMatrix(&_modelViewMV);
+    world->DrawDebugData();
+    kmGLLoadMatrix(&oldMV);
+}
+#endif
 
 void Box2DTestLayer::addNewSpriteAtPosition(Point p)
 {
