a5960c325241defb35d969b57a9ff8d0832f7e7e
Dhilan007 <wenhai.lin@chukong-inc.com>
2013-12-30 15:58:04 +0800

    recover java activity on android.

---
+++ samples/Cpp/TestCpp/proj.android/src/org/cocos2dx/testcpp/Cocos2dxActivity.java
@@ -0,0 +1,18 @@
+package org.cocos2dx.testcpp;
+
+import android.app.NativeActivity;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+
+public class Cocos2dxActivity extends NativeActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		//For supports translucency.
+		//You need configure egl attribs of the color buffer size in cocos\2d\platform\android\nativeactivity.cpp on some android versions(eg.4.4) 
+		//getWindow().setFormat(PixelFormat.TRANSLUCENT);
+	}
+}
