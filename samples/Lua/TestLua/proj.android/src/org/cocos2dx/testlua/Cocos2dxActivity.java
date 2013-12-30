4066ced5eb6a8d208f7a19ccfd2ccc5f692bf05a
Dhilan007 <wenhai.lin@chukong-inc.com>
2013-12-30 15:58:04 +0800

    recover java activity on android.

---
+++ samples/Lua/TestLua/proj.android/src/org/cocos2dx/testlua/Cocos2dxActivity.java
@@ -0,0 +1,17 @@
+package org.cocos2dx.testlua;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class Cocos2dxActivity extends NativeActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		//For supports translucency.
+		//You need configure egl attribs of the color buffer size in cocos\2d\platform\android\nativeactivity.cpp on some android versions(eg.4.4) 
+		//getWindow().setFormat(PixelFormat.TRANSLUCENT);
+	}
+}
