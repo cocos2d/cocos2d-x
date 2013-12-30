646786a44c7ed348b05d3a86b23510e71d1090af
Dhilan007 <wenhai.lin@chukong-inc.com>
2013-12-30 16:30:08 +0800

    update describe for support translucency on android

--- template/multi-platform-cpp/proj.android/src/org/cocos2dx/hellocpp/Cocos2dxActivity.java
+++ template/multi-platform-cpp/proj.android/src/org/cocos2dx/hellocpp/Cocos2dxActivity.java
@@ -10,8 +10,19 @@ public class Cocos2dxActivity extends NativeActivity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
-		//For supports translucency.
-		//You need configure egl attribs of the color buffer size in cocos\2d\platform\android\nativeactivity.cpp on some android versions(eg.4.4) 
+		//1.Set the format of window for supports translucency
 		//getWindow().setFormat(PixelFormat.TRANSLUCENT);
+		
+		//2.You need configure egl attribs in cocos\2d\platform\android\nativeactivity.cpp on some android versions(eg.4.4) for supports translucency.
+		//eg:
+		/*const EGLint attribs[] = {
+	            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+	            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+	            EGL_BUFFER_SIZE, 32,
+	            EGL_DEPTH_SIZE, 16,
+	            EGL_STENCIL_SIZE, 8,
+	            EGL_NONE
+	    };*/
+		
 	}
 }
