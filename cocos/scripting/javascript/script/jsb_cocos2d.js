171cdf56b0bdac01d0cd53733314c6008cda99fe
Dhilan007 <wenhai.lin@chukong-inc.com>
2014-01-03 21:33:48 +0800

    update js binding for studio change.

--- cocos/scripting/javascript/script/jsb_cocos2d.js
+++ cocos/scripting/javascript/script/jsb_cocos2d.js
@@ -142,14 +142,80 @@ cc.log = cc._cocosplayerLog || cc.log || log;
 //
 cc.c3b = function( r, g, b )
 {
-    return {r:r, g:g, b:b };
+    switch (arguments.length) {
+        case 0:
+            return {r:0, g:0, b:0 };
+        case 1:
+            if (r && r instanceof cc.c3b) {
+            	  return {r:r.r, g:r.g, b:r.b };
+            } else {
+                return {r:0, g:0, b:0 };
+            }
+        case 3:
+            return {r:r, g:g, b:b };
+        default:
+            throw "unknown argument type";
+            break;
+    }
 };
+
+cc.integerToColor3B = function (intValue) {
+    intValue = intValue || 0;
+
+    var offset = 0xff;
+    var retColor = {r:0, g:0, b:0 };
+    retColor.r = intValue & (offset);
+    retColor.g = (intValue >> 8) & offset;
+    retColor.b = (intValue >> 16) & offset;
+    return retColor;
+};
+
 cc._c3b = function( r, g, b )
 {
     cc._reuse_color3b.r = r;
     cc._reuse_color3b.g = g;
     cc._reuse_color3b.b = b;
     return cc._reuse_color3b;
+};
+
+cc.c3BEqual = function(color1, color2){
+    return color1.r === color2.r && color1.g === color2.g && color1.b === color2.b;
+};
+
+cc.white = function () {
+    return cc.c3b(255, 255, 255);
+};
+
+cc.yellow = function () {
+    return cc.c3b(255, 255, 0);
+};
+
+cc.blue = function () {
+    return cc.c3b(0, 0, 255);
+};
+
+cc.green = function () {
+    return cc.c3b(0, 255, 0);
+};
+
+cc.red = function () {
+    return cc.c3b(255, 0, 0);
+};
+
+cc.magenta = function () {
+    return cc.c3b(255, 0, 255);
+};
+
+cc.black = function () {
+    return cc.c3b(0, 0, 0);
+};
+
+cc.orange = function () {
+    return cc.c3b(255, 127, 0);
+};
+
+cc.gray = function () {
+    return cc.c3b(166, 166, 166);
 };
 
 //
@@ -191,6 +257,22 @@ cc.convertColor3BtoHexString = function (clr) {
 cc.c4f = function( r, g, b, a )
 {
     return {r:r, g:g, b:b, a:a };
+};
+
+cc.c4FFromccc3B = function (c) {
+    return cc.c4f(c.r / 255.0, c.g / 255.0, c.b / 255.0, 1.0);
+};
+
+cc.c4FFromccc4B = function (c) {
+    return cc.c4f(c.r / 255.0, c.g / 255.0, c.b / 255.0, c.a / 255.0);
+};
+
+cc.c4BFromccc4F = function (c) {
+    return cc.c4f(0 | (c.r * 255), 0 | (c.g * 255), 0 | (c.b * 255), 0 | (c.a * 255));
+};
+
+cc.c4FEqual = function (a, b) {
+    return a.r == b.r && a.g == b.g && a.b == b.b && a.a == b.a;
 };
 
 //
