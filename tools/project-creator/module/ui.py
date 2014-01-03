3e5f7c873f7b38a0b58a0d460b289977f770a896
chuanweizhang <chuanweizhang2013@gmail.com>
2014-01-03 19:09:13 +0800

    add cocos files list configure script

--- tools/project-creator/module/ui.py
+++ tools/project-creator/module/ui.py
@@ -67,8 +67,13 @@ class ThreadedTask(threading.Thread):
         #delete exist project.
         if os.path.exists(os.path.join(self.projectPath, self.projectName)):
             print ("###begin remove... " + self.projectName)
-            shutil.rmtree(os.path.join(self.projectPath, self.projectName))
-            print ("###remove finish:  " + self.projectName)
+            try:
+                shutil.rmtree(os.path.join(self.projectPath, self.projectName))
+                print ("###remove finish:  " + self.projectName)
+            except:
+                print ("###remove folder failure %s" %self.projectName)
+                putMsg = "end@%d@%d@%s" %(100, 100, "create failure")
+                self.queue.put(putMsg)
         putMsg = "begin@%d@%d@%s" %(0, 100, "begin create")
         self.queue.put(putMsg)
 
