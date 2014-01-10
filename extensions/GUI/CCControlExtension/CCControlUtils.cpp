16367b75944b729543241eed867e3b234c9802a4
WuHuan <akof1314@sina.com>
2014-01-10 14:20:00 +0800

    solve conflicted with math.h isnan

--- extensions/GUI/CCControlExtension/CCControlUtils.cpp
+++ extensions/GUI/CCControlExtension/CCControlUtils.cpp
@@ -92,7 +92,7 @@ RGBA ControlUtils::RGBfromHSV(HSV value)
     
     if (value.s <= 0.0) // < is bogus, just shuts up warnings
     {       
-        if (std::isnan(value.h)) // value.h == NAN
+        if (isnan(value.h)) // value.h == NAN
         {   
             out.r = value.v;
             out.g = value.v;
