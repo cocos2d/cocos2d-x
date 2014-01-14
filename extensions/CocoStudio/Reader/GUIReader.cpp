716611978a036ec46434771a8664a9b14f92d42c
CaiWenzhi <wenzhi.cai@chukong-inc.com>
2014-01-14 15:20:49 +0800

    Fixed bugs.

--- extensions/CocoStudio/Reader/GUIReader.cpp
+++ extensions/CocoStudio/Reader/GUIReader.cpp
@@ -573,16 +573,12 @@ void WidgetPropertiesReader0250::setPropsForLabelFromJsonDictionary(cocos2d::gui
     {
         label->setFontName(DICTOOL->getStringValue_json(options, "fontName"));
     }
-    bool handleDimensions = DICTOOL->getBooleanValue_json(options, "handleDimensions");
-    if (handleDimensions)
+    bool aw = DICTOOL->checkObjectExist_json(options, "areaWidth");
+    bool ah = DICTOOL->checkObjectExist_json(options, "areaHeight");
+    if (aw && ah)
     {
-        bool aw = DICTOOL->checkObjectExist_json(options, "areaWidth");
-        bool ah = DICTOOL->checkObjectExist_json(options, "areaHeight");
-        if (aw && ah)
-        {
-            CCSize size = CCSize(DICTOOL->getFloatValue_json(options, "areaWidth"),DICTOOL->getFloatValue_json(options,"areaHeight"));
-            label->setTextAreaSize(size);
-        }
+        CCSize size = CCSize(DICTOOL->getFloatValue_json(options, "areaWidth"),DICTOOL->getFloatValue_json(options,"areaHeight"));
+        label->setTextAreaSize(size);
     }
     bool ha = DICTOOL->checkObjectExist_json(options, "hAlignment");
     if (ha)
@@ -1463,16 +1459,12 @@ void WidgetPropertiesReader0300::setPropsForLabelFromJsonDictionary(cocos2d::gui
     {
         label->setFontName(DICTOOL->getStringValue_json(options, "fontName"));
     }
-    bool handleDimensions = DICTOOL->getBooleanValue_json(options, "handleDimensions");
-    if (handleDimensions)
+    bool aw = DICTOOL->checkObjectExist_json(options, "areaWidth");
+    bool ah = DICTOOL->checkObjectExist_json(options, "areaHeight");
+    if (aw && ah)
     {
-        bool aw = DICTOOL->checkObjectExist_json(options, "areaWidth");
-        bool ah = DICTOOL->checkObjectExist_json(options, "areaHeight");
-        if (aw && ah)
-        {
-            CCSize size = CCSize(DICTOOL->getFloatValue_json(options, "areaWidth"),DICTOOL->getFloatValue_json(options,"areaHeight"));
-            label->setTextAreaSize(size);
-        }
+        CCSize size = CCSize(DICTOOL->getFloatValue_json(options, "areaWidth"),DICTOOL->getFloatValue_json(options,"areaHeight"));
+        label->setTextAreaSize(size);
     }
     bool ha = DICTOOL->checkObjectExist_json(options, "hAlignment");
     if (ha)
