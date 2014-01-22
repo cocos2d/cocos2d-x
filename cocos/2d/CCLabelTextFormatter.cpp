2cc3167c1a526fdcb6346b014da330e3bf69513d
Dhilan007 <wenhai.lin@chukong-inc.com>
2014-01-22 17:45:56 +0800

    close #3642:fix bounding box of the new label is a bit off.

--- cocos/2d/CCLabelTextFormatter.cpp
+++ cocos/2d/CCLabelTextFormatter.cpp
@@ -274,8 +274,8 @@ bool LabelTextFormatter::createStringSprites(Label *theLabel)
     int quantityOfLines         = theLabel->getStringNumLines();
     int commonLineHeight        = theLabel->getCommonLineHeight();
     
-    totalHeight                 =     commonLineHeight * quantityOfLines;
-    nextFontPositionY           = 0 - (commonLineHeight - totalHeight);
+    totalHeight                 = commonLineHeight * quantityOfLines;
+    nextFontPositionY           = totalHeight;
     
     Rect charRect;
     int charXOffset = 0;
@@ -312,11 +312,8 @@ bool LabelTextFormatter::createStringSprites(Label *theLabel)
             continue;
         }
         
-        // See issue 1343. cast( signed short + unsigned integer ) == unsigned integer (sign is lost!)
-        int yOffset = commonLineHeight - charYOffset;
-        
         Point fontPos = Point((float)nextFontPositionX + charXOffset + kernings[i],
-            (float)nextFontPositionY + yOffset);
+            (float)nextFontPositionY - charYOffset);
                
         if( theLabel->recordLetterInfo(CC_POINT_PIXELS_TO_POINTS(fontPos),c,i) == false)
         {
