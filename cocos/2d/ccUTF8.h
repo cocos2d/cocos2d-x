7484286f25f69b0ffa6946696925c10b11b3c53a
Pisces000221 <1786762946@qq.com>
2014-02-08 14:52:59 +0800

    Rename ischinese_unicode as iscjk_unicode

--- cocos/2d/ccUTF8.h
+++ cocos/2d/ccUTF8.h
@@ -42,14 +42,15 @@ CC_DLL void cc_utf8_trim_ws(std::vector<unsigned short>* str);
 CC_DLL bool isspace_unicode(unsigned short ch);
 
 /**
- * Whether the character is a Chinese character.
+ * Whether the character is a Chinese/Japanese/Korean character.
  *
  * @param ch    the unicode character
  * @returns     whether the character is a Chinese character.
  *
  * @see http://www.searchtb.com/2012/04/chinese_encode.html
+ * @see http://tieba.baidu.com/p/748765987
  * */
-CC_DLL bool ischinese_unicode(unsigned short ch);
+CC_DLL bool iscjk_unicode(unsigned short ch);
 
 /**
  * Returns the length of the string in characters.
