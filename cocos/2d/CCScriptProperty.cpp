f8e47d4c533efd8cc08902a32f970f35b7e3503a
James Chen <jianhua.chen@cocos2d-x.org>
2014-02-18 23:03:55 +0800

    issue #4058: Adds CCScriptProperty.h/.cpp.

---
+++ cocos/2d/CCScriptProperty.cpp
@@ -0,0 +1,9 @@
+//
+//  ScriptProperty.cpp
+//  cocos2d_libs
+//
+//  Created by James Chen on 14-2-18.
+//
+//
+
+#include "ScriptProperty.h"
---
+++ cocos/2d/CCScriptProperty.h
@@ -0,0 +1,14 @@
+//
+//  ScriptProperty.h
+//  cocos2d_libs
+//
+//  Created by James Chen on 14-2-18.
+//
+//
+
+#ifndef __cocos2d_libs__ScriptProperty__
+#define __cocos2d_libs__ScriptProperty__
+
+#include <iostream>
+
+#endif /* defined(__cocos2d_libs__ScriptProperty__) */
