b1e44a7ed69891848635b08faec9559037df9feb
James Chen <jianhua.chen@cocos2d-x.org>
2014-02-20 17:02:20 +0800

    issue #4097: Enables script bindings by default.

--- cocos/2d/ccConfig.h
+++ cocos/2d/ccConfig.h
@@ -274,7 +274,7 @@ To enable set it to a value different than 0. Disabled by default.
 
 /** Enable Script binding */
 #ifndef CC_ENABLE_SCRIPT_BINDING
-#define CC_ENABLE_SCRIPT_BINDING 0
+#define CC_ENABLE_SCRIPT_BINDING 1
 #endif
 
 #endif // __CCCONFIG_H__
