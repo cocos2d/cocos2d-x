4e994dcef4d8f5d4619e63d4958d60b24819fd91
Dale Stammen <dalestam@microsoft.com>
2014-02-25 14:46:20 -0800

    fixed typo in comment

--- cocos2dx/platform/wp8-xaml/Cocos2dRenderer.cpp
+++ cocos2dx/platform/wp8-xaml/Cocos2dRenderer.cpp
@@ -97,7 +97,7 @@ void Cocos2dRenderer::OnBackKeyPress()
 {
     // handle the backkey in your app here.
     // call Cocos2dEvent::TerminateApp if it is time to exit your app.
-    // ie. the use is on your first page and wishes to exit your app.
+    // ie. the user is on your first page and wishes to exit your app.
     m_delegate->Invoke(Cocos2dEvent::TerminateApp);
 }
 
