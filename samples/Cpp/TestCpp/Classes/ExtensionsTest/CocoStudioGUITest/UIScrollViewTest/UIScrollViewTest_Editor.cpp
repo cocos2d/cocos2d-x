d40630709186b0e78da4ede246a901bdc7015c40
CaiWenzhi <wenzhi.cai@chukong-inc.com>
2014-03-07 21:05:51 +0800

    Fixed resource path error

--- samples/Cpp/TestCpp/Classes/ExtensionsTest/CocoStudioGUITest/UIScrollViewTest/UIScrollViewTest_Editor.cpp
+++ samples/Cpp/TestCpp/Classes/ExtensionsTest/CocoStudioGUITest/UIScrollViewTest/UIScrollViewTest_Editor.cpp
@@ -34,7 +34,7 @@ bool UIScrollViewTest_Vertical_Editor::init()
         _sceneTitle = static_cast<ui::Label*>(UIHelper::seekWidgetByName(root, "UItest"));
         
         Button* left_button = Button::create();
-        left_button->loadTextures("images/b1.png", "images/b2.png", "");
+        left_button->loadTextures("Images/b1.png", "Images/b2.png", "");
         left_button->setPosition(ccp(_layout->getSize().width / 2 - left_button->getSize().width,
                                      left_button->getSize().height * 0.625));
         left_button->setTouchEnabled(true);
@@ -43,7 +43,7 @@ bool UIScrollViewTest_Vertical_Editor::init()
         _layout->addChild(left_button);
         
         Button* right_button = Button::create();
-        right_button->loadTextures("images/f1.png", "images/f2.png", "");
+        right_button->loadTextures("Images/f1.png", "Images/f2.png", "");
         right_button->setPosition(ccp(_layout->getSize().width / 2 + right_button->getSize().width,
                                       right_button->getSize().height * 0.625));
         right_button->setTouchEnabled(true);
@@ -89,7 +89,7 @@ bool UIScrollViewTest_Horizontal_Editor::init()
         _sceneTitle = static_cast<ui::Label*>(UIHelper::seekWidgetByName(root, "UItest"));
         
         Button* left_button = Button::create();
-        left_button->loadTextures("images/b1.png", "images/b2.png", "");
+        left_button->loadTextures("Images/b1.png", "Images/b2.png", "");
         left_button->setPosition(ccp(_layout->getSize().width / 2 - left_button->getSize().width,
                                      left_button->getSize().height * 0.625));
         left_button->setTouchEnabled(true);
@@ -98,7 +98,7 @@ bool UIScrollViewTest_Horizontal_Editor::init()
         _layout->addChild(left_button);
         
         Button* right_button = Button::create();
-        right_button->loadTextures("images/f1.png", "images/f2.png", "");
+        right_button->loadTextures("Images/f1.png", "Images/f2.png", "");
         right_button->setPosition(ccp(_layout->getSize().width / 2 + right_button->getSize().width,
                                       right_button->getSize().height * 0.625));
         right_button->setTouchEnabled(true);
@@ -144,7 +144,7 @@ bool UIScrollViewTest_Both_Editor::init()
         _sceneTitle = static_cast<ui::Label*>(UIHelper::seekWidgetByName(root, "UItest"));
         
         Button* left_button = Button::create();
-        left_button->loadTextures("images/b1.png", "images/b2.png", "");
+        left_button->loadTextures("Images/b1.png", "Images/b2.png", "");
         left_button->setPosition(ccp(_layout->getSize().width / 2 - left_button->getSize().width,
                                      left_button->getSize().height * 0.625));
         left_button->setTouchEnabled(true);
@@ -153,7 +153,7 @@ bool UIScrollViewTest_Both_Editor::init()
         _layout->addChild(left_button);
         
         Button* right_button = Button::create();
-        right_button->loadTextures("images/f1.png", "images/f2.png", "");
+        right_button->loadTextures("Images/f1.png", "Images/f2.png", "");
         right_button->setPosition(ccp(_layout->getSize().width / 2 + right_button->getSize().width,
                                       right_button->getSize().height * 0.625));
         right_button->setTouchEnabled(true);
