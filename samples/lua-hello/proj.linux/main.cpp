27729db97b748b87baf1fbb6bab93bf6fb90208b
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-10 20:07:20 +0800

    Re-add hellocpp, hellolua

---
+++ samples/lua-hello/proj.linux/main.cpp
@@ -0,0 +1,18 @@
+#include "../Classes/AppDelegate.h"
+#include "cocos2d.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+
+USING_NS_CC;
+
+int main(int argc, char **argv)
+{
+    // create the application instance
+    AppDelegate app;
+    EGLView eglView;
+    eglView.init("HelloLua",900,640);
+    return Application::getInstance()->run();
+}
