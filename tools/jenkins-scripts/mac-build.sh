38b6cc6f0c1e7377a084a5a077e4fbf6aa160107
heliclei <jiusheng.lei@chukong-inc.com>
2014-03-10 14:38:20 +0800

    fix proj name

--- tools/jenkins-scripts/mac-build.sh
+++ tools/jenkins-scripts/mac-build.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COCOS2DX_ROOT="$DIR"/../..
-xcodebuild -project "$COCOS2DX_ROOT"/build/cocos2d_tests.xcodeproj -scheme "build all tests iOS"  -destination "platform=OS X" clean
-xcodebuild -project "$COCOS2DX_ROOT"/build/cocos2d_tests.xcodeproj -scheme "build all tests iOS"  -destination "platform=OS X" build
+xcodebuild -project "$COCOS2DX_ROOT"/build/cocos2d_samples.xcodeproj -scheme "build all tests iOS"  -destination "platform=OS X" clean
+xcodebuild -project "$COCOS2DX_ROOT"/build/cocos2d_samples.xcodeproj -scheme "build all tests iOS"  -destination "platform=OS X" build
