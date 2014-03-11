79e979ac7107bdf87b5b53441e733d635f46194a
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-11 03:18:19 -0700

    updates CMakeLists.txt

--- tests/cpp-empty-test/proj.linux/main.cpp
+++ tests/cpp-empty-test/proj.linux/main.cpp
@@ -12,7 +12,5 @@ int main(int argc, char **argv)
 {
     // create the application instance
     AppDelegate app;
-    EGLView eglView;
-    eglView.init("HelloCpp",900,640);
     return Application::getInstance()->run();
 }
