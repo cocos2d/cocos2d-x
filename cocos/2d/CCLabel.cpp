f634afb986ac4054bb1508ca2b687fbee9614461
Dhilan007 <wenhai.lin@chukong-inc.com>
2014-03-11 15:03:16 +0800

    fix incorrect implementation of Label::getContentSize

--- cocos/2d/CCLabel.cpp
+++ cocos/2d/CCLabel.cpp
@@ -1155,11 +1155,11 @@ std::string Label::getDescription() const
     return StringUtils::format("<Label | Tag = %d, Label = '%s'>", _tag, cc_utf16_to_utf8(_currentUTF16String,-1,nullptr,nullptr));
 }
 
-const Size& Label::getContentSize()
+const Size& Label::getContentSize() const
 {
     if (_contentDirty)
     {
-        updateContent();
+        const_cast<Label*>(this)->updateContent();
     }
     return Node::getContentSize();
 }
