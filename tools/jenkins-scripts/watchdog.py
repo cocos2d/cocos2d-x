784948b2c04c09f16c7d30931ffe9391ce55067a
linchaogithub <linchaococos2dx@gmail.com>
2014-03-12 15:36:09 +0800

    [Jenkins] watchdog

---
+++ tools/jenkins-scripts/watchdog.py
@@ -0,0 +1,47 @@
+import jenkinsapi
+from jenkinsapi.jenkins import Jenkins
+import sys
+import time
+import os
+
+def main():
+    
+    J = Jenkins('http://115.28.134.83:8000','redmine','cocos2dx2013')
+    job = J['cocos-2dx-pull-request-build']
+    duration = os.environ['duration']
+    #Get the numerical ID of the last build.
+    buildnu = job.get_last_buildnumber()
+    build = job.get_last_build()
+    running = build.is_running()
+    print 'running:',running
+
+    if not running:
+        return False
+    print "buildnumber:#",buildnu
+    nowtime = time.strftime('%M',time.localtime(time.time()))
+    #print 'nowtime:',nowtime
+    timeb = build.get_timestamp()
+    #print 'buildtime:',str(timeb)[14:16]
+    buildtime = int(str(timeb)[14:16])
+    subtime = 0
+    if int(nowtime) >= buildtime:
+        subtime = int(nowtime)-buildtime
+    else:
+        subtime = 60-buildtime+int(nowtime)
+    if subtime > duration:
+        #print 'subtime',subtime
+        build.stop()
+    
+    return(0)
+    
+
+# -------------- main --------------
+if __name__ == '__main__':
+    sys_ret = 0
+    try:    
+        sys_ret = main()
+    except:
+        traceback.print_exc()
+        sys_ret = 1
+    finally:
+        sys.exit(sys_ret)
