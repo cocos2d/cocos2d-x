06a0753e5a0dd2922d54b4cfbf9db188f3dfb925
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-12 14:16:42 +0800

    CCLOG -> CCLOGINFO for change of EditBox's position.

--- extensions/GUI/CCEditBox/CCEditBoxImplIOS.mm
+++ extensions/GUI/CCEditBox/CCEditBoxImplIOS.mm
@@ -575,7 +575,7 @@ static CGPoint convertDesignCoordToScreenCoord(const Point& designCoord, bool bI
         screenPos.x = screenPos.x / 2.0f;
         screenPos.y = screenPos.y / 2.0f;
     }
-    CCLOG("[EditBox] pos x = %f, y = %f", screenGLPos.x, screenGLPos.y);
+    CCLOGINFO("[EditBox] pos x = %f, y = %f", screenGLPos.x, screenGLPos.y);
     return screenPos;
 }
 
