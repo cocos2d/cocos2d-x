1c32c6d96f877c7551fc99ad7fcef586d5b03211
samuele3hu <qingkui.hu@cocos2d-x.org>
2014-03-12 17:51:34 +0800

    Replace cc.LabelTTF:create with cc.Label:create

--- tests/lua-tests/src/Texture2dTest/Texture2dTest.lua
+++ tests/lua-tests/src/Texture2dTest/Texture2dTest.lua
@@ -959,7 +959,8 @@ local function TextureAsync()
 
     local size =cc.Director:getInstance():getWinSize()
 
-    local label = cc.LabelTTF:create("Loading...", "Marker Felt", 32)
+    local label = cc.Label:create("Loading...", s_markerFeltFontPath, 32)
+    label:setAnchorPoint(cc.p(0.5, 0.5))
     label:setPosition(cc.p( size.width/2, size.height/2))
     ret:addChild(label, 10)
 
