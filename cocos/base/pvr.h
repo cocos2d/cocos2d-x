fdd7d3d1d9ae578ce9e90a3d9cb425a9adf824ca
Sergey <non_2001@mail.ru>
2014-03-13 09:16:18 +0400

    sotware pvr decompressions suport event listener improvements getAllTouches() - it is like UIEvent in iPhone sdk  – allTouches getCurrentLanguageCode() - get iso 639-1 code iOS template improvement(useful for game center integration or some libs that need RootViewController)

---
+++ cocos/base/pvr.h
@@ -0,0 +1,16 @@
+//
+//  PVRDecompressor.h
+//  inferno
+//
+//  Created by user-i121 on 09.07.13.
+//
+//
+
+#ifndef __PVR_H__
+#define __PVR_H__
+
+
+int PVRTDecompressPVRTC(const void * const pCompressedData,const int XDim,const int YDim,void *pDestData,const bool Do2bitMode);
+
+
+#endif //__PVR_H__
