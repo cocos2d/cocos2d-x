f70947ee6be7121f0c43941cdb095e8e1b94b75c
minggo <ming@cocos2d-x.org>
2014-03-13 10:25:01 +0800

    initialize m_fSecondsPerframe

--- cocos2dx/CCDirector.cpp
+++ cocos2dx/CCDirector.cpp
@@ -130,6 +130,7 @@ bool CCDirector::init(void)
     m_uTotalFrames = m_uFrames = 0;
     m_pszFPS = new char[10];
     m_pLastUpdate = new struct cc_timeval();
+    m_fSecondsPerFrame = 0.0f;
 
     // paused ?
     m_bPaused = false;
