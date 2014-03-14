5854c392d542b1530ebe2e1964e4a9fd08236073
qiaofeng00oo <qiaofeng00oo@126.com>
2014-03-14 10:36:30 +0800

    modify comment trigger for console job.

--- tools/jenkins-scripts/job-comment-trigger.py
+++ tools/jenkins-scripts/job-comment-trigger.py
@@ -26,7 +26,14 @@ def main():
     print comment_body
     pattern = re.compile("\[ci(\s+)rebuild\]", re.I)
     result = pattern.search(comment_body)
-    if result is None:
+
+    # will check console/console create
+    patternCrt = re.compile("\[console(\s+)create\]", re.I)
+    resCrt = patternCrt.search(comment_body)
+    patternCon = re.compile("\[console\]", re.I)
+    resCon = patternCon.search(comment_body)
+
+    if result is None and resCrt is None and resCon is None:
         print 'skip build for pull request #' + str(pr_num)
         return(0)
     
@@ -75,7 +82,14 @@ def main():
     except:
         traceback.print_exc()
 
-    job_trigger_url = os.environ['JOB_TRIGGER_URL']
+    job_trigger_url = ''
+    if result:
+        job_trigger_url = os.environ['JOB_TRIGGER_URL']
+    if resCrt:
+        job_trigger_url = os.environ['JOB_CONSOLE_TEST_URL']
+        payload_forword['create'] = 'create'
+    if result:
+        job_trigger_url = os.environ['JOB_CONSOLE_TEST_URL']
     #send trigger and payload
     post_data = {'payload':""}
     post_data['payload']= json.dumps(payload_forword)
