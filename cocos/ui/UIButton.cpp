2dffcb980865c0300dd7177d40451c325155b97f
CaiWenzhi <wenzhi.cai@chukong-inc.com>
2014-03-24 15:25:44 +0800

    Add ProtectedNode and remove "addNode"

--- cocos/ui/UIButton.cpp
+++ cocos/ui/UIButton.cpp
@@ -102,10 +102,10 @@ void Button::initRenderer()
     _titleRenderer = Label::create();
     _titleRenderer->setAnchorPoint(Point::ANCHOR_MIDDLE);
 
-    Node::addChild(_buttonNormalRenderer, NORMAL_RENDERER_Z, -1);
-    Node::addChild(_buttonClickedRenderer, PRESSED_RENDERER_Z, -1);
-    Node::addChild(_buttonDisableRenderer, DISABLED_RENDERER_Z, -1);
-    Node::addChild(_titleRenderer, TITLE_RENDERER_Z, -1);
+    addProtectedChild(_buttonNormalRenderer, NORMAL_RENDERER_Z, -1);
+    addProtectedChild(_buttonClickedRenderer, PRESSED_RENDERER_Z, -1);
+    addProtectedChild(_buttonDisableRenderer, DISABLED_RENDERER_Z, -1);
+    addProtectedChild(_titleRenderer, TITLE_RENDERER_Z, -1);
 }
 
 void Button::setScale9Enabled(bool able)
@@ -116,9 +116,9 @@ void Button::setScale9Enabled(bool able)
     }
     _brightStyle = BRIGHT_NONE;
     _scale9Enabled = able;
-    Node::removeChild(_buttonNormalRenderer);
-    Node::removeChild(_buttonClickedRenderer);
-    Node::removeChild(_buttonDisableRenderer);
+    removeProtectedChild(_buttonNormalRenderer);
+    removeProtectedChild(_buttonClickedRenderer);
+    removeProtectedChild(_buttonDisableRenderer);
     _buttonNormalRenderer = nullptr;
     _buttonClickedRenderer = nullptr;
     _buttonDisableRenderer = nullptr;
@@ -138,9 +138,9 @@ void Button::setScale9Enabled(bool able)
     loadTextureNormal(_normalFileName.c_str(), _normalTexType);
     loadTexturePressed(_clickedFileName.c_str(), _pressedTexType);
     loadTextureDisabled(_disabledFileName.c_str(), _disabledTexType);
-    Node::addChild(_buttonNormalRenderer, NORMAL_RENDERER_Z, -1);
-    Node::addChild(_buttonClickedRenderer, PRESSED_RENDERER_Z, -1);
-    Node::addChild(_buttonDisableRenderer, DISABLED_RENDERER_Z, -1);
+    addProtectedChild(_buttonNormalRenderer, NORMAL_RENDERER_Z, -1);
+    addProtectedChild(_buttonClickedRenderer, PRESSED_RENDERER_Z, -1);
+    addProtectedChild(_buttonDisableRenderer, DISABLED_RENDERER_Z, -1);
     if (_scale9Enabled)
     {
         bool ignoreBefore = _ignoreSize;
