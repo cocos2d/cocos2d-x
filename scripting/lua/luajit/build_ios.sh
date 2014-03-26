4f799aab5dfe927942e0953b5b6dab30b707e5ce
dualface <dualface@gmail.com>
2014-03-26 16:40:55 +0800

    upgrade LuaJIT to 2.0.3

--- scripting/lua/luajit/build_ios.sh
+++ scripting/lua/luajit/build_ios.sh
@@ -1,26 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIPO="xcrun -sdk iphoneos lipo"
 STRIP="xcrun -sdk iphoneos strip"
 
-SRCDIR=$DIR/LuaJit-2.0.1
+SRCDIR=$DIR/LuaJit-2.0.3
 DESTDIR=$DIR/ios
 IXCODE=`xcode-select -print-path`
 ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
-ISDKVER=iPhoneOS6.1.sdk
-ISDKP=$ISDK/usr/bin/
+ISDKVER=iPhoneOS7.1.sdk
+# ISDKP=$ISDK/usr/bin/
+ISDKP=$IXCODE/usr/bin/
 
 rm "$DESTDIR"/*.a
 cd $SRCDIR
 
 make clean
 ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER"
-make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
+make HOST_CC="gcc -m32 -arch i386" TARGET_FLAGS="$ISDKF" TARGET=arm TARGET_SYS=iOS
+# make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
 mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7.a
 
 make clean
 ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER"
-make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
+make HOST_CC="gcc -m32 -arch i386" TARGET_FLAGS="$ISDKF" TARGET=arm TARGET_SYS=iOS
+# make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
 mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7s.a
 
 make clean
--- scripting/lua/luajit/build_mac.sh
+++ scripting/lua/luajit/build_mac.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRCDIR=$DIR/LuaJit-2.0.1
-DESTDIR=$DIR/proj.mac
+SRCDIR=$DIR/LuaJit-2.0.3
+DESTDIR=$DIR/mac
 MACOSX_DEPLOYMENT_TARGET="10.6"
 
-rm "$DESTDIR"/*.a
+if [ -f "$DESTDIR"/*.a ]; then
+    rm "$DESTDIR"/*.a
+fi;
 
 cd $SRCDIR
 make clean
-make CC="gcc -m32 -arch i386" clean all
+make CC="clang -arch x86_64" clean all
 
 if [ -f $SRCDIR/src/libluajit.a ]; then
     mv $SRCDIR/src/libluajit.a $DESTDIR/libluajit2.a
