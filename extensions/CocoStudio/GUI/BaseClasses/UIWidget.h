d32cb80784e436e89c540ceba20d79e9024a80ac
lihex <hexuan.li@cocos2d-x.org>
2014-03-26 15:37:38 +0800

    update bindings for cocostudio

--- extensions/CocoStudio/GUI/BaseClasses/UIWidget.h
+++ extensions/CocoStudio/GUI/BaseClasses/UIWidget.h
@@ -641,13 +641,15 @@ public:
     /*temp action*/
     void setActionTag(int tag);
 	int getActionTag();
-protected:
-    //call back function called when size changed.
-    virtual void onSizeChanged();
     
+CC_CONSTRUCTOR_ACCESS:
     //initializes state of widget.
     virtual bool init();
     
+protected:
+    //call back function called when size changed.
+    virtual void onSizeChanged();
+
     //initializes renderer of widget.
     virtual void initRenderer(){};
     
