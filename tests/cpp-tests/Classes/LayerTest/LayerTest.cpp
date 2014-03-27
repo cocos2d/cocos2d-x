413e0010f5d7c04c0d10f7af108051d9e65aa6f6
pandamicro <lphuabin@gmail.com>
2014-03-27 10:22:04 +0800

    Fixed #4566: Fix LayerTest bug

--- tests/cpp-tests/Classes/LayerTest/LayerTest.cpp
+++ tests/cpp-tests/Classes/LayerTest/LayerTest.cpp
@@ -637,7 +637,7 @@ std::string LayerGradientTest::subtitle() const
 //------------------------------------------------------------------
 LayerGradientTest2::LayerGradientTest2()
 {
-    auto layer = new LayerGradient;
+    auto layer = LayerGradient::create();
     layer->initWithColor(Color4B(255,0,0,255), Color4B(255,255,0,255));
     layer->autorelease();
     addChild(layer);
