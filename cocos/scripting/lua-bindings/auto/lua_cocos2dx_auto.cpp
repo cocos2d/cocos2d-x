66a5bb73d2f09db6e09584e9b3e984bf400a3b50
CocosRobot <redmine@cocos2d-x.org>
2014-03-28 08:20:29 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.cpp
+++ cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.cpp
@@ -32169,50 +32169,6 @@ int lua_cocos2dx_Label_getFontDefinition(lua_State* tolua_S)
 
     return 0;
 }
-int lua_cocos2dx_Label_getBoundingBox(lua_State* tolua_S)
-{
-    int argc = 0;
-    cocos2d::Label* cobj = nullptr;
-    bool ok  = true;
-
-#if COCOS2D_DEBUG >= 1
-    tolua_Error tolua_err;
-#endif
-
-
-#if COCOS2D_DEBUG >= 1
-    if (!tolua_isusertype(tolua_S,1,"cc.Label",0,&tolua_err)) goto tolua_lerror;
-#endif
-
-    cobj = (cocos2d::Label*)tolua_tousertype(tolua_S,1,0);
-
-#if COCOS2D_DEBUG >= 1
-    if (!cobj) 
-    {
-        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_Label_getBoundingBox'", nullptr);
-        return 0;
-    }
-#endif
-
-    argc = lua_gettop(tolua_S)-1;
-    if (argc == 0) 
-    {
-        if(!ok)
-            return 0;
-        cocos2d::Rect ret = cobj->getBoundingBox();
-        rect_to_luaval(tolua_S, ret);
-        return 1;
-    }
-    CCLOG("%s has wrong number of arguments: %d, was expecting %d \n", "getBoundingBox",argc, 0);
-    return 0;
-
-#if COCOS2D_DEBUG >= 1
-    tolua_lerror:
-    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_Label_getBoundingBox'.",&tolua_err);
-#endif
-
-    return 0;
-}
 int lua_cocos2dx_Label_getFontName(lua_State* tolua_S)
 {
     int argc = 0;
@@ -33629,7 +33585,6 @@ int lua_register_cocos2dx_Label(lua_State* tolua_S)
         tolua_function(tolua_S,"setBMFontFilePath",lua_cocos2dx_Label_setBMFontFilePath);
         tolua_function(tolua_S,"getFontAtlas",lua_cocos2dx_Label_getFontAtlas);
         tolua_function(tolua_S,"getFontDefinition",lua_cocos2dx_Label_getFontDefinition);
-        tolua_function(tolua_S,"getBoundingBox",lua_cocos2dx_Label_getBoundingBox);
         tolua_function(tolua_S,"getFontName",lua_cocos2dx_Label_getFontName);
         tolua_function(tolua_S,"updateContent",lua_cocos2dx_Label_updateContent);
         tolua_function(tolua_S,"getStringLength",lua_cocos2dx_Label_getStringLength);
