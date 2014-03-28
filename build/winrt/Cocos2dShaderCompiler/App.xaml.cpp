71430ba3554cb3ca8aaa1efdd9bba3220b0a829b
Dale Stammen <dalestam@microsoft.com>
2014-03-28 11:33:09 -0700

    added wp8 build files

---
+++ build/winrt/Cocos2dShaderCompiler/App.xaml.cpp
@@ -0,0 +1,89 @@
+﻿//
+// App.xaml.cpp
+// Implementation of the App class.
+//
+
+#include "pch.h"
+#include "DirectXPage.xaml.h"
+
+using namespace Cocos2dShaderCompiler;
+
+using namespace Platform;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Storage;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+/// <summary>
+/// Initializes the singleton application object.  This is the first line of authored code
+/// executed, and as such is the logical equivalent of main() or WinMain().
+/// </summary>
+App::App()
+{
+	InitializeComponent();
+	Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+	Resuming += ref new EventHandler<Object^>(this, &App::OnResuming);
+}
+
+/// <summary>
+/// Invoked when the application is launched normally by the end user.  Other entry points
+/// will be used when the application is launched to open a specific file, to display
+/// search results, and so forth.
+/// </summary>
+/// <param name="e">Details about the launch request and process.</param>
+void App::OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e)
+{
+#if _DEBUG
+	if (IsDebuggerPresent())
+	{
+		DebugSettings->EnableFrameRateCounter = true;
+	}
+#endif
+
+	m_directXPage = ref new DirectXPage();
+
+	if (e->PreviousExecutionState == ApplicationExecutionState::Terminated)
+	{
+		m_directXPage->LoadInternalState(ApplicationData::Current->LocalSettings->Values);
+	}
+
+	// Place the page in the current window and ensure that it is active.
+	Window::Current->Content = m_directXPage;
+	Window::Current->Activate();
+}
+
+/// <summary>
+/// Invoked when application execution is being suspended.  Application state is saved
+/// without knowing whether the application will be terminated or resumed with the contents
+/// of memory still intact.
+/// </summary>
+/// <param name="sender">The source of the suspend request.</param>
+/// <param name="e">Details about the suspend request.</param>
+void App::OnSuspending(Object^ sender, SuspendingEventArgs^ e)
+{
+	(void) sender;	// Unused parameter
+	(void) e;	// Unused parameter
+
+	m_directXPage->SaveInternalState(ApplicationData::Current->LocalSettings->Values);
+}
+
+/// <summary>
+/// Invoked when application execution is being resumed.
+/// </summary>
+/// <param name="sender">The source of the resume request.</param>
+/// <param name="args">Details about the resume request.</param>
+void App::OnResuming(Object ^sender, Object ^args)
+{
+	(void) sender; // Unused parameter
+	(void) args; // Unused parameter
+
+	m_directXPage->LoadInternalState(ApplicationData::Current->LocalSettings->Values);
+}
---
+++ build/winrt/Cocos2dShaderCompiler/App.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// App.xaml.h
+// Declaration of the App class.
+//
+
+#pragma once
+
+#include "App.g.h"
+#include "DirectXPage.xaml.h"
+
+namespace Cocos2dShaderCompiler
+{
+		/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	ref class App sealed
+	{
+	public:
+		App();
+		virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e) override;
+
+	private:
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
+		void OnResuming(Platform::Object ^sender, Platform::Object ^args);
+		DirectXPage^ m_directXPage;
+	};
+}
