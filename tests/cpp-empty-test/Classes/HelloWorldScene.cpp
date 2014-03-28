adddc3ff5da88d20fad3ee52ced5f4e7cb138c2e
Dale Stammen <dalestam@microsoft.com>
2014-03-28 11:27:43 -0700

    can't close app on WP8

--- tests/cpp-empty-test/Classes/HelloWorldScene.cpp
+++ tests/cpp-empty-test/Classes/HelloWorldScene.cpp
@@ -78,6 +78,11 @@ bool HelloWorld::init()
 
 void HelloWorld::menuCloseCallback(Ref* sender)
 {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
     Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
