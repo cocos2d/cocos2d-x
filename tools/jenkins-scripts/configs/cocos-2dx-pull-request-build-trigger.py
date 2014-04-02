754d74bb6419df30809497ca504cb4865b5cc625
lm <sixskater@gmail.com>
2014-04-02 15:21:44 +0800

    [Jenkins][ci skip]: add jenkins job configs

---
+++ tools/jenkins-scripts/configs/cocos-2dx-pull-request-build-trigger.py
@@ -0,0 +1,8 @@
+import os
+#os.system('git checkout develop')
+#os.system('git pull origin develop')
+ret = os.system('python -u tools/jenkins-scripts/job-trigger.py')
+if ret == 0: 
+  exit(0)
+else:
+  exit(1)
