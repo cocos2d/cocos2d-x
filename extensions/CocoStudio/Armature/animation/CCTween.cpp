1137d20a5dcd38e7f2c93049bb640fd049d6cc73
mike.weib@gmail.com <mike.weib@gmail.com>
2014-04-02 17:44:38 +0800

    don't set m_bIsComplete to true.

--- extensions/CocoStudio/Armature/animation/CCTween.cpp
+++ extensions/CocoStudio/Armature/animation/CCTween.cpp
@@ -196,7 +196,6 @@ void CCTween::updateHandler()
         case SINGLE_FRAME:
         {
             m_fCurrentPercent = 1;
-            m_bIsComplete = true;
             m_bIsPlaying = false;
         }
         break;
