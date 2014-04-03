85a86c837d8512334f92439b8a6c74fccdaa926d
yinkaile <kaile.yin@chukong-inc.com>
2014-04-03 11:20:11 +0800

    fixed set blend function

--- extensions/CocoStudio/Armature/display/CCDisplayManager.cpp
+++ extensions/CocoStudio/Armature/display/CCDisplayManager.cpp
@@ -299,6 +299,7 @@ void CCDisplayManager::setCurrentDecorativeDisplay(CCDecorativeDisplay *decoDisp
         if (CCArmature *armature = dynamic_cast<CCArmature *>(m_pDisplayRenderNode))
         {
             m_pBone->setChildArmature(armature);
+            armature->setParentBone(m_pBone);
         }
         else if (CCParticleSystemQuad *particle = dynamic_cast<CCParticleSystemQuad *>(m_pDisplayRenderNode))
         {
