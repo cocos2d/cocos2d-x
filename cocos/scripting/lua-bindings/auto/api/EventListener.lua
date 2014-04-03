c0a9128a330d4d372f3cd9d261fd880dcb0ce9ef
CocosRobot <redmine@cocos2d-x.org>
2014-04-03 10:12:05 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/EventListener.lua
+++ cocos/scripting/lua-bindings/auto/api/EventListener.lua
@@ -4,9 +4,19 @@
 -- @extend Ref
 
 --------------------------------
+-- @function [parent=#EventListener] setEnabled 
+-- @param self
+-- @param #bool bool
+        
+--------------------------------
 -- @function [parent=#EventListener] clone 
 -- @param self
 -- @return EventListener#EventListener ret (return value: cc.EventListener)
+        
+--------------------------------
+-- @function [parent=#EventListener] isEnabled 
+-- @param self
+-- @return bool#bool ret (return value: bool)
         
 --------------------------------
 -- @function [parent=#EventListener] checkAvailable 
