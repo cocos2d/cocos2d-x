331f5b1115e8a1718f925f36262e569118cca790
shujunqiao <shujun.qiao@chukong-inc.com>
2014-04-04 16:30:33 +0800

    add & in ccconsole.[ci skip]

--- cocos/base/CCConsole.cpp
+++ cocos/base/CCConsole.cpp
@@ -994,7 +994,7 @@ void Console::log(const char* buf)
     }
 }
 
-void Console::sendSocket(int fd, const std::string str)
+void Console::sendSocket(int fd, const std::string& str)
 {
     CCLOG("fd: %d, str:%s.", fd, str.c_str());
     send(fd, str.c_str(), str.length(), 0);
