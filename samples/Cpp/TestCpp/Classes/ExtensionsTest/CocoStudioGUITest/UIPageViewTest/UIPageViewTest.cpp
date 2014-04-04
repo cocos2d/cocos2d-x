741caa02fe9682555fbefaba04b4ad2d855c00ff
andyque <guanghui8827@126.com>
2014-04-04 14:36:49 +0800

    issue #4636. finish UIPageViewTest

--- samples/Cpp/TestCpp/Classes/ExtensionsTest/CocoStudioGUITest/UIPageViewTest/UIPageViewTest.cpp
+++ samples/Cpp/TestCpp/Classes/ExtensionsTest/CocoStudioGUITest/UIPageViewTest/UIPageViewTest.cpp
@@ -21,21 +21,18 @@ bool UIPageViewTest::init()
         CCSize widgetSize = m_pWidget->getSize();
         
         // Add a label in which the dragpanel events will be displayed
-        m_pDisplayValueLabel = UILabel::create();
-        m_pDisplayValueLabel->setText("Move by horizontal direction");
-        m_pDisplayValueLabel->setFontName("Marker Felt");
-        m_pDisplayValueLabel->setFontSize(32);
+        m_pDisplayValueLabel = UILabel::create("Move by horizontal direction","Marker Felt",32);
         m_pDisplayValueLabel->setAnchorPoint(ccp(0.5f, -1));
-        m_pDisplayValueLabel->setPosition(ccp(widgetSize.width / 2.0f, widgetSize.height / 2.0f + m_pDisplayValueLabel->getContentSize().height * 1.5));
+        m_pDisplayValueLabel->setPosition(ccp(widgetSize.width / 2.0f,
+                                              widgetSize.height / 2.0f +
+                                              m_pDisplayValueLabel->getContentSize().height * 1.5));
         m_pUiLayer->addWidget(m_pDisplayValueLabel);
         
         // Add the black background
-        UILabel *alert = UILabel::create();
-        alert->setText("PageView");
-        alert->setFontName("Marker Felt");
-        alert->setFontSize(30);
+        UILabel *alert = UILabel::create("PageView","Marker Felt",30);
         alert->setColor(ccc3(159, 168, 176));
-        alert->setPosition(ccp(widgetSize.width / 2.0f, widgetSize.height / 2.0f - alert->getSize().height * 3.075));
+        alert->setPosition(ccp(widgetSize.width / 2.0f,
+                               widgetSize.height / 2.0f - alert->getSize().height * 3.075));
         m_pUiLayer->addWidget(alert);
         
         UILayout *background = dynamic_cast<UILayout*>(m_pUiLayer->getWidgetByName("background_Panel"));
@@ -55,10 +52,8 @@ bool UIPageViewTest::init()
             UILayout* layout = UILayout::create();
             layout->setSize(CCSizeMake(240, 130));
             
-            UIImageView* imageView = UIImageView::create();
-            imageView->setTouchEnabled(true);
+            UIImageView* imageView = UIImageView::create("cocosgui/scrollviewbg.png");
             imageView->setScale9Enabled(true);
-            imageView->loadTexture("cocosgui/scrollviewbg.png");
             imageView->setSize(CCSizeMake(240, 130));
             imageView->setPosition(ccp(layout->getSize().width / 2, layout->getSize().height / 2));
             layout->addChild(imageView);
