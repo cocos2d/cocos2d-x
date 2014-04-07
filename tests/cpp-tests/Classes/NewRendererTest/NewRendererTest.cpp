beda294be1071aec62a15bae3aff8602b8d94bd3
Huabing.Xu <dabingnn@gmail.com>
2014-04-07 22:02:43 +0800

    issue #4629: add test case for group command

--- tests/cpp-tests/Classes/NewRendererTest/NewRendererTest.cpp
+++ tests/cpp-tests/Classes/NewRendererTest/NewRendererTest.cpp
@@ -35,6 +35,7 @@ static std::function<Layer*()> createFunctions[] =
 {
     CL(NewSpriteTest),
     CL(NewSpriteBatchTest),
+    CL(GroupCommandTest),
     CL(NewClippingNodeTest),
     CL(NewDrawNodeTest),
     CL(NewCullingTest),
@@ -220,6 +221,28 @@ std::string NewSpriteTest::title() const
 std::string NewSpriteTest::subtitle() const
 {
     return "SpriteTest";
+}
+
+GroupCommandTest::GroupCommandTest()
+{
+    auto sprite = Sprite::create("Images/grossini.png");
+    Size winSize = Director::getInstance()->getWinSize();
+    sprite->setPosition(winSize.width/2,winSize.height/2);
+    addChild(sprite);
+}
+
+GroupCommandTest::~GroupCommandTest()
+{
+}
+
+std::string GroupCommandTest::title() const
+{
+    return "Renderer";
+}
+
+std::string GroupCommandTest::subtitle() const
+{
+    return "GroupCommandTest: You should see a sprite";
 }
 
 //-------- New Sprite Batch Test
