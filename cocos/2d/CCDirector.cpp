49bba30409df9a16e017915505b466729febbc90
James Chen <jianhua.chen@cocos2d-x.org>
2014-04-08 15:08:34 +0800

    closed #4710: [win32] Crash in EventDispatcher if game starts up with mouse moving frequently

--- cocos/2d/CCDirector.cpp
+++ cocos/2d/CCDirector.cpp
@@ -393,7 +393,10 @@ void Director::setOpenGLView(GLView *openGLView)
 
         CHECK_GL_ERROR_DEBUG();
 
-//        _touchDispatcher->setDispatchEvents(true);
+		if (_eventDispatcher)
+		{
+			_eventDispatcher->setEnabled(true);
+		}
     }
 }
 
@@ -747,9 +750,11 @@ void Director::purgeDirector()
     // cleanup scheduler
     getScheduler()->unscheduleAll();
     
-    // don't release the event handlers
-    // They are needed in case the director is run again
-//    _touchDispatcher->removeAllDelegates();
+    // Disable event dispatching
+	if (_eventDispatcher)
+	{
+		_eventDispatcher->setEnabled(false);
+	}
 
     if (_runningScene)
     {
