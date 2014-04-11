2ebdf5c25954712f9dce100d77f7bed6aede5699
Dale Stammen <dalestam@microsoft.com>
2014-04-11 10:14:17 -0700

    fixed bad #ifdef

--- cocos2dx/shaders/CCGLProgram.cpp
+++ cocos2dx/shaders/CCGLProgram.cpp
@@ -296,7 +296,7 @@ bool CCGLProgram::link()
     
     m_uVertShader = m_uFragShader = 0;
 	
-#if DEBUG || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
+#if defined(DEBUG) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
     glGetProgramiv(m_uProgram, GL_LINK_STATUS, &status);
 	
     if (status == GL_FALSE)
