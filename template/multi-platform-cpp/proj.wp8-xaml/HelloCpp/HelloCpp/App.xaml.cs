b76ac86161cc386a4170e8f2f75f0fb60a78a5f8
Dale Stammen <dalestam@microsoft.com>
2014-04-11 09:59:36 -0700

    updated winrt and wp8 template projects

--- template/multi-platform-cpp/proj.wp8-xaml/HelloCpp/HelloCpp/App.xaml.cs
+++ template/multi-platform-cpp/proj.wp8-xaml/HelloCpp/HelloCpp/App.xaml.cs
@@ -41,7 +41,7 @@ namespace PhoneDirect3DXamlAppInterop
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 // Display the current frame rate counters.
-                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+                Application.Current.Host.Settings.EnableFrameRateCounter = false;
 
                 // Show the areas of the app that are being redrawn in each frame.
                 //Application.Current.Host.Settings.EnableRedrawRegions = true;
