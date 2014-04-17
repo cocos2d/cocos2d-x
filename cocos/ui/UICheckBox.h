19f6526ef8554be54a2dc67e7eab9f63b7c90f00
CaiWenzhi <wenzhi.cai@chukong-inc.com>
2014-04-17 14:08:25 +0800

    Widget supports contentsize

--- cocos/ui/UICheckBox.h
+++ cocos/ui/UICheckBox.h
@@ -164,8 +164,6 @@ public:
      */
     bool getSelectedState();
 
-    //override "setAnchorPoint" method of widget.
-    virtual void setAnchorPoint(const Point &pt) override;
 
     //add a call back function would called when checkbox is selected or unselected.
     void addEventListenerCheckBox(Ref* target,SEL_SelectedStateEvent selector);
@@ -173,8 +171,8 @@ public:
     //override "onTouchEnded" method of widget.
     virtual void onTouchEnded(Touch *touch, Event *unusedEvent);
 
-    //override "getContentSize" method of widget.
-    virtual const Size& getContentSize() const override;
+    //override "getVirtualRendererSize" method of widget.
+    virtual const Size& getVirtualRendererSize() const override;
 
     //override "getVirtualRenderer" method of widget.
     virtual Node* getVirtualRenderer() override;
@@ -213,6 +211,7 @@ protected:
     void frontCrossDisabledTextureScaleChangedWithSize();
     virtual Widget* createCloneInstance() override;
     virtual void copySpecialProperties(Widget* model) override;
+    virtual void adaptRenderers() override;
 protected:
     Sprite* _backGroundBoxRenderer;
     Sprite* _backGroundSelectedBoxRenderer;
@@ -235,6 +234,12 @@ protected:
     std::string _frontCrossFileName;
     std::string _backGroundDisabledFileName;
     std::string _frontCrossDisabledFileName;
+    
+    bool _backGroundBoxRendererAdaptDirty;
+    bool _backGroundSelectedBoxRendererAdaptDirty;
+    bool _frontCrossRendererAdaptDirty;
+    bool _backGroundBoxDisabledRendererAdaptDirty;
+    bool _frontCrossDisabledRendererAdaptDirty;
 };
 
 }
