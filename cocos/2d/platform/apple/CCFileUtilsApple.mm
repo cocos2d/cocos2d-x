8ea246d89b3c49acacf2e5c2a5ae17a7ceb4b85b
Nick Barrios <nick@wefiends.com>
2014-04-17 03:08:57 -0400

    CCFileUtilsApple: strRet should be assigned from documentsDirectory.

--- cocos/2d/platform/apple/CCFileUtilsApple.mm
+++ cocos/2d/platform/apple/CCFileUtilsApple.mm
@@ -329,7 +329,7 @@ std::string FileUtilsApple::getWritablePath() const
     // Save to Documents folder
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    std::string strRet = [bundleDirectory UTF8String];
+    std::string strRet = [documentsDirectory UTF8String];
     strRet.append("/");
     return strRet;
 #else
