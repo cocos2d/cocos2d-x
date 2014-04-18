6a573b511fcf644425b42bc18a558da6b941898e
cw <chuanweizhang2013@gmail.com>
2014-04-18 11:40:51 +0800

    amend comment

--- templates/lua-template-runtime/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
+++ templates/lua-template-runtime/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
@@ -62,7 +62,7 @@ public class AppActivity extends Cocos2dxActivity{
 		
 		//2.Set the format of window
 		
-		// Check the wifi is open when the android:debuggable is "true".
+		// Check the wifi is opened when the android:debuggable is "true".
 		if(0!=((this.getApplicationInfo().flags) & ApplicationInfo.FLAG_DEBUGGABLE))
 		{
 			if(!isWifiConnected())
