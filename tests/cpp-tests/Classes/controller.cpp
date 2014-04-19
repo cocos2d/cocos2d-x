bad448c78f8fb3a6ca604f6a9337830024d2e9fb
Dale Stammen <dalestam@microsoft.com>
2014-04-19 10:08:01 -0700

    enabled console for wp8

--- tests/cpp-tests/Classes/controller.cpp
+++ tests/cpp-tests/Classes/controller.cpp
@@ -42,19 +42,16 @@ Controller g_aTestNames[] = {
 	{ "Chipmunk", []() { return new ChipmunkAccelTouchTestScene(); } },
 	{ "Click and Move", [](){return new ClickAndMoveTestScene(); } },
 	{ "Configuration", []() { return new ConfigurationTestScene(); } },
-#if (CC_TARGET_PLATFORM != CC_PLATFORM_WP8)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
 	{ "Console", []() { return new ConsoleTestScene(); } },
+
 #endif
-#endif
-#if (CC_TARGET_PLATFORM != CC_PLATFORM_WP8)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_EMSCRIPTEN)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_NACL)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_MARMALADE)
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_BADA)
 	{ "Curl", []() { return new CurlTestScene(); } },
-#endif
 #endif
 #endif
 #endif
@@ -251,7 +248,7 @@ void TestController::onMouseScroll(Event *event)
     s_tCurPos   = nextPos;
 }
 
-#if (CC_TARGET_PLATFORM != CC_PLATFORM_WP8) && (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
+#if (CC_TARGET_PLATFORM != CC_PLATFORM_WINRT)
 void TestController::addConsoleAutoTest()
 {
     auto console = Director::getInstance()->getConsole();
