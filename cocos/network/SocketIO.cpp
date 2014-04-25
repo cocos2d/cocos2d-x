42c374157d02b8b25604a6ffc8535e6466ffe967
Jason Xu <xhcnb@qq.com>
2014-04-25 17:32:21 +0800

    CC_CALLBACK_2 not std::bind directly

--- cocos/network/SocketIO.cpp
+++ cocos/network/SocketIO.cpp
@@ -124,7 +124,7 @@ void SIOClientImpl::handshake()
     request->setUrl(pre.str().c_str());
     request->setRequestType(HttpRequest::Type::GET);
 
-    request->setResponseCallback(std::bind(&SIOClientImpl::handshakeResponse, this, std::placeholders::_1, std::placeholders::_2));
+    request->setResponseCallback(CC_CALLBACK_2(SIOClientImpl::handshakeResponse, this));
     request->setTag("handshake");
 
     log("SIOClientImpl::handshake() waiting");
