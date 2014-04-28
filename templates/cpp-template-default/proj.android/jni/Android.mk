692099e562b9031f6fec735ad7d44c2ad8d9c40c
Alex-G <Alex-G@users.noreply.github.com>
2014-04-28 13:18:57 +0200

    Update Android.mk
    With this change you don't need add new cpp files manually if they are in the root of the Classes folder.

--- templates/cpp-template-default/proj.android/jni/Android.mk
+++ templates/cpp-template-default/proj.android/jni/Android.mk
@@ -6,9 +6,10 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/hellocpp/main.cpp)
+LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp)
+
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
