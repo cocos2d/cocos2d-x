7aabe1b9a98d22000ec4a12986146c829a6e95c7
AlexV <asmodehn@gmail.com>
2014-04-30 17:29:25 +0900

    first version of MyGame running properly on emulator from android studio

--- templates/cpp-template-default/proj.android_studio/app/jni/Android.mk
+++ templates/cpp-template-default/proj.android_studio/app/jni/Android.mk
@@ -7,10 +7,10 @@ LOCAL_MODULE := cocos2dcpp_shared
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../../Classes/AppDelegate.cpp \
+                   ../../../Classes/HelloWorldScene.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
