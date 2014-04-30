de3ae96e295cc01d41f19a002b21cbe5345c79d3
AlexV <asmodehn@gmail.com>
2014-04-30 11:38:40 +0900

    copy of working java build with gradle on androidstudio

--- templates/cpp-template-default/proj.android_studio/MyGame/app/src/main/java/com/your_company/mygame/app/AppActivity.java
+++ templates/cpp-template-default/proj.android_studio/MyGame/app/src/main/java/com/your_company/mygame/app/AppActivity.java
@@ -8,30 +8,4 @@ import android.view.MenuItem;
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 public class AppActivity extends Cocos2dxActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
