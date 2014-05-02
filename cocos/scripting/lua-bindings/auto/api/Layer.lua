d75a7c51a3ea79c10f628d1452de55ee1ce01d62
CocosRobot <redmine@cocos2d-x.org>
2014-05-02 00:37:21 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/Layer.lua
+++ cocos/scripting/lua-bindings/auto/api/Layer.lua
@@ -4,22 +4,7 @@
 -- @extend Node
 
 --------------------------------
--- @function [parent=#Layer] updatePhysics 
--- @param self
--- @param #float float
-        
---------------------------------
--- @function [parent=#Layer] getPhysicsWorld 
--- @param self
--- @return PhysicsWorld#PhysicsWorld ret (return value: cc.PhysicsWorld)
-        
---------------------------------
 -- @function [parent=#Layer] create 
--- @param self
--- @return Layer#Layer ret (return value: cc.Layer)
-        
---------------------------------
--- @function [parent=#Layer] createWithPhysics 
 -- @param self
 -- @return Layer#Layer ret (return value: cc.Layer)
         
