c29c0554e0f7f35f18718f751b4030cbf6ed06b6
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-04 21:49:55 +0800

    Update download-deps.py

--- download-deps.py
+++ download-deps.py
@@ -24,21 +24,21 @@ def download_file(url, file_name):
     u = urllib2.urlopen(url)
     f = open(file_name, 'wb')
     meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print("Downloading: %s Bytes: %s" % (file_name, file_size))
+    #file_size = int(meta.getheaders("Content-Length")[0])
+    print("Downloading: %s , please wait ... " % (file_name))
 
-    file_size_dl = 0
+   # file_size_dl = 0
     block_sz = 8192
     while True:
         buffer = u.read(block_sz)
         if not buffer:
             break
 
-        file_size_dl += len(buffer)
+        #file_size_dl += len(buffer)
         f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print(status),
+        #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        #status = status + chr(8)*(len(status)+1)
+        #print(status),
     f.close()
 
 def unzip(src,dst):
