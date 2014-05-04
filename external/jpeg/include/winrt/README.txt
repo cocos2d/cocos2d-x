3b3c1c4dbbe8ba2deefc8ea38e6aa35e512015ab
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-04 14:36:16 +0800

    issue #4674: Removed large files and updated .gitignore

--- external/jpeg/include/winrt/README.txt
+++
@@ -1,3 +0,0 @@
-Add the contents of downloaded jpeg-8b folder to the directory jpeg-8b
-
-http://www.ijg.org/files/jpegsr8b.zip
--- external/jpeg/prebuilt/android/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cocos_jpeg_static
-LOCAL_MODULE_FILENAME := jpeg
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjpeg.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
-include $(PREBUILT_STATIC_LIBRARY)
