a160412fb5e705530c2c9dc0e6ae6c6d3c4733ff
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-04 19:05:01 +0800

    Updates download-deps.py

--- download-deps.py
+++ download-deps.py
@@ -24,7 +24,7 @@ def download_file(url, file_name):
     f = open(file_name, 'wb')
     meta = u.info()
     file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+    print("Downloading: %s Bytes: %s" % (file_name, file_size))
 
     file_size_dl = 0
     block_sz = 8192
@@ -37,7 +37,7 @@ def download_file(url, file_name):
         f.write(buffer)
         status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
         status = status + chr(8)*(len(status)+1)
-        print status,
+        print(status)
     f.close()
 
 def unzip(src,dst):
@@ -86,5 +86,5 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print e
+        print(e)
         sys.exit(1)
