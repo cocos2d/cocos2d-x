20d3c6d538a7826e8dfea26f8d9a05756a4d3b63
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-04 19:08:43 +0800

    Updates download-deps.py

--- download-deps.py
+++ download-deps.py
@@ -37,7 +37,7 @@ def download_file(url, file_name):
         f.write(buffer)
         status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
         status = status + chr(8)*(len(status)+1)
-        print(status)
+        print(status),
     f.close()
 
 def unzip(src,dst):
