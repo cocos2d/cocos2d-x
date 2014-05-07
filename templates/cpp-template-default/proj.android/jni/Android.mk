976ef3e2a4d85cd6f72f896afa89f2aa9947c7d2
minggo <ming@cocos2d-x.org>
2014-05-07 14:06:36 +0800

    add some comment for dependences module in Android.mk to make it simple to use other module

--- templates/cpp-template-default/proj.android/jni/Android.mk
+++ templates/cpp-template-default/proj.android/jni/Android.mk
@@ -14,11 +14,23 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
-LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+
+# LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+# LOCAL_WHOLE_STATIC_LIBRARIES := cocosbuilder_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
+# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
 $(call import-module,audio/android)
-$(call import-module,Box2D)
+
+# $(call import-module,Box2D)
+# $(call import-module,editor-support/cocosbuilder)
+# $(call import-module,editor-support/spine)
+# $(call import-module,editor-support/cocostudio)
+# $(call import-module,network)
+# $(call import-module,extensions)
