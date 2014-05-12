5dc69026acce288906f31e792708b975f8a38325
James Chen <jianhua.chen@cocos2d-x.org>
2014-05-12 17:53:51 +0800

    Updates download-deps.py to support download several files.

--- download-deps.py
+++ download-deps.py
@@ -48,157 +48,216 @@ from sys import stdout
 from distutils.errors import DistutilsError
 from distutils.dir_util import copy_tree, remove_tree
 
-_workpath=''
-_current_3rd_libs_version = ''
-_repo_name = ''
-_filename = ''
-_downloaded_file_url = ''
-_extracted_folder_name = ''
-_zip_file_size = 0
-
-def get_input_value(prompt):
-    ret = raw_input(prompt)
-    ret.rstrip(" \t")
-    return ret
-
-def download_file(url, filename):
-    print("==> Ready to download '%s' from '%s'" % (filename, url))
-    import urllib2
-    try:
-        u = urllib2.urlopen(url)
-    except urllib2.HTTPError as e:
-        if e.code == 404:
-            print("==> Error: Could not find the file from url: '%s'" % (url))
-        print("==> Http request failed, error code: " + str(e.code) + ", reason: " + e.read())
-        sys.exit(1)
-
-    f = open(filename, 'wb')
-    meta = u.info()
-    content_len = meta.getheaders("Content-Length")
-    file_size = 0
-    if content_len and len(content_len) > 0:
-        file_size = int(content_len[0])
-    else:
-        # github server may not reponse a header information which contains `Content-Length`,
-        # therefore, the size needs to be written hardcode here. While server doesn't return
-        # `Content-Length`, use it instead
-        print("==> WARNING: Couldn't grab the file size from remote, use 'zip_file_size' section in 'external/config.json'")
-        file_size = _zip_file_size
-
-    print("==> Start to download, please wait ...")
-
-    file_size_dl = 0
-    block_sz = 8192
-    block_size_per_second = 0
-    old_time=time()
-
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        block_size_per_second += len(buffer)
-        f.write(buffer)
-        new_time = time()
-        if (new_time - old_time) > 1:
-            speed = block_size_per_second / (new_time - old_time) / 1000.0
-            status = ""
-            if file_size != 0:
-                percent = file_size_dl * 100. / file_size
-                status = r"Downloaded: %6dK / Total: %dK, Percent: %3.2f%%, Speed: %6.2f KB/S " % (file_size_dl / 1000, file_size / 1000, percent, speed)
-            else:
-                status = r"Downloaded: %6dK, Speed: %6.2f KB/S " % (file_size_dl / 1000, speed)
-
-            status = status + chr(8)*(len(status)+1)
-            print(status),
-            sys.stdout.flush()
-            block_size_per_second = 0
-            old_time = new_time
-
-    print("==> Downloading finished!")
-    f.close()
-
-def default_filter(src,dst):
-    """The default progress/filter callback; returns True for all files"""
-    return dst
-
-def ensure_directory(target):
-    if not os.path.exists(target):
-        os.mkdir(target)
-
-
 class UnrecognizedFormat:
     def __init__(self, prompt):
         self._prompt = prompt
     def __str__(self):
         return self._prompt
 
-def unpack_zipfile(filename, extract_dir, progress_filter=default_filter):
-    """Unpack zip `filename` to `extract_dir`
+class CocosZipInstaller(object):
+    def __init__(self, workpath, config_path, version_path, remote_version_key = None):
+        self._workpath = workpath
+        self._config_path = config_path
+        self._version_path = version_path
 
-    Raises ``UnrecognizedFormat`` if `filename` is not a zipfile (as determined
-    by ``zipfile.is_zipfile()``).  See ``unpack_archive()`` for an explanation
-    of the `progress_filter` argument.
-    """
+        data = self.load_json_file(config_path)
 
-    if not zipfile.is_zipfile(filename):
-        raise UnrecognizedFormat("%s is not a zip file" % (filename))
+        self._current_version = data["version"]
+        self._repo_name = data["repo_name"]
+        self._filename = self._current_version + '.zip'
+        self._url = data["repo_parent"] + self._repo_name + '/archive/' + self._filename
+        self._zip_file_size = int(data["zip_file_size"])
+        # 'v' letter was swallowed by github, so we need to substring it from the 2nd letter
+        self._extracted_folder_name = self._repo_name + '-' + self._current_version[1:]
 
-    print("==> Extracting files, please wait ...")
-    z = zipfile.ZipFile(filename)
-    try:
-        for info in z.infolist():
-            name = info.filename
-
-            # don't extract absolute paths or ones with .. in them
-            if name.startswith('/') or '..' in name:
-                continue
-
-            target = os.path.join(extract_dir, *name.split('/'))
-            target = progress_filter(name, target)
-            if not target:
-                continue
-            if name.endswith('/'):
-                # directory
-                ensure_directory(target)
+        try:
+            data = self.load_json_file(version_path)
+            if remote_version_key == None:
+                self._downloaded_version = data["version"]
             else:
-                # file
-                data = z.read(info.filename)
-                f = open(target,'wb')
-                try:
-                    f.write(data)
-                finally:
-                    f.close()
-                    del data
-            unix_attributes = info.external_attr >> 16
-            if unix_attributes:
-                os.chmod(target, unix_attributes)
-    finally:
-        z.close()
-        print("==> Extraction done!")
+                self._downloaded_version = data["remote_version_key"]
+        except:
+            print("==> version file doesn't exist")
+
+    def get_input_value(self, prompt):
+        ret = raw_input(prompt)
+        ret.rstrip(" \t")
+        return ret
+
+    def download_file(self):
+        print("==> Ready to download '%s' from '%s'" % (self._filename, self._url))
+        import urllib2
+        try:
+            u = urllib2.urlopen(self._url)
+        except urllib2.HTTPError as e:
+            if e.code == 404:
+                print("==> Error: Could not find the file from url: '%s'" % (self._url))
+            print("==> Http request failed, error code: " + str(e.code) + ", reason: " + e.read())
+            sys.exit(1)
+
+        f = open(self._filename, 'wb')
+        meta = u.info()
+        content_len = meta.getheaders("Content-Length")
+        file_size = 0
+        if content_len and len(content_len) > 0:
+            file_size = int(content_len[0])
+        else:
+            # github server may not reponse a header information which contains `Content-Length`,
+            # therefore, the size needs to be written hardcode here. While server doesn't return
+            # `Content-Length`, use it instead
+            print("==> WARNING: Couldn't grab the file size from remote, use 'zip_file_size' section in '%s'" % self._config_path)
+            file_size = self._zip_file_size
+
+        print("==> Start to download, please wait ...")
+
+        file_size_dl = 0
+        block_sz = 8192
+        block_size_per_second = 0
+        old_time=time()
+
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            block_size_per_second += len(buffer)
+            f.write(buffer)
+            new_time = time()
+            if (new_time - old_time) > 1:
+                speed = block_size_per_second / (new_time - old_time) / 1000.0
+                status = ""
+                if file_size != 0:
+                    percent = file_size_dl * 100. / file_size
+                    status = r"Downloaded: %6dK / Total: %dK, Percent: %3.2f%%, Speed: %6.2f KB/S " % (file_size_dl / 1000, file_size / 1000, percent, speed)
+                else:
+                    status = r"Downloaded: %6dK, Speed: %6.2f KB/S " % (file_size_dl / 1000, speed)
+
+                status = status + chr(8)*(len(status)+1)
+                print(status),
+                sys.stdout.flush()
+                block_size_per_second = 0
+                old_time = new_time
+
+        print("==> Downloading finished!")
+        f.close()
+
+    def ensure_directory(self, target):
+        if not os.path.exists(target):
+            os.mkdir(target)
+
+    def unpack_zipfile(self, extract_dir):
+        """Unpack zip `filename` to `extract_dir`
+
+        Raises ``UnrecognizedFormat`` if `filename` is not a zipfile (as determined
+        by ``zipfile.is_zipfile()``).
+        """
+
+        if not zipfile.is_zipfile(self._filename):
+            raise UnrecognizedFormat("%s is not a zip file" % (self._filename))
+
+        print("==> Extracting files, please wait ...")
+        z = zipfile.ZipFile(self._filename)
+        try:
+            for info in z.infolist():
+                name = info.filename
+
+                # don't extract absolute paths or ones with .. in them
+                if name.startswith('/') or '..' in name:
+                    continue
+
+                target = os.path.join(extract_dir, *name.split('/'))
+                if not target:
+                    continue
+                if name.endswith('/'):
+                    # directory
+                    self.ensure_directory(target)
+                else:
+                    # file
+                    data = z.read(info.filename)
+                    f = open(target,'wb')
+                    try:
+                        f.write(data)
+                    finally:
+                        f.close()
+                        del data
+                unix_attributes = info.external_attr >> 16
+                if unix_attributes:
+                    os.chmod(target, unix_attributes)
+        finally:
+            z.close()
+            print("==> Extraction done!")
 
 
-def ask_to_delete_downloaded_zip_file(filename):
-    ret = get_input_value("==> Whether to delete the downloaded zip file? It may be reused when you execute this script next time! (yes/no): ")
-    ret = ret.strip()
-    if ret != 'yes' and ret != 'no':
-        print("==> Invalid answer, please answer 'yes' or 'no'!")
-        return ask_to_delete_downloaded_zip_file(filename)
-    else:
-        return True if ret == 'yes' else False
+    def ask_to_delete_downloaded_zip_file(self):
+        ret = self.get_input_value("==> Whether to delete '%s' file? It may be reused when you execute this script next time! (yes/no): " % self._filename)
+        ret = ret.strip()
+        if ret != 'yes' and ret != 'no':
+            print("==> Invalid answer, please answer 'yes' or 'no'!")
+            return self.ask_to_delete_downloaded_zip_file()
+        else:
+            return True if ret == 'yes' else False
 
-def download_and_unzip_file(url, filename):
-    if not os.path.isfile(filename):
-        download_file(url, filename)
-    try:
-        unpack_zipfile(filename, _workpath)
-    except UnrecognizedFormat as e:
-        print("==> Unrecognized zip format from your local '%s' file!" % (filename))
-        if os.path.isfile(filename):
-            os.remove(filename)
-        print("==> Download it from internet again, please wait...")
-        download_and_unzip_file(url, filename)
+    def download_zip_file(self):
+        if not os.path.isfile(self._filename):
+            self.download_file()
+        try:
+            if not zipfile.is_zipfile(self._filename):
+                raise UnrecognizedFormat("%s is not a zip file" % (self._filename))
+        except UnrecognizedFormat as e:
+            print("==> Unrecognized zip format from your local '%s' file!" % (self._filename))
+            if os.path.isfile(self._filename):
+                os.remove(self._filename)
+            print("==> Download it from internet again, please wait...")
+            self.download_zip_file()
+
+    def need_to_update(self):
+        if not os.path.isfile(self._version_path):
+            return True
+
+        with open(self._version_path) as data_file:
+            data = json.load(data_file)
+
+        if data['version'] == self._current_version:
+            return False
+        return True
+
+    def load_json_file(self, file_path):
+        if not os.path.isfile(file_path):
+            raise Exception("Could not find (%s)" % (file_path))
+
+        with open(file_path) as data_file:
+            data = json.load(data_file)
+        return data
+
+    def run(self, folder_for_extracting, remove_downloaded, force_update, download_only):
+        if not force_update and not self.need_to_update():
+            print("==> Not need to update!")
+            return
+
+        if os.path.exists(self._extracted_folder_name):
+            shutil.rmtree(self._extracted_folder_name)
+
+        self.download_zip_file()
+
+        if not download_only:
+            self.unpack_zipfile(self._workpath)
+            print("==> Copying files...")
+            if not os.path.exists(folder_for_extracting):
+                os.mkdir(folder_for_extracting)
+            distutils.dir_util.copy_tree(self._extracted_folder_name, folder_for_extracting)
+            print("==> Cleaning...")
+            if os.path.exists(self._extracted_folder_name):
+                shutil.rmtree(self._extracted_folder_name)
+            if os.path.isfile(self._filename):
+                if remove_downloaded != None:
+                    if remove_downloaded == 'yes':
+                        os.remove(self._filename)
+                elif self.ask_to_delete_downloaded_zip_file():
+                    os.remove(self._filename)
+        else:
+            print("==> Download (%s) finish!" % self._filename)
+
 
 def _check_python_version():
     major_ver = sys.version_info[0]
@@ -209,85 +268,37 @@ def _check_python_version():
 
     return True
 
-def need_to_update_3rd_libs():
-    version_file_path = os.path.join(_workpath, "external", "version.json")
-    if not os.path.isfile(version_file_path):
-        return True
-
-    with open(version_file_path) as data_file:
-        data = json.load(data_file)
-
-    if data['prebuilt_libs_version'] == _current_3rd_libs_version:
-        return False
-    return True
-
-def grab_config():
-    config_file_path = os.path.join(_workpath, "external", "config.json")
-    if not os.path.isfile(config_file_path):
-        raise Exception("Could not find 'external/config.json'")
-
-    with open(config_file_path) as data_file:
-        data = json.load(data_file)
-
-    global _current_3rd_libs_version
-    global _repo_name
-    global _filename
-    global _downloaded_file_url
-    global _extracted_folder_name
-    global _zip_file_size
-
-    _current_3rd_libs_version = data["current_libs_version"]
-    _repo_name = data["repo_name"]
-    _filename = _current_3rd_libs_version + '.zip'
-    _downloaded_file_url = data["repo_parent"] + _repo_name + '/archive/' + _filename
-    _zip_file_size = int(data["zip_file_size"])
-    # 'v' letter was swallowed by github, so we need to substring it from the 2nd letter
-    _extracted_folder_name = _repo_name + '-' + _current_3rd_libs_version[1:]
-
 def main():
-    _workpath = os.path.dirname(os.path.realpath(__file__))
+    workpath = os.path.dirname(os.path.realpath(__file__))
 
     if not _check_python_version():
         exit()
 
     parser = OptionParser()
     parser.add_option('-r', '--remove-download',
-                      action="store", type="string", dest='to_remove_downloaded_zip_file', default=None,
+                      action="store", type="string", dest='remove_downloaded', default=None,
                       help="Whether to remove downloaded zip file, 'yes' or 'no'")
 
     parser.add_option("-f", "--force-update",
                       action="store_true", dest="force_update", default=False,
                       help="Whether to force update the third party libraries")
 
+    parser.add_option("-d", "--download-only",
+                      action="store_true", dest="download_only", default=False,
+                      help="Only download zip file of the third party libraries, will not extract it")
+
     (opts, args) = parser.parse_args()
 
-    grab_config()
+    print("=======================================================")
+    print("==> Prepare to download external libraries!")
+    installer = CocosZipInstaller(workpath, os.path.join(workpath, 'external', 'config.json'), os.path.join(workpath, 'external', 'version.json'), "prebuilt_libs_version")
+    installer.run("external", opts.remove_downloaded, opts.force_update, opts.download_only)
 
-    if not opts.force_update and not need_to_update_3rd_libs():
-        print("==> Not need to update the third party libraries!")
-        exit()
-
-    if os.path.exists(_extracted_folder_name):
-        shutil.rmtree(_extracted_folder_name)
-
-    download_and_unzip_file(_downloaded_file_url, _filename)
-
-    print("==> Copying files...")
-    folder_for_extracting = os.path.join(_workpath, 'external')
-    if not os.path.exists(folder_for_extracting):
-        os.mkdir(folder_for_extracting)
-    distutils.dir_util.copy_tree(_extracted_folder_name, folder_for_extracting)
-    print("==> Cleaning...")
-    if os.path.exists(_extracted_folder_name):
-        shutil.rmtree(_extracted_folder_name)
-    if os.path.isfile(_filename):
-        if opts.to_remove_downloaded_zip_file != None:
-            if opts.to_remove_downloaded_zip_file == 'yes':
-                os.remove(_filename)
-        elif ask_to_delete_downloaded_zip_file(_filename):
-            os.remove(_filename)
-
-    print("==> DONE! Prebuilt libraries were installed successfully! Cheers!")
+    print("=======================================================")
+    print("==> Prepare to download lua runtime binaries")
+    runtime_path = os.path.join(workpath, 'templates', 'lua-template-runtime', 'runtime')
+    installer = CocosZipInstaller(workpath, os.path.join(runtime_path, 'config.json'), os.path.join(runtime_path, 'version.json'))
+    installer.run(runtime_path, opts.remove_downloaded, opts.force_update, opts.download_only)
 
 # -------------- main --------------
 if __name__ == '__main__':
