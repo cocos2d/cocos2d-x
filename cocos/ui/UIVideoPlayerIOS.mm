e6a36fde18d7678e572572b4b693ba32844a7f7a
andyque <guanghui8827@126.com>
2014-05-14 11:18:11 +0800

    issue #5110, refactor VideoPlayer

--- cocos/ui/UIVideoPlayerIOS.mm
+++ cocos/ui/UIVideoPlayerIOS.mm
@@ -35,15 +35,15 @@ using namespace cocos2d::experimental::ui;
 
 @property (strong,nonatomic) MPMoviePlayerController * moviePlayer;
 
-- (void) setVideoRect:(int) left :(int) top :(int) width :(int) height;
-- (void) setVideoURL:(int) videoSource :(std::string&) videoUrl;
-- (void) startVideo;
-- (void) pauseVideo;
-- (void) resumeVideo;
-- (void) stopVideo;
-- (void) seekVideoTo:(float) sec;
-- (void) setVideoVisible:(bool) visible;
-- (void) setVideoKeepRatioEnabled:(bool) enabled;
+- (void) setFrame:(int) left :(int) top :(int) width :(int) height;
+- (void) setURL:(int) videoSource :(std::string&) videoUrl;
+- (void) play;
+- (void) pause;
+- (void) resume;
+- (void) stop;
+- (void) seekTo:(float) sec;
+- (void) setVisible:(bool) visible;
+- (void) setKeepRatioEnabled:(bool) enabled;
 - (void) setFullScreenEnabled:(bool) enabled;
 - (bool) isFullScreenEnabled;
 
@@ -92,7 +92,7 @@ using namespace cocos2d::experimental::ui;
     [super dealloc];
 }
 
--(void) setVideoRect:(int)left :(int)top :(int)width :(int)height
+-(void) setFrame:(int)left :(int)top :(int)width :(int)height
 {
     _left = left;
     _width = width;
@@ -119,7 +119,7 @@ using namespace cocos2d::experimental::ui;
     return false;
 }
 
--(void) setVideoURL:(int)videoSource :(std::string &)videoUrl
+-(void) setURL:(int)videoSource :(std::string &)videoUrl
 {
     if (self.moviePlayer != nullptr) {
         [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:self.moviePlayer];
@@ -169,7 +169,7 @@ using namespace cocos2d::experimental::ui;
     {
         if([self.moviePlayer playbackState] != MPMoviePlaybackStateStopped)
         {
-            _videoPlayer->onVideoEvent(VideoPlayer::EventType::COMPLETED);
+            _videoPlayer->onPlayEvent(VideoPlayer::EventType::COMPLETED);
         }
     }
 }
@@ -179,13 +179,13 @@ using namespace cocos2d::experimental::ui;
     MPMoviePlaybackState state = [self.moviePlayer playbackState];
     switch (state) {
         case MPMoviePlaybackStatePaused:
-            _videoPlayer->onVideoEvent(VideoPlayer::EventType::PAUSED);
+            _videoPlayer->onPlayEvent(VideoPlayer::EventType::PAUSED);
             break;
         case MPMoviePlaybackStateStopped:
-            _videoPlayer->onVideoEvent(VideoPlayer::EventType::STOPPED);
+            _videoPlayer->onPlayEvent(VideoPlayer::EventType::STOPPED);
             break;
         case MPMoviePlaybackStatePlaying:
-            _videoPlayer->onVideoEvent(VideoPlayer::EventType::PLAYING);
+            _videoPlayer->onPlayEvent(VideoPlayer::EventType::PLAYING);
             break;
         case MPMoviePlaybackStateInterrupted:
             break;
@@ -198,21 +198,21 @@ using namespace cocos2d::experimental::ui;
     }
 }
 
--(void) seekVideoTo:(float)sec
+-(void) seekTo:(float)sec
 {
     if (self.moviePlayer != NULL) {
         [self.moviePlayer setCurrentPlaybackTime:(sec)];
     }
 }
 
--(void) setVideoVisible:(bool)visible
+-(void) setVisible:(bool)visible
 {
     if (self.moviePlayer != NULL) {
         [self.moviePlayer.view setHidden:visible];
     }
 }
 
--(void) setVideoKeepRatioEnabled:(bool)enabled
+-(void) setKeepRatioEnabled:(bool)enabled
 {
     _keepRatioEnabled = enabled;
     if (self.moviePlayer != NULL) {
@@ -224,7 +224,7 @@ using namespace cocos2d::experimental::ui;
     }
 }
 
--(void) startVideo
+-(void) play
 {
     if (self.moviePlayer != NULL) {
         [self.moviePlayer.view setFrame:CGRectMake(_left, _top, _width, _height)];
@@ -232,14 +232,14 @@ using namespace cocos2d::experimental::ui;
     }
 }
 
--(void) pauseVideo
+-(void) pause
 {
     if (self.moviePlayer != NULL) {
         [self.moviePlayer pause];
     }
 }
 
--(void) resumeVideo
+-(void) resume
 {
     if (self.moviePlayer != NULL) {
         if([self.moviePlayer playbackState] == MPMoviePlaybackStatePaused)
@@ -249,7 +249,7 @@ using namespace cocos2d::experimental::ui;
     }
 }
 
--(void) stopVideo
+-(void) stop
 {
     if (self.moviePlayer != NULL) {
         [self.moviePlayer stop];
@@ -281,7 +281,7 @@ using namespace cocos2d::experimental::ui;
 
 VideoPlayer::VideoPlayer()
 : _videoPlayerIndex(-1)
-, _callback(nullptr)
+, _eventCallback(nullptr)
 , _fullScreenEnabled(false)
 , _fullScreenDirty(false)
 , _keepAspectRatioEnabled(false)
@@ -298,18 +298,18 @@ VideoPlayer::~VideoPlayer()
     }
 }
 
-void VideoPlayer::setVideoFileName(const std::string& fileName)
+void VideoPlayer::setFileName(const std::string& fileName)
 {
-    _videoUrl = fileName;
-    _videoSource = VideoPlayer::VideoSource::FILENAME;
-    [((UIVideoViewWrapperIos*)_videiView) setVideoURL:(int)_videoSource :_videoUrl];
+    _videoURL = fileName;
+    _videoSource = VideoPlayer::Source::FILENAME;
+    [((UIVideoViewWrapperIos*)_videiView) setURL:(int)_videoSource :_videoURL];
 }
 
-void VideoPlayer::setVideoURL(const std::string& videoUrl)
+void VideoPlayer::setURL(const std::string& videoUrl)
 {
-    _videoUrl = videoUrl;
-    _videoSource = VideoPlayer::VideoSource::URL;
-    [((UIVideoViewWrapperIos*)_videiView) setVideoURL:(int)_videoSource :_videoUrl];
+    _videoURL = videoUrl;
+    _videoSource = VideoPlayer::Source::URL;
+    [((UIVideoViewWrapperIos*)_videiView) setURL:(int)_videoSource :_videoURL];
 }
 
 void VideoPlayer::draw(Renderer* renderer, const Matrix &transform, bool transformUpdated)
@@ -331,7 +331,7 @@ void VideoPlayer::draw(Renderer* renderer, const Matrix &transform, bool transfo
         auto uiLeft = (frameSize.width / 2 + (leftBottom.x - winSize.width / 2 ) * glView->getScaleX()) / scaleFactor;
         auto uiTop = (frameSize.height /2 - (rightTop.y - winSize.height / 2) * glView->getScaleY()) / scaleFactor;
         
-        [((UIVideoViewWrapperIos*)_videiView) setVideoRect:uiLeft :uiTop
+        [((UIVideoViewWrapperIos*)_videiView) setFrame :uiLeft :uiTop
                                                           :(rightTop.x - leftBottom.x) * glView->getScaleX() / scaleFactor
                                                           :( (rightTop.y - leftBottom.y) * glView->getScaleY()/scaleFactor)];
     }
@@ -358,7 +358,7 @@ void VideoPlayer::setKeepAspectRatioEnabled(bool enable)
     if (_keepAspectRatioEnabled != enable)
     {
         _keepAspectRatioEnabled = enable;
-        [((UIVideoViewWrapperIos*)_videiView) setVideoKeepRatioEnabled:enable];
+        [((UIVideoViewWrapperIos*)_videiView) setKeepRatioEnabled:enable];
     }
 }
 
@@ -387,43 +387,43 @@ void VideoPlayer::drawDebugData()
 }
 #endif
 
-void VideoPlayer::startVideo()
+void VideoPlayer::play()
 {
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) startVideo];
+        [((UIVideoViewWrapperIos*)_videiView) play];
     }
 }
 
-void VideoPlayer::pauseVideo()
+void VideoPlayer::pause()
 {
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) pauseVideo];
+        [((UIVideoViewWrapperIos*)_videiView) pause];
     }
 }
 
-void VideoPlayer::resumeVideo()
+void VideoPlayer::resume()
 {
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) resumeVideo];
+        [((UIVideoViewWrapperIos*)_videiView) resume];
     }
 }
 
-void VideoPlayer::stopVideo()
+void VideoPlayer::stop()
 {
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) stopVideo];
+        [((UIVideoViewWrapperIos*)_videiView) stop];
     }
 }
 
-void VideoPlayer::seekVideoTo(float sec)
+void VideoPlayer::seekTo(float sec)
 {
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) seekVideoTo:sec];
+        [((UIVideoViewWrapperIos*)_videiView) seekTo:sec];
     }
 }
 
@@ -436,18 +436,18 @@ void VideoPlayer::setVisible(bool visible)
 {
     cocos2d::ui::Widget::setVisible(visible);
     
-    if (! _videoUrl.empty())
+    if (! _videoURL.empty())
     {
-        [((UIVideoViewWrapperIos*)_videiView) setVideoVisible:visible];
+        [((UIVideoViewWrapperIos*)_videiView) setVisible:visible];
     }
 }
 
-void VideoPlayer::addEventListener(const VideoPlayer::EventCallback& callback)
+void VideoPlayer::addEventListener(const VideoPlayer::ccVideoPlayerCallback& callback)
 {
-    _callback = callback;
+    _eventCallback = callback;
 }
 
-void VideoPlayer::onVideoEvent(VideoPlayer::EventType event)
+void VideoPlayer::onPlayEvent(VideoPlayer::EventType event)
 {
     if (event == VideoPlayer::EventType::PLAYING) {
         _isPlaying = true;
@@ -455,9 +455,9 @@ void VideoPlayer::onVideoEvent(VideoPlayer::EventType event)
         _isPlaying = false;
     }
     
-    if (_callback)
+    if (_eventCallback)
     {
-        _callback(this,event);
+        _eventCallback(this,event);
     }
 }
 
