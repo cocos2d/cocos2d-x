e098ed7a5c187256be8313ec5d66294d3554c914
yangxiao <yangxiao@cocos2d-x.org>
2014-05-16 16:02:27 -0700

    bind buffer to 0 when finish draw

--- cocos/renderer/CCMeshCommand.cpp
+++ cocos/renderer/CCMeshCommand.cpp
@@ -147,6 +147,9 @@ void MeshCommand::execute()
     
     //restore render state
     restoreRenderState();
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 NS_CC_END
