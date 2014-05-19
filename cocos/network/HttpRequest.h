ce3ac85abad8c3ed3944838b29208a51dc0b9d0a
gonjay <assyer@gmail.com>
2014-05-19 22:56:26 +0800

    HttpRequset.h translate into Chinese

--- cocos/network/HttpRequest.h
+++ cocos/network/HttpRequest.h
@@ -43,8 +43,8 @@ typedef void (cocos2d::Ref::*SEL_HttpResponse)(HttpClient* client, HttpResponse*
 #define httpresponse_selector(_SELECTOR) (cocos2d::network::SEL_HttpResponse)(&_SELECTOR)
 
 /** 
- @brief defines the object which users must packed for HttpClient::send(HttpRequest*) method.
- Please refer to tests/test-cpp/Classes/ExtensionTest/NetworkTest/HttpClientTest.cpp as a sample
+ @brief 定义了用户必须为 HttpClient::send(HttpRequest*) 方法打包的对象
+ 请移步 tests/test-cpp/Classes/ExtensionTest/NetworkTest/HttpClientTest.cpp 作为参考
  @since v2.0.2
  */
 
@@ -57,7 +57,7 @@ typedef void (cocos2d::Ref::*SEL_HttpResponse)(HttpClient* client, HttpResponse*
 class HttpRequest : public Ref
 {
 public:
-    /** Use this enum type as param in setReqeustType(param) */
+    /** 在setRequestType(param)时使用这个枚举类型作为参数 */
     enum class Type
     {
         GET,
@@ -67,11 +67,11 @@ public:
         UNKNOWN,
     };
     
-    /** Constructor
-        Because HttpRequest object will be used between UI thead and network thread,
-        requestObj->autorelease() is forbidden to avoid crashes in AutoreleasePool
-        new/retain/release still works, which means you need to release it manually
-        Please refer to HttpRequestTest.cpp to find its usage
+    /** 构造
+        因为 HttpRequset 对象会被用在UI线程和network线程之间，
+        为了避免崩溃，在AutoreleasePool中，requestObj->autorelease()是被禁止的
+        new/retain/release 依然工作，意味着你需要手动释放它。
+        请移步 HttpRequestTest.cpp 找到用例
      */
     HttpRequest()
     {
@@ -85,7 +85,7 @@ public:
         _pUserData = nullptr;
     };
     
-    /** Destructor */
+    /** 析构 */
     virtual ~HttpRequest()
     {
         if (_pTarget)
@@ -94,7 +94,7 @@ public:
         }
     };
     
-    /** Override autorelease method to avoid developers to call it */
+    /** 复写 autorelease 方法来避免开发者调用它 */
     Ref* autorelease(void)
     {
         CCASSERT(false, "HttpResponse is used between network thread and ui thread \
@@ -102,40 +102,40 @@ public:
         return NULL;
     }
             
-    // setter/getters for properties
+    // 属性的 setter/getters
      
-    /** Required field for HttpRequest object before being sent.
-        kHttpGet & kHttpPost is currently supported
+    /** 在发送之前，HttpRequest对象的必填字段
+        目前支持 kHttpGet & kHttpPost 
      */
     inline void setRequestType(Type type)
     {
         _requestType = type;
     };
-    /** Get back the kHttpGet/Post/... enum value */
+    /** 取回 kHttpGet/Post/... 的枚举值*/
     inline Type getRequestType()
     {
         return _requestType;
     };
     
-    /** Required field for HttpRequest object before being sent.
+    /** 在发送之前，HttpRequest对象的必填字段
      */
     inline void setUrl(const char* url)
     {
         _url = url;
     };
-    /** Get back the setted url */
+    /** 取回设置的url */
     inline const char* getUrl()
     {
         return _url.c_str();
     };
     
-    /** Option field. You can set your post data here
+    /** 可选字段。你可以在这里设置你的post数据
      */
     inline void setRequestData(const char* buffer, size_t len)
     {
         _requestData.assign(buffer, buffer + len);
     };
-    /** Get the request data pointer back */
+    /** 取回请求数据的指针 */
     inline char* getRequestData()
     {
         if(_requestData.size() != 0)
@@ -143,42 +143,42 @@ public:
 
         return nullptr;
     }
-    /** Get the size of request data back */
+    /** 取回请求数据的大小 */
     inline ssize_t getRequestDataSize()
     {
         return _requestData.size();
     }
     
-    /** Option field. You can set a string tag to identify your request, this tag can be found in HttpResponse->getHttpRequest->getTag()
+    /** 可选字段。你可以设置一个string tag来标记你的请求，这个tag能通过HttpResponse->getHttpRequest->getTag()被找到。
      */
     inline void setTag(const char* tag)
     {
         _tag = tag;
     };
-    /** Get the string tag back to identify the request. 
-        The best practice is to use it in your MyClass::onMyHttpRequestCompleted(sender, HttpResponse*) callback
+    /** 获取string tag来认出请求。
+        最佳实践是在你的 MyClass::onMyHttpRequestCompleted(sender, HttpResponse*) callback 中使用
      */
     inline const char* getTag()
     {
         return _tag.c_str();
     };
     
-    /** Option field. You can attach a customed data in each request, and get it back in response callback.
-        But you need to new/delete the data pointer manully
+    /** 可选字段。你可以在每个请求中附上自定义的数据，并在响应回调中获取它。
+        但是你需要手动 new/delete 数据指针
      */
     inline void setUserData(void* pUserData)
     {
         _pUserData = pUserData;
     };
-    /** Get the pre-setted custom data pointer back.
-        Don't forget to delete it. HttpClient/HttpResponse/HttpRequest will do nothing with this pointer
+    /** 获取预先设定好的自定义数据指针。
+        不要忘了删除它。HttpClient/HttpResponse/HttpRequest 不会对这个指针做任何处理
      */
     inline void* getUserData()
     {
         return _pUserData;
     };
     
-    /** Required field. You should set the callback selector function at ack the http request completed
+    /** 必填字段。你应该在ack http 请求完成设置回调selector函数(You should set the callback selector function at ack the http request completed)
      */
     CC_DEPRECATED_ATTRIBUTE inline void setResponseCallback(Ref* pTarget, SEL_CallFuncND pSelector)
     {
@@ -201,14 +201,14 @@ public:
         _pCallback = callback;
     }
     
-    /** Get the target of callback selector funtion, mainly used by HttpClient */
+    /** 得到回调selector函数的目标，主要被HttpClient使用 */
     inline Ref* getTarget()
     {
         return _pTarget;
     }
 
-    /* This sub class is just for migration SEL_CallFuncND to SEL_HttpResponse, 
-       someday this way will be removed */
+    /* 这个内部类仅仅为了将 SEL_CallFuncND 迁移到 SEL_HttpResponse,
+       在未来某天会被去掉 */
     class _prxy
     {
     public:
@@ -220,7 +220,7 @@ public:
         SEL_HttpResponse _cb;
     };
     
-    /** Get the selector function pointer, mainly used by HttpClient */
+    /** 得到回调selector函数的指针，主要被HttpClient使用 */
     inline _prxy getSelector()
     {
         return _prxy(_pSelector);
@@ -231,29 +231,29 @@ public:
         return _pCallback;
     }
     
-    /** Set any custom headers **/
+    /** 设置任何自定义头部 **/
     inline void setHeaders(std::vector<std::string> pHeaders)
    	{
    		_headers=pHeaders;
    	}
    
-    /** Get custom headers **/
+    /** 获取自定义头部 **/
    	inline std::vector<std::string> getHeaders()
    	{
    		return _headers;
    	}
     
 protected:
-    // properties
-    Type                        _requestType;    /// kHttpRequestGet, kHttpRequestPost or other enums
-    std::string                 _url;            /// target url that this request is sent to
-    std::vector<char>           _requestData;    /// used for POST
-    std::string                 _tag;            /// user defined tag, to identify different requests in response callback
-    Ref*                        _pTarget;        /// callback target of pSelector function
-    SEL_HttpResponse            _pSelector;      /// callback function, e.g. MyLayer::onHttpResponse(HttpClient *sender, HttpResponse * response)
-    ccHttpRequestCallback       _pCallback;      /// C++11 style callbacks
-    void*                       _pUserData;      /// You can add your customed data here 
-    std::vector<std::string>    _headers;		      /// custom http headers
+    // 属性
+    Type                        _requestType;    /// kHttpRequestGet, kHttpRequestPost 或者其他枚举类型
+    std::string                 _url;            /// 这个请求发送的目标url
+    std::vector<char>           _requestData;    /// 用于POST
+    std::string                 _tag;            /// 用户定义的tag，用来在响应回调中识别不同的请求
+    Ref*                        _pTarget;        /// pSelector 函数的回调目标
+    SEL_HttpResponse            _pSelector;      /// 回调函数，例如：MyLayer::onHttpResponse(HttpClient *sender, HttpResponse * response)
+    ccHttpRequestCallback       _pCallback;      /// C++11 风格的回调
+    void*                       _pUserData;      /// 你可以在这里添加你的自定义数据
+    std::vector<std::string>    _headers;		 /// 自定义的http头部 
 };
 
 }
