b2b4edfecd17fe774caeb94fe202fa7a85cbc3c2
pandamicro <lphuabin@gmail.com>
2014-05-21 10:21:54 +0800

    Feature #4977: Remove lua manual bindings for old AssetsManager

--- cocos/scripting/lua-bindings/manual/lua_cocos2dx_extension_manual.h
+++ cocos/scripting/lua-bindings/manual/lua_cocos2dx_extension_manual.h
@@ -37,15 +37,6 @@ extern "C" {
 TOLUA_API int register_all_cocos2dx_extension_manual(lua_State* tolua_S);
 TOLUA_API int register_cocos2dx_extension_CCBProxy(lua_State* tolua_S);
 
-struct LuaAssetsManagerEventData
-{
-    int value;
-
-    LuaAssetsManagerEventData(int _value = 0):value(_value)
-    {
-    }
-};
-
 struct LuaTableViewEventData
 {
     void* value;
