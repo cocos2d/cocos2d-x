88d46a08463982ace5a7bec63ae34325c9866aa0
MagicLinuxRing <MagicLinuxRing@users.noreply.github.com>
2014-05-21 15:01:14 +0800

    CCDouble

--- cocos/deprecated/CCDouble.h
+++ cocos/deprecated/CCDouble.h
@@ -52,7 +52,7 @@ public:
         return pRet;
     }
 
-    /* override functions */
+    /* 重载方法 */
     virtual void acceptVisitor(DataVisitor &visitor) { visitor.visit(this); }
     
     __Double* clone() const
