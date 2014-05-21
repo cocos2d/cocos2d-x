472951e9043019876e93609a01fb7b1d67105457
MagicLinuxRing <MagicLinuxRing@users.noreply.github.com>
2014-05-21 15:50:10 +0800

    CCString

--- cocos/deprecated/CCString.h
+++ cocos/deprecated/CCString.h
@@ -72,106 +72,106 @@ public:
      */
     virtual ~__String();
     
-    /* override assignment operator 
+    /* 重载赋值运算符
      * @js NA
      * @lua NA
      */
     __String& operator= (const __String& other);
 
-    /** init a string with format, it's similar with the c function 'sprintf' 
+    /** 初始化字符串格式, 和C语言的'sprintf' 函数类似
      * @js NA
      * @lua NA
      */
     bool initWithFormat(const char* format, ...) CC_FORMAT_PRINTF(2, 3);
 
-    /** convert to int value 
+    /** 转换为int类型 的值
      * @js NA
      */
     int intValue() const;
 
-    /** convert to unsigned int value 
+    /** 转化为unsigned int 的值 
      * @js NA
      */
     unsigned int uintValue() const;
 
-    /** convert to float value 
+    /** 转化为float value 
      * @js NA
      */
     float floatValue() const;
 
-    /** convert to double value 
+    /** 转化为 double value 
      * @js NA
      */
     double doubleValue() const;
 
-    /** convert to bool value 
+    /** 转化为 bool value 
      * @js NA
      */
     bool boolValue() const;
 
-    /** get the C string 
+    /** 获取C string 
      * @js NA
      */
     const char* getCString() const;
 
-    /** get the length of string 
+    /** 获取 string长度 
      * @js NA
      */
     int length() const;
 
-    /** compare to a c string 
+    /** 比较C String 
      * @js NA
      */
     int compare(const char *) const;
 
-    /** append additional characters at the end of its current value 
+    /** 在当前字符串的末尾追加额外的字符
      * @js NA
      * @lua NA
      */
     void append(const std::string& str);
 
-    /** append(w/ format) additional characters at the end of its current value 
+    /** 追加(w/ 格式) 额外的字符 在当前字符串末尾 
      * @js NA
      * @lua NA
      */
     void appendWithFormat(const char* format, ...);
 
-    /** split a string 
+    /** 分开一个string 
      * @js NA
      * @lua NA
      */
     __Array* componentsSeparatedByString(const char *delimiter);
     
-    /* override functions 
+    /* 重载方法
      * @js NA
      */
     virtual bool isEqual(const Ref* pObject);
 
-    /** create a string with std string, you can also pass a c string pointer because the default constructor of std::string can access a c string pointer. 
-     *  @return A String pointer which is an autorelease object pointer,
-     *          it means that you needn't do a release operation unless you retain it.
+    /** 根据std string创建一个string, 你也能使用一个 c string 指针 因为默认的 std::string构造 能得到一个 c string 指针. 
+     *  @return 一个String pointer是一个 autorelease 对象的 pointer,
+     *          i这意味着你不需要release 操作 除非 你 retain 了它.
      * @js NA
      */
     static __String* create(const std::string& str);
 
-    /** create a string with format, it's similar with the c function 'sprintf', the default buffer size is (1024*100) bytes,
-     *  if you want to change it, you should modify the kMax__StringLen macro in __String.cpp file.
-     *  @return A String pointer which is an autorelease object pointer,
-     *          it means that you needn't do a release operation unless you retain it.
+    /** 根据某个格式创建一个string, 它和 c语言 函数 'sprintf'类似, 默认的 buffer 大小是 (1024*100) bytes,
+     *  如果你想改变这个值, 你需要在__String.cpp 文件里面改变 kMax__StringLen 这个宏 .
+     *  @return 这是一个 autorelease object pointer,
+     *          所以你不需要release 操作除非你 retain 了它.
      * @js NA
      */
     static __String* createWithFormat(const char* format, ...) CC_FORMAT_PRINTF(1, 2);
 
-    /** create a string with binary data 
-     *  @return A String pointer which is an autorelease object pointer,
-     *          it means that you needn't do a release operation unless you retain it.
+    /** 创建一个二进制数据的字符串 
+     *  @return 这是一个 autorelease object pointer,
+     *          所以你不需要release 操作除非你 retain 了它.
      * @js NA
      */
     static __String* createWithData(const unsigned char* pData, size_t nLen);
 
-    /** create a string with a file, 
-     *  @return A String pointer which is an autorelease object pointer,
-     *          it means that you needn't do a release operation unless you retain it.
+    /** 创建一个文件名字字符串, 
+     *  @return 这是一个 autorelease object pointer,
+     *          所以你不需要release 操作除非你 retain 了它.
      * @js NA
      */
     static __String* createWithContentsOfFile(const std::string& filename);
