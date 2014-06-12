db992b6d0f28a6d1c0f62dd452fd9df5169f0f00
2youyou2 <kaile.yin@chukong-inc.com>
2014-06-12 16:26:18 +0800

    modify cocostudio Android.mk

--- cocos/editor-support/cocostudio/Android.mk
+++ cocos/editor-support/cocostudio/Android.mk
@@ -53,6 +53,12 @@ WidgetReader/TextAtlasReader/TextAtlasReader.cpp \
 WidgetReader/TextBMFontReader/TextBMFontReader.cpp \
 WidgetReader/TextFieldReader/TextFieldReader.cpp \
 WidgetReader/TextReader/TextReader.cpp \
+TimelineAction/CCFrame.cpp \
+TimelineAction/CCNodeCache.cpp \
+TimelineAction/CCTimeLine.cpp \
+TimelineAction/CCTimelineAction.cpp \
+TimelineAction/CCTimelineActionCache.cpp \
+
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/.. \
 $(LOCAL_PATH)/../../../external
