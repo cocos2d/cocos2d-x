68649695b226c6b6b816e90e018c2b7c865fce18
CocosRobot <redmine@cocos2d-x.org>
2014-06-25 09:17:16 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/VBox.lua
@@ -0,0 +1,20 @@
+
+--------------------------------
+-- @module VBox
+-- @extend Layout
+
+--------------------------------
+-- overload function: create(size_table)
+--          
+-- overload function: create()
+--          
+-- @function [parent=#VBox] create
+-- @param self
+-- @param #size_table size
+-- @return VBox#VBox ret (retunr value: ccui.VBox)
+
+--------------------------------
+-- @function [parent=#VBox] VBox 
+-- @param self
+        
+return nil
