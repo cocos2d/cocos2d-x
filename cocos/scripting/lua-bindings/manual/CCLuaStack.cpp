32c355dca4e87471304809dd9bc2bf65cbd607a5
samuele3hu <qingkui.hu@cocos2d-x.org>
2014-06-26 16:19:22 +0800

    Add VideoPlayer Lua bindings and update bindings-generator submodule

--- cocos/scripting/lua-bindings/manual/CCLuaStack.cpp
+++ cocos/scripting/lua-bindings/manual/CCLuaStack.cpp
@@ -66,6 +66,7 @@ extern "C" {
 #include "lua_cocos2dx_physics_manual.hpp"
 #include "lua_cocos2dx_ui_auto.hpp"
 #include "lua_cocos2dx_ui_manual.hpp"
+#include "lua_cocos2dx_experimental_auto.hpp"
 
 namespace {
 int lua_print(lua_State * luastate)
@@ -172,6 +173,10 @@ bool LuaStack::init(void)
 #endif
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
     LuaObjcBridge::luaopen_luaoc(_state);
+#endif
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+    register_all_cocos2dx_experimental(_state);
 #endif
     
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
