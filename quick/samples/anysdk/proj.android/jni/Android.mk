0fd262cd9a2492cd81aa17a33a737e222c9c650f
htl <htlxyz@126.com>
2014-07-01 19:35:01 +0800

    add anysdk

---
+++ quick/samples/anysdk/proj.android/jni/Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH := $(call my-dir)
+$(call import-add-path,$(LOCAL_PATH)/../)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := game_shared
+
+LOCAL_MODULE_FILENAME := libgame
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+    ../../sources/AppDelegate.cpp \
+    ../../sources/SimulatorConfig.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../sources
+
+#LOCAL_CFLAGS += -std=c++11 -Wno-psabi -DCC_LUA_ENGINE_ENABLED=1 $(ANDROID_COCOS2D_BUILD_FLAGS)
+
+LOCAL_LDLIBS := -lGLESv2 \
+                -llog \
+                -lz \
+                -landroid
+
+LOCAL_EXPORT_LDLIBS := -lGLESv1_CM \
+                       -lGLESv2 \
+                       -lEGL \
+                       -llog \
+                       -lz \
+                       -landroid
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos_lua_static
+LOCAL_WHOLE_STATIC_LIBRARIES += PluginProtocolStatic
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,cocos2dx/lua-bindings)
+
+$(call import-module,protocols/android)
+
