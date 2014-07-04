abcda2f2ff55611ba4328287190059c75777c337
jiri.formanek <jiri.formanek@gmail.com>
2014-07-04 20:35:32 +0200

    ProgressTimer opacity recursion fix

--- cocos/2d/CCProgressTimer.h
+++ cocos/2d/CCProgressTimer.h
@@ -117,6 +117,7 @@ public:
     virtual const Color3B& getColor() const override;
     virtual void setOpacity(GLubyte opacity) override;
     virtual GLubyte getOpacity() const override;
+    virtual void updateDisplayedOpacity(GLubyte parentOpacity) override;
     
 CC_CONSTRUCTOR_ACCESS:
     /**
