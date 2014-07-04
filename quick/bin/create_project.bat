7936834c1331204f03da52e9a0af9b8d832568b0
SunLightJuly <ygqy8181@qq.com>
2014-07-04 14:38:54 +0800

    modify create_project script files

--- quick/bin/create_project.bat
+++ quick/bin/create_project.bat
@@ -1,4 +1,4 @@
 @echo off
 set DIR=%~dp0
-set TEMPLATE_ROOT=%DIR%..\template
+set TEMPLATE_ROOT=%DIR%..\templates
 %DIR%win32\php.exe "%DIR%lib\create_project.php" -t "%TEMPLATE_ROOT%\PROJECT_TEMPLATE_01" %*
