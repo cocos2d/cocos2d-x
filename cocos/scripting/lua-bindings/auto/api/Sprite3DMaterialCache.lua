07550680ab6d6219ae6af3cdd9b22e659ddc8a12
CocosRobot <redmine@cocos2d-x.org>
2014-07-09 01:38:31 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/Sprite3DMaterialCache.lua
@@ -0,0 +1,36 @@
+
+--------------------------------
+-- @module Sprite3DMaterialCache
+-- @parent_module cc
+
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] removeAllSprite3DMaterial 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] addSprite3DMaterial 
+-- @param self
+-- @param #string str
+-- @param #cc.Texture2D texture2d
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] removeUnusedSprite3DMaterial 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] getSprite3DMaterial 
+-- @param self
+-- @param #string str
+-- @return Texture2D#Texture2D ret (return value: cc.Texture2D)
+        
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] destroyInstance 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Sprite3DMaterialCache] getInstance 
+-- @param self
+-- @return Sprite3DMaterialCache#Sprite3DMaterialCache ret (return value: cc.Sprite3DMaterialCache)
+        
+return nil
