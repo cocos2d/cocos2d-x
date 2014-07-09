ae12030745e322380efe2b8ca062d2febd982ea3
CocosRobot <redmine@cocos2d-x.org>
2014-07-09 05:42:06 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_auto_api.lua
+++ cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_auto_api.lua
@@ -1212,16 +1212,6 @@
 
 
 --------------------------------------------------------
--- the cc SimpleAudioEngine
--- @field [parent=#cc] SimpleAudioEngine#SimpleAudioEngine SimpleAudioEngine preloaded module
-
-
---------------------------------------------------------
--- the cc ProtectedNode
--- @field [parent=#cc] ProtectedNode#ProtectedNode ProtectedNode preloaded module
-
-
---------------------------------------------------------
 -- the cc Animation3D
 -- @field [parent=#cc] Animation3D#Animation3D Animation3D preloaded module
 
@@ -1229,6 +1219,16 @@
 --------------------------------------------------------
 -- the cc Animate3D
 -- @field [parent=#cc] Animate3D#Animate3D Animate3D preloaded module
+
+
+--------------------------------------------------------
+-- the cc SimpleAudioEngine
+-- @field [parent=#cc] SimpleAudioEngine#SimpleAudioEngine SimpleAudioEngine preloaded module
+
+
+--------------------------------------------------------
+-- the cc ProtectedNode
+-- @field [parent=#cc] ProtectedNode#ProtectedNode ProtectedNode preloaded module
 
 
 return nil
