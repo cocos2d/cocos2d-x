87aaf7dcefe0971f8937e5fd43cf0f772d92bf7b
CocosRobot <redmine@cocos2d-x.org>
2014-07-11 09:48:20 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/Controller.lua
@@ -0,0 +1,51 @@
+
+--------------------------------
+-- @module Controller
+-- @parent_module cc
+
+--------------------------------
+-- @function [parent=#Controller] receiveExternalKeyEvent 
+-- @param self
+-- @param #int int
+-- @param #bool bool
+        
+--------------------------------
+-- @function [parent=#Controller] getDeviceName 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+-- @function [parent=#Controller] isConnected 
+-- @param self
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
+-- @function [parent=#Controller] getDeviceId 
+-- @param self
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- @function [parent=#Controller] setTag 
+-- @param self
+-- @param #int int
+        
+--------------------------------
+-- @function [parent=#Controller] getTag 
+-- @param self
+-- @return int#int ret (return value: int)
+        
+--------------------------------
+-- @function [parent=#Controller] startDiscoveryController 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Controller] stopDiscoveryController 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Controller] getControllerByTag 
+-- @param self
+-- @param #int int
+-- @return Controller#Controller ret (return value: cc.Controller)
+        
+return nil
