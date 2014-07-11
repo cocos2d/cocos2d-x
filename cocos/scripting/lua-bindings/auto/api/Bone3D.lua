20c4e6530d95f43d823ef7ef475ec019d7d11bea
CocosRobot <redmine@cocos2d-x.org>
2014-07-11 04:07:14 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/Bone3D.lua
@@ -0,0 +1,99 @@
+
+--------------------------------
+-- @module Bone3D
+-- @extend Ref
+-- @parent_module cc
+
+--------------------------------
+-- @function [parent=#Bone3D] getInverseBindPose 
+-- @param self
+-- @return mat4_table#mat4_table ret (return value: mat4_table)
+        
+--------------------------------
+-- @function [parent=#Bone3D] clearBoneBlendState 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Bone3D] resetPose 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Bone3D] setAnimationValue 
+-- @param self
+-- @param #float float
+-- @param #float float
+-- @param #float float
+-- @param #void void
+-- @param #float float
+        
+--------------------------------
+-- @function [parent=#Bone3D] getChildBoneByIndex 
+-- @param self
+-- @param #int int
+-- @return Bone3D#Bone3D ret (return value: cc.Bone3D)
+        
+--------------------------------
+-- @function [parent=#Bone3D] removeChildBone 
+-- @param self
+-- @param #cc.Bone3D bone3d
+        
+--------------------------------
+-- @function [parent=#Bone3D] getParentBone 
+-- @param self
+-- @return Bone3D#Bone3D ret (return value: cc.Bone3D)
+        
+--------------------------------
+-- @function [parent=#Bone3D] getName 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+-- @function [parent=#Bone3D] removeAllChildBone 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Bone3D] addChildBone 
+-- @param self
+-- @param #cc.Bone3D bone3d
+        
+--------------------------------
+-- @function [parent=#Bone3D] getChildBoneCount 
+-- @param self
+-- @return long#long ret (return value: long)
+        
+--------------------------------
+-- @function [parent=#Bone3D] updateJointMatrix 
+-- @param self
+-- @param #vec4_table vec4
+        
+--------------------------------
+-- @function [parent=#Bone3D] setInverseBindPose 
+-- @param self
+-- @param #mat4_table mat4
+        
+--------------------------------
+-- @function [parent=#Bone3D] getWorldMat 
+-- @param self
+-- @return mat4_table#mat4_table ret (return value: mat4_table)
+        
+--------------------------------
+-- @function [parent=#Bone3D] updateWorldMat 
+-- @param self
+        
+--------------------------------
+-- @function [parent=#Bone3D] setOriPose 
+-- @param self
+-- @param #mat4_table mat4
+        
+--------------------------------
+-- @function [parent=#Bone3D] removeChildBoneByIndex 
+-- @param self
+-- @param #int int
+        
+--------------------------------
+-- @function [parent=#Bone3D] create 
+-- @param self
+-- @param #string str
+-- @return Bone3D#Bone3D ret (return value: cc.Bone3D)
+        
+return nil
