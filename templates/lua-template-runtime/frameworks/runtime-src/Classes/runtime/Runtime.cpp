390dc3781b205d8165e0132085d095a835e81efa
chuanweizhang2013 <chuanweizhanng2013@gmail.com>
2014-07-16 20:19:19 +0800

    support custom console port

--- templates/lua-template-runtime/frameworks/runtime-src/Classes/runtime/Runtime.cpp
+++ templates/lua-template-runtime/frameworks/runtime-src/Classes/runtime/Runtime.cpp
@@ -277,7 +277,6 @@ void FileServer::readResFileFinfo()
         _filecfgjson.SetObject();
     }
 
-#if(CC_PLATFORM_MAC != CC_TARGET_PLATFORM && CC_PLATFORM_WIN32 != CC_TARGET_PLATFORM))
     //save file info to disk every ten second
     Director::getInstance()->getScheduler()->schedule([&](float){
         rapidjson::StringBuffer buffer;
@@ -292,7 +291,6 @@ void FileServer::readResFileFinfo()
         fwrite(str,sizeof(char),strlen(str),pFile);
         fclose(pFile);
     },this, 5.0f, false, "fileinfo");
-#endif
 }
 
 void FileServer::addResFileInfo(const char* filename,uint64_t u64)
@@ -808,16 +806,19 @@ public:
         for (int i=0;i< sizeof(commands)/sizeof(Console::Command);i++) {
             _console->addCommand(commands[i]);
         }
-        _console->listenOnTCP(6010);
+        _console->listenOnTCP(ConfigParser::getInstance()->getConsolePort());
 
+#if(CC_PLATFORM_MAC != CC_TARGET_PLATFORM && CC_PLATFORM_WIN32 != CC_TARGET_PLATFORM)
         _fileserver= FileServer::getShareInstance();
         _fileserver->listenOnTCP(6020);
         _fileserver->readResFileFinfo();
+#endif
     }
 
     ~ConsoleCustomCommand()
     {
         Director::getInstance()->getConsole()->stop();
+        if(_fileserver)
         _fileserver->stop();
     }
 
