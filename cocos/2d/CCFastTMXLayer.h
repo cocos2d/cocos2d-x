4f5cd2a368bf42d3951184c1efba4bd8bc6ab142
andyque <guanghui8827@126.com>
2014-07-17 14:09:53 +0800

    change FastTileMap to TileMap

--- cocos/2d/CCFastTMXLayer.h
+++ cocos/2d/CCFastTMXLayer.h
@@ -78,20 +78,20 @@ http://www.cocos2d-iphone.org/wiki/doku.php/prog_guide:tiled_maps
 @since v3.2
 */
 
-class CC_DLL FastTMXLayer : public Node
+class CC_DLL TMXLayer : public Node
 {
 public:
     /** creates a FastTMXLayer with an tileset info, a layer info and a map info */
-    static FastTMXLayer * create(TMXTilesetInfo *tilesetInfo, TMXLayerInfo *layerInfo, TMXMapInfo *mapInfo);
+    static TMXLayer * create(TMXTilesetInfo *tilesetInfo, TMXLayerInfo *layerInfo, TMXMapInfo *mapInfo);
     /**
      * @js ctor
      */
-    FastTMXLayer();
+    TMXLayer();
     /**
      * @js NA
      * @lua NA
      */
-    virtual ~FastTMXLayer();
+    virtual ~TMXLayer();
 
     /** returns the tile gid at a given tile coordinate. It also returns the tile flags.
      */
