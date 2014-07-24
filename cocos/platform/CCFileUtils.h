cd4df205b0ee2051d0f9ea4f6426e07635cf4742
Ningyu <nyhere@qq.com>
2014-07-24 13:30:10 +0800

    replace tab with spaces again and again and again

--- cocos/platform/CCFileUtils.h
+++ cocos/platform/CCFileUtils.h
@@ -320,7 +320,7 @@ public:
      *  Converts the contents of a file to a ValueMap.
      *  @note This method is used internally.
      */
-	virtual ValueMap getValueMapFromData(const char* filedata, int filesize);
+    virtual ValueMap getValueMapFromData(const char* filedata, int filesize);
 
     /**
      *  Write a ValueMap to a plist file.
