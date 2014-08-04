97f06306e3057b600b34901414bca5dade1c4aa4
zii <gamcat@gmail.com>
2014-08-04 11:45:50 +0800

    Update ccUtils.cpp
    希望加入一个简单获得绝对时间的方法, 包装了gettimeofday.

--- cocos/base/ccUtils.cpp
+++ cocos/base/ccUtils.cpp
@@ -183,6 +183,14 @@ double atof(const char* str)
     
     return ::atof(buf);
 }
+
+double gettime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	return (double)tv.tv_sec + (double)tv.tv_usec/1000000;
+}
     
 }
 
