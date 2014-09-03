68ce6449e865159c3d3c6c9c8c8a3b586c8b64fe
songchengjiang <songcheng.jiang@chukong-inc.com>
2014-09-03 17:17:57 +0800

    repaired camera orientation Testing RotateBy action

--- cocos/3d/CCBillBoard.cpp
+++ cocos/3d/CCBillBoard.cpp
@@ -97,14 +97,15 @@ void BillBorad::draw(Renderer *renderer, const Mat4 &transform, uint32_t flags)
     const Mat4& camWorldMat = camera->getNodeToWorldTransform();
     if (memcmp(_camWorldMat.m, camWorldMat.m, sizeof(float) * 16) != 0 || memcmp(_transform.m, transform.m, sizeof(float) * 16) != 0)
     {
-        Vec3 camDir(transform.m[12] - camWorldMat.m[12], transform.m[13] - camWorldMat.m[13], transform.m[14] - camWorldMat.m[14]);
-        
+        Vec3 camDir;
+        camWorldMat.transformVector(Vec3(0.0f, 0.0f, -1.0f), &camDir);
+
         if (camDir.length() < MATH_TOLERANCE)
         {
             camDir.set(camWorldMat.m[8], camWorldMat.m[9], camWorldMat.m[10]);
         }
         camDir.normalize();
-        
+
         static Vec3 upAxis(0.0f, 1.0f, 0.0f);
         Vec3 x, y;
         camWorldMat.transformVector(upAxis, &y);
@@ -114,12 +115,12 @@ void BillBorad::draw(Renderer *renderer, const Mat4 &transform, uint32_t flags)
         float xlen = sqrtf(transform.m[0] * transform.m[0] + transform.m[1] * transform.m[1] + transform.m[2] * transform.m[2]);
         float ylen = sqrtf(transform.m[4] * transform.m[4] + transform.m[5] * transform.m[5] + transform.m[6] * transform.m[6]);
         float zlen = sqrtf(transform.m[8] * transform.m[8] + transform.m[9] * transform.m[9] + transform.m[10] * transform.m[10]);
-        
+
         _billboardTransform.m[0] = x.x * xlen; _billboardTransform.m[1] = x.y * xlen; _billboardTransform.m[2] = x.z * xlen;
         _billboardTransform.m[4] = y.x * ylen; _billboardTransform.m[5] = y.y * ylen; _billboardTransform.m[6] = y.z * ylen;
         _billboardTransform.m[8] = -camDir.x * zlen; _billboardTransform.m[9] = -camDir.y * zlen; _billboardTransform.m[10] = -camDir.z * zlen;
         _billboardTransform.m[12] = transform.m[12]; _billboardTransform.m[13] = transform.m[13]; _billboardTransform.m[14] = transform.m[14];
-        
+
         const Mat4 &viewMat = camWorldMat.getInversed();
         _zDepthInView = -(viewMat.m[2] * transform.m[12] + viewMat.m[6] * transform.m[13] + viewMat.m[10] * transform.m[14] + viewMat.m[14]);
         _transform = transform;
