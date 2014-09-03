1727212d544cb352c7d1b8d31f7b79a42eba9d27
Jeff <wohaaitinciu@gmail.com>
2014-09-03 09:44:04 +0800

    Revert "fixed some error"
    This reverts commit fd1184b80a32301c72b83e57bf74b5b69b4a0c7a.

--- extensions/proj.win32/Win32InputBox.cpp
+++ extensions/proj.win32/Win32InputBox.cpp
@@ -405,7 +405,7 @@ void CWin32InputBox::SetText(const char* pText)
 
 char* CWin32InputBox::AnsiToUtf8(const char* strAnsi)
 {
-	char* ret = NULL;
+	char* ret;
     size_t lenAnsi = strlen(strAnsi);
     if (lenAnsi > 0)
 	{	
