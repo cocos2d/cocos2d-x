f1f6373b6ee18749a0fcb973cf722e1b08a5dcba
huangshiwu <huangsw1@gmail.com>
2014-09-12 15:29:10 +0800

    fix Cocos2dShaderCompile's compile error --use chipmunk static library

--- cocos/platform/CCGL.h
+++ cocos/platform/CCGL.h
@@ -36,7 +36,7 @@ THE SOFTWARE.
 #include "platform/android/CCGL-android.h"
 #elif CC_TARGET_PLATFORM == CC_PLATFORM_WIN32
 #include "platform/win32/CCGL-win32.h"
-#elif CC_TARGET_PLATFORM == CC_PLATFORM_WP8
+#elif CC_TARGET_PLATFORM == CC_PLATFORM_WP8 || CC_TARGET_PLATFORM == CC_PLATFORM_WINRT
 #include "platform/winrt/CCGL.h"
 #elif CC_TARGET_PLATFORM == CC_PLATFORM_LINUX
 #include "platform/linux/CCGL-linux.h"
