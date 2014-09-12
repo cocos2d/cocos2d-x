b332dcdce45a4920436241999411dfdf99bc46dd
CocosRobot <redmine@cocos2d-x.org>
2014-09-12 07:23:24 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/DrawNode.lua
+++ cocos/scripting/lua-bindings/auto/api/DrawNode.lua
@@ -5,13 +5,59 @@
 -- @parent_module cc
 
 --------------------------------
---  draw a quadratic bezier curve with color and number of segments 
--- @function [parent=#DrawNode] drawQuadraticBezier 
+-- 
+-- @function [parent=#DrawNode] drawLine 
+-- @param self
+-- @param #vec2_table origin
+-- @param #vec2_table destination
+-- @param #color4f_table color
+        
+--------------------------------
+-- @overload self, vec2_table, vec2_table, vec2_table, vec2_table, color4f_table         
+-- @overload self, vec2_table, vec2_table, color4f_table         
+-- @function [parent=#DrawNode] drawRect
+-- @param self
+-- @param #vec2_table lb
+-- @param #vec2_table lt
+-- @param #vec2_table rt
+-- @param #vec2_table rb
+-- @param #color4f_table color
+
+--------------------------------
+-- @overload self, vec2_table, float, float, unsigned int, color4f_table         
+-- @overload self, vec2_table, float, float, unsigned int, float, float, color4f_table         
+-- @function [parent=#DrawNode] drawSolidCircle
+-- @param self
+-- @param #vec2_table center
+-- @param #float radius
+-- @param #float angle
+-- @param #unsigned int segments
+-- @param #float scaleX
+-- @param #float scaleY
+-- @param #color4f_table color
+
+--------------------------------
+-- 
+-- @function [parent=#DrawNode] onDrawGLPoint 
+-- @param self
+-- @param #mat4_table transform
+-- @param #unsigned int flags
+        
+--------------------------------
+--  draw a dot at a position, with a given radius and color 
+-- @function [parent=#DrawNode] drawDot 
+-- @param self
+-- @param #vec2_table pos
+-- @param #float radius
+-- @param #color4f_table color
+        
+--------------------------------
+--  draw a segment with a radius and color 
+-- @function [parent=#DrawNode] drawSegment 
 -- @param self
 -- @param #vec2_table from
--- @param #vec2_table control
 -- @param #vec2_table to
--- @param #unsigned int segments
+-- @param #float radius
 -- @param #color4f_table color
         
 --------------------------------
@@ -22,9 +68,35 @@
 -- @param #unsigned int flags
         
 --------------------------------
---  Clear the geometry in the node's buffer. 
--- @function [parent=#DrawNode] clear 
+-- @overload self, vec2_table, float, float, unsigned int, bool, color4f_table         
+-- @overload self, vec2_table, float, float, unsigned int, bool, float, float, color4f_table         
+-- @function [parent=#DrawNode] drawCircle
 -- @param self
+-- @param #vec2_table center
+-- @param #float radius
+-- @param #float angle
+-- @param #unsigned int segments
+-- @param #bool drawLineToCenter
+-- @param #float scaleX
+-- @param #float scaleY
+-- @param #color4f_table color
+
+--------------------------------
+-- 
+-- @function [parent=#DrawNode] drawQuadBezier 
+-- @param self
+-- @param #vec2_table origin
+-- @param #vec2_table control
+-- @param #vec2_table destination
+-- @param #unsigned int segments
+-- @param #color4f_table color
+        
+--------------------------------
+-- 
+-- @function [parent=#DrawNode] onDrawGLLine 
+-- @param self
+-- @param #mat4_table transform
+-- @param #unsigned int flags
         
 --------------------------------
 --  draw a triangle with color 
@@ -36,31 +108,35 @@
 -- @param #color4f_table color
         
 --------------------------------
---  draw a dot at a position, with a given radius and color 
--- @function [parent=#DrawNode] drawDot 
+--  Clear the geometry in the node's buffer. 
+-- @function [parent=#DrawNode] clear 
 -- @param self
--- @param #vec2_table pos
--- @param #float radius
+        
+--------------------------------
+-- 
+-- @function [parent=#DrawNode] drawSolidRect 
+-- @param self
+-- @param #vec2_table origin
+-- @param #vec2_table destination
+-- @param #color4f_table color
+        
+--------------------------------
+-- 
+-- @function [parent=#DrawNode] drawPoint 
+-- @param self
+-- @param #vec2_table point
+-- @param #float pointSize
 -- @param #color4f_table color
         
 --------------------------------
 --  draw a cubic bezier curve with color and number of segments 
 -- @function [parent=#DrawNode] drawCubicBezier 
 -- @param self
--- @param #vec2_table from
+-- @param #vec2_table origin
 -- @param #vec2_table control1
 -- @param #vec2_table control2
--- @param #vec2_table to
+-- @param #vec2_table destination
 -- @param #unsigned int segments
--- @param #color4f_table color
-        
---------------------------------
---  draw a segment with a radius and color 
--- @function [parent=#DrawNode] drawSegment 
--- @param self
--- @param #vec2_table from
--- @param #vec2_table to
--- @param #float radius
 -- @param #color4f_table color
         
 --------------------------------
