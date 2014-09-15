4097a68ea9b583332f97c392cf1337641155b6d7
CocosRobot <redmine@cocos2d-x.org>
2014-09-15 15:38:05 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_audioengine_auto_api.lua
@@ -0,0 +1,14 @@
+--------------------------------
+-- @module cc
+
+--------------------------------------------------------
+-- the cc AudioProfile
+-- @field [parent=#cc] AudioProfile#AudioProfile AudioProfile preloaded module
+
+
+--------------------------------------------------------
+-- the cc AudioEngine
+-- @field [parent=#cc] AudioEngine#AudioEngine AudioEngine preloaded module
+
+
+return nil
