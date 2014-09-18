11c0d6ff2504fee4a75a97ca78759f521a829a84
honghui <honghui.zhang@chukong-inc.com>
2014-09-18 12:40:26 +0800

    fix:runtime crash when upload file to Mac/Win32

--- templates/lua-template-runtime/frameworks/runtime-src/Classes/runtime/Runtime.cpp
+++ templates/lua-template-runtime/frameworks/runtime-src/Classes/runtime/Runtime.cpp
@@ -453,7 +453,7 @@ static void recvBuf(int fd, char *pbuf, unsigned long bufsize)
     }
 }
 
-static void sendBuf(int fd, char *pbuf, unsigned long bufsize)
+static void sendBuf(int fd, const char *pbuf, unsigned long bufsize)
 {
     unsigned long leftLength = bufsize;
     while (leftLength != 0)
@@ -1026,9 +1026,7 @@ public:
                 
                 string msg(msgLength + msgContent);
                 
-                char dataBuf[1024] = {0};
-                memcpy(dataBuf, msg.c_str(), msg.size());
-                sendBuf(fd, dataBuf, msg.size());
+                sendBuf(fd, msg.c_str(), msg.size());
             }
         });
     }
