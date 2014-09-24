cc39ed288a8dcc4cfd800d911a45177081046d43
Huabing.Xu <dabingnn@gmail.com>
2014-09-24 17:21:38 +0800

    android can link with neon(neon code has some bugs)

--- cocos/math/MathUtil.cpp
+++ cocos/math/MathUtil.cpp
@@ -21,7 +21,12 @@ This file was modified to fit the cocos2d-x project
 #include "MathUtil.h"
 #include "base/ccMacros.h"
 
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+#include <cpu-features.h>
+#endif
+
 #if defined (__ARM_NEON__) || ((CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) &&!defined(__SSE__))
+#define USE_NEON
 #include "MathUtilNeon.inl"
 #endif
 
@@ -51,10 +56,36 @@ void MathUtil::smooth(float* x, float target, float elapsedTime, float riseTime,
     }
 }
 
+bool MathUtil::isNeonEnabled()
+{
+#ifdef __ARM_NEON__
+    return true;
+#elif defined (USE_NEON)
+    if (android_getCpuFamily() == ANDROID_CPU_FAMILY_ARM && (android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0)
+        return true;
+    else
+        return false;
+#else
+    return false;
+#endif
+}
+
+bool MathUtil::isSSEEnabled()
+{
+#ifdef __SSE__
+    return true;
+#else
+    return false;
+#endif
+}
+
 void MathUtil::addMatrix(const float* m, float scalar, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::addMatrix(m, scalar, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::addMatrix(m, scalar, dst);
+    else MathUtilC::addMatrix(m, scalar, dst);
 #else
     MathUtilC::addMatrix(m, scalar, dst);
 #endif
@@ -64,6 +95,9 @@ void MathUtil::addMatrix(const float* m1, const float* m2, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::addMatrix(m1, m2, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::addMatrix(m1, m2, dst);
+    else MathUtilC::addMatrix(m1, m2, dst);
 #else
     MathUtilC::addMatrix(m1, m2, dst);
 #endif
@@ -73,6 +107,9 @@ void MathUtil::subtractMatrix(const float* m1, const float* m2, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::subtractMatrix(m1, m2, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::subtractMatrix(m1, m2, dst);
+    else MathUtilC::subtractMatrix(m1, m2, dst);
 #else
     MathUtilC::subtractMatrix(m1, m2, dst);
 #endif
@@ -82,6 +119,9 @@ void MathUtil::multiplyMatrix(const float* m, float scalar, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::multiplyMatrix(m, scalar, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::multiplyMatrix(m, scalar, dst);
+    else MathUtilC::multiplyMatrix(m, scalar, dst);
 #else
     MathUtilC::multiplyMatrix(m, scalar, dst);
 #endif
@@ -91,6 +131,9 @@ void MathUtil::multiplyMatrix(const float* m1, const float* m2, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::multiplyMatrix(m1, m2, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::multiplyMatrix(m1, m2, dst);
+    else MathUtilC::multiplyMatrix(m1, m2, dst);
 #else
     MathUtilC::multiplyMatrix(m1, m2, dst);
 #endif
@@ -100,6 +143,9 @@ void MathUtil::negateMatrix(const float* m, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::negateMatrix(m, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::negateMatrix(m, dst);
+    else MathUtilC::negateMatrix(m, dst);
 #else
     MathUtilC::negateMatrix(m, dst);
 #endif
@@ -109,6 +155,9 @@ void MathUtil::transposeMatrix(const float* m, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::transposeMatrix(m, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::transposeMatrix(m, dst);
+    else MathUtilC::transposeMatrix(m, dst);
 #else
     MathUtilC::transposeMatrix(m, dst);
 #endif
@@ -118,6 +167,9 @@ void MathUtil::transformVec4(const float* m, float x, float y, float z, float w,
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::transformVec4(m, x, y, z, w, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::transformVec4(m, x, y, z, w, dst);
+    else MathUtilC::transformVec4(m, x, y, z, w, dst);
 #else
     MathUtilC::transformVec4(m, x, y, z, w, dst);
 #endif
@@ -127,6 +179,9 @@ void MathUtil::transformVec4(const float* m, const float* v, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::transformVec4(m, v, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::transformVec4(m, v, dst);
+    else MathUtilC::transformVec4(m, v, dst);
 #else
     MathUtilC::transformVec4(m, v, dst);
 #endif
@@ -136,6 +191,9 @@ void MathUtil::crossVec3(const float* v1, const float* v2, float* dst)
 {
 #ifdef __ARM_NEON__
     MathUtilNeon::crossVec3(v1, v2, dst);
+#elif defined (USE_NEON)
+    if(isNeonEnabled()) MathUtilNeon::crossVec3(v1, v2, dst);
+    else MathUtilC::crossVec3(v1, v2, dst);
 #else
     MathUtilC::crossVec3(v1, v2, dst);
 #endif
