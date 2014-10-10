ffd83bbdd02788b03083b5e11bac963ae03821bd
pipu <zhounan@chukong-inc.com>
2014-10-10 22:38:34 +0800

    Modifying win32 and wp8 protocol buffers lib

--- external/protocolBuf/prebuilt/android/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cocos_protocolBuffer_static
-LOCAL_MODULE_FILENAME := protocolbuffer
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/protocolbuffer.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
-include $(PREBUILT_STATIC_LIBRARY)
