4a084a4ebc21bd6e6673ee7c52fedaa3de4138d3
CocosRobot <redmine@cocos2d-x.org>
2014-10-16 09:55:07 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/SkeletonAnimation.lua
+++ cocos/scripting/lua-bindings/auto/api/SkeletonAnimation.lua
@@ -1,16 +1,68 @@
 
 --------------------------------
 -- @module SkeletonAnimation
--- @extend Skeleton
+-- @extend SkeletonRenderer
 -- @parent_module sp
 
 --------------------------------
 -- 
+-- @function [parent=#SkeletonAnimation] setStartListener 
+-- @param self
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setTrackEventListener 
+-- @param self
+-- @param #spTrackEntry entry
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setTrackCompleteListener 
+-- @param self
+-- @param #spTrackEntry entry
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setTrackStartListener 
+-- @param self
+-- @param #spTrackEntry entry
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setCompleteListener 
+-- @param self
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setTrackEndListener 
+-- @param self
+-- @param #spTrackEntry entry
+-- @param #function listener
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setEventListener 
+-- @param self
+-- @param #function listener
+        
+--------------------------------
+-- 
 -- @function [parent=#SkeletonAnimation] setMix 
 -- @param self
--- @param #char fromAnimation
--- @param #char toAnimation
+-- @param #string fromAnimation
+-- @param #string toAnimation
 -- @param #float duration
+        
+--------------------------------
+-- 
+-- @function [parent=#SkeletonAnimation] setEndListener 
+-- @param self
+-- @param #function listener
         
 --------------------------------
 -- 
@@ -22,4 +74,14 @@
 -- @function [parent=#SkeletonAnimation] clearTrack 
 -- @param self
         
+--------------------------------
+-- @overload self, string, string, float         
+-- @overload self, string, spAtlas, float         
+-- @function [parent=#SkeletonAnimation] createWithFile
+-- @param self
+-- @param #string skeletonDataFile
+-- @param #spAtlas atlas
+-- @param #float scale
+-- @return SkeletonAnimation#SkeletonAnimation ret (retunr value: sp.SkeletonAnimation)
+
 return nil
