2bfad920cedf1be845dd6e852794a39eb788240c
CocosRobot <redmine@cocos2d-x.org>
2014-10-16 04:01:10 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/Manifest.lua
@@ -0,0 +1,43 @@
+
+--------------------------------
+-- @module Manifest
+-- @extend Ref
+-- @parent_module cc
+
+--------------------------------
+--  @brief Gets remote manifest file url.
+-- @function [parent=#Manifest] getManifestFileUrl 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+--  @brief Check whether the version informations have been fully loaded
+-- @function [parent=#Manifest] isVersionLoaded 
+-- @param self
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
+--  @brief Check whether the manifest have been fully loaded
+-- @function [parent=#Manifest] isLoaded 
+-- @param self
+-- @return bool#bool ret (return value: bool)
+        
+--------------------------------
+--  @brief Gets remote package url.
+-- @function [parent=#Manifest] getPackageUrl 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+--  @brief Gets manifest version.
+-- @function [parent=#Manifest] getVersion 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+--------------------------------
+--  @brief Gets remote version file url.
+-- @function [parent=#Manifest] getVersionFileUrl 
+-- @param self
+-- @return string#string ret (return value: string)
+        
+return nil
