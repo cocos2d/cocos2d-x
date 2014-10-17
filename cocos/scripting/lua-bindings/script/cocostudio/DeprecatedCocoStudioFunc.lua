ca50bd38341204478cc12e70f98d03abeed76d0a
samuele3hu <qingkui.hu@cocos2d-x.org>
2014-10-17 14:15:30 +0800

    Update the Deprecated Func

--- cocos/scripting/lua-bindings/script/cocostudio/DeprecatedCocoStudioFunc.lua
+++ cocos/scripting/lua-bindings/script/cocostudio/DeprecatedCocoStudioFunc.lua
@@ -9,13 +9,13 @@ function GUIReaderDeprecated.shareReader()
     deprecatedTip("GUIReader:shareReader","ccs.GUIReader:getInstance")
     return ccs.GUIReader:getInstance()
 end
-rawset(GUIReader,"shareReader",GUIReaderDeprecated.shareReader)
+GUIReader.shareReader = GUIReaderDeprecated.shareReader
 
 function GUIReaderDeprecated.purgeGUIReader()
     deprecatedTip("GUIReader:purgeGUIReader","ccs.GUIReader:destroyInstance")
     return ccs.GUIReader:destroyInstance()
 end
-rawset(GUIReader,"purgeGUIReader",GUIReaderDeprecated.purgeGUIReader)
+GUIReader.purgeGUIReader = GUIReaderDeprecated.purgeGUIReader
 --functions of GUIReader will be deprecated end
 
 --functions of SceneReader will be deprecated begin
@@ -24,13 +24,13 @@ function SceneReaderDeprecated.sharedSceneReader()
     deprecatedTip("SceneReader:sharedSceneReader","ccs.SceneReader:getInstance")
     return ccs.SceneReader:getInstance()
 end
-rawset(SceneReader,"sharedSceneReader",SceneReaderDeprecated.sharedSceneReader)
+SceneReader.sharedSceneReader = SceneReaderDeprecated.sharedSceneReader
 
 function SceneReaderDeprecated.purgeSceneReader(self)
     deprecatedTip("SceneReader:purgeSceneReader","ccs.SceneReader:destroyInstance")
     return self:destroyInstance()
 end
-rawset(SceneReader,"purgeSceneReader",SceneReaderDeprecated.purgeSceneReader)
+SceneReader.purgeSceneReader = SceneReaderDeprecated.purgeSceneReader
 --functions of SceneReader will be deprecated end
 
 
@@ -40,7 +40,7 @@ function CCSGUIReaderDeprecated.purgeGUIReader()
     deprecatedTip("ccs.GUIReader:purgeGUIReader","ccs.GUIReader:destroyInstance")
     return ccs.GUIReader:destroyInstance()
 end
-rawset(ccs.GUIReader,"purgeGUIReader",CCSGUIReaderDeprecated.purgeGUIReader)
+ccs.GUIReader.purgeGUIReader = CCSGUIReaderDeprecated.purgeGUIReader
 --functions of ccs.GUIReader will be deprecated end
 
 --functions of ccs.ActionManagerEx will be deprecated begin
@@ -49,7 +49,7 @@ function CCSActionManagerExDeprecated.destroyActionManager()
     deprecatedTip("ccs.ActionManagerEx:destroyActionManager","ccs.ActionManagerEx:destroyInstance")
     return ccs.ActionManagerEx:destroyInstance()
 end
-rawset(ccs.ActionManagerEx,"destroyActionManager",CCSActionManagerExDeprecated.destroyActionManager)
+ccs.ActionManagerEx.destroyActionManager = CCSActionManagerExDeprecated.destroyActionManager
 --functions of ccs.ActionManagerEx will be deprecated end
 
 --functions of ccs.SceneReader will be deprecated begin
@@ -58,7 +58,7 @@ function CCSSceneReaderDeprecated.destroySceneReader(self)
     deprecatedTip("ccs.SceneReader:destroySceneReader","ccs.SceneReader:destroyInstance")
     return self:destroyInstance()
 end
-rawset(ccs.SceneReader,"destroySceneReader",CCSSceneReaderDeprecated.destroySceneReader)
+ccs.SceneReader.destroySceneReader = CCSSceneReaderDeprecated.destroySceneReader
 --functions of ccs.SceneReader will be deprecated end
 
 --functions of CCArmatureDataManager will be deprecated begin
@@ -67,11 +67,11 @@ function CCArmatureDataManagerDeprecated.sharedArmatureDataManager()
     deprecatedTip("CCArmatureDataManager:sharedArmatureDataManager","ccs.ArmatureDataManager:getInstance")
     return ccs.ArmatureDataManager:getInstance()
 end
-rawset(CCArmatureDataManager,"sharedArmatureDataManager",CCArmatureDataManagerDeprecated.sharedArmatureDataManager)
+CCArmatureDataManager.sharedArmatureDataManager = CCArmatureDataManagerDeprecated.sharedArmatureDataManager
 
 function CCArmatureDataManagerDeprecated.purge()
     deprecatedTip("CCArmatureDataManager:purge","ccs.ArmatureDataManager:destoryInstance")
     return ccs.ArmatureDataManager:destoryInstance() 
 end
-rawset(CCArmatureDataManager,"purge",CCArmatureDataManagerDeprecated.purge)
+CCArmatureDataManager.purge = CCArmatureDataManagerDeprecated.purge
 --functions of CCArmatureDataManager will be deprecated end
