8ab54a90c329533037791cadd7dd9aad1cea4132
Dale Stammen <dalestam@microsoft.com>
2014-10-19 12:50:14 -0700

    added universal app platform files

---
+++ cocos/platform/win8.1-universal/OpenGLES.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// OpenGL ES includes
+#include <GLES3/gl3.h>
+#include <GLES3/gl3ext.h>
+
+// EGL includes
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+#include <angle_windowsstore.h>
+
+
+class OpenGLES
+{
+public:
+    OpenGLES();
+    ~OpenGLES();
+
+    EGLSurface CreateSurface(Windows::UI::Xaml::Controls::SwapChainPanel^ panel, const Windows::Foundation::Size* renderSurfaceSize);
+    void DestroySurface(const EGLSurface surface);
+    void MakeCurrent(const EGLSurface surface);
+    EGLBoolean SwapBuffers(const EGLSurface surface);
+    void Reset();
+
+private:
+    void Initialize();
+    void Cleanup();
+
+private:
+    EGLDisplay mEglDisplay;
+    EGLContext mEglContext;
+    EGLConfig  mEglConfig;
+};
