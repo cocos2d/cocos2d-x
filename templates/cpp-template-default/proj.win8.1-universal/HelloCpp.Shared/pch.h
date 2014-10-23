c38a37837aff74e05b292243da077ab7cca3b0cd
Dale Stammen <dalestam@microsoft.com>
2014-10-23 07:40:42 -0700

    added cpp template for windows 8.1 universal apps

---
+++ templates/cpp-template-default/proj.win8.1-universal/HelloCpp.Shared/pch.h
@@ -0,0 +1,16 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include "cocos2d.h"
+#include "cocos-ext.h"
+
+#include "App.xaml.h"
+
+
