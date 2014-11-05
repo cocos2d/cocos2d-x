f75d180f4767079e75db7cb2464ad1aa26704bfe
SunLightJuly <ygqy8181@qq.com>
2014-11-05 20:04:16 +0800

    export  QUICK_V3_ROOT  in script files

--- quick/bin/create_project.sh
+++ quick/bin/create_project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# source $DIR/_setenv.sh
+export QUICK_V3_ROOT=`cat ~/.QUICK_V3_ROOT`
 TEMPLATE_ROOT=`dirname "$DIR"`/templates
 php "$DIR/lib/create_project.php" -t "$TEMPLATE_ROOT/lua-template-quick" $*
