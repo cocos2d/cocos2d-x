334f14f3ba0d69437c020755ade5aa27d6213aef
Dale Stammen <dalestam@microsoft.com>
2014-11-06 10:43:35 -0800

    both vert and frag shaders must be precision mediump float for WINRT_8_1

--- cocos2dx/shaders/CCGLProgram.cpp
+++ cocos2dx/shaders/CCGLProgram.cpp
@@ -192,6 +192,8 @@ bool CCGLProgram::compileShader(GLuint * shader, GLenum type, const GLchar* sour
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_WIN32 && CC_TARGET_PLATFORM != CC_PLATFORM_LINUX && CC_TARGET_PLATFORM != CC_PLATFORM_MAC)
 #if CC_TARGET_PLATFORM == CC_PLATFORM_NACL
         "precision highp float;\n"
+#elif defined (WINRT_8_1)
+        "precision mediump float;\n"
 #else
         (type == GL_VERTEX_SHADER ? "precision highp float;\n" : "precision mediump float;\n"),
 #endif
