445827df840ead0562fc142bca6678ef7548fd66
SunLightJuly <ygqy8181@qq.com>
2014-11-06 14:33:08 +0800

    cleanup

--- templates/lua-template-runtime/frameworks/runtime-src/Classes/runtime/ResData.h
+++
@@ -1,9 +0,0 @@
-#ifndef  _RESDATA__H_
-#define  _RESDATA__H_
-
-extern const unsigned char __shinePngData[11776];
-extern const unsigned char __portraitPngData[122214];
-extern const unsigned char __playEnablePngData[20254];
-extern const unsigned char __landscapePngData[115832];
-
-#endif // _RESDATA__H_
--- tests/cpp-tests/Resources/Shaders3D/OutLine.frag
+++
@@ -1,7 +0,0 @@
-uniform vec3 OutLineColor;
-uniform vec4 u_color;
-
-void main(void)
-{
-    gl_FragColor =  vec4(OutLineColor,1.0) * u_color;
-}
