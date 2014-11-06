0e71ab8704a2884e255a8f1e081b79b11268ec53
Dale Stammen <dalestam@microsoft.com>
2014-11-06 12:46:44 -0800

    added event support

---
+++ cocos2dx/platform/win-8.1/InputEvent.cpp
@@ -0,0 +1,153 @@
+/****************************************************************************
+Copyright (c) 2013 cocos2d-x.org
+Copyright (c) Microsoft Open Technologies, Inc.
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#include "InputEvent.h"
+#include "CCWinRTUtils.h"
+#include "CCEGLView-Win8_1.h"
+
+NS_CC_BEGIN
+
+AccelerometerEvent::AccelerometerEvent(const CCAcceleration& event)
+    : m_event(event)
+{
+
+}
+
+void AccelerometerEvent::execute()
+{
+#if 0
+    auto dispatcher = Director::getInstance()->getEventDispatcher();
+    cocos2d::EventAcceleration accEvent(m_event);
+    dispatcher->dispatchEvent(&accEvent);
+#endif // 0
+
+}
+
+
+PointerEvent::PointerEvent(PointerEventType type, Windows::UI::Core::PointerEventArgs^ args)
+    : m_type(type), m_args(args)
+{
+
+}
+
+void PointerEvent::execute()
+{
+    switch(m_type)
+    {
+    case PointerEventType::PointerPressed:
+        CCEGLView::sharedOpenGLView()->OnPointerPressed(m_args.Get());
+        break;
+    case PointerEventType::PointerMoved:
+        CCEGLView::sharedOpenGLView()->OnPointerMoved(m_args.Get());
+        break;           
+    case PointerEventType::PointerReleased:
+        CCEGLView::sharedOpenGLView()->OnPointerReleased(m_args.Get());
+        break;
+    }
+}
+
+KeyboardEvent::KeyboardEvent(Cocos2dKeyEvent type)
+    : m_type(type), m_text(nullptr)
+{
+
+}
+
+KeyboardEvent::KeyboardEvent(Cocos2dKeyEvent type, Platform::String^ text)
+    : m_type(type), m_text(text)
+{
+
+}
+
+void KeyboardEvent::execute()
+{
+    switch(m_type)
+    {
+    case Cocos2dKeyEvent::Text:
+    {
+        char szUtf8[8] = { 0 };
+        int nLen = WideCharToMultiByte(CP_UTF8, 0, (LPCWSTR) m_text.Get()->Data(), 1, szUtf8, sizeof(szUtf8), NULL, NULL);
+        CCIMEDispatcher::sharedDispatcher()->dispatchInsertText(szUtf8, nLen);
+        break;
+    }
+
+    default:
+        switch (m_type)
+        {
+        case Cocos2dKeyEvent::Escape:
+            CCDirector::sharedDirector()->getKeypadDispatcher()->dispatchKeypadMSG(kTypeBackClicked);
+            break;
+        case Cocos2dKeyEvent::Back:
+            CCIMEDispatcher::sharedDispatcher()->dispatchDeleteBackward();
+            break;
+        case Cocos2dKeyEvent::Enter:
+            CCIMEDispatcher::sharedDispatcher()->dispatchInsertText("\n", 1);
+            break;
+        default:
+            break;
+        }        
+        break;
+    }
+}
+
+
+BackButtonEvent::BackButtonEvent()
+{
+
+}
+
+void BackButtonEvent::execute()
+{
+    //CCEGLView::sharedOpenGLView()->OnBackKeyPress();
+}
+
+CustomInputEvent::CustomInputEvent(const std::function<void()>& fun)
+: m_fun(fun)
+{
+}
+
+void CustomInputEvent::execute()
+{
+    m_fun();
+}
+
+UIEditBoxEvent::UIEditBoxEvent(Platform::Object^ sender, Platform::String^ text, Windows::Foundation::EventHandler<Platform::String^>^ handle) 
+    : m_sender(sender)
+    , m_text(text)
+    , m_handler(handle)
+{
+
+}
+
+void UIEditBoxEvent::execute()
+{
+    if (m_handler.Get())
+    {
+        m_handler.Get()->Invoke(m_sender.Get(), m_text.Get());
+    }
+}
+
+NS_CC_END
+
+
---
+++ cocos2dx/platform/win-8.1/InputEvent.h
@@ -0,0 +1,123 @@
+/****************************************************************************
+Copyright (c) 2013 cocos2d-x.org
+Copyright (c) Microsoft Open Technologies, Inc.
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#ifndef __INPUT_EVENT__
+#define __INPUT_EVENT__
+
+#include "cocos2d.h"
+#include "InputEventTypes.h"
+#include <agile.h>
+
+NS_CC_BEGIN
+
+public delegate void Cocos2dEventDelegate(Cocos2dEvent event, Platform::String^ text);
+public delegate void Cocos2dMessageBoxDelegate(Platform::String^  title, Platform::String^ text);
+public delegate void Cocos2dEditBoxDelegate(Platform::String^ strPlaceHolder, Platform::String^ strText, int maxLength, int inputMode, int inputFlag, Windows::Foundation::EventHandler<Platform::String^>^ receiveHandler);
+public delegate void Cocos2dOpenURLDelegate(Platform::String^ url);
+
+enum PointerEventType
+{
+    PointerPressed,
+    PointerMoved,
+    PointerReleased,
+};
+
+class CC_DLL InputEvent
+{
+public:
+    InputEvent() {};
+    virtual ~InputEvent() {};
+    virtual void execute() = 0;
+};
+
+
+class CC_DLL AccelerometerEvent : public InputEvent
+{
+public:
+    AccelerometerEvent(const cocos2d::CCAcceleration& event);
+    virtual void execute();
+
+private:
+    cocos2d::CCAcceleration m_event;
+};
+
+class CC_DLL PointerEvent : public InputEvent
+{
+public:
+    PointerEvent(PointerEventType type, Windows::UI::Core::PointerEventArgs^ args);
+    virtual void execute();
+
+
+private:
+    PointerEventType m_type;
+    Platform::Agile<Windows::UI::Core::PointerEventArgs> m_args;
+};
+
+class CC_DLL KeyboardEvent : public InputEvent
+
+{
+public:
+    KeyboardEvent(Cocos2dKeyEvent type);
+    KeyboardEvent(Cocos2dKeyEvent type, Platform::String^ text);
+    virtual void execute();
+
+private:
+    Cocos2dKeyEvent m_type;
+    Platform::Agile<Platform::String> m_text;
+};
+
+class CC_DLL BackButtonEvent : public InputEvent
+{
+public:
+    BackButtonEvent();
+    virtual void execute();
+};
+
+class CC_DLL CustomInputEvent : public InputEvent
+{
+public:
+    CustomInputEvent(const std::function<void()>&);
+    virtual void execute();
+private:
+    std::function<void()> m_fun;
+};
+
+class UIEditBoxEvent : public cocos2d::InputEvent
+{
+public:
+    UIEditBoxEvent(Platform::Object^ sender, Platform::String^ text, Windows::Foundation::EventHandler<Platform::String^>^ handle);
+
+    virtual void execute();
+
+private:
+    Platform::Agile<Platform::Object^> m_sender;
+    Platform::Agile<Platform::String^> m_text;
+    Platform::Agile<Windows::Foundation::EventHandler<Platform::String^>^> m_handler;
+};
+
+NS_CC_END
+
+#endif // #ifndef __INPUT_EVENT__
+
