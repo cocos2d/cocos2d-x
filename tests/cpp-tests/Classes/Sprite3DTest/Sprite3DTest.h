3c0779885d4901724f9af732a7831914820ae606
yangxiao <yangxiao@cocos2d-x.org>
2014-11-19 16:50:09 +0800

    refact reskin test code

--- tests/cpp-tests/Classes/Sprite3DTest/Sprite3DTest.h
+++ tests/cpp-tests/Classes/Sprite3DTest/Sprite3DTest.h
@@ -268,11 +268,8 @@ public:
     virtual std::string subtitle() const override;
     void onTouchesEnded(const std::vector<Touch*>& touches, Event* event);
     void addNewSpriteWithCoords(Vec2 p);
-    void menuCallback_switchHair(Ref* sender);
-    void menuCallback_switchGlasses(Ref* sender);
-    void menuCallback_switchCoat(Ref* sender);
-    void menuCallback_switchPants(Ref* sender);
-    void menuCallback_switchShoes(Ref* sender);
+    
+    void menuCallback_reSkin(Ref* sender);
 protected:
     void applyCurSkin();
     
@@ -285,8 +282,11 @@ protected:
         FACE,
         HAND,
         GLASSES,
+        MAX_TYPE,
     };
-    std::map<SkinType, std::string>  _curSkin;
+    
+    std::vector<std::string> _skins[(int)SkinType::MAX_TYPE]; //all skins
+    int                      _curSkin[(int)SkinType::MAX_TYPE]; //current skin index
     cocos2d::Sprite3D* _sprite;
 };
 class Sprite3DWithOBBPerfromanceTest : public Sprite3DTestDemo
