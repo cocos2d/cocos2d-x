769b2cbc75c9cec38a30fdbe832649f99355e5eb
youyou <501251991@qq.com>
2014-11-19 20:06:10 +0800

    add file to mac project

--- cocos/editor-support/cocostudio/ActionTimeline/CCActionTimeLineNode.cpp
+++ cocos/editor-support/cocostudio/ActionTimeline/CCActionTimeLineNode.cpp
@@ -1,26 +1,26 @@
 /****************************************************************************
-Copyright (c) 2013 cocos2d-x.org
-
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ Copyright (c) 2013 cocos2d-x.org
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 
 #include "CCActionTimeLineNode.h"
 #include "CSLoader.h"
@@ -42,8 +42,8 @@ ActionTimelineNode* ActionTimelineNode::create(Node* root, ActionTimeline* actio
 }
 
 ActionTimelineNode::ActionTimelineNode()
-    : _root(nullptr)
-    , _action(nullptr)
+: _root(nullptr)
+, _action(nullptr)
 {
 }
 
@@ -51,17 +51,22 @@ ActionTimelineNode::~ActionTimelineNode()
 {
 }
 
+bool ActionTimelineNode::init()
+{
+    return Node::init();
+}
+
 bool ActionTimelineNode::init(Node* root, ActionTimeline* action)
 {
     _root = root;
     _action = action;
-
+    
     if(_root)
     {
         _root->removeFromParent();
         addChild(_root);
     }
-
+    
     return true;
 }
 
