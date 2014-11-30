a5d74b6602081573e74fc6c5cc465b810ea16186
SunLightJuly <ygqy8181@qq.com>
2014-11-30 22:48:13 +0800

    fixed Applicatiion.mk

--- quick/templates/lua-template-quick/frameworks/runtime-src/proj.android/jni/Application.mk
+++ quick/templates/lua-template-quick/frameworks/runtime-src/proj.android/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_STL := gnustl_static
+APP_STL := c++_static
+NDK_TOOLCHAIN_VERSION=clang
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
