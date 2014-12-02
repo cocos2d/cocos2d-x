fc97a3d0c641082aa66c838f63079e45e5ca3b9a
pipu <zhounan@chukong-inc.com>
2014-12-02 16:11:46 +0800

    Modify serialize property "value" of VisibleFrame

--- cocos/editor-support/cocostudio/FlatBuffersSerialize.cpp
+++ cocos/editor-support/cocostudio/FlatBuffersSerialize.cpp
@@ -687,7 +687,7 @@ Offset<TimeLineBoolFrame> FlatBuffersSerialize::createTimeLineBoolFrame(const ti
         
         if (name == "Value")
         {
-            attrivalue = (attrivalue == "True") ? true : false;
+            value = (attrivalue == "True") ? true : false;
         }
         else if (name == "FrameIndex")
         {
