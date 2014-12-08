c3365a4654d334981ee918f90d0e496f21d7745b
pipu <zhounan@chukong-inc.com>
2014-12-08 14:32:33 +0800

    Add interface that create node with flat buffers object for simulator

--- cocos/editor-support/cocostudio/ActionTimeline/CCActionTimelineCache.cpp
+++ cocos/editor-support/cocostudio/ActionTimeline/CCActionTimelineCache.cpp
@@ -37,6 +37,8 @@ THE SOFTWARE.
 #include "flatbuffers/flatbuffers.h"
 #include "flatbuffers/util.h"
 
+#include "cocostudio/FlatBuffersSerialize.h"
+
 #include <fstream>
 
 using namespace cocos2d;
@@ -689,6 +691,41 @@ Frame* ActionTimelineCache::loadTextureFrameWithFlatBuffers(const flatbuffers::T
     frame->setTween(tween);
     
     return frame;
+}
+    
+ActionTimeline* ActionTimelineCache::createActionWithFlatBuffersForSimulator(const std::string& fileName)
+{
+    FlatBuffersSerialize* fbs = FlatBuffersSerialize::getInstance();
+    fbs->_isSimulator = true;
+    auto builder = fbs->createFlatBuffersWithXMLFileForSimulator(fileName);
+    
+    ActionTimeline* action = ActionTimeline::create();
+    
+    auto csparsebinary = GetCSParseBinary(builder->GetBufferPointer());
+    auto nodeAction = csparsebinary->action();
+    
+    action = ActionTimeline::create();
+    
+    int duration = nodeAction->duration();
+    action->setDuration(duration);
+    
+    float speed = nodeAction->speed();
+    action->setTimeSpeed(speed);
+    
+    auto timeLines = nodeAction->timeLines();
+    int timelineLength = timeLines->size();
+    for (int i = 0; i < timelineLength; i++)
+    {
+        auto timelineFlatBuf = timeLines->Get(i);
+        Timeline* timeline = loadTimelineWithFlatBuffers(timelineFlatBuf);
+        
+        if (timeline)
+            action->addTimeline(timeline);
+    }
+    
+    fbs->deleteFlatBufferBuilder();
+    
+    return action;
 }
 
 }
