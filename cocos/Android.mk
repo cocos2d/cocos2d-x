ae0888ba8d0a039b46178c0ae4fc745f31e46888
yangxiao <yangxiao@cocos2d-x.org>
2014-12-17 10:06:07 +0800

    merge cocos

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -92,6 +92,7 @@ math/TransformUtils.cpp \
 math/Vec2.cpp \
 math/Vec3.cpp \
 math/Vec4.cpp \
+base/CCAsyncTaskPool.cpp \
 base/CCAutoreleasePool.cpp \
 base/CCConfiguration.cpp \
 base/CCConsole.cpp \
