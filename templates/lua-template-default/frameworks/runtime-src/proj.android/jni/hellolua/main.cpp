cf331c3abd38201c40b7dcd748bf88c961a5368e
yinjimmy <jimmy.yin5@gmail.com>
2015-01-12 23:42:37 +0800

    update lua-template-default

--- templates/lua-template-default/frameworks/runtime-src/proj.android/jni/hellolua/main.cpp
+++ templates/lua-template-default/frameworks/runtime-src/proj.android/jni/hellolua/main.cpp
@@ -3,7 +3,6 @@
 #include "platform/android/jni/JniHelper.h"
 #include <jni.h>
 #include <android/log.h>
-#include "ConfigParser.h"
 
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
@@ -17,11 +16,6 @@ void cocos_android_app_init (JNIEnv* env, jobject thiz) {
 
 extern "C"
 {
-	bool Java_org_cocos2dx_lua_AppActivity_nativeIsLandScape(JNIEnv *env, jobject thisz)
-	{
-		return ConfigParser::getInstance()->isLanscape();
-	}
-
 	bool Java_org_cocos2dx_lua_AppActivity_nativeIsDebug(JNIEnv *env, jobject thisz)
 	{
 #if (COCOS2D_DEBUG > 0)
