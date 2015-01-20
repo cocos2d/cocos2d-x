ba7b3785c422fcf23022ed8e90887c2131c7791a
G17hao <670788361@qq.com>
2015-01-20 19:16:58 +0800

    Update CCGroupCommand.cpp
    add a vector for save unused ID. 
    traversal hash map is too slow. use this container to optimize.

--- cocos/renderer/CCGroupCommand.cpp
+++ cocos/renderer/CCGroupCommand.cpp
@@ -49,13 +49,12 @@ bool GroupCommandManager::init()
 int GroupCommandManager::getGroupID()
 {
     //Reuse old id
-    for(auto it = _groupMapping.begin(); it != _groupMapping.end(); ++it)
+    if (!_unusedIDs.empty())
     {
-        if(!it->second)
-        {
-            _groupMapping[it->first] = true;
-            return it->first;
-        }
+        int groupID = *_unusedIDs.rbegin();
+        _unusedIDs.pop_back();
+        _groupMapping[groupID] = true;
+        return groupID;
     }
 
     //Create new ID
