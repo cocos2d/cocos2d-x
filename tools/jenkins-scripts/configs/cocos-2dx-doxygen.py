0854f980fa99594a2bd591c4497bf6a981ae80a6
kompjoefriek <roel@kompjoefriek.nl>
2015-01-29 01:40:19 +0100

    fixed indentation of Python files
    - Changed tabs to spaces
    - Changed 2 spaces to 4 spaces

--- tools/jenkins-scripts/configs/cocos-2dx-doxygen.py
+++ tools/jenkins-scripts/configs/cocos-2dx-doxygen.py
@@ -5,11 +5,11 @@ print '    Branch:develop'
 print '    Target:Doxygen'
 print '    doxygen doxygen.config'
 if(os.path.exists('docs/doxygen.config') == False):
-  node_name = os.environ['NODE_NAME']
-  source_dir = '../cocos-2dx-develop-base-repo/node/' + node_name
-  source_dir = source_dir.replace("/", os.sep)
-  os.system("xcopy " + source_dir + " . /E /Y /H")
-  os.system('git pull origin develop')
+    node_name = os.environ['NODE_NAME']
+    source_dir = '../cocos-2dx-develop-base-repo/node/' + node_name
+    source_dir = source_dir.replace("/", os.sep)
+    os.system("xcopy " + source_dir + " . /E /Y /H")
+    os.system('git pull origin develop')
 
 os.system('git submodule update --init --force')
 os.chdir('docs/')
@@ -19,6 +19,6 @@ os.system('git clean -xdf -f')
 print 'build exit'
 print ret
 if ret == 0:
-  exit(0)
+    exit(0)
 else:
-  exit(1)
+    exit(1)
