9421ff857fd3464291730167799265d21ce97d99
andyque <guanghui8827@126.com>
2015-02-03 17:28:42 +0800

    fix pull request builder

--- tools/jenkins-scripts/pull-request-builder.py
+++ tools/jenkins-scripts/pull-request-builder.py
@@ -256,6 +256,7 @@ def main():
                     pr_desc = pr_desc + '<h3>size of libcpp_empty_test.so is:' + str(filesize / 1024) + 'kb</h3>'
                     set_description(pr_desc, target_url)
                     save_build_stats(db, pr_num, 'libcpp_empty_test', filesize / 1024)
+                    
             ret = os.system("python build/android-build.py -p 10 -b release lua-empty-test")
             if(ret == 0):
                 _path = 'tests/lua-empty-test/project/proj.android/libs/armeabi/liblua_empty_test.so'
@@ -264,14 +265,14 @@ def main():
                 set_description(pr_desc, target_url)
                 save_build_stats(db, pr_num, 'liblua_empty_test', filesize / 1024)
             close_db(db)
-    elif(node_name == 'win32_win7'):
-        ret = subprocess.call('"%VS110COMNTOOLS%..\IDE\devenv.com" "build\cocos2d-win32.vc2012.sln" /Build "Debug|Win32"', shell=True)
-    elif(node_name == 'ios_mac'):
-        ret = os.system(jenkins_script_path + "ios-build.sh")
-    elif(node_name == 'mac_mac'):
-        ret = os.system(jenkins_script_path + "mac-build.sh")
-    elif(node_name == 'linux_centos'):
-        ret = os.system(jenkins_script_path + "linux-build.sh")
+        elif(node_name == 'win32_win7'):
+            ret = subprocess.call('"%VS110COMNTOOLS%..\IDE\devenv.com" "build\cocos2d-win32.vc2012.sln" /Build "Debug|Win32"', shell=True)
+        elif(node_name == 'ios_mac'):
+            ret = os.system(jenkins_script_path + "ios-build.sh")
+        elif(node_name == 'mac_mac'):
+            ret = os.system(jenkins_script_path + "mac-build.sh")
+        elif(node_name == 'linux_centos'):
+            ret = os.system(jenkins_script_path + "linux-build.sh")
 
     #get build result
     print "build finished and return " + str(ret)
