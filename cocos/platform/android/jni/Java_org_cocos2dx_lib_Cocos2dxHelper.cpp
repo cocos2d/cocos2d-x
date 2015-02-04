ed38cce164810cb08f06a5cfbdf4225db109d865
Wenhai Lin <wenhai.lin@chukong-inc.com>
2015-02-04 15:38:25 +0800

    Fixed compile error cause by remove deprecated Class.

--- cocos/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxHelper.cpp
+++ cocos/platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxHelper.cpp
@@ -29,7 +29,6 @@ THE SOFTWARE.
 #include "JniHelper.h"
 #include "CCFileUtils-android.h"
 #include "android/asset_manager_jni.h"
-#include "deprecated/CCString.h"
 #include "Java_org_cocos2dx_lib_Cocos2dxHelper.h"
 
 #define  LOG_TAG    "Java_org_cocos2dx_lib_Cocos2dxHelper.cpp"
