35403439f33d282a49c67cdded326f5832f27787
Krishna <krishna.nm86@gmail.com>
2015-03-04 10:48:30 +0100

    Fixed compilation issue. Introduced missing CLASS_NAME

--- cocos/audio/android/jni/cddandroidAndroidJavaEngine.cpp
+++ cocos/audio/android/jni/cddandroidAndroidJavaEngine.cpp
@@ -295,7 +295,7 @@ namespace CocosDenshion {
             cocos2d::JniMethodInfo methodInfo;
             std::string fullPath = CocosDenshion::android::getFullPathWithoutAssetsPrefix(pszFilePath);
         
-            if (! cocos2d::JniHelper::getStaticMethodInfo(methodInfo, loadEffectName, "(Ljava/lang/String;)V")) {
+            if (! cocos2d::JniHelper::getStaticMethodInfo(methodInfo, CLASS_NAME, loadEffectName, "(Ljava/lang/String;)V")) {
                 return ;
             }
         
