cd95905a04d1e1d1a46995bac477e4a8f90e6977
WenhaiLin <wenhai.lin@chukong-inc.com>
2015-03-05 11:27:53 +0800

    Label:Fixed transform not update correctly if this camera mask of Label isn't visible by the current visiting camera

--- cocos/2d/CCLabel.cpp
+++ cocos/2d/CCLabel.cpp
@@ -1075,7 +1075,7 @@ void Label::drawTextSprite(Renderer *renderer, uint32_t parentFlags)
 
 void Label::visit(Renderer *renderer, const Mat4 &parentTransform, uint32_t parentFlags)
 {
-    if (! _visible || _originalUTF8String.empty() || !isVisitableByVisitingCamera())
+    if (! _visible || _originalUTF8String.empty())
     {
         return;
     }
@@ -1103,6 +1103,11 @@ void Label::visit(Renderer *renderer, const Mat4 &parentTransform, uint32_t pare
         _transformDirty = _inverseDirty = true;
 
         _shadowDirty = false;
+    }
+
+    if (!isVisitableByVisitingCamera())
+    {
+        return;
     }
 
     // IMPORTANT:
