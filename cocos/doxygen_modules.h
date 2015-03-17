6bbb89a09895502ef5da116aa6383375d055ba37
Vincent Yang <wensheng.yang@cocos2d-x.org>
2015-03-17 11:26:33 +0800

    Add doxygen config for white book and cocos/doxygen_modules.h for doc mudules.

---
+++ cocos/doxygen_modules.h
@@ -0,0 +1,39 @@
+/**@defgroup two_d 2D
+ **/
+
+/**@defgroup three_d 3D
+ **/
+
+/**@defgroup audio Audio
+ **/
+
+/**@defgroup base Base
+ **/
+
+/**@defgroup deprecated Deprecated
+ **/
+
+/**@defgroup math Math
+ **/
+
+/**@defgroup network Network
+ **/
+
+/**@defgroup physics Physics
+ **/
+
+/**@defgroup platform Platform
+ **/
+
+/**@defgroup renderer Renderer
+ **/
+
+/**@defgroup scripting Script Binding
+ **/
+
+/**@defgroup storage Storage
+ **/
+
+/**@defgroup ui UI
+ **/
+
