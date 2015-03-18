caf94f1c6acf191acec46373cd5d1a996c62042f
Huabing.Xu <dabingnn@gmail.com>
2015-03-18 21:57:20 +0800

    deprecate matrix stack functions in director

--- cocos/base/CCDirector.h
+++ cocos/base/CCDirector.h
@@ -392,13 +392,13 @@ public:
      */
     float getFrameRate() const { return _frameRate; }
 
-    void pushMatrix(MATRIX_STACK_TYPE type);
-    void popMatrix(MATRIX_STACK_TYPE type);
-    void loadIdentityMatrix(MATRIX_STACK_TYPE type);
-    void loadMatrix(MATRIX_STACK_TYPE type, const Mat4& mat);
-    void multiplyMatrix(MATRIX_STACK_TYPE type, const Mat4& mat);
-    const Mat4& getMatrix(MATRIX_STACK_TYPE type);
-    void resetMatrixStack();
+    CC_DEPRECATED(v4) void pushMatrix(MATRIX_STACK_TYPE type);
+    CC_DEPRECATED(v4) void popMatrix(MATRIX_STACK_TYPE type);
+    CC_DEPRECATED(v4) void loadIdentityMatrix(MATRIX_STACK_TYPE type);
+    CC_DEPRECATED(v4) void loadMatrix(MATRIX_STACK_TYPE type, const Mat4& mat);
+    CC_DEPRECATED(v4) void multiplyMatrix(MATRIX_STACK_TYPE type, const Mat4& mat);
+    CC_DEPRECATED(v4) const Mat4& getMatrix(MATRIX_STACK_TYPE type);
+    CC_DEPRECATED(v4) void resetMatrixStack();
 
 protected:
     void reset();
