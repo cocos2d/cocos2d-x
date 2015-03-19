eeaa3262b3204c816e14bbf4b7c09a367b436d22
CocosRobot <redmine@cocos2d-x.org>
2015-03-19 12:15:31 +0000

    [AUTO]: updating luabinding automatically

--- cocos/scripting/lua-bindings/auto/api/PhysicsShapeEdgeBox.lua
+++ cocos/scripting/lua-bindings/auto/api/PhysicsShapeEdgeBox.lua
@@ -5,7 +5,12 @@
 -- @parent_module cc
 
 --------------------------------
--- 
+-- Creates a PhysicsShapeEdgeBox with specified value.<br>
+-- param   size Size contains this box's width and height.<br>
+-- param   material A PhysicsMaterial object, the default value is PHYSICSSHAPE_MATERIAL_DEFAULT.<br>
+-- param   border It's a edge's border width.<br>
+-- param   offset A Vec2 object, it is the offset from the body’s center of gravity in body local coordinates.<br>
+-- return  An autoreleased PhysicsShapeEdgeBox object pointer.
 -- @function [parent=#PhysicsShapeEdgeBox] create 
 -- @param self
 -- @param #size_table size
@@ -15,7 +20,8 @@
 -- @return PhysicsShapeEdgeBox#PhysicsShapeEdgeBox ret (return value: cc.PhysicsShapeEdgeBox)
         
 --------------------------------
--- 
+-- Get this box's position offset.<br>
+-- return A Vec2 object.
 -- @function [parent=#PhysicsShapeEdgeBox] getOffset 
 -- @param self
 -- @return vec2_table#vec2_table ret (return value: vec2_table)
