b4390a505385f211588da2f6ac9a9aa6bd73f9cd
jianglong0156 <jianglong0156@gmail.com>
2015-04-01 14:53:36 +0800

    add enableShadow shadow color

--- cocos/2d/CCLabelTTF.cpp
+++ cocos/2d/CCLabelTTF.cpp
@@ -186,6 +186,12 @@ void LabelTTF::setFontName(const std::string& fontName)
     _contentDirty = true;
 }
 
+void LabelTTF::enableShadow(const Color4B &shadowColor, const Size &shadowOffset, float shadowBlur)
+{
+    _renderLabel->enableShadow(shadowColor,shadowOffset,shadowBlur);
+    _contentDirty = true;
+}
+
 void LabelTTF::enableShadow(const Size &shadowOffset, float shadowOpacity, float shadowBlur, bool updateTexture)
 {
     Color4B temp(Color3B::BLACK);
@@ -195,6 +201,8 @@ void LabelTTF::enableShadow(const Size &shadowOffset, float shadowOpacity, float
 }
 
 void LabelTTF::disableShadow(bool updateTexture)
+
+
 {
     _renderLabel->disableEffect();
     _contentDirty = true;
