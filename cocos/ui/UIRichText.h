4bba268552b7c813a149e5acbad49d8e7c4bef40
B_y <342854406@qq.com>
2015-04-04 00:54:09 +0800

    Update UIRichText.h
    support underline cache label/sprite

--- cocos/ui/UIRichText.h
+++ cocos/ui/UIRichText.h
@@ -27,55 +27,25 @@
 
 #include "ui/UIWidget.h"
 #include "ui/GUIExport.h"
+#include "2d/CCSprite.h"
+#include "2d/CCLabel.h"
+#include <set>
 
 NS_CC_BEGIN
-/**
- * @addtogroup ui
- * @{
- */
 
 namespace ui {
     
-/**
- *@brief Rich text element base class.
- * It defines the basic common properties for all rich text element.
- */
 class CC_GUI_DLL RichElement : public Ref
 {
 public:
-    /**
-     *@brief Rich element type.
-     */
     enum class Type
     {
         TEXT,
         IMAGE,
         CUSTOM
     };
-    
-    /**
-     * @brief Default constructor.
-     * @js ctor
-     * @lua new
-     */
     RichElement(){};
-    
-    /**
-     * @brief Default destructor.
-     * @js NA
-     * @lua NA
-     */
     virtual ~RichElement(){};
-
-    
-    /**
-     * @brief Initialize a rich element with different arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in @see `Color3B`.
-     * @param opacity A opacity value in `GLubyte`.
-     * @return True if initialize success, false otherwise.
-     */
     bool init(int tag, const Color3B& color, GLubyte opacity);
 protected:
     Type _type;
@@ -85,107 +55,28 @@ protected:
     friend class RichText;
 };
     
-/**
- *@brief Rich element for displaying text.
- */
 class CC_GUI_DLL RichElementText : public RichElement
 {
 public:
-    
-    /**
-     *@brief Default constructor.
-     * @js ctor
-     * @lua new
-     */
     RichElementText(){_type = Type::TEXT;};
-
-    
-    /**
-     *@brief Default destructor.
-     * @js NA
-     * @lua NA
-     */
     virtual ~RichElementText(){};
-    
-    /**
-     * @brief Initialize a RichElementText with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param text Content string.
-     * @param fontName Content font name.
-     * @param fontSize Content font size.
-     * @return True if initialize scucess, false otherwise.
-     */
-    bool init(int tag, const Color3B& color, GLubyte opacity, const std::string& text, const std::string& fontName, float fontSize);
-
-    
-    /**
-     * @brief Create a RichElementText with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param text Content string.
-     * @param fontName Content font name.
-     * @param fontSize Content font size.
-     * @return RichElementText instance.
-     */
-    static RichElementText* create(int tag, const Color3B& color, GLubyte opacity, const std::string& text, const std::string& fontName, float fontSize);
+    bool init(int tag, const Color3B& color, GLubyte opacity, const std::string& text, const std::string& fontName, float fontSize,bool isUnderLine);
+    static RichElementText* create(int tag, const Color3B& color, GLubyte opacity, const std::string& text, const std::string& fontName, float fontSize,bool isUnderLine = false);
 protected:
     std::string _text;
     std::string _fontName;
     float _fontSize;
+	bool _isUnderLine;
     friend class RichText;
     
 };
     
-/**
- *@brief Rich element for displaying images.
- */
 class CC_GUI_DLL RichElementImage : public RichElement
 {
 public:
-    
-    /**
-     * @brief Default constructor.
-     * @js ctor
-     * @lua new
-     *
-     */
     RichElementImage(){_type = Type::IMAGE;};
-
-    
-    /**
-     * @brief Default destructor.
-     * @js NA
-     * @lua NA
-     */
     virtual ~RichElementImage(){};
-
-    
-    /**
-     * @brief Initialize a RichElementImage with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param filePath A image file name.
-     * @return True if initialize success, false otherwise.
-     */
     bool init(int tag, const Color3B& color, GLubyte opacity, const std::string& filePath);
-
-    
-    /**
-     * @brief Create a RichElementImage with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param filePath A image file name.
-     * @return A RichElementImage instance.
-     */
     static RichElementImage* create(int tag, const Color3B& color, GLubyte opacity, const std::string& filePath);
 protected:
     std::string _filePath;
@@ -194,149 +85,81 @@ protected:
     friend class RichText;
 };
     
-/**
- *@brief Rich element for displaying custom node type.
- */
 class CC_GUI_DLL RichElementCustomNode : public RichElement
 {
 public:
-    
-    /**
-     * @brief Default constructor.
-     * @js ctor
-     * @lua new
-     */
     RichElementCustomNode(){_type = Type::CUSTOM;};
-
-    
-    /**
-     * @brief Default destructor.
-     * @js NA
-     * @lua NA
-     */
     virtual ~RichElementCustomNode(){CC_SAFE_RELEASE(_customNode);};
-
-    
-    /**
-     * @brief Initialize a RichElementCustomNode with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param customNode A custom node pointer.
-     * @return True if initialize success, false otherwise.
-     */
     bool init(int tag, const Color3B& color, GLubyte opacity, Node* customNode);
-    
-    /**
-     * @brief Create a RichElementCustomNode with various arguments.
-     *
-     * @param tag A integer tag value.
-     * @param color A color in Color3B.
-     * @param opacity A opacity in GLubyte.
-     * @param customNode A custom node pointer.
-     * @return A RichElementCustomNode instance.
-     */
     static RichElementCustomNode* create(int tag, const Color3B& color, GLubyte opacity, Node* customNode);
 protected:
     Node* _customNode;
     friend class RichText;
 };
     
-/**
- *@brief A container for displaying various RichElements.
- * We could use it to display texts with images easily.
- */
+class RichCacheElement : public Ref
+{
+public:
+	RichCacheElement():_isUse(false),_node(nullptr){};
+	virtual ~RichCacheElement();
+	bool init(bool isUse, Node* node);
+	static RichCacheElement* create(bool isUse, Node* node);
+protected:
+	bool _isUse;
+	Node* _node;
+	friend class RichText;
+};
+
+typedef std::vector< std::string > stringVector;
 class CC_GUI_DLL RichText : public Widget
 {
 public:
-    
-    /**
-     * @brief Default constructor.
-     * @js ctor
-     * @lua new
-     */
     RichText();
-    
-    /**
-     * @brief Default destructor.
-     * @js NA
-     * @lua NA
-     */
     virtual ~RichText();
-    
-    /**
-     * @brief Create a empty RichText.
-     *
-     * @return RichText instance.
-     */
     static RichText* create();
-    
-    /**
-     * @brief Insert a RichElement at a given index.
-     *
-     * @param element A RichElement type.
-     * @param index A given index.
-     */
     void insertElement(RichElement* element, int index);
-    
-    /**
-     * @brief Add a RichElement at the end of RichText.
-     *
-     * @param element A RichElement instance.
-     */
     void pushBackElement(RichElement* element);
-    
-    /**
-     * @brief Remove a RichElement at a given index.
-     *
-     * @param index A integer index value.
-     */
     void removeElement(int index);
-    
-    /**
-     * @brief Remove specific RichElement.
-     *
-     * @param element A RichElement type.
-     */
     void removeElement(RichElement* element);
-    
-    /**
-     * @brief Set vertical space between each RichElement.
-     *
-     * @param space Point in float.
-     */
-    void setVerticalSpace(float space);
-    
-    /**
-     * @brief Rearrange all RichElement in the RichText.
-     * It's usually called internally.
-     */
-    void formatText();
 
-    //override functions.
-    virtual void setAnchorPoint(const Vec2 &pt) override;
+    void setVerticalSpace(float space);
+    virtual void setAnchorPoint(const Vec2 &pt);
     virtual Size getVirtualRendererSize() const override;
-    virtual void ignoreContentAdaptWithSize(bool ignore) override;
+	void cleanAllElement();
+    void formatText();
+    virtual void ignoreContentAdaptWithSize(bool ignore);
     virtual std::string getDescription() const override;
+	Node* getContainer(){ return _elementRenderersContainer; };
+	stringVector split(const std::string& s,const std::string& delim);
     
+    virtual void draw(Renderer *renderer, const Mat4 &transform, uint32_t flags) override;
 CC_CONSTRUCTOR_ACCESS:
     virtual bool init() override;
     
 protected:
-    virtual void adaptRenderers() override;
+    void onDraw(const Mat4 &transform, uint32_t flags);
+    CustomCommand _customCommand;
+    virtual void adaptRenderers();
 
-    virtual void initRenderer() override;
+    virtual void initRenderer();
     void pushToContainer(Node* renderer);
-    void handleTextRenderer(const std::string& text, const std::string& fontName, float fontSize, const Color3B& color, GLubyte opacity);
-    void handleImageRenderer(const std::string& fileParh, const Color3B& color, GLubyte opacity);
-    void handleCustomRenderer(Node* renderer);
+    void handleTextRenderer(const std::string& text, const std::string& fontName, float fontSize,bool isUnder, const Color3B& color, GLubyte opacity,int tag);
+    void handleImageRenderer(const std::string& fileParh, const Color3B& color, GLubyte opacity,int tag);
+    void handleCustomRenderer(Node* renderer,int tag);
     void formarRenderers();
     void addNewLine();
+
+	Label* getCacheLabel();
+	Sprite* getCacheImage();
+	Node* makeLabel(Label* pTarget, const std::string& filePath, const std::string& fontName, int fontSize,bool isUnder,int tag);
+	Node* makeImage(Sprite* pTarget, const std::string& filePath,int tag);
 protected:
     bool _formatTextDirty;
     Vector<RichElement*> _richElements;
     std::vector<Vector<Node*>*> _elementRenders;
+	Vector<RichCacheElement*> _cacheLabElements;
+	Vector<RichCacheElement*> _cacheImgElements;
+    std::set<int> _underLineTags;
     float _leftSpaceWidth;
     float _verticalSpace;
     Node* _elementRenderersContainer;
@@ -344,8 +167,6 @@ protected:
     
 }
 
-// end of ui group
-/// @}
 NS_CC_END
 
 #endif /* defined(__UIRichText__) */
