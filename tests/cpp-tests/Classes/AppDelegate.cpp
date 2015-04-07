6bcddbc7793763025bc94848326083c3531e6b53
WenhaiLin <wenhai.lin@chukong-inc.com>
2015-04-07 16:14:27 +0800

    Test automation support:Catch crash event on WIN32/MAC/IOS/ANDROID

--- tests/cpp-tests/Classes/AppDelegate.cpp
+++ tests/cpp-tests/Classes/AppDelegate.cpp
@@ -33,6 +33,7 @@
 USING_NS_CC;
 
 AppDelegate::AppDelegate()
+: _testController(nullptr)
 {
 }
 
@@ -179,13 +180,21 @@ bool AppDelegate::applicationDidFinishLaunching()
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
-    _testController->onEnterBackground();
+    if (_testController)
+    {
+        _testController->onEnterBackground();
+    }
+    
     Director::getInstance()->stopAnimation();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground()
 {
-    _testController->onEnterForeground();
+    if (_testController)
+    {
+        _testController->onEnterForeground();
+    }
+    
     Director::getInstance()->startAnimation();
 }
