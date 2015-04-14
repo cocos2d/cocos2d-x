888c289fd13f0ccfc61207b17432d0ca089ae913
songchengjiang <songcheng.jiang@chukong-inc.com>
2015-04-14 18:02:51 +0800

    add the parameters of Physics3D in Android.mk

--- tests/cpp-tests/proj.android/jni/Android.mk
+++ tests/cpp-tests/proj.android/jni/Android.mk
@@ -171,6 +171,7 @@ LOCAL_SRC_FILES := main.cpp \
 ../../Classes/PerformanceTest/PerformanceCallbackTest.cpp \
 ../../Classes/PerformanceTest/PerformanceMathTest.cpp \
 ../../Classes/PhysicsTest/PhysicsTest.cpp \
+../../Classes/Physics3DTest/Physics3DTest.cpp \
 ../../Classes/ReleasePoolTest/ReleasePoolTest.cpp \
 ../../Classes/RenderTextureTest/RenderTextureTest.cpp \
 ../../Classes/RotateWorldTest/RotateWorldTest.cpp \
