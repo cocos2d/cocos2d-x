87e29db218435d66b55b87eb7ffd3718b74d5851
CocosRobot <redmine@cocos2d-x.org>
2015-04-20 05:40:58 +0000

    [AUTO]: updating luabinding automatically

---
+++ cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_experimental_webview_auto_api.lua
@@ -0,0 +1,9 @@
+--------------------------------
+-- @module ccexp
+
+--------------------------------------------------------
+-- the ccexp WebView
+-- @field [parent=#ccexp] WebView#WebView WebView preloaded module
+
+
+return nil
