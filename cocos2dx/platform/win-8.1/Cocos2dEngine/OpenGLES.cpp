10ed9ec68b917c454ac14c1273657bcce1ed5d8b
Dale Stammen <dalestam@microsoft.com>
2015-05-04 10:40:48 -0700

    updated to Angle 1d2cd58a3e

--- cocos2dx/platform/win-8.1/Cocos2dEngine/OpenGLES.cpp
+++ cocos2dx/platform/win-8.1/Cocos2dEngine/OpenGLES.cpp
@@ -1,5 +1,22 @@
-#include "OpenGLES.h"
+/*
+* cocos2d-x   http://www.cocos2d-x.org
+*
+* Copyright (c) 2010-2014 - cocos2d-x community
+*
+* Portions Copyright (c) Microsoft Open Technologies, Inc.
+* All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and limitations under the License.
+*/
 
+#include "OpenGLES.h"
 using namespace Platform;
 using namespace Windows::UI::Xaml::Controls;
 using namespace Windows::Foundation;
@@ -42,6 +59,15 @@ void OpenGLES::Initialize()
         // These are the default display attributes, used to request ANGLE's D3D11 renderer.
         // eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
         EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+
+        // EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
+        // Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+
+        // EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE is an option that enables ANGLE to automatically call 
+        // the IDXGIDevice3::Trim method on behalf of the application when it gets suspended. 
+        // Calling IDXGIDevice3::Trim when an application is suspended is a Windows Store application certification requirement.
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
         EGL_NONE,
     };
     
@@ -52,6 +78,8 @@ void OpenGLES::Initialize()
         EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
         EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
         EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE, 
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
         EGL_NONE,
     };
 
@@ -60,7 +88,9 @@ void OpenGLES::Initialize()
         // These attributes can be used to request D3D11 WARP.
         // They are used if eglInitialize fails with both the default display attributes and the 9_3 display attributes.
         EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
-        EGL_PLATFORM_ANGLE_USE_WARP_ANGLE, EGL_TRUE,
+        EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_DEVICE_TYPE_WARP_ANGLE,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE, 
+        EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
         EGL_NONE,
     };
     
@@ -73,8 +103,24 @@ void OpenGLES::Initialize()
         throw Exception::CreateException(E_FAIL, L"Failed to get function eglGetPlatformDisplayEXT");
     }
 
+    //
+    // To initialize the display, we make three sets of calls to eglGetPlatformDisplayEXT and eglInitialize, with varying 
+    // parameters passed to eglGetPlatformDisplayEXT:
+    // 1) The first calls uses "defaultDisplayAttributes" as a parameter. This corresponds to D3D11 Feature Level 10_0+.
+    // 2) If eglInitialize fails for step 1 (e.g. because 10_0+ isn't supported by the default GPU), then we try again 
+    //    using "fl9_3DisplayAttributes". This corresponds to D3D11 Feature Level 9_3.
+    // 3) If eglInitialize fails for step 2 (e.g. because 9_3+ isn't supported by the default GPU), then we try again 
+    //    using "warpDisplayAttributes".  This corresponds to D3D11 Feature Level 11_0 on WARP, a D3D11 software rasterizer.
+    //
+    // Note: On Windows Phone, we #ifdef out the first set of calls to eglPlatformDisplayEXT and eglInitialize.
+    //       Windows Phones devices only support D3D11 Feature Level 9_3, but the Windows Phone emulator supports 11_0+.
+    //       We use this #ifdef to limit the Phone emulator to Feature Level 9_3, making it behave more like
+    //       real Windows Phone devices.
+    //       If you wish to test Feature Level 10_0+ in the Windows Phone emulator then you should remove this #ifdef.
+    //
+    
 #if (WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP)
-    // Firstly get the display, and apply the default display attributes. They correspond to D3D11 Feature Level 10_0+.
+    // This tries to initialize EGL to D3D11 Feature Level 10_0+. See above comment for details.
     mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, defaultDisplayAttributes);
     if (mEglDisplay == EGL_NO_DISPLAY)
     {
@@ -82,11 +128,9 @@ void OpenGLES::Initialize()
     }
 
     if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
+#endif    
     {
-#endif
-        // This call to eglInitialize (after applying the default display attributes) will return EGL_FALSE if Feature Level 10_0+ 
-        // is not supported by the hardware graphics card. This could happen on Windows Phone devices or on certain Windows tablets.
-        // Instead, we try applying the D3D11 Feature Level 9_3 display attributes and try to initialize the display again.
+        // This tries to initialize EGL to D3D11 Feature Level 9_3, if 10_0+ is unavailable (e.g. on Windows Phone, or certain Windows tablets).
         mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, fl9_3DisplayAttributes);
         if (mEglDisplay == EGL_NO_DISPLAY)
         {
@@ -95,8 +139,7 @@ void OpenGLES::Initialize()
 
         if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
         {
-            // This call to eglInitialize will return EGL_FALSE if Feature Level 9_3 isn't supported either (for example, on a Surface RT).
-            // We must now use WARP, a fully-featured D3D11 software rasterizer. WARP supports all valid D3D11 feature levels.
+            // This initializes EGL to D3D11 Feature Level 11_0 on WARP, if 9_3+ is unavailable on the default GPU (e.g. on Surface RT).
             mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, warpDisplayAttributes);
             if (mEglDisplay == EGL_NO_DISPLAY)
             {
@@ -109,9 +152,8 @@ void OpenGLES::Initialize()
                 throw Exception::CreateException(E_FAIL, L"Failed to initialize EGL");
             }
         }
-#if (WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP)
     }
-#endif
+
     EGLint numConfigs = 0;
     if ((eglChooseConfig(mEglDisplay, configAttributes, &mEglConfig, 1, &numConfigs) == EGL_FALSE) || (numConfigs == 0))
     {
@@ -155,6 +197,14 @@ EGLSurface OpenGLES::CreateSurface(SwapChainPanel^ panel, const Size* renderSurf
 
     EGLSurface surface = EGL_NO_SURFACE;
 
+    const EGLint surfaceAttributes[] =
+    {
+        // EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER is part of the same optimization as EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER (see above).
+        // If you have compilation issues with it then please update your Visual Studio templates.
+        EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE
+    };
+    
     // Create a PropertySet and initialize with the EGLNativeWindowType.
     PropertySet^ surfaceCreationProperties = ref new PropertySet();
     surfaceCreationProperties->Insert(ref new String(EGLNativeWindowTypeProperty), panel);
@@ -165,7 +215,7 @@ EGLSurface OpenGLES::CreateSurface(SwapChainPanel^ panel, const Size* renderSurf
         surfaceCreationProperties->Insert(ref new String(EGLRenderSurfaceSizeProperty), PropertyValue::CreateSize(*renderSurfaceSize));
     }
 
-    surface = eglCreateWindowSurface(mEglDisplay, mEglConfig, reinterpret_cast<IInspectable*>(surfaceCreationProperties), NULL);
+    surface = eglCreateWindowSurface(mEglDisplay, mEglConfig, reinterpret_cast<IInspectable*>(surfaceCreationProperties), surfaceAttributes);
     if (surface == EGL_NO_SURFACE)
     {
         throw Exception::CreateException(E_FAIL, L"Failed to create EGL surface");
