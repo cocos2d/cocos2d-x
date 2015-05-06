10b71464a792963c51463f72478f7bd51a5e5cf0
Ricardo Quesada <ricardoquesada@gmail.com>
2015-05-05 17:17:41 -0700

    No throws in cocos2d-x code

--- tests/cpp-tests/Classes/controller.cpp
+++ tests/cpp-tests/Classes/controller.cpp
@@ -16,7 +16,6 @@ class RootTests : public TestList
 public:
     RootTests()
     {
-        addTest("SpritePolygon", [](){return new (std::nothrow) SpritePolygonTest(); });
         addTest("ActionManager", [](){return new (std::nothrow) ActionManagerTests(); });
         addTest("Actions - Basic", [](){ return new (std::nothrow) ActionsTests(); });
         addTest("Actions - Ease", [](){return new (std::nothrow) ActionsEaseTests(); });
@@ -66,6 +65,7 @@ public:
         addTest("Node: Spine", [](){return new SpineTests(); });
         addTest("Node: Sprite", [](){return new SpriteTests(); });
         addTest("Node: Sprite3D", [](){  return new Sprite3DTests(); });
+        addTest("Node: SpritePolygon", [](){return new (std::nothrow) SpritePolygonTest(); });
         addTest("Node: Terrain", [](){  return new TerrainTests(); });
         addTest("Node: TileMap", [](){return new TileMapTests(); });
         addTest("Node: FastTileMap", [](){return new FastTileMapTests(); });
