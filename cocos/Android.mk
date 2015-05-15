384110193f1d581c4a0fb7a2c771c55f1db63cbf
andyque <guanghui8827@126.com>
2015-05-15 17:59:21 +0800

    fix lua and Android compile error

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -103,7 +103,7 @@ math/TransformUtils.cpp \
 math/Vec2.cpp \
 math/Vec3.cpp \
 math/Vec4.cpp \
-base/CCNinePatchImageParser \
+base/CCNinePatchImageParser.cpp \
 base/CCAsyncTaskPool.cpp \
 base/CCAutoreleasePool.cpp \
 base/CCConfiguration.cpp \
