c8d97d9178ff193829574520ff1ccb0bfa6398ed
hugo <hugohuang1111@gmail.com>
2015-05-22 17:43:31 +0800

    [M] invoke native bat command on windows

--- tools/framework-compile/bin/utils_cocos.py
+++ tools/framework-compile/bin/utils_cocos.py
@@ -8,6 +8,59 @@ import shutil
 if sys.platform == 'win32':
 	import _winreg
 
+def execute_command(cmdstring, cwd=None, timeout=None, shell=True):
+	""" 执行一个SHELL命令
+		封装了subprocess的Popen方法, 支持超时判断，支持读取stdout和stderr
+		参数:
+		cwd: 运行命令时更改路径，如果被设定，子进程会直接先更改当前路径到cwd
+		timeout: 超时时间，秒，支持小数，精度0.1秒
+		shell: 是否通过shell运行
+		Returns: return_code
+		Raises:  Exception: 执行超时
+	"""
+
+	import shlex
+	import datetime
+	import subprocess
+	import time
+
+	#if os_is_win32():
+	#	cmdstring = convert_to_python_path(cmdstring)
+
+	print("")
+	print("Execute command:")
+	print(cmdstring)
+	print("")
+
+	if shell:
+		cmdstring_list = cmdstring
+	else:
+		cmdstring_list = shlex.split(cmdstring)
+	if timeout:
+		end_time = datetime.datetime.now() + datetime.timedelta(seconds=timeout)
+
+	# 没有指定标准输出和错误输出的管道,因此会打印到屏幕上
+	sub = None
+	try:
+		sub = subprocess.Popen(cmdstring_list, cwd=cwd, stdin=subprocess.PIPE, shell=shell, bufsize=4096)
+	except Exception, e:
+		print "execute command fail:%s" % cmdstring
+		raise e
+
+	# subprocess.poll()方法：检查子进程是否结束了，如果结束了，设定并返回码，放在subprocess.returncode变量中
+	while sub.poll() is None:
+		time.sleep(0.1)
+		if timeout:
+			if end_time <= datetime.datetime.now():
+				raise Exception("Timeout：%s"%cmdstring)
+
+	if 0 != sub.returncode :
+		errStr = "[ERROR] execute command fail:%s" % cmdstring
+		print errStr
+		raise Exception(errStr)
+
+	return sub.returncode
+
 def get_required_vs_version(proj_file):
 		# Now VS2012 is the mini version required
 		return "11.0"
@@ -64,12 +117,11 @@ def get_vs_cmd_path(vs_reg, proj_path, vs_version):
 	return (commandPath, needUpgrade)
 
 def rmdir(folder):
-	if os.path.exists(folder):
-		def errorHandler(fun, path, excinfo):
-			print "[utils_cocos:rmdir] remove error file:%s" % path
-			print excinfo[1]
-
-		shutil.rmtree(folder, ignore_errors=False, onerror= errorHandler)
+	if os.path.eaxists(folder):
+		if sys.platform == 'win32':
+			execute_command("rd /s/q %s" % folder)
+		elsae:
+			shutil.rmtree(folder)
 
 def mkdir(folder):
 	if not os.path.exists(folder):
