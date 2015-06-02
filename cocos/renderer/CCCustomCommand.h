4b69b4560dcaad652b0a0686bcc56e7eca88f275
Huabing.Xu <dabingnn@gmail.com>
2015-06-02 17:43:02 +0800

    [ci skip]add chinese comments for CCCustomCommand.h

--- cocos/renderer/CCCustomCommand.h
+++ cocos/renderer/CCCustomCommand.h
@@ -35,38 +35,60 @@
 
 NS_CC_BEGIN
 /**
+@class CustomCommand
+@brief
+@~english
 Custom command is used for call custom openGL command which can not be done by other commands,
 such as stencil function, depth functions etc. The render command is executed by calling a call back function.
+ * @~chinese 
+ * Custom command用于调用自定义openGL命令,
+ * 如模板功能,深度函数等,命令通过调用回调函数来执行。
 */
 class CC_DLL CustomCommand : public RenderCommand
 {
 public:
-	/**Constructor.*/
+	/**
+    @~english Constructor. 
+    @~chinese 构造函数。
+    */
     CustomCommand();
-    /**Destructor.*/
+    /**
+    @~english Destructor. 
+    @~chinese 析构函数。
+    */
     ~CustomCommand();
     
 public:
-	/**
+	/**@~english
 	Init function.
-	@param globalZOrder GlobalZOrder of the render command.
-	@param modelViewTransform When in 3D mode, depth sorting needs modelViewTransform.
-	@param flags Use to identify that the render command is 3D mode or not.
+  * @~chinese 
+  * 初始化。
+	@param globalZOrder @~english GlobalZOrder of the render command.
+  * @~chinese 命令的GlobalZOrder。
+	@param modelViewTransform @~english When in 3D mode, depth sorting needs modelViewTransform.
+  * @~chinese 在3d模式下,深度排序需要modelView矩阵。
+	@param flags @~english Use to identify that the render command is 3D mode or not.
+  * @~chinese 是否处于3D渲染模式。
 	*/
     void init(float globalZOrder, const Mat4& modelViewTransform, uint32_t flags);
-    /**
+    /**@~english
     Init function. The render command will be in 2D mode.
-    @param globalZOrder GlobalZOrder of the render command.
+     * @~chinese 
+     * Init函数。渲染命令将在2d模式下。
+    @param globalZOrder @~english GlobalZOrder of the render command.
+     * @~chinese 命令的GlobalZOrder。
     */
     void init(float globalZOrder);
 
-    /**
+    /**@~english
     Execute the render command and call callback functions.
+     * @~chinese 
+     * 执行渲染命令和调用回调函数。
     */
     void execute();
     //TODO: This function is not used, it should be removed.
     inline bool isTranslucent() { return true; }
-    /**Callback function.*/
+    /**@~english Callback function. @~chinese 回调函数。*/
     std::function<void()> func;
 
 protected:
