a51a32fa9d3bf036c3c1727e9a5ca74b6ad70c9e
zhangcheng <zhangcheng@chukong-inc.com>
2015-06-03 15:10:48 +0800

    1, add onEnter and onExit of components to CCNode's onEnter and onExit function.

--- cocos/2d/CCNode.cpp
+++ cocos/2d/CCNode.cpp
@@ -1414,6 +1414,11 @@ void Node::onEnter()
     if (_onEnterCallback)
         _onEnterCallback();
 
+    if (_componentContainer && !_componentContainer->isEmpty())
+    {
+        _componentContainer->onEnter();
+    }
+
 #if CC_ENABLE_SCRIPT_BINDING
     if (_scriptType == kScriptTypeJavascript)
     {
@@ -1493,6 +1498,11 @@ void Node::onExit()
     if (_onExitCallback)
         _onExitCallback();
     
+    if (_componentContainer && !_componentContainer->isEmpty())
+    {
+        _componentContainer->onExit();
+    }
+
 #if CC_ENABLE_SCRIPT_BINDING
     if (_scriptType == kScriptTypeJavascript)
     {
