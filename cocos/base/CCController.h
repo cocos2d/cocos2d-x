6155a408da2c61e4aefb9431a1c862312edb72d1
Jacky <fusijie@vip.qq.com>
2015-06-03 18:41:03 +0800

    CCController API Chinesization.

--- cocos/base/CCController.h
+++ cocos/base/CCController.h
@@ -37,22 +37,29 @@ class EventListenerController;
 class EventController;
 class EventDispatcher;
 
+
 /**
  * @addtogroup base
  * @{
  */
 
+
 /**
  * @class Controller
- * @brief A Controller object represents a connected physical game controller.
+ * @brief @~english A Controller object represents a connected physical game controller.
+ * @~chinese 一个控制器对象表示一个与物理游戏连接的控制器。
  * @js NA
  */
 class Controller
 {
 public:
-    /** 
+
+    /** @~english
      * Controllers' standard  key
      * Controller receives only standard key which contained within enum Key by default.
+     * @~chinese 
+     * 控制器的标准键
+     * 默认地，控制器只接收包含在enum Key中的标准key。
      */
     enum Key
     {
@@ -90,93 +97,157 @@ public:
         KEY_MAX
     };
 
+
     /**
      * @struct KeyStatus
-     * 
+     *
      */
     typedef struct _keyStatus
     {
-        /** A Boolean value that indicates whether the key is considered pressed. */
+
+        /** @~english A Boolean value that indicates whether the key is considered pressed.  @~chinese 一个布尔值,表明键是否按下。*/
         bool isPressed;
-        /** The value of key.This value is used in conjunction with the isPressed parameter. */
+
+        /** @~english The value of key.This value is used in conjunction with the isPressed parameter.  @~chinese 键值。这个值是与isPressed参数一起使用。*/
         float value;
-        /** A Boolean value that indicates whether the value of key is analog.
+
+        /** @~english A Boolean value that indicates whether the value of key is analog.
          * If isAnalog is true, the key value might be a float from -1 to 1. 
          * If isAnalog is false, the key value would be contain one number: 0 or 1.
+         * @~chinese 一个布尔值,指示键值是否是模拟的。
+         * 如果isAnalog是真的,键值可能是一个浮点数从1到1。
+         * 如果isAnalog是假的,键值将包含一个数字:0或1。
          */
         bool isAnalog;
     }KeyStatus;
     
     static const int TAG_UNSET = -1;
 
-    /**
+
+    /**@~english
      * Gets all Controller objects.
+     * @~chinese 
+     * 获取所有控制器对象。
+     * @return @~english all Controller objects.
+     * @~chinese 所有控制器对象。
      */
     static const std::vector<Controller*>& getAllController(){ return s_allController;}
 
-    /** 
+
+    /** @~english
      * Gets a Controller object with tag.
      *
-     * @param tag   An identifier to find the controller.
-     * @return A Controller object.
+     * @~chinese 
+     * 根据标签得到一个控制器对象。
+     * 
+     * @param tag   @~english An identifier to find the controller.
+     * @~chinese 一个用于查找控制器的标识。
+     * @return @~english A Controller object.
+     * @~chinese 一个控制器对象。
      */
     static Controller* getControllerByTag(int tag);
 
-    /**
+
+    /**@~english
      * Start discovering new controllers.
      *
-     * @warning The API only work on the IOS platform. Empty implementation on other platform.
+     * @~chinese 
+     * 开始发现新控制器。
+     * 
+     * @warning @~english The API only work on the IOS platform. Empty implementation on other platform.
+     * @~chinese 此API只在IOS平台上工作。其他平台为空实现。
      */
     static void startDiscoveryController();
 
-    /** 
+
+    /** @~english
      * Stop the discovery process.
      *
-     * @warning The API only work on the IOS platform.Empty implementation on other platform.
+     * @~chinese 
+     * 停止发现控制器。
+     * 
+     * @warning @~english The API only work on the IOS platform.Empty implementation on other platform.
+     * @~chinese 此API只在IOS平台上工作。其他平台上为空实现。
      */
     static void stopDiscoveryController();
 
-    /**
+
+    /**@~english
      * Gets the name of this Controller object.
+     * @~chinese 
+     * 获取这个控制器对象的名称。
+     * @return @~english The name of the Controller object.
+     8 @~chinese 控制器对象的名称。
      */
     const std::string& getDeviceName() const { return _deviceName;}
 
-    /**
+
+    /**@~english
      * Gets the Controller id.
+     * @~chinese 
+     * 获取控制器对象id。
+     * @return @~english The Controller id.
+     * @~chinese 控制器对象id。
      */
     int getDeviceId() const { return _deviceId;}
 
-    /**
+
+    /**@~english
      * Indicates whether the Controller is connected.
+     * @~chinese 
+     * 指示是否连接控制器。
      */
     bool isConnected() const;
 
-    /**
-     * 
+
+    /** @~english 
+     * Get key status by key code.
+     * @~chinese
+     * 通过按键code获取按键的状态。
+     * @return @~english key status.
+     * @~chinese 按键状态。
      */
     const KeyStatus& getKeyStatus(int keyCode);
     
-    /** 
+
+    /** @~english
      * Activate receives key event from external key. e.g. back,menu.
      * Controller receives only standard key which contained within enum Key by default.
      *
-     * @warning The API only work on the android platform for support diversified game controller.
+     * @~chinese 
+     * 激活从外部按键接收按键事件。例如,菜单。
+     * 默认地，控制器只接收包含在enum Key中的标准key。
+     * 
+     * @warning @~english The API only work on the android platform for support diversified game controller.
      *
-     * @param externalKeyCode   External key code.
-     * @param receive   True if external key event on this controller should be receive, false otherwise.
+     * @~chinese 此API只支持android平台上多元化的游戏控制器。
+     * 
+     * @param externalKeyCode   @~english External key code.
+     * @~chinese 外部按键代码。
+     * @param receive   @~english True if external key event on this controller should be receive, false otherwise.
+     * @~chinese 如果控制器应该收到外部按键事件则为真,否则假。
      */
     void receiveExternalKeyEvent(int externalKeyCode,bool receive);
 
-    /** 
+
+    /** @~english
      * Changes the tag that is used to identify the controller easily.
-     * @param tag   A integer that identifies the controller.
+     * @~chinese 
+     * 更改标签,方便用于识别控制器。
+     * @param tag   @~english A integer that identifies the controller.
+     * @~chinese 一个标识控制器的整数。
      */
     void setTag(int tag) { _controllerTag = tag;}
 
-    /**
+
+    /**@~english
      * Returns a tag that is used to identify the controller easily.
      *
-     * @return An integer that identifies the controller.
+     * @~chinese 
+     * 返回一个用于识别控制器的标识。
+     * 
+     * @return @~english An integer that identifies the controller.
+     * @~chinese 一个标识控制器的整数。
      */
     int getTag() const { return _controllerTag;}
 
