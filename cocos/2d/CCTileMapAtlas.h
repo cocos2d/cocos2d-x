128b26739b5ae3ac2986a663ae0b99ea262a68a3
Bin Zhang <zhangbin@cocos2d-x.org>
2015-06-03 17:23:52 +0800

    Update comments of CCTileMapAtlas.h.

--- cocos/2d/CCTileMapAtlas.h
+++ cocos/2d/CCTileMapAtlas.h
@@ -36,27 +36,53 @@ NS_CC_BEGIN
 
 struct sImageTGA;
 
-/** @brief TileMapAtlas is a subclass of AtlasNode.
-
-It knows how to render a map based of tiles.
-The tiles must be in a .PNG format while the map must be a .TGA file.
-
-For more information regarding the format, please see this post:
-http://www.cocos2d-iphone.org/archives/27
-
-All features from AtlasNode are valid in TileMapAtlas
-
-IMPORTANT:
-This class is deprecated. It is maintained for compatibility reasons only.
-You SHOULD not use this class.
-Instead, use the newer TMX file format: TMXTiledMap
-@js NA
-*/
+/** @brief @~english TileMapAtlas is a subclass of AtlasNode.
+ * 
+ * It knows how to render a map based of tiles.
+ * The tiles must be in a .PNG format while the map must be a .TGA file.
+ * 
+ * For more information regarding the format, please see this post:
+ * http://www.cocos2d-iphone.org/archives/27
+ * 
+ * All features from AtlasNode are valid in TileMapAtlas
+ * 
+ * IMPORTANT:
+ * This class is deprecated. It is maintained for compatibility reasons only.
+ * You SHOULD not use this class.
+ * Instead, use the newer TMX file format: TMXTiledMap
+ *
+ * @~chinese TileMapAtlas 是 AtlasNode的一个子类。
+ * 
+ * 用于绘制 TileMap。
+ * TileMap 所使用的图片资源必须在同一个 .png 格式的文件中，而且 TileMap 的配置文件必须为 .tga 格式。
+ * 
+ * 关于格式的更多信息，请参阅这篇文章:
+ * http://www.cocos2d-iphone.org/archives/27
+ * 
+ * AtlasNode 的所有功能在 TileMapAtlas 里面都是可用的。
+ * 
+ * **注意**:
+ * 这个类是弃用的。只是为保持兼容性才保留了这个类。
+ * 不推荐继续使用这个类。请使用更新的 TMX 文件格式：TMXTiledMap
+ * @js NA
+ */
 class CC_DLL TileMapAtlas : public AtlasNode 
 {
 public:
-    /** creates a TileMap with a tile file (atlas) with a map file and the width and height of each tile in points.
-     The tile file will be loaded using the TextureMgr.
+    /** @~english creates a TileMap with a tile file (atlas) with a map file and the width and height of each tile in points.
+     * The tile file will be loaded using the TextureMgr.
+     * @~chinese 使用指定的图片文件，tga 配置文件，格子宽度和格子高度创建一个 TileMapAtlas 实例。
+     * 图片文件将被 TextureMgr 加载。
+     * @param tile @~english The texture file for the TileMap.
+     * @~chinese TileMap 使用的图片文件路径。
+     * @param mapFile @~english The .tga file of the TileMap.
+     * @~chinese TileMap tga 格式配置文件的路径。
+     * @param tileWidth @~english The width of a tile.
+     * @~chinese TileMap 一个格子的宽度。
+     * @param tileHeight @~english The height of a tile.
+     * @~chinese TileMap 一个格子的高度。
+     * @return @~english A TileMapAtlas object.
+     * @~chinese 返回 TileMapAtlas 的实例。
      */
     static TileMapAtlas * create(const std::string& tile, const std::string& mapFile, int tileWidth, int tileHeight);
     /**
@@ -69,36 +95,62 @@ public:
      */
     virtual ~TileMapAtlas();
     
-    /** initializes a TileMap with a tile file (atlas) with a map file and the width and height of each tile in points.
-    The file will be loaded using the TextureMgr.
-    */
+    /** @~english creates a TileMap with a tile file (atlas) with a map file and the width and height of each tile in points.
+     * The tile file will be loaded using the TextureMgr.
+     * @~chinese 使用指定的图片文件，tga 配置文件，格子宽度和格子高度初始化一个 TileMapAtlas 实例。
+     * 图片文件将被 TextureMgr 加载。
+     * @param tile @~english The texture file for the TileMap.
+     * @~chinese TileMap 使用的图片文件路径。
+     * @param mapFile @~english The .tga file of the TileMap.
+     * @~chinese TileMap tga 格式配置文件的路径。
+     * @param tileWidth @~english The width of a tile.
+     * @~chinese TileMap 一个格子的宽度。
+     * @param tileHeight @~english The height of a tile.
+     * @~chinese TileMap 一个格子的高度。
+     * @return @~english If the initialization success, return true; else return false.
+     * @~chinese 如果初始化成功，返回 true；否则返回 false。
+     */
     bool initWithTileFile(const std::string& tile, const std::string& mapFile, int tileWidth, int tileHeight);
-    /**
+    /**@~english
      * Returns a tile from position x,y.
-     *For the moment only channel R is used
+     * For the moment only channel R is used
+     * @~chinese 
+     * 返回 x,y 位置的格子。
+     * 目前只使用 R 通道
      */
     Color3B getTileAt(const Vec2& position) const;
-    /**
+    /**@~english
      * Returns a tile from position x,y.
-     *For the moment only channel R is used
+     * For the moment only channel R is used
+     * @~chinese 
+     * 返回 x,y 位置的格子。
+     * 目前只使用 R 通道
      */
     CC_DEPRECATED_ATTRIBUTE Color3B tileAt(const Vec2& position) const { return getTileAt(position); };
-    /** sets a tile at position x,y.
+    /** @~english sets a tile at position x,y.
     For the moment only channel R is used
+     * @~chinese 设置 x,y 位置的格子。
+     * 目前只使用 R 通道
     */
     void setTile(const Color3B& tile, const Vec2& position);
-    /** dealloc the map from memory */
+    /** @~english dealloc the map from memory  @~chinese 释放地图的内存。*/
     void releaseMap();
     
-    /**
+    /**@~english
      * Query TGA image info.
-     *@return The TGA image info.
+     * @~chinese 
+     * 获取 tga 的图像信息。
+     *@return @~english The TGA image info.
+     * @~chinese tga 的图像信息。
      */
     inline struct sImageTGA* getTGAInfo() const { return _TGAInfo; };
 
-    /**
+    /**@~english
      * Set the TGA image info for TileMapAtlas
-     *@param TGAInfo The TGA info in sImageTGA.
+     * @~chinese 
+     * 设置 TileMapAtlas 的 tga 图像信息
+     *@param TGAInfo @~english The TGA info in sImageTGA.
+     * @~chinese tga 图像信息。
      */
     inline void setTGAInfo(struct sImageTGA* TGAInfo) { _TGAInfo = TGAInfo; };
 
@@ -113,7 +165,7 @@ protected:
     ValueMap _posToAtlasIndex;
     //! numbers of tiles to render
     int _itemsToRender;
-    /** TileMap info */
+    /** @~english TileMap info  @~chinese TileMap信息*/
     struct sImageTGA* _TGAInfo;
 };
 
