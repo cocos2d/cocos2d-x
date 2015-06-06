c2ec361519c8413009a77e025550b9cd8a5e1de9
VisualSJ <itharbors@outlook.com>
2015-06-06 16:50:20 +0800

    Translation Chinese Documents

--- cocos/ui/UIImageView.h
+++ cocos/ui/UIImageView.h
@@ -37,7 +37,8 @@ NS_CC_BEGIN
 namespace ui {
     class Scale9Sprite;
 /**
- * @brief A widget to display images.
+ * @brief @~english A widget to display images.
+ * @~chinese 涓?涓皬閮ㄤ欢鏄剧ず鍥惧儚銆?
  */
 class CC_GUI_DLL ImageView : public Widget
 {
@@ -46,72 +47,119 @@ class CC_GUI_DLL ImageView : public Widget
     
 public:
     /**
+     * @~english
      * Default constructor
+     * @~chinese 
+     * 构造函数
+     *
      * @js ctor
      * @lua new
      */
     ImageView();
 
-    /**
+    /**@~english
      * Default destructor
+     * @~chinese 
+     * 析构函数
+     *
      * @js NA
      * @lua NA
      */
     virtual ~ImageView();
 
     /**
+     * @~english
      * Create a empty ImageView.
+     * @~chinese 
+     * 创建一个空的ImageView对象
      */
     static ImageView* create();
     
     /**
+     * @~english
      * Create a  imageview  with a image name.
      *
-     * @param imageFileName   file name of texture.
-     * @param texType    @see `Widget::TextureResType`
-     * @return A ImageView instance.
+     * @~chinese 
+     * 用一个文件名创建一个ImageView对象
+     * 
+     * @param imageFileName @~english file name of texture.
+     * @~chinese 纹理文件的名字
+     *
+     * @param texType @see `Widget::TextureResType`
+     *
+     * @return @~english A ImageView instance.
+     * @~chinese ImageView实例
      */
     static ImageView* create(const std::string& imageFileName, TextureResType texType = TextureResType::LOCAL);
     
 
     /**
+     * @~english
      * Load texture for imageview.
      *
-     * @param fileName   file name of texture.
-     * @param texType    @see `Widget::TextureResType`
+     * @~chinese 
+     * 加载纹理
+     * 
+     * @param fileName @~english file name of texture.
+     * @~chinese 纹理文件的名字
+     * @param texType @see `Widget::TextureResType`
      */
     void loadTexture(const std::string& fileName,TextureResType texType = TextureResType::LOCAL);
 
     /**
+     * @~english
      * Updates the texture rect of the ImageView in points.
      * It will call setTextureRect:rotated:untrimmedSize with rotated = NO, and utrimmedSize = rect.size.
+     * @~chinese
+     * 更新ImageView纹理矩形内的点数据
+     * 它会调用setTextureRect:rotated:untrimmedSize，rotated默认为No，utrimmedSize默认为矩形的尺寸
      */
     void setTextureRect(const Rect& rect);
 
     /**
+     * @~english
      * Enable scale9 renderer.
      *
-     * @param enabled Set to true will use scale9 renderer, false otherwise.
+     * @~chinese 
+     * 开启9宫格渲染模式
+     * 
+     * @param enabled @~english Set to true will use scale9 renderer, false otherwise.
+     * @~chinese 设置为true则开启9宫格渲染，为false则关闭
      */
     void setScale9Enabled(bool enabled);
 
     /**
+     * @~english
      * Query whether button is using scale9 renderer or not.
-     *@return whether button use scale9 renderer or not.
+     * @~chinese 
+     * 判断是否开启了9宫格渲染
+     * @return @~english whether button use scale9 renderer or not.
+     * @~chinese 返回true或false表示是否开启
      */
     bool isScale9Enabled()const;
 
     /**
+     * @~english
      * Sets capInsets for imageview.
      * The capInsets affects the ImageView's renderer only if `setScale9Enabled(true)` is called.
      *
-     * @param capInsets    capinsets for imageview
+     * @~chinese 
+     * 设置ImageView的capInsets
+     * 只有当`setScale9Enabled(true)`被调用之后，该capInsets才会影响的ImageView的渲染。
+     * 
+     * @param capInsets @~english capInsets for imageview
+     * @~chinese 这个ImageView的capInset
      */
     void setCapInsets(const Rect &capInsets);
 
     /**
+     * @~english
      * Get ImageView's capInsets size.
-     * @return Query capInsets size in Rect
+     * @~chinese 
+     * 获取一个ImageView的capInsets尺寸
+     *
+     * @return @~english Query capInsets size in Rect
+     * @~chinese capInsets尺寸
      * @see `setCapInsets(const Rect&)`
      */
     const Rect& getCapInsets()const;
