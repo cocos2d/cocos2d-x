a451fc7b0dc2360f58213bdb09bce64d9da1b44f
hanxi <me@hanxi.info>
2015-06-06 14:20:16 +0800

    add UIEditBox support linux platform.

--- build/install-deps-linux.sh
+++ build/install-deps-linux.sh
@@ -28,6 +28,7 @@ DEPENDS+=' libfontconfig1-dev'
 DEPENDS+=' libsqlite3-dev'
 DEPENDS+=' libglew-dev'
 DEPENDS+=' libssl-dev'
+DEPENDS+=' libgtk-3-dev'
 
 MISSING=
 echo "Checking for missing packages ..."
