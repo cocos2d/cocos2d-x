67a94c4485a197a3622c3a7f857be09e4ed389d0
Huabing.Xu <dabingnn@gmail.com>
2015-06-09 17:57:07 +0800

    compiles on android and linux

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -183,7 +183,7 @@ renderer/CCVertexAttribBinding.cpp \
 renderer/CCVertexIndexBuffer.cpp \
 renderer/CCVertexIndexData.cpp \
 renderer/ccGLStateCache.cpp \
-renderer/CCFrameBufferObject.cpp \
+renderer/CCFrameBuffer.cpp \
 renderer/ccShaders.cpp \
 deprecated/CCArray.cpp \
 deprecated/CCDeprecated.cpp \
