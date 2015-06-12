51df1068bfed462de0d0fe5475b93c9322a5b244
chengstory <zhangcheng@chukong-inc.com>
2015-06-12 11:47:02 +0800

    1, update comment

--- cocos/editor-support/cocostudio/CCComRender.h
+++ cocos/editor-support/cocostudio/CCComRender.h
@@ -31,93 +31,129 @@ THE SOFTWARE.
 
 namespace cocostudio {
 
- /** 
- @类名 ComRender
- @主题 渲染组件 包装挂载到对象的渲染对象,确保组件实现的一致性
- @细节 实现获取对象的渲染对象
- @组 组件
- */
+/** 
+ * @class ComRender
+ * @brief @~english ComRender.
+ * @~chinese 渲染组件.
+ * @details @~english Package render the target object of cocos2d-x, to ensure consistency component implementation.
+ * @~chinese 渲染组件 包装挂载到对象的渲染对象,确保组件实现的一致性.
+*/
 class CC_STUDIO_DLL ComRender : public cocos2d::Component
 {
     DECLARE_CLASS_COMPONENT_INFO
 CC_CONSTRUCTOR_ACCESS:
     /**
-     *  @js ctor
+     * @brief @~english ComRender constructor.
+     * @~chinese 渲染组件构造函数。
+     * @js ctor
      */
-    /**
-    @主题 渲染组件构造函数
-    */
     ComRender(void);
+
     /**
     @主题 渲染组件构造函数
     @参数 node 包装渲染对象指针
     @参数 comName 渲染组件名称
     */
-    ComRender(cocos2d::Node *node, const char *comName);
     /**
+     * @brief @~english ComRender constructor.
+     * @~chinese 渲染组件构造函数。
+     * @param node @~english Node point 
+     * @~chinese 渲染对象指针
+     * @param comName @~english component name 
+     * @~chinese 组件名称
+     * @js ctor
+     */
+    ComRender(cocos2d::Node *node, const char *comName);
+
+    /**
+     * @brief @~english ComRender destructor.
+     * @~chinese 渲染组件析构函数。
      * @js NA
      * @lua NA
      */
-    /**
-    @主题 渲染组件析构函数
-    */
     virtual ~ComRender(void);
     
 public:
     /**
+     * @brief @~english Event callback that is invoked every time when Node enters the 'stage'.
+     * If the Node enters the 'stage' with a transition, this event is called when the transition starts.
+     * During onEnter you can't access a "sister/brother" node.
+     * If you override onEnter, you shall call its parent's one, e.g., Node::onEnter().
+     * @~chinese 每次当Node进入“stage”时才调用事件回调。
+     * 如果Node进入“stage”状态时伴随着一个转换（transition）,那么事件将会在这个转换开始的时候被调用。
+     * 在onEnter过程中，你不能够接入“sister/brother”兄妹节点。
+     * 如果你重写了onEnter方法，你应该调用它的父类，e.g., Node::onEnter().
      * @js NA
      * @lua NA
      */
-    /**
-    @主题 node调用onEnter的时候，会调用组件的onEnter
-    @返回 无
-    */
     virtual void onEnter() override;
+
     /**
+     * @brief @~english Event callback that is invoked every time the Node leaves the 'stage'.
+     * If the Node leaves the 'stage' with a transition, this event is called when the transition finishes.
+     * During onExit you can't access a sibling node.
+     * If you override onExit, you shall call its parent's one, e.g., Node::onExit().
+     * @~chinese 每次当Node离开“stage”时才调用事件回调。
+     * 如果Node离开“stage”状态时伴随着一个转换（transition）, 那么事件将会在这个转换结束的时候被调用。
+     * 在onEnter过程中中你不能够接入一个兄妹节点。
+     * 如果你重写onExit, 你应该调用它的父类, e.g., Node::onExit().
      * @js NA
      * @lua NA
      */
-    /**
-    @主题 node调用onExit的时候，会调用组件的onExit
-    @返回 无
-    */
     virtual void onExit() override;
+
     /**
-    @主题 根据cocos studio 1.x 场景编辑器导出的数据反序列化
-    @参数 r SerData对象指针
-    @返回 true 反序列化成功，false 反序列化失败
-    */
+     * @brief @~english deserialization export data of cocos studio 1.x scene editor.
+     * @~chinese 反序列化cocos studio 1.x 场景编辑器导出的数据。
+     * @param r @~english SerData object point.
+     * @~chinese SerData对象指针。
+     * @return @~english true deserialize successfully false failed to deserialize.
+     * @~chinese true 反序列化成功，否则false。
+     */
     virtual bool serialize(void* r) override;
+
     /**
-    @主题 获取渲染组件包装的渲染对象的指针
-    @返回 渲染对象指针
-    */
+     * @brief @~english get Node point.
+     * @~chinese 获取渲染对象指针。
+     */
     virtual cocos2d::Node* getNode();
+
     /**
-    @主题 设置渲染组件包装的渲染对象指针
-    @参数 渲染对象指针
-    @返回 无
-    */
+     * @brief @~english set Node point.
+     * @~chinese 设置渲染组件包装的渲染对象指针。
+     */
     virtual void setNode(cocos2d::Node *node);
-   /**
-    @主题 创建渲染组件
-    @返回 渲染组件指针
-    */
+
+    /**
+     * @brief @~english Allocates and initializes a ComRender.
+     * @~chinese 分配并且初始化渲染组件。
+     * @return @~english A initialized ComRender which is marked as "autorelease".
+     * @~chinese 一个初始化的自定义组件，该节点会自动被标记为“autorelease”(自动释放)。
+     */
     static ComRender* create(void);
-   /**
-    @主题 根据渲染对象指针与组建名称创建渲染组件
-    @参数 node 渲染对象指针
-    @参数 comName 渲染组件名称
-    @返回 渲染组件指针
-    */
+
+    /**
+     * @brief @~english Allocates and initializes a ComRender.
+     * @~chinese 分配并且初始化渲染组件。
+     * @param node @~english Node point 
+     * @~chinese 渲染对象指针
+     * @param comName @~english component name 
+     * @~chinese 组件名称
+     * @return @~english A initialized ComRender which is marked as "autorelease".
+     * @~chinese 一个初始化的自定义组件，该节点会自动被标记为“autorelease”(自动释放)。
+     */
     static ComRender* create(cocos2d::Node *node, const char *comName);
 private:
-    /**
-    @主题 解析cocos studio 1.x 场景编辑器导出的json数据，给doc赋值
-    @参数 fileName cocos studio 1.x 场景编辑器导出的json路径
-    @参数 doc json对象
-    @返回 true 解析json成功，否则解析json失败
-    */
+     /**
+     * @brief @~english Parse export json data of cocos studio 1.x Scene Editor and assign to doc.
+     * @~chinese 解析cocos studio 1.x 场景编辑器导出json数据，判断json的有效性，给json对象doc赋值。
+     * @param fileName @~english json file path.
+     * @~chinese json路径。
+     * @param doc @~english json data object
+     * @~chinese json数据对象。
+     * @return @~english true parse successfully, false failed to parse.
+     * @~chinese true 解析成功，否则false。
+     */
     bool readJson(const std::string &fileName, rapidjson::Document &doc);
 
 private:
