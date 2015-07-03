698f70f5b649be0bd68cf88a2ec260a3101032e1
andyque <guanghui8827@126.com>
2015-07-03 15:38:53 +0800

    fix runtime template compilation

--- tools/jenkins-scripts/templates-daily-build.py
+++ tools/jenkins-scripts/templates-daily-build.py
@@ -80,7 +80,8 @@ def build_templates(mode, templates, is_runtime):
 
             if node_name == "windows-universal":
                 #run wp8.1 and windows 8.1
-                if is_runtime and type == "lua":
+                # lua & js don't have runtime template
+                if is_runtime:
                     continue
                 wp_ret = os.system("cocos compile -p wp8_1 " + build_mode_args + " -s " + test_project_prefix + type + " --compile-script 0")
                 winrt_ret = os.system("cocos compile -p metro " + build_mode_args + " -s " + test_project_prefix + type + " --compile-script 0")
@@ -89,6 +90,8 @@ def build_templates(mode, templates, is_runtime):
             else:
                 if node_name == "android":
                     compile_project_commands += " --app-abi armeabi:armeabi-v7a:x86 "
+                if node_name == "linux" and is_runtime:
+                    continue
                 compile_ret = os.system(compile_project_commands + build_mode_args)
                 ret += compile_ret
 
