c4dc9b04fdc4d6a5a1a60996d7a069b5477ca796
Neo Kim <neo.kim@neofect.com>
2015-07-06 12:56:49 +0900

    Add a guard filtering tries to set same value.

--- cocos/ui/UIScrollView.cpp
+++ cocos/ui/UIScrollView.cpp
@@ -1034,6 +1034,11 @@ bool ScrollView::isInertiaScrollEnabled() const
 
 void ScrollView::setScrollBarEnabled(bool enabled)
 {
+	if(_scrollBarEnabled == enabled)
+	{
+		return;
+	}
+	
     if(_scrollBarEnabled)
     {
         removeScrollBar();
