1c6a238012042049e12aeee3f05e697b9e85ebf8
Vincent Yang <wensheng.yang@cocos2d-x.org>
2015-07-16 16:48:23 +0800

    Fixed #11073 : mutiTouch trigger touches always only one size.

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -33,6 +33,10 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.inputmethod.InputMethodManager;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+@SuppressWarnings("unused")
 public class Cocos2dxGLSurfaceView extends GLSurfaceView {
     // ===========================================================
     // Constants
@@ -55,6 +59,65 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
 
     private Cocos2dxRenderer mCocos2dxRenderer;
     private Cocos2dxEditText mCocos2dxEditText;
+
+    private class TouchInfo {
+        public int id;
+        public float x;
+        public float y;
+    }
+    private class TouchEvent {
+        public int action = Integer.MAX_VALUE;
+        public long timestamp = 0;
+
+        private ArrayList<TouchInfo> mInfo = new ArrayList<>();
+        private int mTop;
+
+        // this function should called in main thread
+        public int addTouchInfo(int id, float x, float y) {
+            if (mTop == mInfo.size()) {
+                mInfo.add(new TouchInfo());
+            }
+            // get top
+            TouchInfo info = mInfo.get(mTop);
+            mTop = mTop + 1;
+            info.id = id;
+            info.x = x;
+            info.y = y;
+
+            return mTop;
+        }
+
+        // this function should called in GL thread
+        private void sendTouchEvent() {
+            final int num = mTop;
+            // Log.d("cocos2d-x", "sendTouchEvent[ action: " + action + ", num: " + num + ", delay: " + (System.currentTimeMillis() - timestamp) + "]");
+            if (0 >= num && action != Integer.MAX_VALUE) {
+                Log.w("cocos2d-x", "Cocos2dxGLSurfaceView.TouchEvent.sendTouchEvent called but no info in it.");
+                return;
+            }
+            // convert touch info to native format
+            final int[] ids = new int[num];
+            final float[] xs = new float[num];
+            final float[] ys = new float[num];
+            for (int i = 0; i < num; i++) {
+                TouchInfo info = mInfo.get(i);
+                ids[i] = info.id;
+                xs[i] = info.x;
+                ys[i] = info.y;
+            }
+            // send event
+            // MotionEvent.ACTION_POINTER_DOWN has been covert to ACTION_DOWN in onTouchEvent
+            if (action == MotionEvent.ACTION_POINTER_UP) {
+                action = MotionEvent.ACTION_UP;
+            }
+            Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleTouchEvent(action, ids, xs, ys);
+            // clear touch info
+            mTop = 0;
+            action = Integer.MAX_VALUE;
+        }
+    }
+    private final LinkedList<TouchEvent> mProductQueue = new LinkedList<>();
+    private final LinkedList<TouchEvent> mConsumerQueue = new LinkedList<>();
 
     // ===========================================================
     // Constructors
@@ -179,95 +242,111 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
         //super.onPause();
     }
 
+    private void postTouchEventProcessRunable() {
+        this.queueEvent(new Runnable() {
+            @Override
+            public void run() {
+                TouchEvent event;
+                synchronized (mConsumerQueue) {
+                    event = mConsumerQueue.pollFirst();
+                    if (event == null) {
+                        Log.w("cocos2d-x", "Cocos2dxGLSurfaceView.onTouchEvent post an event GL thread, but missed.");
+                    } else if (0 == mConsumerQueue.size() &&    // no more event
+                            (event.action == MotionEvent.ACTION_DOWN || // action down/pointer up event need buffer
+                                    event.action == MotionEvent.ACTION_POINTER_UP)) {
+                        // if the time interval less than designed delay time, push event back
+                        if (16 > (System.currentTimeMillis() - event.timestamp)) {
+                            mConsumerQueue.offerFirst(event);
+                            event = null;
+                            mCocos2dxGLSurfaceView.postTouchEventProcessRunable();
+                        }
+                    }
+                }
+                if (null == event) {
+                    return;
+                }
+                event.sendTouchEvent();
+                synchronized (mProductQueue) {
+                    mProductQueue.offerLast(event);
+                }
+            }
+        });
+    }
+
     @Override
-    public boolean onTouchEvent(final MotionEvent pMotionEvent) {
-        // these data are used in ACTION_MOVE and ACTION_CANCEL
-        final int pointerNumber = pMotionEvent.getPointerCount();
-        final int[] ids = new int[pointerNumber];
-        final float[] xs = new float[pointerNumber];
-        final float[] ys = new float[pointerNumber];
+    public boolean onTouchEvent(@SuppressWarnings("NullableProblems") final MotionEvent pMotionEvent) {
+        int action = pMotionEvent.getAction() & MotionEvent.ACTION_MASK;
+        // Log.d("cocos2d-x", "Cocos2dxGLSurfaceView.onTouchEvent: " + action);
 
-        for (int i = 0; i < pointerNumber; i++) {
-            ids[i] = pMotionEvent.getPointerId(i);
-            xs[i] = pMotionEvent.getX(i);
-            ys[i] = pMotionEvent.getY(i);
+        // treat ACTION_POINTER_DOWN as ACTION_DOWN
+        if (action == MotionEvent.ACTION_POINTER_DOWN) {
+            action = MotionEvent.ACTION_DOWN;
         }
 
-        switch (pMotionEvent.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_POINTER_DOWN:
-                final int indexPointerDown = pMotionEvent.getAction() >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-                final int idPointerDown = pMotionEvent.getPointerId(indexPointerDown);
-                final float xPointerDown = pMotionEvent.getX(indexPointerDown);
-                final float yPointerDown = pMotionEvent.getY(indexPointerDown);
-
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionDown(idPointerDown, xPointerDown, yPointerDown);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_DOWN:
-                // there are only one finger on the screen
-                final int idDown = pMotionEvent.getPointerId(0);
-                final float xDown = xs[0];
-                final float yDown = ys[0];
-
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionDown(idDown, xDown, yDown);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionMove(ids, xs, ys);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_POINTER_UP:
-                final int indexPointUp = pMotionEvent.getAction() >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-                final int idPointerUp = pMotionEvent.getPointerId(indexPointUp);
-                final float xPointerUp = pMotionEvent.getX(indexPointUp);
-                final float yPointerUp = pMotionEvent.getY(indexPointUp);
-
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionUp(idPointerUp, xPointerUp, yPointerUp);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_UP:
-                // there are only one finger on the screen
-                final int idUp = pMotionEvent.getPointerId(0);
-                final float xUp = xs[0];
-                final float yUp = ys[0];
-
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionUp(idUp, xUp, yUp);
-                    }
-                });
-                break;
-
-            case MotionEvent.ACTION_CANCEL:
-                this.queueEvent(new Runnable() {
-                    @Override
-                    public void run() {
-                        Cocos2dxGLSurfaceView.this.mCocos2dxRenderer.handleActionCancel(ids, xs, ys);
-                    }
-                });
-                break;
+        boolean isActionUp = false;
+        // treat ACTION_UP as ACTION_POINTER_UP
+        if (action == MotionEvent.ACTION_UP) {
+            action = MotionEvent.ACTION_POINTER_UP;
+            isActionUp = true;
         }
+
+        TouchEvent event = null;
+        // if event is down/pointer up/up, try to merge it to last event
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_POINTER_UP) {
+            // try get last event in consumer queue
+            synchronized (mConsumerQueue) {
+                event = mConsumerQueue.peekLast();
+                if (null != event && action == event.action) {
+                    // the last event same as current event, merge them
+                    int i = pMotionEvent.getAction() >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                    event.addTouchInfo(pMotionEvent.getPointerId(i), pMotionEvent.getX(i), pMotionEvent.getY(i));
+                    if (isActionUp) {
+                        event.action = MotionEvent.ACTION_UP;
+                    }
+                } else {
+                    event = null;
+                }
+            }
+        }
+
+        // if the current event has been merged in to last one, return immediately
+        if (null != event){
+            return true;
+        }
+
+        // get empty TouchEvent from product queue
+        synchronized (mProductQueue) {
+            event = mProductQueue.pollFirst();
+        }
+        // if product queue empty, create a new instance
+        if (null == event) {
+            event = new TouchEvent();
+        }
+
+        // fill touch event with data
+        event.action = action;
+        event.timestamp = System.currentTimeMillis();
+        if (action == MotionEvent.ACTION_MOVE || action == MotionEvent.ACTION_CANCEL) {
+            int pointerNumber = pMotionEvent.getPointerCount();
+            for (int i = 0; i < pointerNumber; i++) {
+                event.addTouchInfo(pMotionEvent.getPointerId(i), pMotionEvent.getX(i), pMotionEvent.getY(i));
+            }
+        } else {
+            // ACTION_DOWN, ACTION_POINTER_UP, ACTION_UP
+            int i = pMotionEvent.getAction() >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+            event.addTouchInfo(pMotionEvent.getPointerId(i), pMotionEvent.getX(i), pMotionEvent.getY(i));
+            if (isActionUp) {
+                event.action = MotionEvent.ACTION_UP;
+            }
+        }
+
+        // add event to consumer queue
+        synchronized (mConsumerQueue) {
+            mConsumerQueue.offerLast(event);
+        }
+
+        // post event
+        postTouchEventProcessRunable();
 
         /*
         if (BuildConfig.DEBUG) {
@@ -289,7 +368,7 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
     }
 
     @Override
-    public boolean onKeyDown(final int pKeyCode, final KeyEvent pKeyEvent) {
+    public boolean onKeyDown(final int pKeyCode, @SuppressWarnings("NullableProblems") final KeyEvent pKeyEvent) {
         switch (pKeyCode) {
             case KeyEvent.KEYCODE_BACK:
                 Cocos2dxVideoHelper.mVideoHandler.sendEmptyMessage(Cocos2dxVideoHelper.KeyEventBack);
