cf4b4a1033b0aacd660e6d35f8230f91a860cd9b
giginet <giginet.net@gmail.com>
2015-07-26 02:33:42 +0900

    Modify cpp template to use CREATE_SCENE_FUNC

--- templates/cpp-template-default/Classes/HelloWorldScene.cpp
+++ templates/cpp-template-default/Classes/HelloWorldScene.cpp
@@ -2,21 +2,6 @@
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
-{
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
-
-    // add layer as a child to scene
-    scene->addChild(layer);
-
-    // return the scene
-    return scene;
-}
-
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
