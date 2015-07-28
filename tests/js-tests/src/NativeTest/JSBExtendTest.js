cc63ff5c03d5ef001bc963246526e8e521e04996
pandamicro <lphuabin@gmail.com>
2015-07-28 18:37:57 +0800

    Fix indent

--- tests/js-tests/src/NativeTest/JSBExtendTest.js
+++ tests/js-tests/src/NativeTest/JSBExtendTest.js
@@ -3,66 +3,66 @@
 var message = "";
 
 var appendMessage = function (tail) {
-	message += "\n" + tail;
-	cc.log(tail);
+    message += "\n" + tail;
+    cc.log(tail);
 };
 
 var ScrollViewExtended = ccui.ScrollView.extend({
-	onEnter: function (){
-		this._super();
-		appendMessage("3. ScrollViewExtended: onEnter");
-	},
+    onEnter: function (){
+        this._super();
+        appendMessage("3. ScrollViewExtended: onEnter");
+    },
 
-	onExit: function () {
-		appendMessage("E-3. ScrollViewExtended: onExit");
-		this._super();
-	}
+    onExit: function () {
+        appendMessage("E-3. ScrollViewExtended: onExit");
+        this._super();
+    }
 });
 
 var ButtonExtended = ccui.Button.extend({
-	onEnter: function (){
-		this._super();
-		appendMessage("4. ButtonExtended: onEnter");
-	},
+    onEnter: function (){
+        this._super();
+        appendMessage("4. ButtonExtended: onEnter");
+    },
 
-	onExit: function () {
-		appendMessage("E-4. ButtonExtended: onExit");
-		this._super();
-	}
+    onExit: function () {
+        appendMessage("E-4. ButtonExtended: onExit");
+        this._super();
+    }
 });
 
 var ImageViewExtended = ccui.ImageView.extend({
-	onEnter: function (){
-		this._super();
-		appendMessage("5. ImageViewExtended: onEnter");
-	},
+    onEnter: function (){
+        this._super();
+        appendMessage("5. ImageViewExtended: onEnter");
+    },
 
-	onExit: function () {
-		appendMessage("E-5. ImageViewExtended: onExit");
-		this._super();
-	}
+    onExit: function () {
+        appendMessage("E-5. ImageViewExtended: onExit");
+        this._super();
+    }
 });
 
 var LayerExtended = cc.Layer.extend({
-	onEnter: function (){
-		this._super();
-		appendMessage("2. LayerExtended: onEnter");
+    onEnter: function (){
+        this._super();
+        appendMessage("2. LayerExtended: onEnter");
 
-		//add main node
+        //add main node
         var scale = cc.winSize.height / 320;
         this.attr({
-        	anchorX: 0, 
-        	anchorY: 0, 
-        	scale: scale, 
-        	x: (cc.winSize.width - 480 * scale) / 2, 
-        	y: (cc.winSize.height - 320 * scale) / 2
+            anchorX: 0, 
+            anchorY: 0, 
+            scale: scale, 
+            x: (cc.winSize.width - 480 * scale) / 2, 
+            y: (cc.winSize.height - 320 * scale) / 2
         });
 
         var json = ccs.load("ccs-res/cocosui/UITest/UITest.json");
         var widget = json.node;
         this.addChild(widget, -1);
 
-		var widgetSize = widget.getContentSize();
+        var widgetSize = widget.getContentSize();
 
         var background = widget.getChildByName("background_Panel");
         background.y += 50;
@@ -106,45 +106,45 @@ var LayerExtended = cc.Layer.extend({
         scrollView.addChild(imageView);
 
         widget.getChildByName("UItest").string = "Please click 'Remove UI' button";
-		widget.removeChild(widget.getChildByName("back"));
-		widget.removeChild(widget.getChildByName("left_Button"));
-		widget.removeChild(widget.getChildByName("middle_Button"));
-		widget.removeChild(widget.getChildByName("right_Button"));
-	},
+        widget.removeChild(widget.getChildByName("back"));
+        widget.removeChild(widget.getChildByName("left_Button"));
+        widget.removeChild(widget.getChildByName("middle_Button"));
+        widget.removeChild(widget.getChildByName("right_Button"));
+    },
 
-	onExitTransitionDidStart: function () {
-		this._super();
-		appendMessage("E-1. LayerExtended: onExitTransitionDidStart");
-	},
+    onExitTransitionDidStart: function () {
+        this._super();
+        appendMessage("E-1. LayerExtended: onExitTransitionDidStart");
+    },
 
-	onExit: function () {
-		appendMessage("E-2. LayerExtended: onExit");
-		this._super();
-	},
-	exit: function () {
-		this.parent.removeUI();
-	}
+    onExit: function () {
+        appendMessage("E-2. LayerExtended: onExit");
+        this._super();
+    },
+    exit: function () {
+        this.parent.removeUI();
+    }
 });
 
 window.JSBExtendTest = cc.Scene.extend({
-	label: null,
-	ui: null,
-	onEnter: function () {
-		message = "";
-		this._super();
+    label: null,
+    ui: null,
+    onEnter: function () {
+        message = "";
+        this._super();
 
-		appendMessage("1. SceneExtended: onEnter");
+        appendMessage("1. SceneExtended: onEnter");
 
-		this.ui = new LayerExtended();
-		this.addChild(this.ui);
+        this.ui = new LayerExtended();
+        this.addChild(this.ui);
 
-		var scale = cc.winSize.height / 320;
-		this.label = new cc.LabelTTF(message);
-		this.label.x = cc.winSize.width/2;
-		this.label.y = (cc.winSize.height - 320 * scale / 2) / 2;
-		this.addChild(this.label);
+        var scale = cc.winSize.height / 320;
+        this.label = new cc.LabelTTF(message);
+        this.label.x = cc.winSize.width/2;
+        this.label.y = (cc.winSize.height - 320 * scale / 2) / 2;
+        this.addChild(this.label);
 
-		var back = new cc.LabelTTF("Back", "Arial", 20);
+        var back = new cc.LabelTTF("Back", "Arial", 20);
         var menuItem = new cc.MenuItemLabel(back, this.toNativeTest, this);
         var menu = new cc.Menu(menuItem);
         menu.x = 0;
@@ -152,16 +152,16 @@ window.JSBExtendTest = cc.Scene.extend({
         menuItem.x = winSize.width - 50;
         menuItem.y = 25;
         this.addChild(menu, 1);
-	},
-	removeUI: function () {
-		this.removeChild(this.ui, true);
-		this.label.y = cc.winSize.height/2;
-		this.label.string = message;
-	},
-	toNativeTest: function () {
-		var scene = new NativeTestScene();
-		scene.runThisTest();
-	}
+    },
+    removeUI: function () {
+        this.removeChild(this.ui, true);
+        this.label.y = cc.winSize.height/2;
+        this.label.string = message;
+    },
+    toNativeTest: function () {
+        var scene = new NativeTestScene();
+        scene.runThisTest();
+    }
 });
 
 })(window);
