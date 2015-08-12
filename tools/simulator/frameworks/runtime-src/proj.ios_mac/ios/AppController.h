9a83ed9b2800f798023f25701c44c674505a4384
andyque <guanghui8827@126.com>
2015-08-12 13:55:36 +0800

    remove unneeded protocol in AppController.h.

--- tools/simulator/frameworks/runtime-src/proj.ios_mac/ios/AppController.h
+++ tools/simulator/frameworks/runtime-src/proj.ios_mac/ios/AppController.h
@@ -25,7 +25,7 @@
 
 @class RootViewController;
 
-@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate>
+@interface AppController : NSObject <UIApplicationDelegate>
 {
     UIWindow *window;
     RootViewController    *viewController;
