a0c270234bbfbe65ec265aa96446b39fdca4bbcf
WenhaiLin <wenhai.lin@chukong-inc.com>
2015-08-14 21:17:51 +0800

    Replace tab with spaces

--- cocos/platform/win32/CCApplication-win32.cpp
+++ cocos/platform/win32/CCApplication-win32.cpp
@@ -204,12 +204,12 @@ LanguageType Application::getCurrentLanguage()
 
 const char * Application::getCurrentLanguageCode()
 {
-	LANGID lid = GetUserDefaultUILanguage();
-	const LCID locale_id = MAKELCID(lid, SORT_DEFAULT);
-	static char code[3] = { 0 };
-	GetLocaleInfoA(locale_id, LOCALE_SISO639LANGNAME, code, sizeof(code));
-	code[2] = '\0';
-	return code;
+    LANGID lid = GetUserDefaultUILanguage();
+    const LCID locale_id = MAKELCID(lid, SORT_DEFAULT);
+    static char code[3] = { 0 };
+    GetLocaleInfoA(locale_id, LOCALE_SISO639LANGNAME, code, sizeof(code));
+    code[2] = '\0';
+    return code;
 }
 
 Application::Platform Application::getTargetPlatform()
