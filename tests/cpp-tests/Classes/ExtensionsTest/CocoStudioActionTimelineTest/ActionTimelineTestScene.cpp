a42353264d362acaefe3340a520717e8ea3c6790
geron-cn <c.ever.fallen@gmail.com>
2015-09-20 16:24:41 +0800

    rename setAnimationEndCallFunc

--- tests/cpp-tests/Classes/ExtensionsTest/CocoStudioActionTimelineTest/ActionTimelineTestScene.cpp
+++ tests/cpp-tests/Classes/ExtensionsTest/CocoStudioActionTimelineTest/ActionTimelineTestScene.cpp
@@ -644,7 +644,7 @@ void TestAnimationClipEndCallBack::onEnter()
      });
      
      // test for animation clip end call back
-     action->setAnimationEndCallBackFunc("stand", [this]{
+     action->setAnimationEndCallFunc("stand", [this]{
          auto text = ui::Text::create();
          text->setString("CallBackAfterStandAnimationClip");
          text->setPosition(Vec2(100, 130));
