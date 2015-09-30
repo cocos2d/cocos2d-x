fe8c1a12b558789f0a86f93e707eac897220802f
dimon4eg <kdl.dima@gmailcom>
2015-09-30 12:39:44 +0300

    Fix for text field

--- cocos/ui/UITextField.cpp
+++ cocos/ui/UITextField.cpp
@@ -536,7 +536,11 @@ bool TextField::onTouchBegan(Touch *touch, Event *unusedEvent)
     bool pass = Widget::onTouchBegan(touch, unusedEvent);
     if (_hitted)
     {
-        requestFocus();
+        if (isFocusEnabled())
+        {
+            requestFocus();
+        }
+
         _textFieldRenderer->attachWithIME();
     }
     else
