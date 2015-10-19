83a48ee50128bdc95eba092fb02a55cdd6458afd
XiaoFeng <xiaofeng@chukong-inc.com>
2015-10-19 19:43:10 +0800

    Fix simulator run time only check screen direction flag, but won't check if size setting need swap

--- tools/simulator/libsimulator/lib/runtime/Runtime.cpp
+++ tools/simulator/libsimulator/lib/runtime/Runtime.cpp
@@ -96,7 +96,13 @@ void resetDesignResolution()
     cocos2d::Size size = ConfigParser::getInstance()->getInitViewSize();
     if (!ConfigParser::getInstance()->isLanscape())
     {
-        std::swap(size.width, size.height);
+        if (size.width > size.height)
+            std::swap(size.width, size.height);
+    }
+    else
+    {
+        if (size.width < size.height)
+            std::swap(size.width, size.height);
     }
     Director::getInstance()->getOpenGLView()->setDesignResolutionSize(size.width, size.height, ResolutionPolicy::EXACT_FIT);
 }
