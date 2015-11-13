9ec1e2069a0cbe9794c32c5a1b78aaba7f8ee8b0
mogemimi <mogemimi@enginetrouble.net>
2015-11-13 14:38:48 +0900

    Fix deprecated warnings

--- cocos/editor-support/cocostudio/CCArmatureDataManager.cpp
+++ cocos/editor-support/cocostudio/CCArmatureDataManager.cpp
@@ -250,7 +250,7 @@ const cocos2d::Map<std::string, TextureData*>& ArmatureDataManager::getTextureDa
     return _textureDatas;
 }
 
-void CCArmatureDataManager::addRelativeData(const std::string& configFilePath)
+void ArmatureDataManager::addRelativeData(const std::string& configFilePath)
 {
     if (_relativeDatas.find(configFilePath) == _relativeDatas.end())
     {
@@ -258,7 +258,7 @@ void CCArmatureDataManager::addRelativeData(const std::string& configFilePath)
     }
 }
 
-RelativeData *CCArmatureDataManager::getRelativeData(const std::string&  configFilePath)
+RelativeData *ArmatureDataManager::getRelativeData(const std::string& configFilePath)
 {
     return &_relativeDatas[configFilePath];
 }
