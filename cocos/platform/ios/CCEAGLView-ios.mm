63ae52c52ca8b46091aff0fff1f76ceabe0d3ba3
Jason.z <ccnuzxg@gmail.com>
2015-11-14 21:08:42 +0800

    ios9 cceditbox keyboard
    fix ios9 cceditbox keyboard bug

--- cocos/platform/ios/CCEAGLView-ios.mm
+++ cocos/platform/ios/CCEAGLView-ios.mm
@@ -718,6 +718,15 @@ Copyright (C) 2008 Apple Inc. All Rights Reserved.
 
 - (void)onUIKeyboardNotification:(NSNotification *)notif;
 {
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
+    {
+        if(originalRect_.size.height > originalRect_.size.width) {
+            CGFloat tempValue = originalRect_.size.height;
+            originalRect_.size.height = originalRect_.size.width;
+            originalRect_.size.width = tempValue;
+        }
+    }
+
     NSString * type = notif.name;
     
     NSDictionary* info = [notif userInfo];
