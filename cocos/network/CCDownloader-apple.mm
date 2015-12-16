ffcf45ef788b91ec17b746b8c65640640ab635ed
pandamicro <lphuabin@gmail.com>
2015-12-16 17:41:31 +0800

    Improved solution for Downloader status code issue on iOS 9

--- cocos/network/CCDownloader-apple.mm
+++ cocos/network/CCDownloader-apple.mm
@@ -389,7 +389,8 @@ namespace cocos2d { namespace network {
     
     if(_outer)
     {
-        if(error) {
+        if(error)
+        {
             std::vector<unsigned char> buf; // just a placeholder
             _outer->onTaskFinish(*[wrapper get],
                              cocos2d::network::DownloadTask::ERROR_IMPL_INTERNAL,
@@ -397,23 +398,46 @@ namespace cocos2d { namespace network {
                              [error.localizedDescription cStringUsingEncoding:NSUTF8StringEncoding],
                              buf);
         }
-        else if(![wrapper get]->storagePath.length()) {
+        else if (![wrapper get]->storagePath.length())
+        {
             // call onTaskFinish for a data task
             // (for a file download task, callback is called in didFinishDownloadingToURL)
-            std::string errorString = cocos2d::StringUtils::format("Downloader: Failed to download %s with no error", [task.originalRequest.URL.absoluteString cStringUsingEncoding:NSUTF8StringEncoding]);
-            
+            std::string errorString;
+
             const int64_t buflen = [wrapper totalBytesReceived];
             char buf[buflen];
-            
             [wrapper transferDataToBuffer:buf lengthOfBuffer:buflen];
-            
             std::vector<unsigned char> data(buf, buf + buflen);
             
             _outer->onTaskFinish(*[wrapper get],
                                  cocos2d::network::DownloadTask::ERROR_NO_ERROR,
-                                  0,
+                                 0,
                                  errorString,
                                  data);
+        }
+        else
+        {
+            NSInteger statusCode = ((NSHTTPURLResponse*)task.response).statusCode;
+            std::vector<unsigned char> buf; // just a placeholder
+            const char *orignalURL = [task.originalRequest.URL.absoluteString cStringUsingEncoding:NSUTF8StringEncoding];
+            std::string errorMessage = "";
+            
+            // Check for error status code
+            if (statusCode >= 400)
+            {
+                errorMessage = cocos2d::StringUtils::format("Downloader: Failed to download %s with status code (%d)", orignalURL, (int)statusCode);
+            }
+            // Unknown issue
+            else
+            {
+                errorMessage = cocos2d::StringUtils::format("Downloader: Failed to download %s with unknown error", orignalURL);
+            }
+            
+            _outer->onTaskFinish(*[wrapper get],
+                                 cocos2d::network::DownloadTask::ERROR_IMPL_INTERNAL,
+                                 0,
+                                 errorMessage,
+                                 buf);
         }
     }
     [self.taskDict removeObjectForKey:task];
@@ -511,22 +535,20 @@ namespace cocos2d { namespace network {
         return;
     }
     
+    // On iOS 9 a response with status code 4xx(Client Error) or 5xx(Server Error)
+    // might end up calling this delegate method, saving the error message to the storage path
+    // and treating this download task as a successful one, so we need to check the status code here
+    NSInteger statusCode = ((NSHTTPURLResponse*)downloadTask.response).statusCode;
+    if (statusCode >= 400)
+    {
+        return;
+    }
+    
     DownloadTaskWrapper *wrapper = [self.taskDict objectForKey:downloadTask];
     const char * storagePath = [wrapper get]->storagePath.c_str();
     NSString *destPath = [NSString stringWithUTF8String:storagePath];
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSURL *destURL = nil;
-
-    // On iOS 9 a response with status code 4xx(Client Error) or 5xx(Server Error)
-    // might end up calling this delegate method, saving the error message to the storage path
-    // and treating this download task as a successful one, so we need to check the status code here
-    NSInteger statusCode = ((NSHTTPURLResponse*)downloadTask.response).statusCode;
-    if (statusCode >= 400) {
-        std::vector<unsigned char> buf; // just a placeholder
-        std::string response = cocos2d::StringUtils::format("Downloader: Failed to download %s with status code (%d)", [downloadTask.originalRequest.URL.absoluteString cStringUsingEncoding:NSUTF8StringEncoding], (int)statusCode);
-        _outer->onTaskFinish(*[wrapper get], cocos2d::network::DownloadTask::ERROR_IMPL_INTERNAL, noErr, response, buf);
-        return;
-    }
 
     do
     {
