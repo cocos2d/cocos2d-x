a8f4c73372e786602d33a07559e025bf990982f2
Sergey <non_2001@mail.ru>
2015-12-17 11:57:43 +0300

    getCachePath() implementation for most platforms

--- cocos/platform/win32/CCFileUtils-win32.h
+++ cocos/platform/win32/CCFileUtils-win32.h
@@ -50,6 +50,7 @@ public:
     /* override functions */
     bool init();
     virtual std::string getWritablePath() const override;
+    virtual std::string getCachePath() const override;
     virtual bool isAbsolutePath(const std::string& strPath) const override;
     virtual std::string getSuitableFOpen(const std::string& filenameUtf8) const override;
     virtual long getFileSize(const std::string &filepath);
