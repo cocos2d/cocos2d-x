bd6ab6cacec9aa7e6a2bb006e2a16f22ab79c8ff
zhangcheng <zhangcheng@chukong-inc.com>
2015-12-23 18:23:23 +0800

    ListView测试例。

--- tests/cpp-empty-test/Classes/AppDelegate.cpp
+++ tests/cpp-empty-test/Classes/AppDelegate.cpp
@@ -1,13 +1,7 @@
 #include "AppDelegate.h"
-
-#include <vector>
-#include <string>
-
 #include "HelloWorldScene.h"
-#include "AppMacros.h"
 
 USING_NS_CC;
-using namespace std;
 
 AppDelegate::AppDelegate() {
 
@@ -17,8 +11,12 @@ AppDelegate::~AppDelegate()
 {
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
 void AppDelegate::initGLContextAttrs()
 {
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
 
     GLView::setGLContextAttrs(glContextAttrs);
@@ -29,57 +27,22 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLViewImpl::create("Cpp Empty Test");
+        glview = GLViewImpl::createWithRect("NewCocosProject20", Rect(0, 0, 960, 640));
         director->setOpenGLView(glview);
     }
 
-    director->setOpenGLView(glview);
+    director->getOpenGLView()->setDesignResolutionSize(960, 640, ResolutionPolicy::SHOW_ALL);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-
-	Size frameSize = glview->getFrameSize();
-    
-    vector<string> searchPath;
-
-    // In this demo, we select resource according to the frame's height.
-    // If the resource size is different from design resolution size, you need to set contentScaleFactor.
-    // We use the ratio of resource's height to the height of design resolution,
-    // this can make sure that the resource's height could fit for the height of design resolution.
-
-    // if the frame's height is larger than the height of medium resource size, select large resource.
-	if (frameSize.height > mediumResource.size.height)
-	{
-        searchPath.push_back(largeResource.directory);
-
-        director->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
-	}
-    // if the frame's height is larger than the height of small resource size, select medium resource.
-    else if (frameSize.height > smallResource.size.height)
-    {
-        searchPath.push_back(mediumResource.directory);
-        
-        director->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium resource size, select small resource.
-	else
-    {
-        searchPath.push_back(smallResource.directory);
-
-        director->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
-    }
-    
-    // set searching path
-    FileUtils::getInstance()->setSearchPaths(searchPath);
-	
     // turn on display FPS
     director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
 
+    FileUtils::getInstance()->addSearchPath("res");
+
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::scene();
+    auto scene = HelloWorld::createScene();
 
     // run
     director->runWithScene(scene);
@@ -92,7 +55,7 @@ void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 
     // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
@@ -100,5 +63,5 @@ void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
 
     // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
--- tests/cpp-empty-test/Classes/HelloWorldScene.h
+++ tests/cpp-empty-test/Classes/HelloWorldScene.h
@@ -6,12 +6,11 @@
 class HelloWorld : public cocos2d::Layer
 {
 public:
-    virtual bool init() override;
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
 
-    static cocos2d::Scene* scene();
-
-    // a selector callback
-    void menuCloseCallback(Ref* sender);
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
