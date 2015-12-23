2a2f1844492e7fa8d5a3bf735b0cbde2594c7ee1
zilongshanren <guanghui8827@126.com>
2015-12-23 14:43:25 +0800

    refine code with DRY.

--- cocos/ui/UILoadingBar.cpp
+++ cocos/ui/UILoadingBar.cpp
@@ -111,29 +111,13 @@ void LoadingBar::setDirection(cocos2d::ui::LoadingBar::Direction direction)
         case Direction::LEFT:
             _barRenderer->setAnchorPoint(Vec2(0.0f,0.5f));
             _barRenderer->setPosition(Vec2(0,_contentSize.height*0.5f));
-            if (!_scale9Enabled)
-            {
-                auto innerSprite = _barRenderer->getSprite();
-                if (nullptr != innerSprite)
-                {
-                    innerSprite->setFlippedX(false);
-                }
-            }
             break;
         case Direction::RIGHT:
             _barRenderer->setAnchorPoint(Vec2(1.0f,0.5f));
             _barRenderer->setPosition(Vec2(_totalLength,_contentSize.height*0.5f));
-            if (!_scale9Enabled)
-            {
-                auto innerSprite = _barRenderer->getSprite();
-                if (nullptr != innerSprite)
-                {
-                    innerSprite->setFlippedX(true);
-                }
-            }
             break;
     }
-
+    this->handleSpriteFlipX();
 }
 
 LoadingBar::Direction LoadingBar::getDirection()const
@@ -183,29 +167,13 @@ void LoadingBar::setupTexture()
     {
         case Direction::LEFT:
             _barRenderer->setAnchorPoint(Vec2(0.0f,0.5f));
-            if (!_scale9Enabled)
-            {
-                auto innerSprite = _barRenderer->getSprite();
-                if (nullptr != innerSprite)
-                {
-                    innerSprite->setFlippedX(false);
-                }
-            }
-            break;
             break;
         case Direction::RIGHT:
             _barRenderer->setAnchorPoint(Vec2(1.0f,0.5f));
-            if (!_scale9Enabled)
-            {
-                auto innerSprite = _barRenderer->getSprite();
-                if (nullptr != innerSprite)
-                {
-                    innerSprite->setFlippedX(true);
-                }
-            }
-            break;
             break;
     }
+    this->handleSpriteFlipX();
+    
     _barRenderer->setCapInsets(_capInsets);
     this->updateChildrenDisplayedRGBA();
 
@@ -215,6 +183,32 @@ void LoadingBar::setupTexture()
 
     this->updateProgressBar();
     _barRendererAdaptDirty = true;
+}
+    
+void LoadingBar::handleSpriteFlipX()
+{
+    if (_direction == Direction::LEFT)
+    {
+        if (!_scale9Enabled)
+        {
+            auto innerSprite = _barRenderer->getSprite();
+            if (nullptr != innerSprite)
+            {
+                innerSprite->setFlippedX(false);
+            }
+        }
+    }
+    else
+    {
+        if (!_scale9Enabled)
+        {
+            auto innerSprite = _barRenderer->getSprite();
+            if (nullptr != innerSprite)
+            {
+                innerSprite->setFlippedX(true);
+            }
+        }
+    }
 }
 
 void LoadingBar::setScale9Enabled(bool enabled)
