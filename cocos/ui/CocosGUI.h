9ba5dffb4cc85de31700ce12d95b1f777b95925f
geron-cn <c.ever.fallen@gmail.com>
2016-03-04 13:35:41 +0800

    add tab control to js/lua binding generate

--- cocos/ui/CocosGUI.h
+++ cocos/ui/CocosGUI.h
@@ -57,6 +57,7 @@ THE SOFTWARE.
 #include "ui/UIScale9Sprite.h"
 #include "ui/UIEditBox/UIEditBox.h"
 #include "ui/UILayoutComponent.h"
+#include "ui/UITabControl.h"
 #include "editor-support/cocostudio/CocosStudioExtension.h"
 
 /**
