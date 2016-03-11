7c35e9444811d35426fba1b1e60597d55ce90819
Dale Stammen <dalestam@microsoft.com>
2016-03-11 09:13:08 -0800

    fixed OnLaunched() to opt out of windows 10 prelaunch

--- tests/cpp-empty-test/proj.win10/App.xaml.h
+++ tests/cpp-empty-test/proj.win10/App.xaml.h
@@ -15,6 +15,7 @@ namespace CocosAppWinRT
     private:
         void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
         void OnResuming(Platform::Object ^sender, Platform::Object ^args);
+        void OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e);
 
         OpenGLESPage^ mPage;
         OpenGLES mOpenGLES;
