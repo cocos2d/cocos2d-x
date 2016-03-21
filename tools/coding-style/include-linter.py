bd2d7c3ee7487f7a68353cf93c4d5fb131af52ae
Xpol Wan <xpolife@gmail.com>
2016-03-21 13:27:44 +0800

    Always use posixpath.

--- tools/coding-style/include-linter.py
+++ tools/coding-style/include-linter.py
@@ -2,6 +2,9 @@
 #coding=utf-8
 
 import os, re, argparse, sys
+import posixpath
+path = posixpath
+
 
 class LintContext:
   def __init__(self, root, fix):
@@ -24,25 +27,25 @@ class LintContext:
     headers = []
     sources = []
     for root, dirnames, filenames in os.walk(top):
-      sources += [os.path.join(root, f)[len(top)+1:] for f in filenames if self._source_to_lint(root, f)]
+      sources += [path.join(root, f)[len(top)+1:] for f in filenames if self._source_to_lint(root, f)]
     self.headers = [f for f in sources if self._is_header(f)]
     self.sources = sources
 
   def _source_to_lint(self, directory, name):
-    if os.path.relpath(os.path.join(directory, name), self.root) in self.exclude:
+    if path.relpath(path.join(directory, name), self.root) in self.exclude:
       return False
-    ext = os.path.splitext(name)[1]
+    ext = path.splitext(name)[1]
     return ext in self.source_exts
 
   def _is_header(self, name):
-    return os.path.splitext(name)[1] in self.header_exts
+    return path.splitext(name)[1] in self.header_exts
 
   # find headers have unique base filenames
   # this is used to get included headers in other search paths
   def _scan_unique_headers(self, headers):
     known = {}
     for f in headers:
-      name = os.path.basename(f)
+      name = path.basename(f)
       if known.has_key(name):
         known[name].append(f)
       else:
@@ -53,21 +56,21 @@ class LintContext:
         uniq[k] = v[0]
     self.uniq = uniq
 
-  def in_search_path(self, path):
-    return os.path.exists(os.path.join(self.root, path))
+  def in_search_path(self, filename):
+    return path.exists(path.join(self.root, filename))
 
   def find_uniq(self, basename):
     return self.uniq[basename] if self.uniq.has_key(basename) else None
 
   def get_include_path(self, original, directory):
     # 1. try search in uniq cocos header names
-    path = self.find_uniq(os.path.basename(original))
-    if not path:
+    p = self.find_uniq(path.basename(original))
+    if not p:
       # 2. try search in current header directory
-      path = os.path.normpath(os.path.join(directory, original))
-      if not self.in_search_path(path):
+      p = path.normpath(path.join(directory, original))
+      if not self.in_search_path(p):
         return None
-    return path
+    return p
 
 def fix(match, cwd, ctx, fixed):
   h = match.group(1)
@@ -84,11 +87,11 @@ def fix(match, cwd, ctx, fixed):
   return '#include "%s"' % (path)
 
 def lint_one(header, ctx):
-  cwd = os.path.dirname(header)
+  cwd = path.dirname(header)
   if not cwd:
     return
 
-  filename = os.path.join(ctx.root, header)
+  filename = path.join(ctx.root, header)
   content = open(filename, 'r').read()
   fixed = {}
   # check all #include "header.*"
@@ -119,12 +122,12 @@ def lint(ctx):
 
 
 def main():
-  default_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+  default_root = path.abspath(path.join(path.dirname(__file__), '..', '..'))
   parser = argparse.ArgumentParser(description='The cocos headers lint script.')
   parser.add_argument('-f','--fix', action='store_true', help='fixe the headers while linting')
   parser.add_argument('root', nargs='?', default= default_root, help='path to cocos2d-x source root directory')
   args = parser.parse_args()
 
-  lint(LintContext(os.path.join(args.root, 'cocos'), args.fix))
+  lint(LintContext(path.join(args.root, 'cocos'), args.fix))
 
 main()
