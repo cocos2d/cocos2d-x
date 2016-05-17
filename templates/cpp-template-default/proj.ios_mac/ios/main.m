378b4bcadad1a30fd8d7ce78b9947d67d1f6208e
Mikhail Shulepov <mikhail.shulepov@gmail.com>
2016-05-17 13:31:42 +0300

    Use @autoreleasepool with and without ARC

--- templates/cpp-template-default/proj.ios_mac/ios/main.m
+++ templates/cpp-template-default/proj.ios_mac/ios/main.m
@@ -1,15 +1,7 @@
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
-    NSString *appControllerClassName = @"AppController";
-#if __has_feature(objc_arc)
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, appControllerClassName);
+        return UIApplicationMain(argc, argv, nil, @"AppController");
     }
-#else
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    const int retVal = UIApplicationMain(argc, argv, nil, appControllerClassName);
-    [pool release];
-    return retVal;
-#endif
 }
