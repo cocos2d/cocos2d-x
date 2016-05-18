3a80e7d8fbf6a3d69474ca84eba06034b40c799a
mogemimi <mogemimi@enginetrouble.net>
2016-05-19 03:34:04 +0900

    Change 2 spaces to 4 spaces indentation

--- cocos/audio/linux/SimpleAudioEngine.cpp
+++ cocos/audio/linux/SimpleAudioEngine.cpp
@@ -9,50 +9,50 @@ using namespace cocos2d::experimental;
 
 
 struct SimpleAudioEngineLinux{
-  SimpleAudioEngine * engine = nullptr; 
-  int musicid; 
-  float effectsvolume;
-  std::string musicpath; 
+    SimpleAudioEngine * engine = nullptr;
+    int musicid;
+    float effectsvolume;
+    std::string musicpath;
 };
 
-SimpleAudioEngineLinux * g_SimpleAudioEngineLinux = nullptr; 
+SimpleAudioEngineLinux * g_SimpleAudioEngineLinux = nullptr;
 
 
 SimpleAudioEngine* SimpleAudioEngine::getInstance(){
-  if(!g_SimpleAudioEngineLinux){
-    g_SimpleAudioEngineLinux = new SimpleAudioEngineLinux();
-    g_SimpleAudioEngineLinux->engine = new SimpleAudioEngine(); 
-  }
-  return g_SimpleAudioEngineLinux->engine; 
+    if(!g_SimpleAudioEngineLinux){
+        g_SimpleAudioEngineLinux = new SimpleAudioEngineLinux();
+        g_SimpleAudioEngineLinux->engine = new SimpleAudioEngine();
+    }
+    return g_SimpleAudioEngineLinux->engine;
 }
 
-  void SimpleAudioEngine::end(){
+void SimpleAudioEngine::end(){
     if(g_SimpleAudioEngineLinux){
-      delete g_SimpleAudioEngineLinux->engine; 
-      delete g_SimpleAudioEngineLinux;       
+        delete g_SimpleAudioEngineLinux->engine;
+        delete g_SimpleAudioEngineLinux;
     }
-    g_SimpleAudioEngineLinux = nullptr; 
-  }
+    g_SimpleAudioEngineLinux = nullptr;
+}
   
   
-    SimpleAudioEngine::SimpleAudioEngine(){
-      g_SimpleAudioEngineLinux->musicid = -1;
-      g_SimpleAudioEngineLinux->effectsvolume = 1.0f; 
-    }
+SimpleAudioEngine::SimpleAudioEngine(){
+    g_SimpleAudioEngineLinux->musicid = -1;
+    g_SimpleAudioEngineLinux->effectsvolume = 1.0f;
+}
 
-    SimpleAudioEngine::~SimpleAudioEngine(){
+SimpleAudioEngine::~SimpleAudioEngine(){
       
-    }
+}
 
-    void SimpleAudioEngine::preloadBackgroundMusic(const char* filePath){
-      g_SimpleAudioEngineLinux->musicpath = filePath; 
-      AudioEngine::preload(filePath); 
-    }
+void SimpleAudioEngine::preloadBackgroundMusic(const char* filePath){
+    g_SimpleAudioEngineLinux->musicpath = filePath;
+    AudioEngine::preload(filePath);
+}
     
-    void SimpleAudioEngine::playBackgroundMusic(const char* filePath, bool loop){
-      g_SimpleAudioEngineLinux->musicpath = filePath; 
-      g_SimpleAudioEngineLinux->musicid = AudioEngine::play2d(filePath, loop); 
-    }
+void SimpleAudioEngine::playBackgroundMusic(const char* filePath, bool loop){
+    g_SimpleAudioEngineLinux->musicpath = filePath;
+    g_SimpleAudioEngineLinux->musicid = AudioEngine::play2d(filePath, loop);
+}
 
     void SimpleAudioEngine::stopBackgroundMusic(bool releaseData){
       AudioEngine::stop(g_SimpleAudioEngineLinux->musicid);
@@ -61,38 +61,38 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       }
     }
 
-    void SimpleAudioEngine::pauseBackgroundMusic(){
-      AudioEngine::pause(g_SimpleAudioEngineLinux->musicid); 
-    }
+void SimpleAudioEngine::pauseBackgroundMusic(){
+    AudioEngine::pause(g_SimpleAudioEngineLinux->musicid);
+}
 
-    void SimpleAudioEngine::resumeBackgroundMusic(){
-      AudioEngine::resume(g_SimpleAudioEngineLinux->musicid);
-    }
+void SimpleAudioEngine::resumeBackgroundMusic(){
+    AudioEngine::resume(g_SimpleAudioEngineLinux->musicid);
+}
 
     void SimpleAudioEngine::rewindBackgroundMusic(){
       AudioEngine::setCurrentTime(g_SimpleAudioEngineLinux->musicid, 0);
     }
 
-    bool SimpleAudioEngine::willPlayBackgroundMusic(){
-      return g_SimpleAudioEngineLinux->musicid != -1; 
-    }
+bool SimpleAudioEngine::willPlayBackgroundMusic(){
+    return g_SimpleAudioEngineLinux->musicid != -1;
+}
 
     bool SimpleAudioEngine::isBackgroundMusicPlaying(){
         return AudioEngine::getState(g_SimpleAudioEngineLinux->musicid) == AudioEngine::AudioState::PLAYING; 
     }
 
-    // 
-    // properties
-    //
+//
+// properties
+//
 
-    /**
-     * The volume of the background music within the range of 0.0 as the minimum and 1.0 as the maximum.
-     * @js getMusicVolume
-     * @lua getMusicVolume
-     */
-    float SimpleAudioEngine::getBackgroundMusicVolume(){
-      return AudioEngine::getVolume(g_SimpleAudioEngineLinux->musicid);
-    }
+/**
+ * The volume of the background music within the range of 0.0 as the minimum and 1.0 as the maximum.
+ * @js getMusicVolume
+ * @lua getMusicVolume
+ */
+float SimpleAudioEngine::getBackgroundMusicVolume(){
+    return AudioEngine::getVolume(g_SimpleAudioEngineLinux->musicid);
+}
 
     /**
      * Set the volume of background music.
@@ -105,12 +105,12 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       AudioEngine::setVolume(g_SimpleAudioEngineLinux->musicid, volume); 
     }
 
-    /**
-     * The volume of the effects within the range of 0.0 as the minimum and 1.0 as the maximum.
-     */
-    float SimpleAudioEngine::getEffectsVolume(){
-      return g_SimpleAudioEngineLinux->effectsvolume;
-    }
+/**
+ * The volume of the effects within the range of 0.0 as the minimum and 1.0 as the maximum.
+ */
+float SimpleAudioEngine::getEffectsVolume(){
+    return g_SimpleAudioEngineLinux->effectsvolume;
+}
 
     /**
      * Set the volume of sound effects.
@@ -121,23 +121,23 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       g_SimpleAudioEngineLinux->effectsvolume = volume; 
     }
 
-    /**
-     * Play sound effect with a file path, pitch, pan and gain.
-     *
-     * @param filePath The path of the effect file.
-     * @param loop Determines whether to loop the effect playing or not. The default value is false.
-     * @param pitch Frequency, normal value is 1.0. Will also change effect play time.
-     * @param pan   Stereo effect, in the range of [-1..1] where -1 enables only left channel.
-     * @param gain  Volume, in the range of [0..1]. The normal value is 1.
-     * @return The sound id.
-     * 
-     * @note Full support is under development, now there are limitations:
-     *     - no pitch effect on Samsung Galaxy S2 with OpenSL backend enabled;
-     *     - no pitch/pan/gain on win32.
-     */
-    unsigned int SimpleAudioEngine::playEffect(const char* filePath, bool loop, float pitch, float pan, float gain){
-        return AudioEngine::play2d(filePath, loop, gain);
-    }
+/**
+ * Play sound effect with a file path, pitch, pan and gain.
+ *
+ * @param filePath The path of the effect file.
+ * @param loop Determines whether to loop the effect playing or not. The default value is false.
+ * @param pitch Frequency, normal value is 1.0. Will also change effect play time.
+ * @param pan   Stereo effect, in the range of [-1..1] where -1 enables only left channel.
+ * @param gain  Volume, in the range of [0..1]. The normal value is 1.
+ * @return The sound id.
+ * 
+ * @note Full support is under development, now there are limitations:
+ *     - no pitch effect on Samsung Galaxy S2 with OpenSL backend enabled;
+ *     - no pitch/pan/gain on win32.
+ */
+unsigned int SimpleAudioEngine::playEffect(const char* filePath, bool loop, float pitch, float pan, float gain){
+    return AudioEngine::play2d(filePath, loop, gain);
+}
 
     /**
      * Pause playing sound effect.
@@ -148,12 +148,12 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       AudioEngine::pause(soundId);
     }
 
-    /**
-     * Pause all playing sound effect.
-     */
-    void SimpleAudioEngine::pauseAllEffects(){
-      AudioEngine::pauseAll();
-    }
+/**
+ * Pause all playing sound effect.
+ */
+void SimpleAudioEngine::pauseAllEffects(){
+    AudioEngine::pauseAll();
+}
 
     /**
      * Resume playing sound effect.
@@ -164,12 +164,12 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       AudioEngine::resume(soundId);
     }
 
-    /**
-     * Resume all playing sound effect.
-     */
-    void SimpleAudioEngine::resumeAllEffects(){
-      AudioEngine::resumeAll();
-    }
+/**
+ * Resume all playing sound effect.
+ */
+void SimpleAudioEngine::resumeAllEffects(){
+    AudioEngine::resumeAll();
+}
 
     /**
      * Stop playing sound effect.
@@ -180,12 +180,12 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       AudioEngine::stop(soundId);
     }
 
-    /**
-     * Stop all playing sound effects.
-     */
-    void SimpleAudioEngine::stopAllEffects(){
-      AudioEngine::stopAll();
-    }
+/**
+ * Stop all playing sound effects.
+ */
+void SimpleAudioEngine::stopAllEffects(){
+    AudioEngine::stopAll();
+}
 
     /**
      * Preload a compressed audio file.
@@ -199,13 +199,13 @@ SimpleAudioEngine* SimpleAudioEngine::getInstance(){
       AudioEngine::preload(filePath);
     }
 
-    /**
-     * Unload the preloaded effect from internal buffer.
-     *
-     * @param filePath The path of the effect file.
-     */
-    void SimpleAudioEngine::unloadEffect(const char* filePath){
-      AudioEngine::uncache(filePath);
-    }
+/**
+ * Unload the preloaded effect from internal buffer.
+ *
+ * @param filePath The path of the effect file.
+ */
+void SimpleAudioEngine::unloadEffect(const char* filePath){
+    AudioEngine::uncache(filePath);
+}
 
     
