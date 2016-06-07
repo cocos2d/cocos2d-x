7e1da2188c6cb3f2613f51dd3bebb2a3a9893c09
minggo <ming@cocos2d-x.org>
2016-06-07 10:31:16 +0800

    ios supports LOWERCASE_ALL_CHARACTERS

--- cocos/ui/UIEditBox/Mac/CCUIEditBoxMac.mm
+++ cocos/ui/UIEditBox/Mac/CCUIEditBoxMac.mm
@@ -44,7 +44,7 @@
         self.frameRect = frameRect;
         
         self.editBox = editBox;
-        self.dataInputMode = cocos2d::ui::EditBox::InputFlag::INITIAL_CAPS_ALL_CHARACTERS;
+        self.dataInputMode = cocos2d::ui::EditBox::InputFlag::LOWERCASE_ALL_CHARACTERS;
         self.keyboardReturnType = cocos2d::ui::EditBox::KeyboardReturnType::DEFAULT;
         
         [self createMultiLineTextField];
@@ -242,6 +242,9 @@
             break;
         case cocos2d::ui::EditBox::InputFlag::SENSITIVE:
             CCLOG("SENSITIVE not implemented");
+            break;
+        case cocos2d::ui::EditBox::InputFlag::LOWERCASE_ALL_CHARACTERS:
+            CCLOG("LOWERCASE_ALL_CHARACTERS not implemented");
             break;
         default:
             break;
