126b60945e5b99f8eb3c76f1034fc50360f21c4c
James Chen <jianhua.chen@cocos2d-x.org>
2016-06-12 11:34:27 +0800

    issue #15652: fix low FPS on some android devices while auto batch is disabled.

--- cocos/renderer/CCRenderer.cpp
+++ cocos/renderer/CCRenderer.cpp
@@ -309,7 +309,14 @@ void Renderer::setupVBOAndVAO()
 void Renderer::setupVBO()
 {
     glGenBuffers(2, &_buffersVBO[0]);
-    mapBuffers();
+    // Issue #15652
+    // Should not initialzie VBO with a large size (VBO_SIZE=65536),
+    // it may cause low FPS on some Android devices like LG G4 & Nexus 5X.
+    // It's probably because some implementations of OpenGLES driver will
+    // copy the whole memory of VBO which initialzied at the first time
+    // once glBufferData/glBufferSubData is invoked.
+    // For more discussion, please refer to https://github.com/cocos2d/cocos2d-x/issues/15652
+//    mapBuffers();
 }
 
 void Renderer::mapBuffers()
