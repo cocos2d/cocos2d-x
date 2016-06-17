c387f953fa262d2f5d51154ff842c6a941a68337
Ricardo Quesada <ricardoquesada@gmail.com>
2016-06-17 16:07:33 -0700

    fix: TextureCache ASSERTS if image is invalid
    Github issue #15776

---
+++ tests/cpp-tests/Classes/BugsTest/Bug-15776.h
@@ -0,0 +1,23 @@
+//
+//  Bug-15776.hpp
+//  cocos2d_tests
+//
+//  Created by Ricardo Quesada on 6/17/16.
+//
+//
+
+#ifndef Bug_15776_hpp
+#define Bug_15776_hpp
+
+#include "BugsTest.h"
+
+class Bug15776Layer : public BugsTestBase
+{
+public:
+    CREATE_FUNC(Bug15776Layer);
+
+    virtual bool init() override;
+};
+
+
+#endif /* Bug_15776_hpp */
