69429290b6c398cfdebb79235015db4fa47b2d72
zilongshanren <guanghui8827@126.com>
2016-06-22 18:41:10 +0800

    simplify get real font name on Android

--- cocos/platform/android/CCDevice-android.cpp
+++ cocos/platform/android/CCDevice-android.cpp
@@ -96,13 +96,15 @@ public:
 
            // Do a full lookup for the font path using FileUtils in case the given font name is a relative path to a font file asset,
            // or the path has been mapped to a different location in the app package:
-           std::string fullPathOrFontName = FileUtils::getInstance()->fullPathForFilename(textDefinition._fontName);
-            
-           // If the path name returned includes the 'assets' dir then that needs to be removed, because the android.content.Context
-           // requires this portion of the path to be omitted for assets inside the app package.
-           if (fullPathOrFontName.find("assets/") == 0)
-           {
-               fullPathOrFontName = fullPathOrFontName.substr(strlen("assets/"));   // Chop out the 'assets/' portion of the path.
+           std::string fullPathOrFontName = textDefinition._fontName;
+           if(FileUtils::getInstance()->isFileExist(fullPathOrFontName)) {
+               fullPathOrFontName = FileUtils::getInstance()->fullPathForFilename(textDefinition._fontName);
+               // If the path name returned includes the 'assets' dir then that needs to be removed, because the android.content.Context
+               // requires this portion of the path to be omitted for assets inside the app package.
+               if (fullPathOrFontName.find("assets/") == 0)
+               {
+                   fullPathOrFontName = fullPathOrFontName.substr(strlen("assets/"));   // Chop out the 'assets/' portion of the path.
+               }
            }
 
            /**create bitmap
@@ -114,7 +116,7 @@ public:
            int count = strlen(text);
            jbyteArray strArray = methodInfo.env->NewByteArray(count);
            methodInfo.env->SetByteArrayRegion(strArray, 0, count, reinterpret_cast<const jbyte*>(text));
-           jstring jstrFont = methodInfo.env->NewStringUTF(fullPathOrFontName.empty() ? textDefinition._fontName.c_str() : fullPathOrFontName.c_str());
+           jstring jstrFont = methodInfo.env->NewStringUTF(fullPathOrFontName.c_str());
 
            if(!methodInfo.env->CallStaticBooleanMethod(methodInfo.classID, methodInfo.methodID, strArray,
                jstrFont, textDefinition._fontSize, textDefinition._fontFillColor.r, textDefinition._fontFillColor.g, 
