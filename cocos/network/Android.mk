26dd7b2698e3c2710dda67f2f1fd33b19555695d
minggo <ming@cocos2d-x.org>
2016-07-21 11:56:56 +0800

    remove curl link for Android

--- cocos/network/Android.mk
+++ cocos/network/Android.mk
@@ -19,7 +19,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../external/curl/include/android \
                     $(LOCAL_PATH)/../../external/websockets/include/android
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_internal_static
-LOCAL_STATIC_LIBRARIES += cocos_curl_static
 LOCAL_STATIC_LIBRARIES += libwebsockets_static
 
 include $(BUILD_STATIC_LIBRARY)
