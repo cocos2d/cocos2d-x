779ccc361d729e738f2b28bfc81516042ba86bb8
Vladimir Perminov <snark.gm@gmail.com>
2016-07-27 17:42:14 +0300

    Copyright

--- cocos/platform/win32/CCUtils-win32.h
+++ cocos/platform/win32/CCUtils-win32.h
@@ -1,4 +1,6 @@
 /****************************************************************************
+Copyright (c) 2010-2012 cocos2d-x.org
+Copyright (c) 2013-2016 Chukong Technologies Inc.
 
 http://www.cocos2d-x.org
 
