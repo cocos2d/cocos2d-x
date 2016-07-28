09aa7d9cf43d0931269515cab18be3ccbdd0c0bb
Vladimir Perminov <snark.gm@gmail.com>
2016-07-28 04:57:29 +0300

    Win32 move unicode utils global. (#16234)
    * Win32 move unicode utils global.
    
    It need for easy converting utf-8 string in all code.
    And use it api in user code also WinRT version.
    
    * Move to CCUtils-win32
    
    Also in WinRT version.
    
    * Copyright

---
+++ cocos/platform/win32/CCUtils-win32.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+Copyright (c) 2010-2012 cocos2d-x.org
+Copyright (c) 2013-2016 Chukong Technologies Inc.
+
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+#ifndef __CC_UTILS_WIN32_H__
+#define __CC_UTILS_WIN32_H__
+
+#include "platform/CCPlatformMacros.h"
+
+#include <string>
+
+NS_CC_BEGIN
+
+std::wstring CC_DLL StringUtf8ToWideChar(const std::string& strUtf8);
+std::string CC_DLL StringWideCharToUtf8(const std::wstring& strWideChar);
+std::string CC_DLL UTF8StringToMultiByte(const std::string& strUtf8);
+
+NS_CC_END
+
+#endif // __CC_UTILS_WIN32_H__
