a58323a388d2753fc48ecad0695e9f88110678c4
Ricardo Quesada <ricardoquesada@gmail.com>
2016-08-04 23:04:39 -0700

    Adds more info regarding bug 15776 (#16315)
    Related to github issue #15776

--- tests/cpp-tests/Classes/BugsTest/Bug-15776.h
+++ tests/cpp-tests/Classes/BugsTest/Bug-15776.h
@@ -17,6 +17,9 @@ public:
     CREATE_FUNC(Bug15776Layer);
 
     virtual bool init() override;
+
+    virtual std::string title() const override;
+    virtual std::string subtitle() const override;
 };
 
 
