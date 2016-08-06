f01b7d85a8e915f91516489a95d0f8b99b99d8dc
Greg Rynkowski <g.rynkowski@platomedia.tv>
2016-08-06 10:03:58 +0100

    Remove AppDelegate memory leak from Android projects
    AppDelegate object and all its members are never released.
    As a solution I propose to use static unique_ptr that could destroy it
    at the end of the application.
    
    Issue:
    https://github.com/cocos2d/cocos2d-x/issues/14110

--- tests/js-tests/project/proj.android/jni/main.cpp
+++ tests/js-tests/project/proj.android/jni/main.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include <android/log.h>
 #include <jni.h>
 
@@ -6,7 +8,11 @@
 #define  LOG_TAG    "main"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 
+namespace {
+std::unique_ptr<AppDelegate> appDelegate;
+}
+
 void cocos_android_app_init(JNIEnv* env) {
     LOGD("cocos_android_app_init");
-    AppDelegate *pAppDelegate = new AppDelegate();
+    appDelegate.reset(new AppDelegate());
 }
