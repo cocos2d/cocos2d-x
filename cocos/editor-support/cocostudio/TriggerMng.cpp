7f36ef06c8bbdc4282f59fceee940b371eb9833f
Sergey <non_2001@mail.ru>
2016-08-16 13:00:07 +0300

    rapidjson updated

--- cocos/editor-support/cocostudio/TriggerMng.cpp
+++ cocos/editor-support/cocostudio/TriggerMng.cpp
@@ -23,7 +23,6 @@ THE SOFTWARE.
 ****************************************************************************/
 
 #include "editor-support/cocostudio/TriggerMng.h"
-#include "json/filestream.h"
 #include "json/prettywriter.h"
 #include "json/stringbuffer.h"
 #include "base/CCDirector.h"
