cba207014ca638f96d24500d24720ac30404b311
halx99 <halx99@live.com>
2016-08-19 23:32:03 +0800

    Update CCLabelTextFormatter.cpp

--- cocos/2d/CCLabelTextFormatter.cpp
+++ cocos/2d/CCLabelTextFormatter.cpp
@@ -218,7 +218,7 @@ bool Label::multilineTextWrap(const std::function<int(const std::u16string&, int
                     nextLetterX += _horizontalKernings[letterIndex + 1];
                 nextLetterX += letterDef.xAdvance * _bmfontScale + _additionalKerning;
 
-                tokenRight = (letterPosition.x + letterDef.width) / contentScaleFactor; // #FIX Label size calculation bug.
+                tokenRight = letterPosition.x + letterDef.width/* Already contains _additionalKerning */ * _bmfontScale; // #FIX Label size calculation bug
             }
             nextChangeSize = true;
 
