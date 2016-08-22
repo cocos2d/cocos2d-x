300d39e56a0d517f2456f778b9836a4a49fdf9a8
Michael Kösel <TheCodez@users.noreply.github.com>
2016-08-22 21:02:23 +0200

    Return defaultValue parameter instead of some default value

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxHelper.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxHelper.java
@@ -532,7 +532,7 @@ public class Cocos2dxHelper {
             }
         }
 
-        return false;
+        return defaultValue;
     }
     
     public static int getIntegerForKey(String key, int defaultValue) {
@@ -560,7 +560,7 @@ public class Cocos2dxHelper {
             }
         }
 
-        return 0;
+        return defaultValue;
     }
     
     public static float getFloatForKey(String key, float defaultValue) {
@@ -588,7 +588,7 @@ public class Cocos2dxHelper {
             }
         }
 
-        return 0.0f;
+        return defaultValue;
     }
     
     public static double getDoubleForKey(String key, double defaultValue) {
