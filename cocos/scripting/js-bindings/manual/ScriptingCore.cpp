e85c77b888b2709467f66ccad81b67a68b15449b
DavidDeSimone <djd231@scarletmail.rutgers.edu>
2016-09-18 22:04:03 -0700

    Fixing issue where ScriptingCore::evalString would always return false, and not evaluate the string, due to an improper null check.

--- cocos/scripting/js-bindings/manual/ScriptingCore.cpp
+++ cocos/scripting/js-bindings/manual/ScriptingCore.cpp
@@ -520,8 +520,8 @@ void ScriptingCore::string_report(JS::HandleValue val) {
 bool ScriptingCore::evalString(const char *string, JS::MutableHandleValue outVal, const char *filename, JSContext* cx, JS::HandleObject global)
 {
     JSAutoCompartment ac(cx, global);
-    JS::PersistentRootedScript script(cx);
-    if (script == nullptr) {
+    JS::RootedScript script(cx);
+    if (&script == nullptr) {
         return false;
     }
     
