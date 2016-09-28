a6f3773aefe16dd996205235314a1a2f3d355289
minggo <xiaoming.zhang@chukong-inc.com>
2016-09-28 16:19:20 +0800

    Luajit (#16620)
    * update console
    
    * load lua according architecture
    
    * install android-13 in travis

--- cocos/scripting/lua-bindings/proj.android/Android.mk
+++ cocos/scripting/lua-bindings/proj.android/Android.mk
@@ -21,15 +21,9 @@ LOCAL_EXPORT_LDLIBS := -lGLESv2 \
                        -llog \
                        -landroid
 
-ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-LUA_STATIC_LIB := lua_static
-LUA_IMPORT_PATH := lua/lua
-LUA_INCLUDE_PATH := $(LOCAL_PATH)/../../../../external/lua/lua
-else
 LUA_STATIC_LIB := luajit_static
 LUA_IMPORT_PATH := lua/luajit/prebuilt/android
 LUA_INCLUDE_PATH := $(LOCAL_PATH)/../../../../external/lua/luajit/include
-endif
 
 LOCAL_STATIC_LIBRARIES := $(LUA_STATIC_LIB)
 
