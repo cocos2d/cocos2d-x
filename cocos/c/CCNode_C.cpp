2efc2f9960c52a0d834169c7e6495e54fe7085e3
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-10-15 00:56:24 -0500

    Created CCocos2D
    Public modular C API for Cocos2D

--- cocos/c/CCNode_C.cpp
+++ cocos/c/CCNode_C.cpp
@@ -24,13 +24,13 @@
 
 #include "2d/CCNode.h"
 
-void* CCNodeCreate()
+extern "C" void* CCNodeCreate()
 {
     cocos2d::Node *node = cocos2d::Node::create();
     return (void *)node;
 }
 
-const char* CCNodeGetDescription(const void *object)
+extern "C" const char* CCNodeGetDescription(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     std::string string = node->getDescription();
@@ -39,73 +39,73 @@ const char* CCNodeGetDescription(const void *object)
     return cString;
 }
 
-int CCNodeGetLocalZOrder(const void *object)
+extern "C" int CCNodeGetLocalZOrder(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getLocalZOrder();
 }
 
-void CCNodeSetLocalZOrder(const void *object, int newValue)
+extern "C" void CCNodeSetLocalZOrder(const void *object, int newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setLocalZOrder(newValue);
 }
 
-int CCNodeGetGlobalZOrder(const void *object)
+extern "C" int CCNodeGetGlobalZOrder(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getGlobalZOrder();
 }
 
-void CCNodeSetGlobalZOrder(const void *object, int newValue)
+extern "C" void CCNodeSetGlobalZOrder(const void *object, int newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setGlobalZOrder(newValue);
 }
 
-float CCNodeGetScaleX(const void *object)
+extern "C" float CCNodeGetScaleX(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getScaleX();
 }
 
-void CCNodeSetScaleX(const void *object, float newValue)
+extern "C" void CCNodeSetScaleX(const void *object, float newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setScaleX(newValue);
 }
 
-float CCNodeGetScaleY(const void *object)
+extern "C" float CCNodeGetScaleY(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getScaleY();
 }
 
-void CCNodeSetScaleY(const void *object, float newValue)
+extern "C" void CCNodeSetScaleY(const void *object, float newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setScaleY(newValue);
 }
 
-float CCNodeGetScaleZ(const void *object)
+extern "C" float CCNodeGetScaleZ(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getScaleZ();
 }
 
-void CCNodeSetScaleZ(const void *object, float newValue)
+extern "C" void CCNodeSetScaleZ(const void *object, float newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setScaleZ(newValue);
 }
 
-float CCNodeGetScale(const void *object)
+extern "C" float CCNodeGetScale(const void *object)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     return node->getScale();
 }
 
-void CCNodeSetScale(const void *object, float newValue)
+extern "C" void CCNodeSetScale(const void *object, float newValue)
 {
     cocos2d::Node *node = (cocos2d::Node *)object;
     node->setScale(newValue);
---
+++ cocos/c/CCocos2D.h
@@ -0,0 +1,16 @@
+//
+//  CCocos2D.h
+//  cocos2d_libs
+//
+//  Created by Alsey Coleman Miller on 10/15/16.
+//
+//
+
+#ifndef CCocos2D_h
+#define CCocos2D_h
+
+#import <CCocos2D/CCDefines.h>
+#import <CCocos2D/CCNode_C.h>
+
+
+#endif /* CCocos2D_h */
