eb026b987decd0033a08f871197fde1b5167eb4b
mogemimi <mogemimi@enginetrouble.net>
2016-10-16 20:16:18 +0900

    Fix typos in comments and strings

--- extensions/GUI/CCControlExtension/CCControlSaturationBrightnessPicker.cpp
+++ extensions/GUI/CCControlExtension/CCControlSaturationBrightnessPicker.cpp
@@ -151,7 +151,7 @@ void ControlSaturationBrightnessPicker::updateSliderPosition(Vec2 sliderPosition
     if (sliderPosition.y < _startPos.y + boxPos)                        sliderPosition.y = _startPos.y + boxPos;
     else if (sliderPosition.y > _startPos.y + boxPos + boxSize)        sliderPosition.y = _startPos.y + boxPos + boxSize;
     
-    // Use the position / slider width to determin the percentage the dragger is at
+    // Use the position / slider width to determine the percentage the dragger is at
     _saturation = 1.0f - fabs((_startPos.x + (float)boxPos - sliderPosition.x)/(float)boxSize);
     _brightness = fabs((_startPos.y + (float)boxPos - sliderPosition.y)/(float)boxSize);
 }
