c18cc2c932094cd8a4ad7ce3154c7f5375fd305f
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-10-16 03:18:44 -0500

    Added CCSpriteCreate

--- cocos/c/CCSprite_C.cpp
+++ cocos/c/CCSprite_C.cpp
@@ -6,5 +6,33 @@
 //
 //
 
-#include "CCSprite_C.h"
+#include <stdio.h>
 
+#include <cstdint>
+#include "base/ccMacros.h"
+#include "base/CCVector.h"
+#include "base/CCProtocols.h"
+#include "base/CCScriptSupport.h"
+#include "math/CCAffineTransform.h"
+#include "math/CCMath.h"
+#include "2d/CCComponentContainer.h"
+#include "2d/CCComponent.h"
+
+#if CC_USE_PHYSICS
+#include "physics/CCPhysicsBody.h"
+#endif
+
+#include "2d/CCNode.h"
+#include "2d/CCSprite.h"
+#include "c/CCVector_C.h"
+#include "c/CCDefines.h"
+
+USING_NS_CC;
+
+#pragma mark - Initialization
+
+extern "C" void* CCSpriteCreate()
+{
+    Sprite *sprite = Sprite::create();
+    return (void *)sprite;
+}
--- cocos/c/CCSprite_C.h
+++ cocos/c/CCSprite_C.h
@@ -1,5 +1,5 @@
 //
-//  CCSprite_C.hpp
+//  CCSprite_C.h
 //  cocos2d_libs
 //
 //  Created by Alsey Coleman Miller on 10/15/16.
@@ -8,7 +8,20 @@
 
 #ifndef CCSprite_C_h
 #define CCSprite_C_h
+#ifndef __cplusplus
+#include <stdint.h>
+#include <stdbool.h>
+#import <CCocos2D/CCDefines.h>
+#import <CCocos2D/CCVector_C.h>
 
+CC_ASSUME_NONNULL_BEGIN
 
+#pragma mark - Initialization
 
-#endif /* CCSprite_C_hpp */
+SWIFT_PRIVATE
+void* CCSpriteCreate();
+
+CC_ASSUME_NONNULL_END
+
+#endif
+#endif /* CCSprite_C_h */
