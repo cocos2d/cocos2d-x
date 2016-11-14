567218c7c134723b2630c36d5f54c3fd713f8b4f
UQ Times <uqtimes@gmail.com>
2016-11-14 11:00:55 +0900

    fix compile error if "USE_STD_UNORDERED_MAP == 0". (#16831)

--- cocos/network/SocketIO.h
+++ cocos/network/SocketIO.h
@@ -60,6 +60,7 @@ in the onClose method the pointer should be set to NULL or used to connect to a 
 #define __CC_SOCKETIO_H__
 
 #include <string>
+#include <unordered_map>
 #include "platform/CCPlatformMacros.h"
 #include "base/CCMap.h"
 
