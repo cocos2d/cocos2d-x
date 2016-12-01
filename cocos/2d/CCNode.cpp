dd647d00338017f51751563ffe8f9d53ac5f34ae
fred <frederic@prettysimplegames.com>
2016-12-01 15:13:45 +0100

    Update CCNode.cpp
    The _transform property of CCNode would not be restored on setAdditionalTransform(nullptr)
    causing in my project a rotation run away ...

--- cocos/2d/CCNode.cpp
+++ cocos/2d/CCNode.cpp
@@ -1786,6 +1786,7 @@ void Node::setAdditionalTransform(const Mat4* additionalTransform)
 {
     if (additionalTransform == nullptr)
     {
+        if(_additionalTransform)  _transform = _additionalTransform[1];
         delete[] _additionalTransform;
         _additionalTransform = nullptr;
     }
