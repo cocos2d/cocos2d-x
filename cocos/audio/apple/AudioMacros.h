8e3e1933b09c009faaef9496d5ca5bdc31a69cc1
James Chen <jianhua.chen@cocos2d-x.org>
2016-12-01 21:58:49 +0800

    Adds license header, removes unused macros.

--- cocos/audio/apple/AudioMacros.h
+++ cocos/audio/apple/AudioMacros.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+ Copyright (c) 2016 Chukong Technologies Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 #pragma once
 
 #define QUEUEBUFFER_NUM (3)
@@ -27,22 +51,6 @@ do { \
 #else
 #define CHECK_AL_ERROR_DEBUG()
 #endif
-
-#define RETURN_IF(condition, message) \
-do {\
-    if (!!(condition)) { \
-        ALOGE("(%s) failed, message: %s", QUOTEME(condition), message); \
-        return; \
-    } \
-} while(false)
-
-#define RETURN_VAL_IF(condition, val, fmt, ...) \
-do {\
-    if (!!(condition)) { \
-        ALOGE("(" QUOTEME(condition) ") failed, message: " fmt, ##__VA_ARGS__); \
-        return val; \
-    } \
-} while(false)
 
 #define BREAK_IF(condition) \
 do {\
