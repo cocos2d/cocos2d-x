8c4c5fec9519c30253f3e3d00c59eb6a3339c68d
effolkronium <effolkronium@gmail.com>
2017-01-03 19:10:48 +0200

    fix typo

--- cocos/2d/CCLabel.h
+++ cocos/2d/CCLabel.h
@@ -410,7 +410,7 @@ public:
     /**
     * Return the outline effect size value.
     */
-    float getOutlineSize() const { _outlineSize; }
+    float getOutlineSize() const { return _outlineSize; }
 
     /**
     * Return current effect type.
