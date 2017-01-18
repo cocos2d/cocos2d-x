b925dde8bfdd8ee4e8f4144d80c3305d0038a25f
mogemimi <mogemimi@enginetrouble.net>
2017-01-19 02:19:00 +0900

    Fix warnings about implicit conversion loses integer precision

--- cocos/3d/CCTerrain.cpp
+++ cocos/3d/CCTerrain.cpp
@@ -436,7 +436,7 @@ void Terrain::calculateNormal()
             _indices.push_back (nLocIndex + _imageWidth+1);
         }
     }
-    for (unsigned int i = 0, size = _indices.size(); i < size; i += 3) {
+    for (size_t i = 0, size = _indices.size(); i < size; i += 3) {
         unsigned int Index0 = _indices[i];
         unsigned int Index1 = _indices[i + 1];
         unsigned int Index2 = _indices[i + 2];
@@ -450,8 +450,8 @@ void Terrain::calculateNormal()
         _vertices[Index2]._normal += Normal;
     }
 
-    for (unsigned int i = 0, size = _vertices.size(); i < size; ++i) {
-        _vertices[i]._normal.normalize();
+    for (auto &vertex : _vertices) {
+        vertex._normal.normalize();
     }
     //global indices no need at all
     _indices.clear();
