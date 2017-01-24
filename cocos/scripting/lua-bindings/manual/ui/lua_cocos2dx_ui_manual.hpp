a7fece5c420ac4558c13dd065ab98c80af2abad5
mogemimi <mogemimi@enginetrouble.net>
2017-01-25 02:36:47 +0900

    Fix typo in function name

--- cocos/scripting/lua-bindings/manual/ui/lua_cocos2dx_ui_manual.hpp
+++ cocos/scripting/lua-bindings/manual/ui/lua_cocos2dx_ui_manual.hpp
@@ -46,7 +46,7 @@ TOLUA_API int register_all_cocos2dx_ui_manual(lua_State* L);
  * If you don't register the ui module, the package size would become smaller .
  * The current mechanism,this registering function is called in the lua_module_register.h
  */
-TOLUA_API int register_ui_moudle(lua_State* L);
+TOLUA_API int register_ui_module(lua_State* L);
 
 // end group
 /// @}
