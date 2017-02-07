05cd8e90fa44adaf0137ecbc2d22018654a5ff70
Jeff Wang <wohaaitinciu@163.com>
2017-02-07 11:11:10 +0800

    add 'makeRef' for easier making a RefPtr

--- cocos/base/CCRefPtr.h
+++ cocos/base/CCRefPtr.h
@@ -269,7 +269,13 @@ private:
     // NOTE: We can ensure T is derived from cocos2d::Ref at compile time here.
     static_assert(std::is_base_of<Ref, typename std::remove_const<T>::type>::value, "T must be derived from Ref");
 };
-    
+
+template <class T> inline
+RefPtr<T> makeRef(T *ptr)
+{
+    return RefPtr<T>(ptr);
+}
+
 template<class T> inline
 bool operator<(const RefPtr<T>& r, std::nullptr_t)
 {
