6d901c5bd6ae80ea64d5eb7d12077b19d558ba2e
CocosRobot <redmine@cocos2d-x.org>
2017-02-14 12:56:43 +0800

    Copyright update: 201?-201? -> 201?-2017

--- cocos/math/Vec4.h
+++ cocos/math/Vec4.h
@@ -1,6 +1,6 @@
 /**
  Copyright 2013 BlackBerry Inc.
- Copyright (c) 2014-2015 Chukong Technologies
+ Copyright (c) 2014-2017 Chukong Technologies
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
--- cocos/scripting/js-bindings/manual/network/jsb_websocket.cpp
+++ cocos/scripting/js-bindings/manual/network/jsb_websocket.cpp
@@ -1,6 +1,6 @@
 /*
  * Created by James Chen
- * Copyright (c) 2013-2016 Chukong Technologies Inc.
+ * Copyright (c) 2013-2017 Chukong Technologies Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
