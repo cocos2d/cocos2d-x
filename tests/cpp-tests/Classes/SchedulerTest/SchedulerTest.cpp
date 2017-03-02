30a4d6a6262e8d63bcefe2c80ea801b14c11a4ab
Julien Jorge <julien.jorge@stuff-o-matic.com>
2017-03-02 08:05:39 +0100

    Prevent unexpected calls to unscheduled selector in long updates.
    If the delta time dt passed to cocos2d::Timer::update(float) is longer than the
    interval of the timer, then the timer will be triggered several times even if
    the selector is unscheduled during the call.
    
    This commit adds a flag in cocos2d::Timer stop the update loop if the selector
    has been unscheduled.

--- tests/cpp-tests/Classes/SchedulerTest/SchedulerTest.cpp
+++ tests/cpp-tests/Classes/SchedulerTest/SchedulerTest.cpp
@@ -32,6 +32,7 @@ SchedulerTests::SchedulerTests()
     ADD_TEST_CASE(SchedulerRemoveAllFunctionsToBePerformedInCocosThread)
     ADD_TEST_CASE(SchedulerIssue17149);
     ADD_TEST_CASE(SchedulerRemoveEntryWhileUpdate);
+    ADD_TEST_CASE(SchedulerRemoveSelectorDuringCall);
 };
 
 //------------------------------------------------------------------
@@ -1355,4 +1356,61 @@ void SchedulerRemoveEntryWhileUpdate::TestClass::update(float dt)
         _scheduler->unscheduleUpdate(_nextObj);
         _nextObj->_cleanedUp = true;
     }
+}
+
+//------------------------------------------------------------------
+//
+// SchedulerRemoveSelectorDuringCall
+//
+//------------------------------------------------------------------
+
+std::string SchedulerRemoveSelectorDuringCall::title() const
+{
+    return "RemoveSelectorDuringCall";
+}
+
+std::string SchedulerRemoveSelectorDuringCall::subtitle() const
+{
+    return "see console, error message must not be shown.";
+}
+
+void SchedulerRemoveSelectorDuringCall::onEnter()
+{
+    SchedulerTestLayer::onEnter();
+
+    Scheduler* const scheduler( Director::getInstance()->getScheduler() );
+    
+    scheduler->setTimeScale( 10 );
+    scheduler->schedule
+      (SEL_SCHEDULE(&SchedulerRemoveSelectorDuringCall::callback), this,
+       0.01f, CC_REPEAT_FOREVER, 0.0f, !isRunning());
+
+    _scheduled = true;
+}
+
+void SchedulerRemoveSelectorDuringCall::onExit()
+{
+    Scheduler* const scheduler( Director::getInstance()->getScheduler() );
+
+    scheduler->setTimeScale( 1 );
+    scheduler->unschedule
+      (SEL_SCHEDULE(&SchedulerRemoveSelectorDuringCall::callback), this);
+
+    _scheduled = false;
+    SchedulerTestLayer::onExit();
+}
+
+void SchedulerRemoveSelectorDuringCall::callback( float )
+{
+    if ( !_scheduled )
+    {
+        cocos2d::log("Error: unscheduled callback must not be called.");
+        return;
+    }
+    
+    _scheduled = false;
+
+    Scheduler* const scheduler( Director::getInstance()->getScheduler() );
+    scheduler->unschedule
+      (SEL_SCHEDULE(&SchedulerRemoveSelectorDuringCall::callback), this);
 }
