f90691eb63d7d03c1f148315003afa1f7a3c59ff
mogemimi <mogemimi@enginetrouble.net>
2017-03-22 10:21:34 +0900

    Fix spelling: billborad -> billboard (#17560)

--- tests/lua-tests/src/BillBoardTest/BillBoardTest.lua
+++ tests/lua-tests/src/BillBoardTest/BillBoardTest.lua
@@ -7,7 +7,7 @@ end)
 
 function BillBoardTest:ctor()
     self._camera = nil
-    self._layerBillBorad = nil
+    self._layerBillBoard = nil
     self._billboards = {}
     self:init()
 end
@@ -20,24 +20,24 @@ function BillBoardTest:subtitle()
     return ""
 end
 
-function BillBoardTest:addNewBillBoradWithCoords(p)
+function BillBoardTest:addNewBillBoardWithCoords(p)
     local imgs = {"Images/Icon.png", "Images/r2.png"}
     for i= 1, 10 do
-        local billborad = cc.BillBoard:create(imgs[math.ceil(math.random() + 0.5)])
-        billborad:setScale(0.5)
-        billborad:setPosition3D(cc.vec3(p.x, p.y,  -150.0 + 30 * i))
-        billborad:setOpacity(math.random() * 128 + 128)
-        self._layerBillBorad:addChild(billborad)
-        table.insert(self._billboards, billborad)
+        local billboard = cc.BillBoard:create(imgs[math.ceil(math.random() + 0.5)])
+        billboard:setScale(0.5)
+        billboard:setPosition3D(cc.vec3(p.x, p.y,  -150.0 + 30 * i))
+        billboard:setOpacity(math.random() * 128 + 128)
+        self._layerBillBoard:addChild(billboard)
+        table.insert(self._billboards, billboard)
     end
 end
 
-function BillBoardTest:addNewAniBillBoradWithCoords(p)
+function BillBoardTest:addNewAniBillBoardWithCoords(p)
     for i = 1, 10 do
-        local billboradAni = cc.BillBoard:create("Images/grossini.png")
-        billboradAni:setScale(0.5)
-        billboradAni:setPosition3D(cc.vec3(p.x, p.y,  -150.0 + 30 * (i - 1)))
-        self._layerBillBorad:addChild(billboradAni)
+        local billboardAni = cc.BillBoard:create("Images/grossini.png")
+        billboardAni:setScale(0.5)
+        billboardAni:setPosition3D(cc.vec3(p.x, p.y,  -150.0 + 30 * (i - 1)))
+        self._layerBillBoard:addChild(billboardAni)
 
         local animation = cc.Animation:create()
         for i=1,14 do
@@ -50,9 +50,9 @@ function BillBoardTest:addNewAniBillBoradWithCoords(p)
         animation:setRestoreOriginalFrame(true)
 
         local action = cc.Animate:create(animation)
-        billboradAni:runAction(cc.RepeatForever:create(action))
-        billboradAni:setOpacity( math.random() * 128 + 128)
-        table.insert(self._billboards, billboradAni)
+        billboardAni:runAction(cc.RepeatForever:create(action))
+        billboardAni:setOpacity( math.random() * 128 + 128)
+        table.insert(self._billboards, billboardAni)
     end
 end
 
@@ -93,13 +93,13 @@ function BillBoardTest:init()
 
     local layer3D = cc.Layer:create()
     self:addChild(layer3D,0)
-    self._layerBillBorad = layer3D
+    self._layerBillBoard = layer3D
 
     local s = cc.Director:getInstance():getWinSize()
     if self._camera == nil then
         self._camera = cc.Camera:createPerspective(60, s.width/s.height, 1, 500)
         self._camera:setCameraFlag(cc.CameraFlag.USER1)
-        self._layerBillBorad:addChild(self._camera)
+        self._layerBillBoard:addChild(self._camera)
     end
 
     local imgs = {"Images/Icon.png", "Images/r2.png"}
@@ -112,7 +112,7 @@ function BillBoardTest:init()
         billboard:setOpacity(math.random() * 128 + 128)
         table.insert(self._billboards, billboard)
         layer:addChild(billboard)
-        self._layerBillBorad:addChild(layer)
+        self._layerBillBoard:addChild(layer)
         layer:runAction( cc.RepeatForever:create( cc.RotateBy:create( math.random() * 10, cc.vec3(0.0, 45.0, 0.0) ) ) )
     end
 
@@ -130,18 +130,18 @@ function BillBoardTest:init()
     sprite3d:setScale(2.0)
     sprite3d:addChild(billboard)
     sprite3d:runAction( cc.RepeatForever:create( cc.RotateBy:create( 10.0, cc.vec3(0.0, 360.0, 0.0) ) ) )
-    self._layerBillBorad:addChild(sprite3d)
+    self._layerBillBoard:addChild(sprite3d)
 
-    self:addNewBillBoradWithCoords(cc.vec3(20,5,0))
-    self:addNewBillBoradWithCoords(cc.vec3(60,5,0))
-    self:addNewBillBoradWithCoords(cc.vec3(100,5,0))
-    self:addNewBillBoradWithCoords(cc.vec3(140,5,0))
-    self:addNewBillBoradWithCoords(cc.vec3(180,5,0))
-    self:addNewAniBillBoradWithCoords(cc.vec3(-20,0,0))
-    self:addNewAniBillBoradWithCoords(cc.vec3(-60,0,0))
-    self:addNewAniBillBoradWithCoords(cc.vec3(-100,0,0))
-    self:addNewAniBillBoradWithCoords(cc.vec3(-140,0,0))
-    self:addNewAniBillBoradWithCoords(cc.vec3(-180,0,0))
+    self:addNewBillBoardWithCoords(cc.vec3(20,5,0))
+    self:addNewBillBoardWithCoords(cc.vec3(60,5,0))
+    self:addNewBillBoardWithCoords(cc.vec3(100,5,0))
+    self:addNewBillBoardWithCoords(cc.vec3(140,5,0))
+    self:addNewBillBoardWithCoords(cc.vec3(180,5,0))
+    self:addNewAniBillBoardWithCoords(cc.vec3(-20,0,0))
+    self:addNewAniBillBoardWithCoords(cc.vec3(-60,0,0))
+    self:addNewAniBillBoardWithCoords(cc.vec3(-100,0,0))
+    self:addNewAniBillBoardWithCoords(cc.vec3(-140,0,0))
+    self:addNewAniBillBoardWithCoords(cc.vec3(-180,0,0))
 
     self._camera:setPosition(cc.vec3(0, 130, 230))
     self._camera:lookAt(cc.vec3(0, 0, 100), cc.vec3(0, 1, 0))
@@ -170,7 +170,7 @@ function BillBoardTest:init()
     menuItem1:setPosition( cc.p( s.width-80, VisibleRect:top().y-160) )
     menuItem2:setPosition( cc.p( s.width-80, VisibleRect:top().y-190) )
     self:addChild(menu, 0)
-    self._layerBillBorad:setCameraMask(2)
+    self._layerBillBoard:setCameraMask(2)
 
     label1 = cc.Label:createWithTTF(ttfConfig,"Point Oriented")
     menuItem1 = cc.MenuItemLabel:create(label1)
