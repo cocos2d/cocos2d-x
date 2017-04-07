0afda5551b266891345f33292f669ea4bce8c661
James Chen <jianhua.chen@cocos2d-x.org>
2017-04-07 14:43:27 +0800

    Minor fix: Adds missing test case in NewAudioEngineTest.

--- tests/cpp-tests/Classes/NewAudioEngineTest/NewAudioEngineTest.cpp
+++ tests/cpp-tests/Classes/NewAudioEngineTest/NewAudioEngineTest.cpp
@@ -48,6 +48,7 @@ AudioEngineTests::AudioEngineTests()
     ADD_TEST_CASE(AudioPlayFileInWritablePath);
     ADD_TEST_CASE(AudioIssue16938Test);
     ADD_TEST_CASE(AudioPlayInFinishedCB);
+    ADD_TEST_CASE(AudioUncacheInFinishedCB);
     
     //FIXME: Please keep AudioSwitchStateTest to the last position since this test case doesn't work well on each platforms.
     ADD_TEST_CASE(AudioSwitchStateTest);
