5dafda67e033156a4ecd6c65b515981448be45d5
CocosRobot#Set <redmine@cocos2d-x.org>
2017-07-10 03:34:59 +0000

    [ci skip][AUTO]: updating luabinding & jsbinding & cocos_file.json automatically

--- cocos/scripting/lua-bindings/auto/lua_cocos2dx_controller_auto.hpp
+++ cocos/scripting/lua-bindings/auto/lua_cocos2dx_controller_auto.hpp
@@ -33,5 +33,6 @@ int register_all_cocos2dx_controller(lua_State* tolua_S);
 
 
 
+
 #endif // __cocos2dx_controller_h__
 #endif //#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
