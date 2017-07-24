ca27979db7b6ad1bc5befb6bfa6af9e94ba2b134
Sergey <non_2001@mail.ru>
2017-07-24 15:43:44 +0300

    android emulator stensil fix

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -265,10 +265,6 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
         // ...add to FrameLayout
         mFrameLayout.addView(this.mGLSurfaceView);
 
-        // Switch to supported OpenGL (ARGB888) mode on emulator
-        if (isAndroidEmulator())
-           this.mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-
         this.mGLSurfaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
         this.mGLSurfaceView.setCocos2dxEditText(edittext);
 
