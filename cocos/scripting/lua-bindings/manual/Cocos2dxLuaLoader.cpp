03635ecc4a2c64c03c024d25633f4cd2defbe109
Valera <v19930312@gmail.com>
2017-08-25 04:32:02 +0300

    Improved Cocos2dxLuaLoader.cpp (#18171)
    * Improved Cocos2dxLuaLoader.cpp
    
    * Added testcase for new LuaLoader

--- cocos/scripting/lua-bindings/manual/Cocos2dxLuaLoader.cpp
+++ cocos/scripting/lua-bindings/manual/Cocos2dxLuaLoader.cpp
@@ -41,17 +41,13 @@ extern "C"
 
         std::string filename(luaL_checkstring(L, 1));
         size_t pos = filename.rfind(BYTECODE_FILE_EXT);
-        if (pos != std::string::npos)
-        {
+        if (pos != std::string::npos && pos == filename.length() - BYTECODE_FILE_EXT.length())
             filename = filename.substr(0, pos);
-        }
         else
         {
             pos = filename.rfind(NOT_BYTECODE_FILE_EXT);
-            if (pos == filename.length() - NOT_BYTECODE_FILE_EXT.length())
-            {
+            if (pos != std::string::npos && pos == filename.length() - NOT_BYTECODE_FILE_EXT.length())
                 filename = filename.substr(0, pos);
-            }
         }
 
         pos = filename.find_first_of(".");
@@ -77,33 +73,55 @@ extern "C"
         {
             if (next == std::string::npos)
                 next = searchpath.length();
-            std::string prefix = searchpath.substr(begin, next);
+            std::string prefix = searchpath.substr(begin, next-begin);
             if (prefix[0] == '.' && prefix[1] == '/')
-            {
                 prefix = prefix.substr(2);
-            }
 
-            pos = prefix.find("?.lua");
-            chunkName = prefix.substr(0, pos) + filename + BYTECODE_FILE_EXT;
-            if (utils->isFileExist(chunkName))
+            pos = prefix.rfind(BYTECODE_FILE_EXT);
+            if (pos != std::string::npos && pos == prefix.length() - BYTECODE_FILE_EXT.length())
+            {
+                prefix = prefix.substr(0, pos);
+            }
+            else
+            {
+                pos = prefix.rfind(NOT_BYTECODE_FILE_EXT);
+                if (pos != std::string::npos && pos == prefix.length() - NOT_BYTECODE_FILE_EXT.length())
+                    prefix = prefix.substr(0, pos);
+            }
+            pos = prefix.find_first_of("?", 0);
+            while (pos != std::string::npos)
+            {
+                prefix.replace(pos, 1, filename);
+                pos = prefix.find_first_of("?", pos + filename.length() + 1);
+            }
+            chunkName = prefix + BYTECODE_FILE_EXT;
+            if (utils->isFileExist(chunkName) && !utils->isDirectoryExist(chunkName))
             {
                 chunk = utils->getDataFromFile(chunkName);
                 break;
             }
             else
             {
-                chunkName = prefix.substr(0, pos) + filename + NOT_BYTECODE_FILE_EXT;
-                if (utils->isFileExist(chunkName))
+                chunkName = prefix + NOT_BYTECODE_FILE_EXT;
+                if (utils->isFileExist(chunkName) && !utils->isDirectoryExist(chunkName))
                 {
                     chunk = utils->getDataFromFile(chunkName);
                     break;
+                }
+                else
+                {
+                    chunkName = prefix;
+                    if (utils->isFileExist(chunkName) && !utils->isDirectoryExist(chunkName))
+                    {
+                        chunk = utils->getDataFromFile(chunkName);
+                        break;
+                    }
                 }
             }
 
             begin = next + 1;
             next = searchpath.find_first_of(";", begin);
-        } while (begin < (int)searchpath.length());
-
+        } while (begin < searchpath.length());
         if (chunk.getSize() > 0)
         {
             LuaStack* stack = LuaEngine::getInstance()->getLuaStack();
