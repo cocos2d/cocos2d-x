f0180e76c9b8bb6ec99363bb8d835d1de1818008
David DeSimone <david.a.desimone@bigfishgames.com>
2017-09-12 15:50:24 -0700

    Fixing issue where Stencil and Depths buffers would not function for a render texture if you backgrounded an application, and then resumed.

--- cocos/2d/CCRenderTexture.h
+++ cocos/2d/CCRenderTexture.h
@@ -326,6 +326,7 @@ protected:
     Texture2D* _textureCopy;    // a copy of _texture
     Image*     _UITextureImage;
     Texture2D::PixelFormat _pixelFormat;
+    GLuint _depthAndStencilFormat;
     
     // code for "auto" update
     GLbitfield   _clearFlags;
@@ -362,6 +363,8 @@ protected:
     void onClearDepth();
 
     void onSaveToFile(const std::string& fileName, bool isRGBA = true);
+
+    void setupDepthAndStencil(int powW, int powH);
     
     Mat4 _oldTransMatrix, _oldProjMatrix;
     Mat4 _transformMatrix, _projectionMatrix;
