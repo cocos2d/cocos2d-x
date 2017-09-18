a73d5893b46e50fb0b2699eccc990882d7abff4b
James Chen <jianhua.chen@cocos2d-x.org>
2017-09-18 02:12:30 -0500

    [android] Fixes AppAcitivity (Sub class of Cocos2dxActivity) is re-created which causes crash if launching app first time and re-opening from icon. (#18271)
    * [android] Fixes AppAcitivity (Sub class of Cocos2dxActivity) is re-created which causes crash if launching app first time and re-opening from icon.
    
    * Update AppActivity.java
    
    * Update AppActivity.java

--- tests/js-memory-gc-tests/project/proj.android/src/org/cocos2dx/js_tests/AppActivity.java
+++ tests/js-memory-gc-tests/project/proj.android/src/org/cocos2dx/js_tests/AppActivity.java
@@ -40,6 +40,16 @@ public class AppActivity extends Cocos2dxActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Workaround in https://stackoverflow.com/questions/16283079/re-launch-of-activity-on-home-button-but-only-the-first-time/16447508
+        if (!isTaskRoot()) {
+            // Android launched another instance of the root activity into an existing task
+            //  so just quietly finish and go away, dropping the user back into the activity
+            //  at the top of the stack (ie: the last state of this task)
+            // Don't need to finish it again since it's finished in super.onCreate .
+            return;
+        }
+        // DO OTHER INITIALIZATION BELOW
+        
         app = this;
     }
 
