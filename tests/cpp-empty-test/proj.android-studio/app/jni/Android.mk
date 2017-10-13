848d0bfe2ccdc1e50a9c5198227b39132e9b9248
minggo <ming@cocos2d-x.org>
2017-10-13 10:53:48 +0800

    cpp empty test works in android studio

--- tests/cpp-empty-test/proj.android-studio/app/jni/Android.mk
+++ tests/cpp-empty-test/proj.android-studio/app/jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+path := $(LOCAL_PATH)
 
 LOCAL_MODULE := cpp_empty_test_shared
 
@@ -10,7 +11,8 @@ LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES := main.cpp \
                    ../../../Classes/AppDelegate.cpp \
-                   ../../../Classes/HelloWorldScene.cpp
+                   ../../../Classes/HelloWorldScene.cpp \
+                   ../../../Classes/DragonBonesHandler.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
                     $(LOCAL_PATH)/../../../../../extensions \
@@ -18,7 +20,11 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
                     $(LOCAL_PATH)/../../../../../cocos/editor-support
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_STATIC_LIBRARIES += creator_reader
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+
+$(call import-add-path, $(path)/../../../Classes/)
+$(call import-module, reader)
