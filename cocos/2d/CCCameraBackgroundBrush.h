d13d55c23ffe7fda0bc0a3cba4dd05760d35b63a
okamura <side.junktown@gmail.com>
2017-10-20 08:43:45 +0700

    Fix CameraBackgroundColorBrush cannot use alpha (#18281)

--- cocos/2d/CCCameraBackgroundBrush.h
+++ cocos/2d/CCCameraBackgroundBrush.h
@@ -184,6 +184,11 @@ public:
     static CameraBackgroundColorBrush* create(const Color4F& color, float depth);
     
     /**
+     * Draw background
+     */
+    virtual void drawBackground(Camera* camera) override;
+    
+    /**
      * Set clear color
      * @param color Color used to clear the color buffer
      */
