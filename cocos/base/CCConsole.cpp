d9b8619d5ffabdb158c0d14097f01e968534fafc
halx99 <halx99@live.com>
2017-11-01 10:50:18 +0800

    Fix cocos2d::log va_list re-use bug

--- cocos/base/CCConsole.cpp
+++ cocos/base/CCConsole.cpp
@@ -123,98 +123,88 @@ namespace {
     {
     }
 #endif
-    
-    void _log(const char *format, va_list args)
-    {
-        int bufferSize = MAX_LOG_LENGTH;
-        char* buf = nullptr;
-        int nret = 0;
-        do
-        {
-            buf = new (std::nothrow) char[bufferSize];
-            if (buf == nullptr)
-                return;
-            /*
-	    pitfall: The behavior of vsnprintf between VS2013 and VS2015/2017 is different
-              VS2013 or Unix-Like System will return -1 when buffer not enough, but VS2015/2017 will return the actural needed length for buffer at this station
-              The _vsnprintf behavior is compatible API which always return -1 when buffer isn't enough at VS2013/2015/2017
-              Yes, The vsnprintf is more efficient implemented by MSVC 19.0 or later, AND it's also standard-compliant, see reference: http://www.cplusplus.com/reference/cstdio/vsnprintf/
-	    */
-            nret = vsnprintf(buf, bufferSize - 3, format, args);
-            if (nret >= 0)
-            { // VS2015/2017
-                if (nret <= bufferSize - 3)
-                {// success, so don't need to realloc
-                    break;
-                }
-                else
-                {
-                    bufferSize = nret + 3;
-                    delete[] buf;
-                }
-            }
-            else // < 0
-            {	// VS2013 or Unix-like System(GCC)
-	        bufferSize *= 2;
-	        delete[] buf;
-            }
-        } while (true);
-        buf[nret] = '\n';
-        buf[++nret] = '\0';
-        
-#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-        __android_log_print(ANDROID_LOG_DEBUG, "cocos2d-x debug info", "%s", buf);
-        
-#elif CC_TARGET_PLATFORM ==  CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_WINRT
-        
-        int pos = 0;
-        int len = nret;
-        char tempBuf[MAX_LOG_LENGTH + 1] = { 0 };
-        WCHAR wszBuf[MAX_LOG_LENGTH + 1] = { 0 };
-        
-        do
-        {
-            std::copy(buf + pos, buf + pos + MAX_LOG_LENGTH, tempBuf);
-            
-            tempBuf[MAX_LOG_LENGTH] = 0;
-            
-            MultiByteToWideChar(CP_UTF8, 0, tempBuf, -1, wszBuf, sizeof(wszBuf));
-            OutputDebugStringW(wszBuf);
-            WideCharToMultiByte(CP_ACP, 0, wszBuf, -1, tempBuf, sizeof(tempBuf), nullptr, FALSE);
-            printf("%s", tempBuf);
-            
-            pos += MAX_LOG_LENGTH;
-            
-        } while (pos < len);
-        SendLogToWindow(buf);
-        fflush(stdout);
-#else
-        // Linux, Mac, iOS, etc
-        fprintf(stdout, "%s", buf);
-        fflush(stdout);
-#endif
-        
-        Director::getInstance()->getConsole()->log(buf);
-        delete [] buf;
-    }
-}
-
-// FIXME: Deprecated
-void CCLog(const char * format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    _log(format, args);
-    va_end(args);
 }
 
 void log(const char * format, ...)
 {
+    int bufferSize = MAX_LOG_LENGTH;
+    char* buf = nullptr;
+    int nret = 0;
     va_list args;
-    va_start(args, format);
-    _log(format, args);
-    va_end(args);
+    do
+    {
+        buf = new (std::nothrow) char[bufferSize];
+        if (buf == nullptr)
+            return;
+        /*
+        pitfall: The behavior of vsnprintf between VS2013 and VS2015/2017 is different
+        VS2013 or Unix-Like System will return -1 when buffer not enough, but VS2015/2017 will return the actural needed length for buffer at this station
+        The _vsnprintf behavior is compatible API which always return -1 when buffer isn't enough at VS2013/2015/2017
+        Yes, The vsnprintf is more efficient implemented by MSVC 19.0 or later, AND it's also standard-compliant, see reference: http://www.cplusplus.com/reference/cstdio/vsnprintf/
+        */
+        va_start(args, format);
+        nret = vsnprintf(buf, bufferSize - 3, format, args);
+        va_end(args);
+
+        if (nret >= 0)
+        { // VS2015/2017
+            if (nret <= bufferSize - 3)
+            {// success, so don't need to realloc
+                break;
+            }
+            else
+            {
+                bufferSize = nret + 3;
+                delete[] buf;
+            }
+        }
+        else // < 0
+        {	// VS2013 or Unix-like System(GCC)
+            bufferSize *= 2;
+            delete[] buf;
+        }
+    } while (true);
+    buf[nret] = '\n';
+    buf[++nret] = '\0';
+
+#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+    __android_log_print(ANDROID_LOG_DEBUG, "cocos2d-x debug info", "%s", buf);
+
+#elif CC_TARGET_PLATFORM ==  CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_WINRT
+
+    int pos = 0;
+    int len = nret;
+    char tempBuf[MAX_LOG_LENGTH + 1] = { 0 };
+    WCHAR wszBuf[MAX_LOG_LENGTH + 1] = { 0 };
+
+    do
+    {
+        std::copy(buf + pos, buf + pos + MAX_LOG_LENGTH, tempBuf);
+
+        tempBuf[MAX_LOG_LENGTH] = 0;
+
+        MultiByteToWideChar(CP_UTF8, 0, tempBuf, -1, wszBuf, sizeof(wszBuf));
+        OutputDebugStringW(wszBuf);
+        WideCharToMultiByte(CP_ACP, 0, wszBuf, -1, tempBuf, sizeof(tempBuf), nullptr, FALSE);
+        printf("%s", tempBuf);
+
+        pos += MAX_LOG_LENGTH;
+
+    } while (pos < len);
+    SendLogToWindow(buf);
+    fflush(stdout);
+#else
+    // Linux, Mac, iOS, etc
+    fprintf(stdout, "%s", buf);
+    fflush(stdout);
+#endif
+
+    Director::getInstance()->getConsole()->log(buf);
+    delete[] buf;
 }
+
+// FIXME: Deprecated
+// void CCLog(const char * format, ...);
 
 //
 //  Utility code
