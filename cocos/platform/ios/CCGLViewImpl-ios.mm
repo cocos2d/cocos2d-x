a1aff7e4c115adf496fba5bb1eaf653a32c344b6
James Chen <jianhua.chen@cocos2d-x.org>
2017-11-22 15:42:31 +0800

    fixed #18474: Provides an interface for getting SafeArea

--- cocos/platform/ios/CCGLViewImpl-ios.mm
+++ cocos/platform/ios/CCGLViewImpl-ios.mm
@@ -32,6 +32,7 @@
 #include "platform/ios/CCDirectorCaller-ios.h"
 #include "platform/ios/CCGLViewImpl-ios.h"
 #include "base/CCTouch.h"
+#include "base/CCDirector.h"
 
 NS_CC_BEGIN
 
@@ -225,6 +226,46 @@ void GLViewImpl::setIMEKeyboardState(bool open)
     {
         [eaglview resignFirstResponder];
     }
+}
+
+Rect GLViewImpl::getSafeAreaRect() const
+{
+    CCEAGLView *eaglview = (CCEAGLView*) _eaglview;
+
+    if (@available(iOS 11.0, *)) {
+        UIEdgeInsets safeAreaInsets = eaglview.safeAreaInsets;
+
+        // Multiply contentScaleFactor since safeAreaInsets return points.
+        safeAreaInsets.left *= eaglview.contentScaleFactor;
+        safeAreaInsets.right *= eaglview.contentScaleFactor;
+        safeAreaInsets.top *= eaglview.contentScaleFactor;
+        safeAreaInsets.bottom *= eaglview.contentScaleFactor;
+
+        // Get leftBottom and rightTop point in UI coordinates
+        Vec2 leftBottom = Vec2(safeAreaInsets.left, _screenSize.height - safeAreaInsets.bottom);
+        Vec2 rightTop = Vec2(_screenSize.width - safeAreaInsets.right, safeAreaInsets.top);
+
+        // Convert a point from UI coordinates to which in design resolution coordinate.
+        leftBottom.x = (leftBottom.x - _viewPortRect.origin.x) / _scaleX,
+        leftBottom.y = (leftBottom.y - _viewPortRect.origin.y) / _scaleY;
+        rightTop.x = (rightTop.x - _viewPortRect.origin.x) / _scaleX,
+        rightTop.y = (rightTop.y - _viewPortRect.origin.y) / _scaleY;
+
+        // Adjust points to make them inside design resolution
+        leftBottom.x = MAX(leftBottom.x, 0);
+        leftBottom.y = MIN(leftBottom.y, _designResolutionSize.height);
+        rightTop.x = MIN(rightTop.x, _designResolutionSize.width);
+        rightTop.y = MAX(rightTop.y, 0);
+
+        // Convert to GL coordinates
+        leftBottom = Director::getInstance()->convertToGL(leftBottom);
+        rightTop = Director::getInstance()->convertToGL(rightTop);
+
+        return Rect(leftBottom.x, leftBottom.y, rightTop.x - leftBottom.x, rightTop.y - leftBottom.y);
+    }
+
+    // If running on iOS devices lower than 11.0, return visiable rect instead.
+    return GLView::getSafeAreaRect();
 }
 
 NS_CC_END
