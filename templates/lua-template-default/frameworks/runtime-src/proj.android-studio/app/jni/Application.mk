4fb4db8a586e4321dfa5f4839d0d2961a405c260
minggo <ming@cocos2d-x.org>
2017-12-01 14:12:13 +0800

    build armv7

--- templates/lua-template-default/frameworks/runtime-src/proj.android-studio/app/jni/Application.mk
+++ templates/lua-template-default/frameworks/runtime-src/proj.android-studio/app/jni/Application.mk
@@ -3,7 +3,7 @@ APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char -Wno-extern-c-compat
 APP_LDFLAGS := -latomic
 
-APP_ABI := armeabi
+APP_ABI := armeabi-v7a
 APP_SHORT_COMMANDS := true
 
 
