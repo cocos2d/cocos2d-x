5e44de383f798954b8df2d786f97fc42be3f33be
James Chen <jianhua.chen@cocos2d-x.org>
2017-12-05 18:01:57 +0800

    Updates appveyor script.

--- tools/appveyor-scripts/before-build.ps1
+++ tools/appveyor-scripts/before-build.ps1
@@ -36,19 +36,20 @@ function Download-Deps
 
 function Download-NDK
 {
-	$url = "http://dl.google.com/android/ndk/android-ndk-r10d-windows-x86.exe"
-    $output = "$env:APPVEYOR_BUILD_FOLDER/../android-ndk-r10d-windows-x86.exe"
+	$url = "https://dl.google.com/android/repository/android-ndk-r16-windows-x86.zip"
+    $output = "$env:APPVEYOR_BUILD_FOLDER/../android-ndk-r16-windows-x86.zip"
     Write-Host "downloading $url"
 	Download-Url $url $output
 	Write-Host "finish downloading $url"
 
 	Write-Host "installing NDK"
-	Push-Location "$env:APPVEYOR_BUILD_FOLDER/../"
-	& $output > $null
-	Pop-Location
+	$zipfile = $output
+    $output = $env:APPVEYOR_BUILD_FOLDER/../
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $output)
 	Write-Host "finish installing NDK"
     
-    $env:NDK_ROOT = "$env:APPVEYOR_BUILD_FOLDER/../android-ndk-r10d"
+    $env:NDK_ROOT = "$env:APPVEYOR_BUILD_FOLDER/../android-ndk-r16"
     Write-Host "set environment NDK_ROOT to $env:NDK_ROOT"
 }
 
