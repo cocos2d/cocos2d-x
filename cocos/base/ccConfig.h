ea62e4cc84cbc698b7555cf1748528737b32cf0c
James Chen <jianhua.chen@cocos2d-x.org>
2017-12-06 14:44:31 +0800

    reverts.

--- cocos/base/ccConfig.h
+++ cocos/base/ccConfig.h
@@ -24,7 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-xxx
+
 #ifndef __CCCONFIG_H__
 #define __CCCONFIG_H__
 
