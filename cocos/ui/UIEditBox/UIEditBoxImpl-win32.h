20b3867f2d8bff5e2533cfb9cd6dbcca48d5e35f
Jeff Wang <summer_insects@163.com>
2017-12-06 23:47:12 +0800

    Fix a fixme

--- cocos/ui/UIEditBox/UIEditBoxImpl-win32.h
+++ cocos/ui/UIEditBox/UIEditBoxImpl-win32.h
@@ -75,8 +75,6 @@ namespace ui {
         static LRESULT CALLBACK hookGLFWWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
         HWND hwndEdit;
-        //FIXME: fontSize should be in parent class
-        int _fontSize;
         bool _changedTextManually;
         bool _hasFocus;
         static WNDPROC s_prevCocosWndProc;
