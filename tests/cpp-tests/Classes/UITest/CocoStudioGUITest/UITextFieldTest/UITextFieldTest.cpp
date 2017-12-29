ea228228753188198f2b5850dab19e7a309a0f59
Ruud Arentsen <ruud.arentsen@gmail.com>
2017-12-29 04:24:02 +0100

    Added support for BMFont in TextField (#18587)
    * Added support for BMFont in TextField
    
    * Added cursor support for BMFont in TextFieldTTF
    Made BMFont detection case insensitive and a bit more fool proof
    
    * Added TextField::createWithBMFont, so that the font size is determined internally.
    
    * Revert "Added TextField::createWithBMFont, so that the font size is determined internally."
    
    This reverts commit 46b4e72ba44979b012ffa52bb78ea0a8493f7869.
    
    * fix BMFont Cursor scale
    
    * remove error add subproject

--- tests/cpp-tests/Classes/UITest/CocoStudioGUITest/UITextFieldTest/UITextFieldTest.cpp
+++ tests/cpp-tests/Classes/UITest/CocoStudioGUITest/UITextFieldTest/UITextFieldTest.cpp
@@ -10,6 +10,7 @@ UITextFieldTests::UITextFieldTests()
     ADD_TEST_CASE(UITextFieldTest_Password);
     ADD_TEST_CASE(UITextFieldTest_LineWrap);
     ADD_TEST_CASE(UITextFieldTest_TrueTypeFont);
+    ADD_TEST_CASE(UITextFieldTest_BMFont);
     ADD_TEST_CASE(UITextFieldTest_PlaceHolderColor);
 }
 
@@ -390,6 +391,82 @@ void UITextFieldTest_TrueTypeFont::textFieldEvent(Ref *pSender, TextField::Event
             Size screenSize = Director::getInstance()->getWinSize();
             textField->runAction(MoveTo::create(0.225f,
                                                   Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f + textField->getContentSize().height / 2.0f)));
+            _displayValueLabel->setString(StringUtils::format("attach with IME"));
+        }
+            break;
+            
+        case TextField::EventType::DETACH_WITH_IME:
+        {
+            TextField* textField = dynamic_cast<TextField*>(pSender);
+            Size screenSize = Director::getInstance()->getWinSize();
+            textField->runAction(MoveTo::create(0.175f, Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f)));
+            _displayValueLabel->setString(StringUtils::format("detach with IME"));
+        }
+            break;
+            
+        case TextField::EventType::INSERT_TEXT:
+            _displayValueLabel->setString(StringUtils::format("insert words"));
+            break;
+            
+        case TextField::EventType::DELETE_BACKWARD:
+            _displayValueLabel->setString(StringUtils::format("delete word"));
+            break;
+            
+        default:
+            break;
+    }
+}
+
+// UITextFieldTest_BMFont
+UITextFieldTest_BMFont::UITextFieldTest_BMFont()
+: _displayValueLabel(nullptr)
+{
+    
+}
+
+UITextFieldTest_BMFont::~UITextFieldTest_BMFont()
+{
+}
+
+bool UITextFieldTest_BMFont::init()
+{
+    if (UIScene::init())
+    {
+        Size widgetSize = _widget->getContentSize();
+        
+        // Add a label in which the textfield events will be displayed
+        _displayValueLabel = Text::create("BMFont Test - No Event","fonts/Marker Felt.ttf",32);
+        _displayValueLabel->setAnchorPoint(Vec2(0.5f, -1.0f));
+        _displayValueLabel->setPosition(Vec2(widgetSize.width / 2.0f, widgetSize.height / 2.0f + _displayValueLabel->getContentSize().height * 1.5f));
+        _uiLayer->addChild(_displayValueLabel);
+        
+        // Add the alert
+        Text* alert = Text::create("TextField","fonts/Marker Felt.ttf",30);
+        alert->setPosition(Vec2(widgetSize.width / 2.0f, widgetSize.height / 2.0f - alert->getContentSize().height * 3.075f));
+        _uiLayer->addChild(alert);
+        
+        // Create the textfield
+        TextField* textField = TextField::create("BMFont Text","fonts/bitmapFontTest3.fnt",30);
+        textField->setCursorEnabled(true);
+        textField->setPosition(Vec2(widgetSize.width / 2.0f, widgetSize.height / 2.0f));
+        textField->addEventListener(CC_CALLBACK_2(UITextFieldTest_BMFont::textFieldEvent, this));
+        _uiLayer->addChild(textField);
+        
+        return true;
+    }
+    return false;
+}
+
+void UITextFieldTest_BMFont::textFieldEvent(Ref *pSender, TextField::EventType type)
+{
+    switch (type)
+    {
+        case TextField::EventType::ATTACH_WITH_IME:
+        {
+            TextField* textField = dynamic_cast<TextField*>(pSender);
+            Size screenSize = Director::getInstance()->getWinSize();
+            textField->runAction(MoveTo::create(0.225f,
+                                                Vec2(screenSize.width / 2.0f, screenSize.height / 2.0f + textField->getContentSize().height / 2.0f)));
             _displayValueLabel->setString(StringUtils::format("attach with IME"));
         }
             break;
