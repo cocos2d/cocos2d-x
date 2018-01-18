da40daa147dc631f7108be2b3db6f8696131420b
Sergey Perepelitsa <non_2001@mail.ru>
2018-01-18 22:54:37 +0300

    reverted default attributes

--- cocos/platform/CCGLView.cpp
+++ cocos/platform/CCGLView.cpp
@@ -91,7 +91,7 @@ namespace {
 }
 
 //default context attributions are set as follows
-GLContextAttrs GLView::_glContextAttrs = {5, 6, 5, 0, 16, 0, 0};
+GLContextAttrs GLView::_glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
 
 void GLView::setGLContextAttrs(GLContextAttrs& glContextAttrs)
 {
