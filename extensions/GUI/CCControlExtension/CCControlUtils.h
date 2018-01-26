139d6658ff2a55ba384bd13f7150129ce4997ca7
drelaptop <li_cheng_zhe@163.com>
2018-01-26 17:40:13 +0800

    another situation, add Copyright before Permission is hereby granted

--- extensions/GUI/CCControlExtension/CCControlUtils.h
+++ extensions/GUI/CCControlExtension/CCControlUtils.h
@@ -9,6 +9,8 @@
  * Modified by Yannick Loriot.
  * http://yannickloriot.com
  * 
+ * Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
