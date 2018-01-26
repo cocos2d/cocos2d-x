ee82591d32353c7ce2e146fe51ef447433b47571
drelaptop <li_cheng_zhe@163.com>
2018-01-26 17:36:34 +0800

    another situation, add Copyright before  "Permission is hereby granted"

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxEngineDataManager.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxEngineDataManager.java
@@ -3,6 +3,8 @@
  * <p/>
  * http://www.cocos2d-x.org
  * <p/>
+ * Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
