f21b81f917e4b05e5dd2d285e838ae0087b3a031
drelaptop <li_cheng_zhe@163.com>
2018-03-02 11:20:55 +0800

    loosen pick js/lua engine lib condition

--- cmake/Modules/CocosPickLibs.cmake
+++ cmake/Modules/CocosPickLibs.cmake
@@ -282,21 +282,17 @@ foreach(_lib ${all_prebuilt_libs})
   endforeach()
 endforeach()
 
-# use engine prebuilt libs
+# use engine prebuilt libs, try to pick all engine libs
 if(USE_COCOS_PREBUILT)
   # cocos2d/jscocos2d/luacocos2d
   set(_cocos2d_prefix COCOS2D)
   cocos_find_prebuilt(cocos2d COCOS2D_LIBRARIES)
 
-  if(USE_JSCOCOS2D OR (CMAKE_PROJECT_NAME STREQUAL Cocos2d-x))
-    set(_jscocos2d_prefix JSCOCOS2D)
-    cocos_find_prebuilt(jscocos2d JSCOCOS2D_LIBRARIES)
-  endif()
+  set(_jscocos2d_prefix JSCOCOS2D)
+  cocos_find_prebuilt(jscocos2d JSCOCOS2D_LIBRARIES)
 
-  if(USE_LUACOCOS2D OR (CMAKE_PROJECT_NAME STREQUAL Cocos2d-x))
-    set(_luacocos2d_prefix LUACOCOS2D)
-    cocos_find_prebuilt(luacocos2d LUACOCOS2D_LIBRARIES)
-  endif()
+  set(_luacocos2d_prefix LUACOCOS2D)
+  cocos_find_prebuilt(luacocos2d LUACOCOS2D_LIBRARIES)
 
   if(ANDROID)
     set(_cocos2d_android_prefix ANDROID_COCOS2D)
