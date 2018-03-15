4ad528b440cbffb63d972551b544c7b85c2a75cc
Jordi Vilalta Prat <jordi.vilalta@socialpoint.es>
2018-03-15 11:46:03 +0100

    Properly clip the top of labels with BMFonts

--- cocos/2d/CCLabel.cpp
+++ cocos/2d/CCLabel.cpp
@@ -921,8 +921,9 @@ bool Label::updateQuads()
                 if (py > _tailoredTopY)
                 {
                     auto clipTop = py - _tailoredTopY;
-                    _reusedRect.origin.y += clipTop;
-                    _reusedRect.size.height -= clipTop;
+                    const float letterClipTop = clipTop / _bmfontScale;
+                    _reusedRect.origin.y += letterClipTop;
+                    _reusedRect.size.height -= letterClipTop;
                     py -= clipTop;
                 }
                 if (py - letterDef.height * _bmfontScale < _tailoredBottomY)
