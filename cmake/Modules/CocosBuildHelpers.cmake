e74e07bde43db7c4379a0b464f6ff4562535be97
leda <li_cheng_zhe@163.com>
2018-05-17 09:07:17 +0800

    refactor cmake copy res, fix #18813 (#18816)
    * refactor cmake cocos copy res function
    
    * refactor cmake dll copy from add_custom_command to configure_file

--- cmake/Modules/CocosBuildHelpers.cmake
+++ cmake/Modules/CocosBuildHelpers.cmake
@@ -18,27 +18,25 @@ function(cocos_find_prebuilt_lib_by_name lib_name lib_out)
     unset(found_lib CACHE)
 endfunction()
 
-# copy resource `FILES` and `FOLDERS` to `COPY_TO` folder before `cocos_target` build
-function(cocos_copy_target_res cocos_target)
+# copy resource `FILES` and `FOLDERS` to `COPY_TO` folder
+function(cocos_copy_res)
     set(oneValueArgs COPY_TO)
     set(multiValueArgs FILES FOLDERS)
     cmake_parse_arguments(opt "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
     # copy files
     foreach(cc_file ${opt_FILES})
         get_filename_component(file_name ${cc_file} NAME)
-        add_custom_command(TARGET ${cocos_target} PRE_BUILD
-                           COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                           ${cc_file}
-                           "${opt_COPY_TO}/${file_name}"
-                           )
+        configure_file(${cc_file} "${opt_COPY_TO}/${file_name}" COPYONLY)
     endforeach()
-    # copy folders
+    # copy folders files
     foreach(cc_folder ${opt_FOLDERS})
-        add_custom_command(TARGET ${cocos_target} PRE_BUILD
-                           COMMAND ${CMAKE_COMMAND} -E copy_directory
-                           ${cc_folder}
-                           ${opt_COPY_TO}
-                           )
+        file(GLOB_RECURSE folder_files "${cc_folder}/*")
+        get_filename_component(folder_abs_path ${cc_folder} ABSOLUTE)
+        foreach(res_file ${folder_files})
+            get_filename_component(res_file_abs_path ${res_file} ABSOLUTE)
+            file(RELATIVE_PATH res_file_relat_path ${folder_abs_path} ${res_file_abs_path})
+            configure_file(${res_file} "${opt_COPY_TO}/${res_file_relat_path}" COPYONLY)
+        endforeach()
     endforeach()
 endfunction()
 
@@ -107,13 +105,10 @@ function(cocos_copy_target_dll cocos_target)
     # remove repeat items
     list(REMOVE_DUPLICATES all_depend_dlls)
     message(STATUS "prepare to copy external dlls for ${cocos_target}:${all_depend_dlls}")
-    foreach(single_target_dll ${all_depend_dlls})
-        add_custom_command(TARGET ${cocos_target} PRE_BUILD
-                           COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                           ${single_target_dll}
-                           ${opt_COPY_TO}
-                           )
-    endforeach(single_target_dll)
+    foreach(cc_dll_file ${all_depend_dlls})
+        get_filename_component(cc_dll_name ${cc_dll_file} NAME)
+        configure_file(${cc_dll_file} "${opt_COPY_TO}/${cc_dll_name}" COPYONLY)
+    endforeach()
 endfunction()
 
 # find dlls in a dir which `LIB_ABS_PATH` located, and save the result in `dlls_out`
