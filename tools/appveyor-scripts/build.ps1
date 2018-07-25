711225b0a1c7df55383e4a2ef2ca538fe62a331a
CHP <crazyhappygame@gmail.com>
2018-07-25 04:08:53 +0200

    switch one win32 build to cmake on appveyor CI (#18956)
    * Update gradle and android gradle plugin
    
    * Switch win32 build to cmake
    
    * Build Release
    
    * Add --config Release
    
    * Rename to win32_bin
    
    * Remove check for mkdir
    
    * Remove comment
    
    * windows32_cocos_new_test cmake cpp_test sln build

--- tools/appveyor-scripts/build.ps1
+++ tools/appveyor-scripts/build.ps1
@@ -54,10 +54,17 @@ If ($env:build_type -eq "android_cpp_tests") {
     & $python -u tools\cocos2d-console\bin\cocos.py --agreement n new -l cpp -p my.pack.qqqq cocos_new_test
     if ($lastexitcode -ne 0) {throw}
 
-    & msbuild $env:APPVEYOR_BUILD_FOLDER\cocos_new_test\proj.win32\cocos_new_test.sln /t:Build /p:Platform="Win32" /p:Configuration="Release" /m /consoleloggerparameters:"PerformanceSummary;NoSummary"
+    & mkdir $env:APPVEYOR_BUILD_FOLDER\cocos_new_test\b
     if ($lastexitcode -ne 0) {throw}
 
-    & 7z a release_win32.7z $env:APPVEYOR_BUILD_FOLDER\cocos_new_test\proj.win32\Release.win32\
+    Push-Location $env:APPVEYOR_BUILD_FOLDER\cocos_new_test\b
+    & cmake -DCMAKE_BUILD_TYPE=Release ..
+    if ($lastexitcode -ne 0) {throw}
+
+    & cmake --build . --config Release
+    if ($lastexitcode -ne 0) {throw}
+
+    & 7z a release_win32.7z $env:APPVEYOR_BUILD_FOLDER\cocos_new_test\b\bin\
     if ($lastexitcode -ne 0) {throw}
 
     Push-AppveyorArtifact release_win32.7z
