7d840ac6e166db2ccc00d3624af80e0d11674372
ggggamer <even.newbie@gmail.com>
2018-09-18 09:14:27 +0800

    CameraBackgroundDepthBrush should handle opengl recreate event. (#19037)

--- cocos/2d/CCCameraBackgroundBrush.h
+++ cocos/2d/CCCameraBackgroundBrush.h
@@ -152,7 +152,13 @@ CC_CONSTRUCTOR_ACCESS:
     virtual ~CameraBackgroundDepthBrush();
 
     virtual bool init() override;
-    
+
+protected:
+#if CC_ENABLE_CACHE_TEXTURE_DATA
+    EventListenerCustom* _backToForegroundListener;
+#endif
+    void initBuffer();
+
 protected:
     float _depth;
     
@@ -194,7 +200,7 @@ public:
      * @param color Color used to clear the color buffer
      */
     void setColor(const Color4F& color);
-    
+
 CC_CONSTRUCTOR_ACCESS:
     CameraBackgroundColorBrush();
     virtual ~CameraBackgroundColorBrush();
