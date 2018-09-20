dc189f19797afd8ff3ad29917dca9125c2518d74
Patrice.J <397136899@qq.com>
2018-09-20 17:51:13 +0800

    simplify code

--- cocos/platform/linux/CCFileUtils-linux.cpp
+++ cocos/platform/linux/CCFileUtils-linux.cpp
@@ -124,7 +124,7 @@ bool FileUtilsLinux::isFileExistInternal(const std::string& strFilePath) const
     }
 
     struct stat sts;
-    return (stat(strPath.c_str(), &sts) != -1) ? S_ISREG(sts.st_mode) : false;
+    return (stat(strPath.c_str(), &sts) == 0) && S_ISREG(sts.st_mode);
 }
 
 NS_CC_END
