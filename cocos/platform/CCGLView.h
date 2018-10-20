3ec0f4bea19b8fc718cb8d48125f92007765afe3
Filipe Lemos <filipe.lemos@pyrosphere.net>
2018-10-20 19:10:09 +0100

    Add `GLView::setCursor` for desktop platforms

--- cocos/platform/CCGLView.h
+++ cocos/platform/CCGLView.h
@@ -196,6 +196,19 @@ public:
     virtual float getFrameZoomFactor() const { return 1.0; }
     
     /**
+     * Sets the cursor for the window with custom image.
+     *
+     * @param filename A path to image file, e.g., "cursors/custom.png".
+     * @param hotspot Cursor hotspot, as a anchor point, default is top left (0, 1)
+     */
+    virtual void setCursor(const std::string& filename, Vec2 hotspot = Vec2::ANCHOR_TOP_LEFT) {}
+
+    /**
+     * Sets the cursor for the window back to default.
+     */
+    virtual void setDefaultCursor() {}
+
+    /**
      * Hide or Show the mouse cursor if there is one.
      *
      * @param isVisible Hide or Show the mouse cursor if there is one.
@@ -363,7 +376,7 @@ public:
 
     /** Set window icon (implemented for windows and linux).
      *
-     * @param filename A path to image file, e.g., "icons/cusom.png". 
+     * @param filename A path to image file, e.g., "icons/custom.png".
      */
     virtual void setIcon(const std::string& filename) const {};
 
