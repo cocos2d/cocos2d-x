41c26fe6245ec445e2f6aedd1ea6de9ae9e06cdb
leda <li_cheng_zhe@163.com>
2018-11-16 13:41:12 +0800

    a workaround to fix black screen on macOS 10.14 (#19090)

--- cocos/platform/CCGLView.h
+++ cocos/platform/CCGLView.h
@@ -413,6 +413,7 @@ public:
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
     virtual id getCocoaWindow() = 0;
+    virtual id getNSGLContext() = 0; // stevetranby: added
 #endif /* (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) */
 
     /**
