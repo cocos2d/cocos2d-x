b6e0fc5477d422bc219ed26401618e4bb6888454
coulsonwang <coulsonwang@126.com>
2018-11-23 10:24:15 +0800

    [Feature] modify testFile

--- tests/cpp-empty-test/Classes/HelloWorldScene.cpp
+++ tests/cpp-empty-test/Classes/HelloWorldScene.cpp
@@ -50,33 +50,33 @@ bool HelloWorld::init()
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        CC_CALLBACK_1(HelloWorld::menuCloseCallback,this));
-
-    closeItem->setPosition(origin + Vec2(visibleSize) - Vec2(closeItem->getContentSize() / 2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, nullptr);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/arial.ttf", TITLE_FONT_SIZE);
-
-    // position the label on the center of the screen
-    label->setPosition(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height);
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
+//    // add a "close" icon to exit the progress. it's an autorelease object
+//    auto closeItem = MenuItemImage::create(
+//                                        "CloseNormal.png",
+//                                        "CloseSelected.png",
+//                                        CC_CALLBACK_1(HelloWorld::menuCloseCallback,this));
+//
+//    closeItem->setPosition(origin + Vec2(visibleSize) - Vec2(closeItem->getContentSize() / 2));
+//
+//    // create menu, it's an autorelease object
+//    auto menu = Menu::create(closeItem, nullptr);
+//    menu->setPosition(Vec2::ZERO);
+//    this->addChild(menu, 1);
+//
+//    /////////////////////////////
+//    // 3. add your codes below...
+//
+//    // add a label shows "Hello World"
+//    // create and initialize a label
+//
+//    auto label = Label::createWithTTF("Hello World", "fonts/arial.ttf", TITLE_FONT_SIZE);
+//
+//    // position the label on the center of the screen
+//    label->setPosition(origin.x + visibleSize.width/2,
+//                            origin.y + visibleSize.height - label->getContentSize().height);
+//
+//    // add the label as a child to this layer
+//    this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::backendCreate("HelloWorld.png");
@@ -87,12 +87,12 @@ bool HelloWorld::init()
     // add the sprite as a child to this layer
     this->addChild(sprite);
 
-    auto drawNode = DrawNode::create();
-    drawNode->setPosition(Vec2(0, 0));
-    addChild(drawNode);
-
-    Rect safeArea = Director::getInstance()->getSafeAreaRect();
-    drawNode->drawRect(safeArea.origin, safeArea.origin + safeArea.size, Color4F::BLUE);
+//    auto drawNode = DrawNode::create();
+//    drawNode->setPosition(Vec2(0, 0));
+//    addChild(drawNode);
+//
+//    Rect safeArea = Director::getInstance()->getSafeAreaRect();
+//    drawNode->drawRect(safeArea.origin, safeArea.origin + safeArea.size, Color4F::BLUE);
 
     return true;
 }
