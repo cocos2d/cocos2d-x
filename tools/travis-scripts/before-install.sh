bcb1f48ca7b2a5b447616ae1fb8213edcc8c0e2c
CHP <crazyhappygame@gmail.com>
2018-11-27 12:09:03 +0100

    Switch travis CI to xenial (#19207)
    * Switch travis CI to xenial
    
    * Remove language: android
    
    * Set language: cpp
    
    * Fix java problem
    
    * Update sdkmanager
    
    * Fix sdkmanger
    
    * next sdkmanager fix
    
    * Remove xenial from android
    
    * revert to sdk-tools-{system}-3859397
    
    * Remove linux cmake install
    
    * Update before-install.sh
    
    * Update .travis.yml

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -28,33 +28,6 @@ function install_linux_environment()
     pushd $HOME/bin
 
     echo "GCC version: `gcc --version`"
-    # install new version cmake
-    CMAKE_VERSION="3.7.2"
-    CMAKE_DOWNLOAD_URL="https://cmake.org/files/v3.7/cmake-${CMAKE_VERSION}.tar.gz"
-    echo "Download ${CMAKE_DOWNLOAD_URL}"
-    ${CURL} -O ${CMAKE_DOWNLOAD_URL}
-    tar -zxf "cmake-${CMAKE_VERSION}.tar.gz"
-    cd "cmake-${CMAKE_VERSION}"
-    ./configure > /dev/null
-    make -j2 > /dev/null
-    sudo make install > /dev/null
-    echo "CMake Version: `cmake --version`"
-    cd ..
-
-    # install new version binutils
-    BINUTILS_VERSION="2.27"
-    BINUTILS_URL="http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.gz"
-    echo "Download ${BINUTILS_URL}"
-    ${CURL} -O ${BINUTILS_URL}
-    tar -zxf "binutils-${BINUTILS_VERSION}.tar.gz"
-    cd "binutils-${BINUTILS_VERSION}"
-    ./configure > /dev/null
-    make -j2 > /dev/null
-    sudo make install > /dev/null
-    echo "ld Version: `ld --version`"
-    echo "which ld: `which ld`"
-    sudo rm /usr/bin/ld
-    popd
     echo "Installing linux dependence packages ..."
     echo -e "y" | bash $COCOS2DX_ROOT/build/install-deps-linux.sh
     echo "Installing linux dependence packages finished!"
