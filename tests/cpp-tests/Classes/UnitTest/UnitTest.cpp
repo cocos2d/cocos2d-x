800e35368b291392c7d183cd96ea56540e1a6c7d
Arnold <397136899@qq.com>
2018-12-02 00:47:39 +0800

    Revert "[cpp test]Unit Test/9 neon release mode crash"
    This reverts commit 9898d9592b33e0ca88c9b37e1b63120f5742d89d.

--- tests/cpp-tests/Classes/UnitTest/UnitTest.cpp
+++ tests/cpp-tests/Classes/UnitTest/UnitTest.cpp
@@ -1449,7 +1449,7 @@ static void __checkMathUtilResult(const char* description, const float* a1, cons
     }
 }
 
-void MathUtilTest::onEnter() __attribute__((optnone))
+void MathUtilTest::onEnter()
 {
     UnitTestDemo::onEnter();
     
