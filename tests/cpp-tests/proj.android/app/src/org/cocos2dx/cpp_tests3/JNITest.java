80b7d46c790ef6078bfe7f79b2d076e1baa2a16e
patricejiang <jiangzhan@chukong-inc.com>
2018-12-06 15:55:09 +0800

    save

--- tests/cpp-tests/proj.android/app/src/org/cocos2dx/cpp_tests3/JNITest.java
+++ tests/cpp-tests/proj.android/app/src/org/cocos2dx/cpp_tests3/JNITest.java
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package org.cocos2dx.cpp_tests;
+package org.cocos2dx.cpp_tests3;
 
 import android.util.Log;
 import java.lang.StringBuilder;
