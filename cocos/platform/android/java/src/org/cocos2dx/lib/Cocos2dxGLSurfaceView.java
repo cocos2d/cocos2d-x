d9979ea007373819c82608a4da43103ec0254248
drelaptop <li_cheng_zhe@163.com>
2018-12-19 16:41:23 +0800

    standardize pause & resume logic on GLSurfaceView

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -199,7 +199,7 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
             }
         });
         this.setRenderMode(RENDERMODE_WHEN_DIRTY);
-        //super.onPause();
+        super.onPause();
     }
 
     @Override
