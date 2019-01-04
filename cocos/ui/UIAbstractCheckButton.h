29412000852815511925c1b854593a2864789ebb
gestern <mapc4@yahoo.com>
2019-01-04 09:08:48 +0200

    Ability to get low coupling inheritance (#19276)
    It is necessary for flexible inheritance from the base class.

--- cocos/ui/UIAbstractCheckButton.h
+++ cocos/ui/UIAbstractCheckButton.h
@@ -115,7 +115,7 @@ public:
      * Set to true will cause the CheckBox's state to "selected", false otherwise.
      *@param selected Set to true will change CheckBox to selected state, false otherwise.
      */
-    void setSelected(bool selected);
+    virtual void setSelected(bool selected);
     
     //override functions
     virtual Size getVirtualRendererSize() const override;
@@ -196,22 +196,22 @@ protected:
     virtual void onPressStateChangedToPressed() override;
     virtual void onPressStateChangedToDisabled() override;
     
-    void setupBackgroundTexture();
+    virtual void setupBackgroundTexture();
     void loadTextureBackGround(SpriteFrame* spriteFrame);
-    void setupBackgroundSelectedTexture();
+    virtual void setupBackgroundSelectedTexture();
     void loadTextureBackGroundSelected(SpriteFrame* spriteFrame);
-    void setupFrontCrossTexture();
+    virtual void setupFrontCrossTexture();
     void loadTextureFrontCross(SpriteFrame* spriteframe);
-    void setupBackgroundDisable();
+    virtual void setupBackgroundDisable();
     void loadTextureBackGroundDisabled(SpriteFrame* spriteframe);
-    void setupFrontCrossDisableTexture();
+    virtual void setupFrontCrossDisableTexture();
     void loadTextureFrontCrossDisabled(SpriteFrame* spriteframe);
     
     virtual void dispatchSelectChangedEvent(bool selected) = 0;
     
     virtual void onSizeChanged() override;
     
-    void backGroundTextureScaleChangedWithSize();
+    virtual void backGroundTextureScaleChangedWithSize();
     void backGroundSelectedTextureScaleChangedWithSize();
     void frontCrossTextureScaleChangedWithSize();
     void backGroundDisabledTextureScaleChangedWithSize();
