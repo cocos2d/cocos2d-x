6259d5df58761f0811fc79d01143f73b514c4b68
Benjamin Prieto <benjaprieto@hotmail.com>
2019-01-18 04:33:06 -0300

    Ui video player new style looping input options (#19297)
    * Implemented looping and disable user input for uiVideoPlayer
    
    * Fixes on how to handle sub states
    
    * Added UI Video Player Demo for Loop, style and user input enabled options

--- cocos/ui/UIVideoPlayer.h
+++ cocos/ui/UIVideoPlayer.h
@@ -58,7 +58,19 @@ namespace experimental{
                 PLAYING = 0,
                 PAUSED,
                 STOPPED,
-                COMPLETED
+                COMPLETED,
+                ERROR
+            };
+            
+            /**
+             * Styles of how the the video player is presented
+             * For now only used on iOS to use either MPMovieControlStyleEmbedded (DEFAULT) or 
+             * MPMovieControlStyleNone (NONE)
+             */
+            enum class StyleType
+            {
+                DEFAULT = 0,
+                NONE
             };
 
             /**
@@ -95,6 +107,27 @@ namespace experimental{
              * @return A remoting URL address.
              */
             virtual const std::string& getURL() const { return _videoURL;}
+            
+            /**
+             * @brief Set if playback is done in loop mode
+             *
+             * @param looping the video will or not automatically restart at the end
+             */
+            virtual void setLooping(bool looping);
+            
+            /**
+             * Set if the player will enable user input for basic pause and resume of video
+             *
+             * @param enableInput If true, input will be handled for basic functionality (pause/resume)
+             */
+            virtual void setUserInputEnabled(bool enableInput);
+            
+            /**
+             * Set the style of the player
+             *
+             * @param style The corresponding style
+             */
+            virtual void setStyle(StyleType style);
 
             /**
              * Starts playback.
@@ -129,6 +162,22 @@ namespace experimental{
              * @return True if currently playing, false otherwise.
              */
             virtual bool isPlaying() const;
+            
+            /**
+             * Checks whether the VideoPlayer is set with looping mode.
+             *
+             * @return true if the videoplayer is set to loop, false otherwise.
+             */
+            virtual bool isLooping() const;
+
+
+            /**
+             * Checks whether the VideoPlayer is set to listen user input to resume and pause the video
+             *
+             * @return true if the videoplayer user input is set, false otherwise.
+             */            
+            virtual bool isUserInputEnabled() const;
+            
 
             /**
              * Causes the video player to keep aspect ratio or no when displaying the video.
@@ -196,16 +245,20 @@ namespace experimental{
             };
 
             bool _isPlaying;
+            bool _isLooping;
+            bool _isUserInputEnabled;
             bool _fullScreenDirty;
             bool _fullScreenEnabled;
             bool _keepAspectRatioEnabled;
+
+            StyleType _styleType;
 
             std::string _videoURL;
             Source _videoSource;
 
             int _videoPlayerIndex;
             ccVideoPlayerCallback _eventCallback;
-
+	
             void* _videoView;
         };
     }
