c1acc2025bb9d66ae8e44804f980305333a3e307
Andrew V. Belousoff <formatCvt@gmail.com>
2019-01-29 04:52:18 +0300

    Add Android P cutout area support (#19286)
    * Add Android P cutout area support
    Related #19285
    
    * coding style fix
    
    * Update AppActivity.java
    
    Fix android build
    
    * suppress lint new api errors

--- templates/cpp-template-default/proj.android/app/src/org/cocos2dx/cpp/AppActivity.java
+++ templates/cpp-template-default/proj.android/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -26,6 +26,9 @@ package org.cocos2dx.cpp;
 
 import android.os.Bundle;
 import org.cocos2dx.lib.Cocos2dxActivity;
+import android.os.Build;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
 public class AppActivity extends Cocos2dxActivity {
 
@@ -40,6 +43,13 @@ public class AppActivity extends Cocos2dxActivity {
             //  at the top of the stack (ie: the last state of this task)
             // Don't need to finish it again since it's finished in super.onCreate .
             return;
+        }
+        // Make sure we're running on Pie or higher to change cutout mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // Enable rendering into the cutout area
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(lp);
         }
         // DO OTHER INITIALIZATION BELOW
         
