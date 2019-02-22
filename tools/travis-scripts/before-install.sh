2922e0746b0ee591f9267d242c6185249622e07f
drelaptop <li_cheng_zhe@163.com>
2019-02-22 12:51:48 +0800

    all targets depend on ndk, for gen bindings

--- tools/travis-scripts/before-install.sh
+++ tools/travis-scripts/before-install.sh
@@ -103,6 +103,7 @@ fi
 if [ "$BUILD_TARGET" == "linux_cocos_new_test" ]; then
     download_deps
     install_linux_environment
+    python $COCOS2DX_ROOT/tools/appveyor-scripts/setup_android.py --ndk_only
     exit 0
 fi
 
