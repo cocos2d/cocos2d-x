40304860e6720a3d9fae9a19aa0b8d33a05b79be
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-03-18 09:33:43 +0800

    Apply suggestions from code review

--- cocos/renderer/backend/opengl/CommandBufferGL.cpp
+++ cocos/renderer/backend/opengl/CommandBufferGL.cpp
@@ -329,7 +329,7 @@ void CommandBufferGL::bindVertexBuffer(ProgramGL *program) const
     const auto& vertexLayouts = getVertexLayouts();
     for (const auto& vertexBuffer : _vertexBuffers)
     {
-        if (! vertexBuffer || attributeInfos.empty())
+        if (! vertexBuffer)
             continue;
         
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer->getHandler());
