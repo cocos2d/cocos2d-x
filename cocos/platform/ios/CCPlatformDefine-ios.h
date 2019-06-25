96e1eb9fc14477f6083140da739090fd407a26ec
Arnold <397136899@qq.com>
2019-06-25 13:59:09 +0800

    fix compile error

--- cocos/platform/ios/CCPlatformDefine-ios.h
+++ cocos/platform/ios/CCPlatformDefine-ios.h
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-#pragma
+#pragma once
 
 #include "platform/CCPlatformConfig.h"
 #if CC_TARGET_PLATFORM == CC_PLATFORM_IOS
