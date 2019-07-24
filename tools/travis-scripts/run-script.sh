36c9e1f1b45fbec2ad4880cebdc6ded81ed47bf2
PatriceJiang#Set <397136899@qq.com>
2019-07-24 17:04:14 +0800

    [ci skip][AUTO]: updating luabinding & cocos_file.json automatically

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # exit this script if any commmand fails
-set -x
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COCOS2DX_ROOT="$DIR"/../..
@@ -178,8 +178,8 @@ function generate_pull_request_for_binding_codes_and_cocosfiles()
     fi
 
 
-    # do not exit on error
-    # set -e 
+    # Exit on error
+    set -e
 
     git add -f --all "$LUA_AUTO_GENERATE_SCRIPT_PATH"
     git add -f --all "$COCOSFILE_PATH"
