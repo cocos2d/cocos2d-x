262f28ad9df88e6c924b0f836db4ec3c7181555d
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-07-25 13:49:07 +0800

    travis git push enable error log (#19982)
    * git push verbose
    
    * enable error log

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -188,7 +188,7 @@ function generate_pull_request_for_binding_codes_and_cocosfiles()
 
     set -x
     echo "Pushing to Robot's repo ..."
-    git push -fq upstream "$COCOS_BRANCH" 2> /dev/null
+    git push -f upstream "$COCOS_BRANCH"
     set +x
 
     echo "Sending Pull Request to base repo ..."
