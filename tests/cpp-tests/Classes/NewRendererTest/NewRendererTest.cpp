45a95d4bcafa4e4d3ef24693f953cb5fd880d895
patricejiang <jiangzhan@chukong-inc.com>
2019-08-05 10:34:13 +0800

    update alg

--- tests/cpp-tests/Classes/NewRendererTest/NewRendererTest.cpp
+++ tests/cpp-tests/Classes/NewRendererTest/NewRendererTest.cpp
@@ -62,7 +62,6 @@ private:
 
 NewRendererTests::NewRendererTests()
 {
-    ADD_TEST_CASE(SpriteCreation);
     ADD_TEST_CASE(NewSpriteTest);
     ADD_TEST_CASE(GroupCommandTest);
     ADD_TEST_CASE(NewClippingNodeTest);
@@ -75,6 +74,7 @@ NewRendererTests::NewRendererTests()
     ADD_TEST_CASE(RendererBatchQuadTri);
     ADD_TEST_CASE(RendererUniformBatch);
     ADD_TEST_CASE(RendererUniformBatch2);
+    ADD_TEST_CASE(SpriteCreation);
     ADD_TEST_CASE(NonBatchSprites);
 }
 
@@ -866,21 +866,35 @@ void NonBatchSprites::createSprite()
 
 void NonBatchSprites::update(float dt)
 {
-    _mavDt = 0.7f * _mavDt  + 0.3f * dt;
-    _rmavDt = 0.5f * _rmavDt  + 0.5f * dt;
-    if(_mavDt <= DEST_DT_30FPS) {
-        _contHit.cancel();
-        auto t2 = DEST_DT_30FPS - _rmavDt;
-        auto delta = (int)(t2 / _rmavDt * _spriteIndex * 0.1);
-        delta =std::min(20, std::max(1, delta));
-        for(int i =0 ;i< delta; i++) {
-            createSprite();
+    
+    if( dt <= 1.0f / 28.0f && dt >= 1.0f/ 31.0f)
+    {
+        _around30fps.hit();
+    }
+    else
+    {
+        _around30fps.cancel();
+    }
+    
+    _maDt = 0.7f * _maDt  + 0.3f * dt;
+    _rmaDt = 0.5f * _rmaDt  + 0.5f * dt;
+    if(_maDt <= DEST_DT_30FPS) {
+        _contSlow.cancel();
+        _contFast.hit();
+        if(_contFast.ok()){
+            auto t2 = DEST_DT_30FPS - _rmaDt;
+            auto delta = (int)(t2 / _rmaDt * _spriteIndex * 0.1);
+            delta =std::min(20, std::max(1, delta));
+            for(int i =0 ;i< delta; i++) {
+                createSprite();
+            }
         }
     }else{
-        _contHit.hit();
+        _contSlow.hit();
+        _contFast.cancel();
     }
 
-    if(_contHit.ok())
+    if(_contSlow.ok() || _around30fps.ok())
     {
         unscheduleUpdate();
         std::stringstream ss;
