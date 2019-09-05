44ad07cfa842cce8af685c8fb58f3994f82b74e5
minggo <nudtzxm@gmail.com>
2019-09-05 17:44:46 +0800

    fix compiling error

--- cocos/ui/UIWebView/UIWebViewImpl-ios.mm
+++ cocos/ui/UIWebView/UIWebViewImpl-ios.mm
@@ -33,12 +33,12 @@
 #import <WebKit/WKNavigationDelegate.h>
 
 #include "ui/UIWebView/UIWebViewImpl-ios.h"
+#include "ui/UIWebView/UIWebView.h"
 #include "renderer/CCRenderer.h"
 #include "base/CCDirector.h"
 #include "platform/CCGLView.h"
 #include "platform/ios/CCEAGLView-ios.h"
 #include "platform/CCFileUtils.h"
-#include "ui/UIWebView.h"
 
 @interface UIWebViewWrapper : NSObject
 @property (nonatomic) std::function<bool(std::string url)> shouldStartLoading;
