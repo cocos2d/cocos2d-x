e6baa721da650b9026531c97950e9dbf0452c445
coulsonwang <coulsonwang@126.com>
2019-10-11 16:59:15 +0800

    remove unused code

--- cocos/renderer/backend/opengl/TextureGL.h
+++ cocos/renderer/backend/opengl/TextureGL.h
@@ -150,7 +150,6 @@ public:
 private:
     void initWithZeros();
 
-    SamplerDescriptor _samplerDescriptor;
     TextureInfoGL _textureInfo;
     EventListener* _backToForegroundListener = nullptr;
 };
