af4a0812f4195d938a288b5cf1963be38b40cde8
minggo <nudtzxm@gmail.com>
2019-11-01 11:57:55 +0800

    fix compiling error

--- cocos/2d/CCAtlasNode.cpp
+++ cocos/2d/CCAtlasNode.cpp
@@ -25,8 +25,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-
 #include "2d/CCAtlasNode.h"
+#include <stddef.h> // offsetof
+#include "base/ccTypes.h"
 #include "renderer/CCTextureAtlas.h"
 #include "base/CCDirector.h"
 #include "renderer/CCTextureCache.h"
