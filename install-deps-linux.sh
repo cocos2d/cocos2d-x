e2c2e330edcce0f0df815eec7184b3323328b5f1
minggo <nudtzxm@gmail.com>
2019-11-04 17:46:30 +0800

    fix issue that can not run release mode with lua projects It is because luajit has 32bit version, and ubuntu does not support 32bit application by default.

--- install-deps-linux.sh
+++ install-deps-linux.sh
@@ -11,13 +11,19 @@ fi
 
 sudo apt-get update
 
-DEPENDS='libx11-dev'
+# run 32bit applicatio: needed for lua relase mode as luajit has 32bit version
+# https://askubuntu.com/questions/454253/how-to-run-32-bit-app-in-ubuntu-64-bit
+sudo dpkg --add-architecture i386
+DEPENDS='libc6:i386 libncurses5:i386 libstdc++6:i386'
+ 
+
+DEPENDS+=' libx11-dev'
 DEPENDS+=' libxmu-dev'
 DEPENDS+=' libglu1-mesa-dev'
 DEPENDS+=' libgl2ps-dev'
 DEPENDS+=' libxi-dev'
 DEPENDS+=' libzip-dev'
-DEPENDS+=' libpng12-dev'
+DEPENDS+=' libpng-dev'
 DEPENDS+=' libcurl4-gnutls-dev'
 DEPENDS+=' libfontconfig1-dev'
 DEPENDS+=' libsqlite3-dev'
