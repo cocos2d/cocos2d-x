00cbbe89d80fbffcdc5373f2f67a01d0f2b3f074
932239167@qq.com <932239167@qq.com>
2024-01-02 20:28:19 +0800

    移除sdktest/paysdktest

--- tests/cpp-tests/proj.ohos/libSysSDKCapabilities/src/main/cpp/napi/modules/LoginSDKNapi.h
+++
@@ -1,6 +0,0 @@
-#include <napi/native_api.h>
-
-class LoginSDKNapi {
-public:
-    static napi_value onLoginSDKCallBack(napi_env env, napi_callback_info info);
-};
--- tests/cpp-tests/proj.ohos/libSysSDKCapabilities/src/main/cpp/napi/modules/PaySDKNapi.h
+++
@@ -1,6 +0,0 @@
-#include <napi/native_api.h>
-
-class PaySDKNapi {
-public:
-    static napi_value onPaySDKCallBack(napi_env env, napi_callback_info info);
-};
