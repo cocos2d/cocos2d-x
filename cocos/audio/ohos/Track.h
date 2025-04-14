bfd78a9dde37c622d1f8364bdfe964636205ec11
IceBBBBB <418776654@qq.com>
2025-04-14 15:07:59 +0800

    3.9 ohos code optimization (#20872)

--- cocos/audio/ohos/Track.h
+++ cocos/audio/ohos/Track.h
@@ -28,6 +28,7 @@ THE SOFTWARE.
 #include "PcmData.h"
 #include "IVolumeProvider.h"
 #include "PcmBufferProvider.h"
+#include "audio/ohos/BigAudioService.h"
 
 #include <functional>
 #include <mutex>
@@ -96,6 +97,7 @@ private:
     bool _isAudioFocus;
 
     friend class AudioMixerController;
+    friend class BigAudioService;
 };
 
 }} // namespace cocos2d { namespace experimental
--- tests/cpp-tests/Classes/FontTest/FontTest.cpp
+++ tests/cpp-tests/Classes/FontTest/FontTest.cpp
@@ -14,16 +14,28 @@ enum {
     kTagColor3,
 };
 
-//you don't need any ifdef anymore
-static std::string fontList[] =
-{
-    "fonts/A Damn Mess.ttf",
-    "fonts/Abberancy.ttf",
-    "fonts/Abduction.ttf",
-    "fonts/Paint Boy.ttf",
-    "fonts/Schwarzwald.ttf",
-    "fonts/Scissor Cuts.ttf",
-};
+static int fontIdx = 0;
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_OHOS)
+    static std::string fontList[] =
+    {
+        "HarmonyOS_Sans.ttf",
+        "HMSymbolVF.ttf",
+        "NotoSans[wdth,wght].ttf",
+        "NotoSerifTibetan[wght].ttf",
+        "NotoSerifHebrew[wdth,wght].ttf",
+        "NotoSerifLao[wdth,wght].ttf",
+    };
+#else
+    static std::string fontList[] =
+    {
+        "fonts/A Damn Mess.ttf",
+        "fonts/Abberancy.ttf",
+        "fonts/Abduction.ttf",
+        "fonts/Paint Boy.ttf",
+        "fonts/Schwarzwald.ttf",
+        "fonts/Scissor Cuts.ttf",
+    };
+#endif
 
 static int vAlignIdx = 0;
 static TextVAlignment verticalAlignment[] =
