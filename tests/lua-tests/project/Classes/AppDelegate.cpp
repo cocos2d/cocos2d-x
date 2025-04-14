ff5defd71d16342a5917ad8ad32a7bf91dc6ed2d
IceBBBBB <418776654@qq.com>
2025-04-14 17:27:54 +0800

    3.11 ohos code optimization (#20873)

--- tests/lua-tests/project/Classes/AppDelegate.cpp
+++ tests/lua-tests/project/Classes/AppDelegate.cpp
@@ -50,11 +50,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     }
     lua_pop(L, 1);
 
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_OHOS)
-    pEngine->executeScriptFile("controller.lua");
-#else
     pEngine->executeScriptFile("src/controller.lua");
-#endif
     return true;
 }
 
--- tests/lua-tests/src/controller.lua
+++ tests/lua-tests/src/controller.lua
@@ -11,9 +11,7 @@ collectgarbage("setstepmul", 5000)
 
 ----------------
 -- run
-if (cc.PLATFORM_OS_OHOS ~= currPlatform) then
 cc.FileUtils:getInstance():addSearchPath("src")
-end
 CC_USE_DEPRECATED_API = true
 require "cocos.init"
 
