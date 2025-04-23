2fd6f8a26b82e371b13232a61202bfe092a3998b
冰冰冰 <418776654@qq.com>
2025-04-23 14:10:42 +0800

    Change the ts file to ets; API15 upgrade; Optimization of safe zones
    Signed-off-by: 冰冰冰 <418776654@qq.com>

--- cocos/platform/ohos/CCGLViewImpl-ohos.cpp
+++ cocos/platform/ohos/CCGLViewImpl-ohos.cpp
@@ -110,110 +110,29 @@ void GLViewImpl::setIMEKeyboardState(bool bOpen) {
 }
 
 Rect GLViewImpl::getSafeAreaRect() const {
-    Rect safeAreaRect = GLView::getSafeAreaRect();
-    float deviceAspectRatio = 0;
-    if(safeAreaRect.size.height > safeAreaRect.size.width) {
-        deviceAspectRatio = safeAreaRect.size.height / safeAreaRect.size.width;
-    } else {
-        deviceAspectRatio = safeAreaRect.size.width / safeAreaRect.size.height;
+    Rect safeAreaRect1;
+    int left;
+    if (auto getSafeAreaLeft = aki::JSBind::GetJSFunction("DeviceUtils.getSafeAreaLeft")) {
+        left = getSafeAreaLeft->Invoke<int>(); 
     }
-
-    float marginX = DEFAULT_MARGIN_OHOS / _scaleX;
-    float marginY = DEFAULT_MARGIN_OHOS / _scaleY;
-    bool isScreenRound;
-    if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.isRoundScreen")) {
-        isScreenRound = function->Invoke<bool>(); 
+    int top;
+    if (auto getSafeAreaTop = aki::JSBind::GetJSFunction("DeviceUtils.getSafeAreaTop")) {
+        top = getSafeAreaTop->Invoke<int>(); 
     }
-    bool hasSoftKeys;
-    if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.hasSoftKeys")) {
-        hasSoftKeys = function->Invoke<bool>(); 
+    int width;
+    if (auto getSafeAreaWidth = aki::JSBind::GetJSFunction("DeviceUtils.getSafeAreaWidth")) {
+        width = getSafeAreaWidth->Invoke<int>(); 
     }
-    bool isCutoutEnabled;
-    if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.isCutoutEnable")) {
-        isCutoutEnabled = function->Invoke<bool>(); 
+    int height;
+    if (auto getSafeAreaHeight = aki::JSBind::GetJSFunction("DeviceUtils.getSafeAreaHeight")) {
+        height = getSafeAreaHeight->Invoke<int>(); 
     }
-
-    if(isScreenRound) {
-        // edge screen
-        if(safeAreaRect.size.width < safeAreaRect.size.height) {
-            safeAreaRect.origin.y += marginY * 2.f;
-            safeAreaRect.size.height -= (marginY * 2.f);
-
-            safeAreaRect.origin.x += marginX;
-            safeAreaRect.size.width -= (marginX * 2.f);
-        } else {
-            safeAreaRect.origin.y += marginY;
-            safeAreaRect.size.height -= (marginY * 2.f);
-
-            // landscape: no changes with X-coords
-        }
-    } else if (deviceAspectRatio >= WIDE_SCREEN_ASPECT_RATIO_OHOS) {
-        // almost all devices on the market have round corners
-        float bottomMarginIfPortrait = 0;
-        if(hasSoftKeys) {
-            bottomMarginIfPortrait = marginY * 2.f;
-        }
-
-        if(safeAreaRect.size.width < safeAreaRect.size.height) {
-            // portrait: double margin space if device has soft menu
-            safeAreaRect.origin.y += bottomMarginIfPortrait;
-            safeAreaRect.size.height -= (bottomMarginIfPortrait + marginY);
-        } else {
-            // landscape: ignore double margin at the bottom in any cases
-            // prepare signle margin for round corners
-            safeAreaRect.origin.y += marginY;
-            safeAreaRect.size.height -= (marginY * 2.f);
-        }
-    } else {
-        if(hasSoftKeys && (safeAreaRect.size.width < safeAreaRect.size.height)) {
-            // portrait: preserve only for soft system menu
-            safeAreaRect.origin.y += marginY * 2.f;
-            safeAreaRect.size.height -= (marginY * 2.f);
-        }
-    }
-
-    if (isCutoutEnabled) {
-        // screen with enabled cutout area
-        
-        int orientation;
-        if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.getOrientation")) {
-            orientation = function->Invoke<int>(); 
-        }
-
-        if(static_cast<int>(GLViewImpl::Orientation::PORTRAIT) == orientation) {
-            int result;
-            if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.getCutoutHeight")) {
-                result = function->Invoke<int>(); 
-            }
-            double height = result / _scaleY;
-            safeAreaRect.origin.y += height;
-            safeAreaRect.size.height -= height;
-        } else if(static_cast<int>(GLViewImpl::Orientation::PORTRAIT_INVERTED) == orientation) {
-            int result;
-            if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.getCutoutHeight")) {
-                result = function->Invoke<int>(); 
-            }
-            double height = result / _scaleY;
-            safeAreaRect.size.height -= height;
-        } else if(static_cast<int>(GLViewImpl::Orientation::LANDSCAPE) == orientation) {
-            int result;
-            if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.getCutoutWidth")) {
-                result = function->Invoke<int>(); 
-            }
-            double width = result / _scaleX;
-            safeAreaRect.size.width -= width;
-        } else if(static_cast<int>(GLViewImpl::Orientation::LANDSCAPE_INVERTED) == orientation) {
-            int result;
-            if (auto function = aki::JSBind::GetJSFunction("DeviceUtils.getCutoutWidth")) {
-                result = function->Invoke<int>(); 
-            }
-            double width = result / _scaleX;
-            safeAreaRect.origin.x += width;
-            safeAreaRect.size.width -= width;
-        }
-    }
-
-    return safeAreaRect;
+    safeAreaRect1.origin.x = left / _scaleX;
+    safeAreaRect1.origin.y = top / _scaleY;
+    safeAreaRect1.size.width = width / _scaleX;
+    safeAreaRect1.size.height = height / _scaleX;
+    OHOS_LOGD("GLViewImpl getsafeAreaRect1, x:%{public}f, y:%{public}f, width:%{public}f, height:%{public}f", safeAreaRect1.origin.x, safeAreaRect1.origin.y, safeAreaRect1.size.width, safeAreaRect1.size.height);
+    return safeAreaRect1;
 }
 NS_CC_END
 
